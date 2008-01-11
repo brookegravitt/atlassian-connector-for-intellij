74faffb1fb6b70d113bae4ca7159222f9d1c38e5
sginter <sginter@localhost>
2008-01-11 13:52:07 +0000

    imports cleanup

--- src/main/java/com/atlassian/theplugin/configuration/PluginConfiguration.java
+++ src/main/java/com/atlassian/theplugin/configuration/PluginConfiguration.java
@@ -8,5 +8,5 @@ package com.atlassian.theplugin.configuration;
  * To change this template use File | Settings | File Templates.
  */
 public interface PluginConfiguration {
-    BambooConfigurationBean getBambooConfiguration();
+    BambooConfiguration getBambooConfiguration();
 }
