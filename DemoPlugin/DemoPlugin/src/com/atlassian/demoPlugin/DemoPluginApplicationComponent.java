13d2a62b7e311f0b9307d3718884fcfbf3e9e425
jjaroczynski <jjaroczynski@localhost>
2008-01-11 09:38:37 +0000

    adding application configuration entry

--- DemoPlugin/DemoPlugin/src/com/atlassian/demoPlugin/DemoPluginApplicationComponent.java
+++ DemoPlugin/DemoPlugin/src/com/atlassian/demoPlugin/DemoPluginApplicationComponent.java
@@ -2,7 +2,14 @@ package com.atlassian.demoPlugin;
 
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NonNls;
+
+import javax.swing.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,7 +18,10 @@ import org.jetbrains.annotations.NotNull;
  * Time: 16:56:49
  * To change this template use File | Settings | File Templates.
  */
-public class DemoPluginApplicationComponent implements ApplicationComponent {
+public class DemoPluginApplicationComponent implements ApplicationComponent, Configurable {
+    private String userName;
+    private DemoPuginConfigurationForm form;
+
     public DemoPluginApplicationComponent() {
     }
 
@@ -30,5 +40,57 @@ public class DemoPluginApplicationComponent implements ApplicationComponent {
 
     public void sayHello() {
         Messages.showMessageDialog("w dupe jeza", "gowniane okienko", Messages.getInformationIcon());
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(final String userName) {
+        this.userName = userName;
+    }
+
+    @Nls
+    public String getDisplayName() {
+        return "Demo Plugin";
+    }
+
+    @Nullable
+    public Icon getIcon() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Nullable
+    @NonNls
+    public String getHelpTopic() {
+        return "Test Help";
+    }
+
+    public JComponent createComponent() {
+        if (form == null) {
+            form = new DemoPuginConfigurationForm();
+        }
+
+        return form.getRootComponent();
+    }
+
+    public boolean isModified() {
+        return form != null && form.isModified(this);
+    }
+
+    public void apply() throws ConfigurationException {
+        if (form != null) {
+            form.getData(this);
+        }
+    }
+
+    public void reset() {
+        if (form != null) {
+            form.setData(this);
+        }
+    }
+
+    public void disposeUIResources() {
+        form = null;
     }
 }
