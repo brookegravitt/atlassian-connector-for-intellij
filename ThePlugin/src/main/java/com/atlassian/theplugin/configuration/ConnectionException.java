8efb28c6c6999641e4e6e334bc3a23b8d8ce6541
jjaroczynski <jjaroczynski@localhost>
2008-01-14 10:53:31 +0000

    test bamboo connection button

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/configuration/ConnectionException.java
@@ -0,0 +1,14 @@
+package com.atlassian.theplugin.configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jacek
+ * Date: 2008-01-14
+ * Time: 11:26:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConnectionException extends Throwable {
+    public ConnectionException(String s) {
+        super(s);
+    }
+}
