54619b0581ae297f9264b67f246aa00a26736905
sginter <sginter@localhost>
2008-01-15 17:08:26 +0000

    differenciating build status (success, failed, error)

---
+++ src/main/java/com/atlassian/theplugin/bamboo/BuildStatus.java
@@ -0,0 +1,14 @@
+package com.atlassian.theplugin.bamboo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sginter
+ * Date: Jan 15, 2008
+ * Time: 5:27:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum BuildStatus {
+    SUCCESS,
+    FAILED,  // build error
+    ERROR   // ststus retrieval error
+}
