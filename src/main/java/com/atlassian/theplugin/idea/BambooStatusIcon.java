a9743baa94d38d61c990441a76092022312537d9
jjaroczynski <jjaroczynski@localhost>
2008-01-17 16:39:04 +0000

    adding icon to status bar and refactoring

--- src/main/java/com/atlassian/theplugin/idea/BambooStatusIcon.java
+++ src/main/java/com/atlassian/theplugin/idea/BambooStatusIcon.java
@@ -1,8 +1,12 @@
 package com.atlassian.theplugin.idea;
 
-import com.atlassian.theplugin.bamboo.BambooDisplayComponent;
+import com.atlassian.theplugin.bamboo.BuildStatus;
+import com.intellij.lang.properties.ResourceBundle;
+import com.intellij.openapi.util.IconLoader;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.Icon;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,10 +15,31 @@ import javax.swing.*;
  * Time: 3:54:00 PM
  * To change this template use File | Settings | File Templates.
  */
-public class BambooStatusIcon extends JLabel implements BambooDisplayComponent {
+public class BambooStatusIcon extends JLabel {
 
-    public void updateBambooStatus(String statusIcon, String fullInfo) {
-        setText(statusIcon);
+    Icon iconRed, iconGreen, iconGrey;
+
+    BambooStatusIcon() {
+        iconRed = IconLoader.getIcon("/icons/red-16.png");
+        iconGreen = IconLoader.getIcon("/icons/green-16.png");
+        iconGrey = IconLoader.getIcon("/icons/grey-16.png");
+
+    }
+
+    public void updateBambooStatus(BuildStatus status, String fullInfo) {
+        setText("");
         setToolTipText(fullInfo);
+
+        switch (status) {
+            case ERROR:
+                setIcon(iconGrey);
+                break;
+            case FAILED:
+                setIcon(iconRed);
+                break;
+            case SUCCESS:
+                setIcon(iconGreen);
+                break;
+        }
     }
 }
