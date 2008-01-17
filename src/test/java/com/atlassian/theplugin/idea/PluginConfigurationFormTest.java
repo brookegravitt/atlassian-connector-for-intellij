a5f75837ce8a8ade0ec3a57dff2a6d8205459c5a
sginter <sginter@localhost>
2008-01-17 15:19:02 +0000

    Some unit test for configuration dialog (more to come)

--- src/test/java/com/atlassian/theplugin/idea/PluginConfigurationFormTest.java
+++ src/test/java/com/atlassian/theplugin/idea/PluginConfigurationFormTest.java
@@ -5,8 +5,14 @@ import junit.framework.TestSuite;
 import junit.framework.TestCase;
 import com.atlassian.theplugin.configuration.PluginConfigurationBean;
 import com.atlassian.theplugin.configuration.SubscribedPlanBean;
+import com.atlassian.theplugin.configuration.ServerBean;
 
 import java.beans.BeanInfo;
+import java.util.ArrayList;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+
+import org.apache.log4j.Logger;
 
 /**
  * PluginConfigurationForm Tester.
@@ -26,11 +32,7 @@ public class PluginConfigurationFormTest extends TestCase {
     public void testSetGetData() throws Exception {
         assertNotNull(pluginConfigurationForm.getRootComponent());
 
-        PluginConfigurationBean inBean = new PluginConfigurationBean();
-        inBean.getBambooConfigurationData().getServerData().setName("name");
-        inBean.getBambooConfigurationData().getServerData().setPassword("password");
-        inBean.getBambooConfigurationData().getServerData().setUrlString("url");
-        inBean.getBambooConfigurationData().getServerData().setUsername("userName");
+        PluginConfigurationBean inBean = createBasicBean();
 
         pluginConfigurationForm.setData(inBean);
 
@@ -38,10 +40,7 @@ public class PluginConfigurationFormTest extends TestCase {
         pluginConfigurationForm.getData(outBean);
 
         assertNotSame(inBean, outBean);
-        assertEquals("name", outBean.getBambooConfigurationData().getServerData().getName());
-        assertEquals("password", outBean.getBambooConfigurationData().getServerData().getPassword());
-        assertEquals("url", outBean.getBambooConfigurationData().getServerData().getUrlString());
-        assertEquals("userName", outBean.getBambooConfigurationData().getServerData().getUsername());
+        checkBasicBean(outBean);
         assertEquals(0, outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().size());
 
         /*  */
@@ -51,10 +50,7 @@ public class PluginConfigurationFormTest extends TestCase {
         
         outBean = new PluginConfigurationBean();
         pluginConfigurationForm.getData(outBean);
-        assertEquals("name", outBean.getBambooConfigurationData().getServerData().getName());
-        assertEquals("password", outBean.getBambooConfigurationData().getServerData().getPassword());
-        assertEquals("url", outBean.getBambooConfigurationData().getServerData().getUrlString());
-        assertEquals("userName", outBean.getBambooConfigurationData().getServerData().getUsername());
+        checkBasicBean(outBean);
         assertEquals(1, outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().size());
         assertEquals("Plan-1", outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().get(0).getPlanId());
         
@@ -65,10 +61,7 @@ public class PluginConfigurationFormTest extends TestCase {
 
         outBean = new PluginConfigurationBean();
         pluginConfigurationForm.getData(outBean);
-        assertEquals("name", outBean.getBambooConfigurationData().getServerData().getName());
-        assertEquals("password", outBean.getBambooConfigurationData().getServerData().getPassword());
-        assertEquals("url", outBean.getBambooConfigurationData().getServerData().getUrlString());
-        assertEquals("userName", outBean.getBambooConfigurationData().getServerData().getUsername());
+        checkBasicBean(outBean);
         assertEquals(2, outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().size());
         assertEquals("Plan-1", outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().get(0).getPlanId());
         assertEquals("Plan-2", outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().get(1).getPlanId());
@@ -80,17 +73,104 @@ public class PluginConfigurationFormTest extends TestCase {
 
         outBean = new PluginConfigurationBean();
         pluginConfigurationForm.getData(outBean);
-        assertEquals("name", outBean.getBambooConfigurationData().getServerData().getName());
-        assertEquals("password", outBean.getBambooConfigurationData().getServerData().getPassword());
-        assertEquals("url", outBean.getBambooConfigurationData().getServerData().getUrlString());
-        assertEquals("userName", outBean.getBambooConfigurationData().getServerData().getUsername());
+        checkBasicBean(outBean);
         assertEquals(3, outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().size());
         assertEquals("Plan-1", outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().get(0).getPlanId());
         assertEquals("Plan-2", outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().get(1).getPlanId());
         assertEquals("Plan-3", outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().get(2).getPlanId());
 
+        /*  */
+        inBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().clear();
+
+        pluginConfigurationForm.setData(inBean);
+
+        outBean = new PluginConfigurationBean();
+        pluginConfigurationForm.getData(outBean);
+
+        checkBasicBean(outBean);
+        assertEquals(0, outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().size());
+
     }
 
+    public void testIsModified() throws Exception {
+        PluginConfigurationBean inBean = createBasicBean();
+
+        pluginConfigurationForm.setData(inBean);
+
+        PluginConfigurationBean outBean = new PluginConfigurationBean();
+
+        outBean = createBasicBean();
+
+        assertFalse(pluginConfigurationForm.isModified(outBean));
+
+        /* with arraylist set */
+        inBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().add(new SubscribedPlanBean() {{setPlanId("Plan-1");}});
+
+        pluginConfigurationForm.setData(inBean);
+        assertTrue(pluginConfigurationForm.isModified(outBean));
+
+        outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().add(new SubscribedPlanBean() {{setPlanId("Plan-1");}});
+
+        assertFalse(pluginConfigurationForm.isModified(outBean));
+
+        /* equals vs == */
+
+        outBean.getBambooConfigurationData().getServerData().setName(new String("name"));
+        outBean.getBambooConfigurationData().getServerData().setPassword(new String("password"));
+        outBean.getBambooConfigurationData().getServerData().setUrlString(new String("url"));
+        outBean.getBambooConfigurationData().getServerData().setUsername(new String("userName"));
+        outBean.getBambooConfigurationData().getServerData().setSubscribedPlansData(new ArrayList<SubscribedPlanBean>());
+        outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().add(new SubscribedPlanBean() {{setPlanId(new String("Plan-1"));}});
+
+
+        assertFalse(pluginConfigurationForm.isModified(outBean));
+
+
+        for (Field f : outBean.getBambooConfigurationData().getServerData().getClass().getDeclaredFields()) {
+            if (f.getType().equals(String.class)) {
+                testForChangedString(outBean, f.getName());
+            }
+        }
+
+        outBean.getBambooConfigurationData().getServerData().getSubscribedPlansData().add(new SubscribedPlanBean() {{setPlanId(new String("Plan-2"));}});
+        assertTrue(pluginConfigurationForm.isModified(outBean));
+
+    }
+
+    public void testFieldSetting() throws Exception {
+
+    }
+
+    private void testForChangedString(PluginConfigurationBean outBean, String property) throws Exception {
+        System.out.println("Testing field " + property);
+        
+        ServerBean serverBean = outBean.getBambooConfigurationData().getServerData();
+        Field f = serverBean.getClass().getDeclaredField(property);
+        f.setAccessible(true);
+        String prev = (String) f.get(serverBean);
+
+        f.set(serverBean, prev + "-chg");
+        assertTrue(pluginConfigurationForm.isModified(outBean));
+        f.set(serverBean, prev);
+    }
+
+    private static PluginConfigurationBean createBasicBean() {
+        PluginConfigurationBean outBean = new PluginConfigurationBean();
+
+        outBean.getBambooConfigurationData().getServerData().setName("name");
+        outBean.getBambooConfigurationData().getServerData().setPassword("password");
+        outBean.getBambooConfigurationData().getServerData().setUrlString("url");
+        outBean.getBambooConfigurationData().getServerData().setUsername("userName");
+
+        return outBean;
+    }
+
+    private static void checkBasicBean(PluginConfigurationBean outBean) {
+        assertEquals("name", outBean.getBambooConfigurationData().getServerData().getName());
+        assertEquals("password", outBean.getBambooConfigurationData().getServerData().getPassword());
+        assertEquals("url", outBean.getBambooConfigurationData().getServerData().getUrlString());
+        assertEquals("userName", outBean.getBambooConfigurationData().getServerData().getUsername());
+    }
 
     public static Test suite() {
         return new TestSuite(PluginConfigurationFormTest.class);
