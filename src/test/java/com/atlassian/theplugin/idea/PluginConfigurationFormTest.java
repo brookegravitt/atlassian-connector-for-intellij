6692e93f5082b61e8d63a8d808f0a513a5b503f7
pmaruszak <pmaruszak@localhost>
2008-01-18 17:13:00 +0000

    Added test for BambooStatusListenerImpl (ToolTip html renderer, status selection algorithm) BambooStatusIcon and

--- src/test/java/com/atlassian/theplugin/idea/PluginConfigurationFormTest.java
+++ src/test/java/com/atlassian/theplugin/idea/PluginConfigurationFormTest.java
@@ -3,6 +3,8 @@ package com.atlassian.theplugin.idea;
 import com.atlassian.theplugin.configuration.PluginConfigurationBean;
 import com.atlassian.theplugin.configuration.ServerBean;
 import com.atlassian.theplugin.configuration.SubscribedPlanBean;
+import com.atlassian.theplugin.bamboo.*;
+import com.intellij.openapi.util.IconLoader;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -11,6 +13,7 @@ import javax.swing.*;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Collection;
 
 /**
  * PluginConfigurationForm Tester.
@@ -20,7 +23,11 @@ import java.util.Iterator;
  * @since <pre>01/17/2008</pre>
  */
 public class PluginConfigurationFormTest extends TestCase {
+
     PluginConfigurationForm pluginConfigurationForm;
+    //statuses as strings returned by bamboo Rest API
+    public static String BUILD_SUCCESSFUL = "Successful";
+    public static String BUILD_FAILED = "Failed";
 
     protected void setUp() throws Exception {
         super.setUp();
@@ -243,6 +250,87 @@ public class PluginConfigurationFormTest extends TestCase {
         return new TestSuite(PluginConfigurationFormTest.class);
     }
 
+    public void testStatusListenerAlgorithm(){
+
+
+        BambooStatusIconHelper statusIcon = new BambooStatusIconHelper();
+        Collection<BambooBuild> buildStatuses = new ArrayList<BambooBuild>();
+
+        //add crap as build status
+        BambooBuild  bambooBuild = new BambooBuildInfo("projectName", "buildNameSuccess", "buildKey", BUILD_SUCCESSFUL, "buildNumber", "buildReason",
+              "buildRelativeBuildDate", "buildDurationDescription", "buildTestSummary");
+
+
+
+        buildStatuses.add(bambooBuild);
+        BambooStatusListener statusListener = new BambooStatusListenerImpl(statusIcon);
+
+        statusListener.updateBuildStatuses(buildStatuses);
+        assertEquals(BuildStatus.SUCCESS, statusIcon.getBuildStatus());
+
+        bambooBuild = new BambooBuildInfo("projectName", "buildNameCrap", "buildKey", "CRAPSTATUS", "buildNumber", "buildReason",
+                "buildRelativeBuildDate", "buildDurationDescription", "buildTestSummary");
+        buildStatuses.add(bambooBuild);
+        statusListener.updateBuildStatuses(buildStatuses);
+        assertEquals(BuildStatus.ERROR, statusIcon.getBuildStatus());
+
+        bambooBuild = new BambooBuildInfo("projectName", "buildNameFailed", "buildKey", BuildStatus.FAILED.toString(), "buildNumber", "buildReason",
+                "buildRelativeBuildDate", "buildDurationDescription", "buildTestSummary");
+        buildStatuses.add(bambooBuild);
+        statusListener.updateBuildStatuses(buildStatuses);
+        assertEquals(BuildStatus.FAILED, statusIcon.getBuildStatus());
+        
+        bambooBuild = new BambooBuildInfo("projectName", "buildNameFailed2", "buildKey", BuildStatus.FAILED.toString(), "buildNumber", "buildReason",
+                "buildRelativeBuildDate", "buildDurationDescription", "buildTestSummary");
+        buildStatuses.add(bambooBuild);
+        statusListener.updateBuildStatuses(buildStatuses);
+        assertEquals(BuildStatus.FAILED, statusIcon.getBuildStatus());
+
+        assertEquals("<html><body>" +
+                      "<table>" +
+                        "<tr><td>buildKey</td><td><font color=\"green\">success</font></td></tr>" +
+                        "<tr><td>buildKey</td><td><font color=\"ltgray\">null</font></td></tr>" +
+                        "<tr><td>buildKey</td><td><font color=\"red\">build failed</font></td></tr>" +
+                        "<tr><td>buildKey</td><td><font color=\"red\">build failed</font></td></tr>" +
+                     "</table>" +
+                    "</body></html>", statusIcon.getFullInfo());
+    }
+
+
+    public void testBambooStatusIcon(){
+        BambooStatusIcon statusIcon = new BambooStatusIcon();
+        Collection<BambooBuild> buildStatuses = new ArrayList<BambooBuild>();
+
+        //add crap as build status
+        BambooBuild  bambooBuild = new BambooBuildInfo("projectName", "buildNameSuccess", "buildKey", BUILD_SUCCESSFUL, "buildNumber", "buildReason",
+              "buildRelativeBuildDate", "buildDurationDescription", "buildTestSummary");
+
+            buildStatuses.add(bambooBuild);
+        BambooStatusListener statusListener = new BambooStatusListenerImpl(statusIcon);
+
+        statusListener.updateBuildStatuses(buildStatuses);
+        assertTrue(statusIcon.getIcon().equals(IconLoader.getIcon("/icons/green-16.png")));
+
+        bambooBuild = new BambooBuildInfo("projectName", "buildNameCrap", "buildKey", "CRAPSTATUS", "buildNumber", "buildReason",
+                "buildRelativeBuildDate", "buildDurationDescription", "buildTestSummary");
+        buildStatuses.add(bambooBuild);
+        statusListener.updateBuildStatuses(buildStatuses);
+        assertTrue(statusIcon.getIcon().equals(IconLoader.getIcon("/icons/grey-16.png")));
+
+        bambooBuild = new BambooBuildInfo("projectName", "buildNameFailed", "buildKey", BuildStatus.FAILED.toString(), "buildNumber", "buildReason",
+                "buildRelativeBuildDate", "buildDurationDescription", "buildTestSummary");
+        buildStatuses.add(bambooBuild);
+        statusListener.updateBuildStatuses(buildStatuses);
+        assertTrue(statusIcon.getIcon().equals(IconLoader.getIcon("/icons/red-16.png")));
+
+        bambooBuild = new BambooBuildInfo("projectName", "buildNameFailed2", "buildKey", BuildStatus.FAILED.toString(), "buildNumber", "buildReason",
+                "buildRelativeBuildDate", "buildDurationDescription", "buildTestSummary");
+        buildStatuses.add(bambooBuild);
+        statusListener.updateBuildStatuses(buildStatuses);
+        assertTrue(statusIcon.getIcon().equals(IconLoader.getIcon("/icons/red-16.png")));
+       
+    }
+
     private class PluginConfigurationFormHelper {
         public JPanel rootComponent;
         public JTabbedPane tabbedPane1;
@@ -264,5 +352,24 @@ public class PluginConfigurationFormTest extends TestCase {
 
             }
         }
+    }
+
+    private class BambooStatusIconHelper extends BambooStatusIcon {
+        private BuildStatus status;
+        private String fullInfo;
+
+        public void updateBambooStatus(BuildStatus status, String fullInfo) {
+
+                this.status = status;
+                this.fullInfo = fullInfo;
+        }
+
+        public String getFullInfo(){
+            return fullInfo;
+        }
+        public BuildStatus getBuildStatus(){
+            return status;
+        }
+
     }
 }
