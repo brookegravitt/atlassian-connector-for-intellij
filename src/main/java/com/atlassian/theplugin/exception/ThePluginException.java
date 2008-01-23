ff883f42def98ba754a48146dc4fdb8f2860538f
jjaroczynski <jjaroczynski@localhost>
2008-01-23 14:36:06 +0000

    tooltip reengineering and improvement

---
+++ src/main/java/com/atlassian/theplugin/exception/ThePluginException.java
@@ -0,0 +1,19 @@
+package com.atlassian.theplugin.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jacek
+ * Date: 2008-01-23
+ * Time: 12:49:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class ThePluginException extends Exception {
+
+	public ThePluginException(String message) {
+		super(message);
+	}
+
+	ThePluginException(String message, Throwable throwable) {
+        super(message, throwable);
+	}
+}
