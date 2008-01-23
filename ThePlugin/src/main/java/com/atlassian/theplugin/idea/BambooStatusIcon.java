8e093ad2a777d12204e8731c04a83fe36682e3c9
jjaroczynski <jjaroczynski@localhost>
2008-01-23 14:36:06 +0000

    tooltip reengineering and improvement

--- ThePlugin/src/main/java/com/atlassian/theplugin/idea/BambooStatusIcon.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/BambooStatusIcon.java
@@ -1,34 +1,46 @@
 package com.atlassian.theplugin.idea;
 
 import com.atlassian.theplugin.bamboo.BuildStatus;
-import com.intellij.lang.properties.ResourceBundle;
+import com.atlassian.theplugin.exception.ThePluginException;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.ide.BrowserUtil;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.Icon;
+import javax.swing.*;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.event.HyperlinkEvent;
+import static javax.swing.event.HyperlinkEvent.EventType.ACTIVATED;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+import java.io.IOException;
 
-/**
- * Created by IntelliJ IDEA.
- * User: sginter
- * Date: Jan 15, 2008
- * Time: 3:54:00 PM
- * To change this template use File | Settings | File Templates.
- */
+
 public class BambooStatusIcon extends JLabel {
 
     Icon iconRed, iconGreen, iconGrey;
 
+    PluginStatusBarToolTip tooltip;
+
+    
     BambooStatusIcon() {
+
+        // load icons
         iconRed = IconLoader.getIcon("/icons/red-16.png");
         iconGreen = IconLoader.getIcon("/icons/green-16.png");
         iconGrey = IconLoader.getIcon("/icons/grey-16.png");
 
+        // show tooltip on mouse over
+		tooltip = new PluginStatusBarToolTip();
+		addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                tooltip.showToltip(e.getXOnScreen(), e.getYOnScreen());
+            }
+        });
+
     }
 
     public void updateBambooStatus(BuildStatus status, String fullInfo) {
-        setText("");
-        setToolTipText(fullInfo);
+
+        tooltip.setHtmlContent(fullInfo);
 
         switch (status) {
             case ERROR:
@@ -40,6 +52,9 @@ public class BambooStatusIcon extends JLabel {
             case SUCCESS:
                 setIcon(iconGreen);
                 break;
+            default:
+                setIcon(iconGrey);
         }
     }
+
 }
