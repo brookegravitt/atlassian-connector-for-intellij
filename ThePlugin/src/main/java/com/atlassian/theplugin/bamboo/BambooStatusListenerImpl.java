42ca2ea5590c4e81f315862e5275edb95f454136
mwent <mwent@localhost>
2008-01-24 12:47:53 +0000

    bamboo windows with links to server

--- ThePlugin/src/main/java/com/atlassian/theplugin/bamboo/BambooStatusListenerImpl.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/bamboo/BambooStatusListenerImpl.java
@@ -21,6 +21,54 @@ public class BambooStatusListenerImpl implements BambooStatusListener {
         statusBarIcon = icon;
     }
 
+    private String getSuccessBuildRow(BambooBuild buildInfo) {
+        StringBuilder sb = new StringBuilder("<tr><td><a href='");
+        sb.append(buildInfo.getPlanUrl());
+        sb.append("'>");
+        sb.append(buildInfo.getBuildKey());
+        sb.append("</a></td><td><a href='");
+        sb.append(buildInfo.getBuildUrl());
+        sb.append("'>");
+        sb.append("build ");
+        sb.append(buildInfo.getBuildNumber());
+        sb.append("</a>");
+        sb.append("</td><td>");
+        sb.append("<font color=\"green\">success</font>");
+        sb.append("</td></tr>");
+
+        return sb.toString();
+    }
+
+    private String getFailedBuildRow(BambooBuild buildInfo) {
+        StringBuilder sb = new StringBuilder("<tr><td><a href='");
+        sb.append(buildInfo.getPlanUrl());
+        sb.append("'>");
+        sb.append(buildInfo.getBuildKey());
+        sb.append("</a></td><td><a href='");
+        sb.append(buildInfo.getBuildUrl());
+        sb.append("'>");
+        sb.append("build ");
+        sb.append(buildInfo.getBuildNumber());
+        sb.append("</a>");
+        sb.append("</td><td>");
+        sb.append("<font color=\"red\">failed</font>");
+        sb.append("</td></tr>");
+
+        return sb.toString();
+    }
+
+    private String getErrorBuildRow(BambooBuild buildInfo) {
+        StringBuilder sb = new StringBuilder("<tr><td><a href='");
+        sb.append(buildInfo.getPlanUrl());
+        sb.append("'>");
+        sb.append(buildInfo.getBuildKey());
+        sb.append("</a></td><td></td><td>");
+        sb.append("<font color=\"ltgray\">").append(buildInfo.getMessage()).append("</font>");
+        sb.append("</td></tr>");
+
+        return sb.toString();
+    }
+
     public void updateBuildStatuses(Collection<BambooBuild> buildStatuses) {
 
         BuildStatus status = BuildStatus.SUCCESS;
@@ -31,27 +79,23 @@ public class BambooStatusListenerImpl implements BambooStatusListener {
         } else {
             sb.append("<table>");
             for (BambooBuild buildInfo : buildStatuses) {
-                sb.append("<tr><td>");
-                sb.append(buildInfo.getBuildKey());
-                sb.append("</td><td>");
                 switch (buildInfo.getStatus()) {
                     case FAILED:
-                        sb.append("<font color=\"red\">build failed</font>");
+                        sb.append(getFailedBuildRow(buildInfo));
                         status = FAILED;
                         break;
                     case ERROR:
-                        sb.append("<font color=\"ltgray\">").append(buildInfo.getMessage()).append("</font>");
+                        sb.append(getErrorBuildRow(buildInfo));
                         if (status != FAILED) {
                             status = BuildStatus.ERROR;
                         }
                         break;
                     case SUCCESS:
-                        sb.append("<font color=\"green\">success</font>");
+                        sb.append(getSuccessBuildRow(buildInfo));
                         break;
                     default:
                         throw new IllegalStateException("Unexpected build status encountered");
                 }
-                sb.append("</td></tr>");
             }
             sb.append("</table>");
         }
