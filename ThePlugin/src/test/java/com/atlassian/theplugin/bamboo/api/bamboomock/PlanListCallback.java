c72b2dee8aad92dc84e090f2ba77608d88496e90
sginter <sginter@localhost>
2008-01-29 17:05:22 +0000

    Use mock-based bamboo server tests

---
+++ ThePlugin/src/test/java/com/atlassian/theplugin/bamboo/api/bamboomock/PlanListCallback.java
@@ -0,0 +1,27 @@
+package com.atlassian.theplugin.bamboo.api.bamboomock;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import org.ddsteps.mock.httpserver.JettyMockServer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PlanListCallback implements JettyMockServer.Callback {
+	public void onExpectedRequest(String target,
+								  HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		assertTrue(request.getPathInfo().endsWith("/api/rest/listBuildNames.action"));
+
+		final String[] authTokens = request.getParameterValues("auth");
+		assertEquals(1, authTokens.length);
+
+		final String authToken = authTokens[0];
+		assertEquals(LoginCallback.AUTH_TOKEN, authToken);
+
+		Util.copyResource(response.getOutputStream(), "planListResponse.xml");
+		response.getOutputStream().flush();
+
+	}
+}
