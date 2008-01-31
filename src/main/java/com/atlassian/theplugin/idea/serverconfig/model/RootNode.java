f36ec56be5e8d8a1dae0ef3cb0cf962207a3cbe6
mwent <mwent@localhost>
2008-01-31 08:50:57 +0000

    prepare for multi server

---
+++ src/main/java/com/atlassian/theplugin/idea/serverconfig/model/RootNode.java
@@ -0,0 +1,13 @@
+package com.atlassian.theplugin.idea.serverconfig.model;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mwent
+ * Date: 2008-01-29
+ * Time: 09:31:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class RootNode extends DefaultMutableTreeNode {
+}
---
+++ src/main/java/com/atlassian/theplugin/idea/serverconfig/model/ServerTypeNode.java
@@ -0,0 +1,50 @@
+package com.atlassian.theplugin.idea.serverconfig.model;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * Represents server type node on servers JTree
+ * User: mwent
+ * Date: 2008-01-29
+ * Time: 09:32:04
+ */
+public class ServerTypeNode extends DefaultMutableTreeNode {
+	ServerType serverType;
+
+	public ServerTypeNode(ServerType aServerType) {
+		this.serverType = aServerType;
+	}
+
+	public ServerType getServerType() {
+		return serverType;
+	}
+
+	public void setServerType(ServerType serverType) {
+		this.serverType = serverType;
+	}
+
+	public String toString() {
+		return serverType.toString();
+	}
+
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		ServerTypeNode that = (ServerTypeNode) o;
+
+		if (serverType != that.serverType) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public int hashCode() {
+		return serverType.hashCode();
+	}
+}
