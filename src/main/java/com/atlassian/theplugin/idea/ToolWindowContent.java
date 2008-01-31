eec457a2ae21026ee05cf595d9999dc79f088762
jjaroczynski <jjaroczynski@localhost>
2008-01-31 16:53:21 +0000

    simple docable tool window on the right side of IDEA (just a simple example, needs refactoring)

---
+++ src/main/java/com/atlassian/theplugin/idea/ToolWindowContent.java
@@ -0,0 +1,37 @@
+package com.atlassian.theplugin.idea;
+
+import com.atlassian.theplugin.bamboo.BambooStatusDisplay;
+import com.atlassian.theplugin.bamboo.BuildStatus;
+import com.intellij.ide.BrowserUtil;
+
+import javax.swing.*;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.event.HyperlinkEvent;
+import static javax.swing.event.HyperlinkEvent.EventType.ACTIVATED;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jacek
+ * Date: 2008-01-31
+ * Time: 17:36:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class ToolWindowContent extends JEditorPane implements BambooStatusDisplay {
+
+	ToolWindowContent () {
+		setEditable(false);
+        setContentType("text/html");
+		addHyperlinkListener(new HyperlinkListener() {
+
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                if (e.getEventType().equals(ACTIVATED)) {
+                    BrowserUtil.launchBrowser(e.getURL().toString());
+                }
+            }
+        });
+	}
+	public void updateBambooStatus(BuildStatus generalBuildStatus, String htmlPage) {
+		this.setText(htmlPage);
+	}
+	
+}
