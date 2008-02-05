caa63243c40321121bc271217e758128c903ffa7
mwent <mwent@localhost>
2008-02-05 14:01:30 +0000

    layouts tweaking

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/serverconfig/HeaderPanel.java
@@ -0,0 +1,26 @@
+package com.atlassian.theplugin.idea.serverconfig;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mwent
+ * Date: 2008-02-05
+ * Time: 11:42:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class HeaderPanel extends JPanel {
+	private JCheckBox enablePlugin;
+
+	public HeaderPanel() {
+		initLayout();
+    }
+
+	private void initLayout() {
+        BorderLayout gb = new BorderLayout();
+		setLayout(gb);
+		enablePlugin = new JCheckBox("Enable plugin");
+		add(enablePlugin, BorderLayout.WEST);
+	}
+}
