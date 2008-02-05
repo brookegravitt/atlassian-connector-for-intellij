5146e5faf4f1ed7d91f364190ee3002db1165866
mwent <mwent@localhost>
2008-02-05 14:01:30 +0000

    layouts tweaking

--- ThePlugin/src/main/java/com/atlassian/theplugin/idea/serverconfig/FooterPanel.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/serverconfig/FooterPanel.java
@@ -6,6 +6,7 @@ import com.intellij.ui.HyperlinkLabel;
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
+import java.awt.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,17 +23,15 @@ public class FooterPanel extends JPanel {
     }
 
 	private void initLayout() {
-//        GridBagLayout gb = new GridBagLayout();
-//		setLayout(gb);
-		
+		BorderLayout gb = new BorderLayout();
+		setLayout(gb);
+
 		openJiraHyperlinkLabel = new HyperlinkLabel("Report a bug/issue/request.");
 		openJiraHyperlinkLabel.addHyperlinkListener(new HyperlinkListener() {
 			public void hyperlinkUpdate(HyperlinkEvent e) {
 				BrowserUtil.launchBrowser("https://studio.atlassian.com/browse/PL");
 			}
 		});	
-
-		add(openJiraHyperlinkLabel);
-//		add(openJiraHyperlinkLabel, new GridBagLayoutConstraints(1, 2).setAnchor(GridBagLayoutConstraints.EAST).setWeight(0.0, 1.0).setFill(GridBagLayoutConstraints.BOTH));		
+		add(openJiraHyperlinkLabel, BorderLayout.EAST);
 	}
 }
