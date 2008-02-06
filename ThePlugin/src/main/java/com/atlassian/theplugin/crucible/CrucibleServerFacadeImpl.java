b56a7ff2cae2c4bb00a702a2cc5cc860df99698c
jjaroczynski <jjaroczynski@localhost>
2008-02-06 09:22:12 +0000

    PL-54 crucible facade basics

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/crucible/CrucibleServerFacadeImpl.java
@@ -0,0 +1,30 @@
+package com.atlassian.theplugin.crucible;
+
+import com.atlassian.theplugin.crucible.api.CrucibleLoginException;
+import com.atlassian.theplugin.crucible.api.CrucibleSession;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jacek
+ * Date: 2008-02-05
+ * Time: 16:27:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class CrucibleServerFacadeImpl implements CrucibleServerFacade {
+	private CrucibleSession crucibleSession = null;
+
+	public CrucibleServerFacadeImpl () {
+		//crucibleSession = session;
+	}
+
+	public void testServerConnection(String serverUrl, String userName, String password) throws CrucibleLoginException {
+		CrucibleSession session = crucibleSession;
+
+		if (session == null) {
+			session = new CrucibleSession(serverUrl);
+		}
+		
+		session.login(userName, password);
+		session.logout();
+	}
+}
