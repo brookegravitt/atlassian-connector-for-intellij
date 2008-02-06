0c2ba28b8dddadd3fa10486a385c989c8e95cf9e
mwent <mwent@localhost>
2008-02-06 08:08:49 +0000

    refactoring

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/config/AbstractContentPanel.java
@@ -0,0 +1,16 @@
+package com.atlassian.theplugin.idea.config;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mwent
+ * Date: 2008-02-06
+ * Time: 09:00:54
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class AbstractContentPanel extends JPanel {
+	public abstract boolean isEnabled();
+	public abstract boolean isModified();
+	public abstract String getTitle();
+}
