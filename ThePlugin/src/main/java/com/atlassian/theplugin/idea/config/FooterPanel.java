a8930bd5ea6af0b5968663b52da40f792fac4768
jgorycki <jgorycki@localhost>
2008-02-07 14:40:09 +0000

    fixed PL-91

--- ThePlugin/src/main/java/com/atlassian/theplugin/idea/config/FooterPanel.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/config/FooterPanel.java
@@ -8,37 +8,106 @@ import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * Created by IntelliJ IDEA.
- * User: mwent
- * Date: 2008-02-05
- * Time: 11:42:01
- * To change this template use File | Settings | File Templates.
- */
 public class FooterPanel extends JPanel {
-	private JLabel versionLabel;
-	private HyperlinkLabel openJiraHyperlinkLabel;
+	private HyperlinkLabel openJiraHyperlinkBugLabel;
+	private HyperlinkLabel openJiraHyperlinkStoryLabel;
+
+	private static final String BASE = "https://studio.atlassian.com/secure/CreateIssueDetails!init.jspa";
+	private static final String PROJECT_ID = "10024";
+	private static final String TICKET_TYPE_BUG = "1";
+	private static final String TICKET_TYPE_STORY = "5";
+
+	// TODO: this version list needs updating every time we add some new version to Jira.
+	// This is all basically incorrect code and will break beyond 2.0, but whatever
+	// - parsing Jira form source would be a ridiculous thing to do at this point
+	private static Map<String, String> versionMap = new HashMap<String, String>();
+
+	{
+		versionMap.put("0", "10031"); // don't remove this entry or I will kill you
+		versionMap.put("0.0.1", "10010");
+		versionMap.put("0.1.0", "10011");
+		versionMap.put("0.2.0", "10012");
+		versionMap.put("0.3.0", "10013");
+		versionMap.put("0.4.0", "10014");
+		versionMap.put("0.5.0", "10015");
+		versionMap.put("1.0.0", "10016");
+		versionMap.put("1.1.0", "10017");
+		versionMap.put("1.2.0", "10018");
+		versionMap.put("1.3.0", "10019");
+		versionMap.put("1.4.0", "10020");
+		versionMap.put("1.5.0", "10021");
+		versionMap.put("2.0.0", "10022");
+	}
 
 	public FooterPanel() {
 		initLayout();
     }
 
 	private void initLayout() {
-		String versionName = "unknown";
+		String versionName = PluginInfoUtil.getVersion();
 
 		BorderLayout gb = new BorderLayout();
 		setLayout(gb);
-		versionName = PluginInfoUtil.getVersion();
-		versionLabel = new JLabel(versionName);
 
-		openJiraHyperlinkLabel = new HyperlinkLabel("Report a bug/issue/request.");
-		openJiraHyperlinkLabel.addHyperlinkListener(new HyperlinkListener() {
+		// versions seen here are formatted:
+		// "x.y.z-SNAPSHOT, SVN:ijk" or "x.y.z, SVN:ijk"
+		// let's check for both possibilities
+		int i = versionName.indexOf('-');
+		if (i == -1) {
+			i = versionName.indexOf(',');
+		}
+
+		String versionForJira;
+		if (i != -1) {
+			versionForJira = versionName.substring(0, i);
+		} else {
+			// this is going to suck and Jira is unlikely to find such a version, but
+			// if we are here, this means that the version string is screwed up somehow
+			// so whatever - it is b0rked anyway. let's pick "0" - it is the first pseudo-version
+			// we have in Jira
+			versionForJira = "0";
+		}
+
+		String versionCodeForJira = versionMap.get(versionForJira);
+		if (versionCodeForJira == null) {
+			// this is broken, but whatever. The user can always reselect the version manually. I hope :)
+			versionCodeForJira = versionMap.get("0");
+		}
+
+		final String bugUrl = BASE
+						+ "?pid=" + PROJECT_ID
+						+ "&versions=" + versionCodeForJira
+						+ "&issuetype=" + TICKET_TYPE_BUG;
+		final String storyUrl = BASE
+				+ "?pid=" + PROJECT_ID
+				+ "&versions=" + versionCodeForJira
+				+ "&issuetype=" + TICKET_TYPE_STORY;
+
+		JPanel linkPanel = new JPanel();
+		
+		openJiraHyperlinkBugLabel = new HyperlinkLabel("bug");
+		openJiraHyperlinkBugLabel.addHyperlinkListener(new HyperlinkListener() {
 			public void hyperlinkUpdate(HyperlinkEvent e) {
-				BrowserUtil.launchBrowser("https://studio.atlassian.com/browse/PL");
+				BrowserUtil.launchBrowser(bugUrl);
 			}
 		});
-		add(versionLabel, BorderLayout.WEST);
-		add(openJiraHyperlinkLabel, BorderLayout.EAST);
+
+		openJiraHyperlinkStoryLabel = new HyperlinkLabel("feature request");
+		openJiraHyperlinkStoryLabel.addHyperlinkListener(new HyperlinkListener() {
+			public void hyperlinkUpdate(HyperlinkEvent e) {
+				BrowserUtil.launchBrowser(storyUrl);
+			}
+		});
+
+		linkPanel.add(new JLabel("report new"));
+		linkPanel.add(openJiraHyperlinkBugLabel);
+		linkPanel.add(new JLabel("/"));
+		linkPanel.add(openJiraHyperlinkStoryLabel);
+		add(linkPanel, BorderLayout.EAST);
+
+		add(new JLabel(versionName), BorderLayout.WEST);
 	}
 }
