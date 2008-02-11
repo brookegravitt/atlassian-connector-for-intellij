cb3d21e9245c6b84cafaf91d3c0c14bede17fa8e
sginter <sginter@localhost>
2008-02-11 09:04:18 +0000

    PL-9 - very initial support. Commit dialog is decorated with our action, which creates a patch (malformed and without context) and prints it out to stdout.

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/crucible/CruciblePatchSubmitExecutor.java
@@ -0,0 +1,38 @@
+package com.atlassian.theplugin.idea.crucible;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.changes.CommitExecutor;
+import com.intellij.openapi.vcs.changes.CommitSession;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class CruciblePatchSubmitExecutor implements CommitExecutor {
+	private final Project project;
+
+	public CruciblePatchSubmitExecutor(Project project) {
+		this.project = project;
+	}
+
+	@NotNull
+	public Icon getActionIcon() {
+		//TODO: implement method getActionIcon
+		throw new UnsupportedOperationException("method getActionIcon not implemented");
+	}
+
+	@Nls
+	public String getActionText() {
+		return "Crucible Patch...";
+	}
+
+	@Nls
+	public String getActionDescription() {
+		return "Creates a patch from the files that would be commited and sends it for review to the Crucible server.";
+	}
+
+	@NotNull
+	public CommitSession createCommitSession() {
+		return new CruciblePatchSubmitCommitSession(project);
+	}
+}
