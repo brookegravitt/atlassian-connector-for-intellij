2c282c73fbcd5a0849132e805f97b1fa6c74f2f2
mwent <mwent@localhost>
2008-02-11 13:36:26 +0000

    don't show empty server types

--- src/main/java/com/atlassian/theplugin/idea/config/serverconfig/ServerTreePanel.java
+++ src/main/java/com/atlassian/theplugin/idea/config/serverconfig/ServerTreePanel.java
@@ -24,74 +24,74 @@ import java.util.Collection;
  */
 public class ServerTreePanel extends JPanel implements TreeSelectionListener {
 
-    private JTree serverTree = null;
-    private PluginConfiguration pluginConfiguration = null;
-    private ServerTreeModel model;
-    private DefaultMutableTreeNode selectedNode = null;
-    private DefaultMutableTreeNode newSelectedNode = null;
-    private DefaultMutableTreeNode firstServerNode = null;
+	private JTree serverTree = null;
+	private PluginConfiguration pluginConfiguration = null;
+	private ServerTreeModel model;
+	private DefaultMutableTreeNode selectedNode = null;
+	private DefaultMutableTreeNode newSelectedNode = null;
+	private DefaultMutableTreeNode firstServerNode = null;
 
-    public void setModel(ServerTreeModel model) {
-        this.model = model;
-    }
+	public void setModel(ServerTreeModel model) {
+		this.model = model;
+	}
 
-    public ServerTreePanel() {
-        initLayout();
-    }
+	public ServerTreePanel() {
+		initLayout();
+	}
 
-    private void initLayout() {
-        setLayout(new BorderLayout());
-        setMinimumSize(new Dimension(150, 250));
-        add(new JScrollPane(getServerTree()), BorderLayout.CENTER);
-    }
+	private void initLayout() {
+		setLayout(new BorderLayout());
+		setMinimumSize(new Dimension(150, 250));
+		add(new JScrollPane(getServerTree()), BorderLayout.CENTER);
+	}
 
-    private JTree getServerTree() {
-        if (serverTree == null) {
-            serverTree = new JTree();
-            serverTree.setName("Server tree");
+	private JTree getServerTree() {
+		if (serverTree == null) {
+			serverTree = new JTree();
+			serverTree.setName("Server tree");
 
-            RootNode root = new RootNode();
-            model = new ServerTreeModel(root);
-            serverTree.setModel(model);
+			RootNode root = new RootNode();
+			model = new ServerTreeModel(root);
+			serverTree.setModel(model);
 
-            serverTree.setRootVisible(false);
-            serverTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-            serverTree.setExpandsSelectedPaths(true);
-            serverTree.setVisibleRowCount(7);
+			serverTree.setRootVisible(false);
+			serverTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+			serverTree.setExpandsSelectedPaths(true);
+			serverTree.setVisibleRowCount(7);
 
-            serverTree.addTreeSelectionListener(this);
+			serverTree.addTreeSelectionListener(this);
 
-            serverTree.setCellRenderer(new ServerTreeRenderer());
-        }
-        return serverTree;
-    }
+			serverTree.setCellRenderer(new ServerTreeRenderer());
+		}
+		return serverTree;
+	}
 
-    public void setEnabled(boolean b) {
-        super.setEnabled(b);
-        getServerTree().setEnabled(b);
-    }
+	public void setEnabled(boolean b) {
+		super.setEnabled(b);
+		getServerTree().setEnabled(b);
+	}
 
-    public String addServer(ServerType serverType) {
-        ServerBean newServer = new ServerBean();
+	public String addServer(ServerType serverType) {
+		ServerBean newServer = new ServerBean();
 
-        Collection<Server> servers = pluginConfiguration.getProductServers(serverType).getServers();
-        newServer.setName(ServerNameUtil.suggestNewName(servers));
-        pluginConfiguration.getProductServers(serverType).storeServer(newServer);
+		Collection<Server> servers = pluginConfiguration.getProductServers(serverType).getServers();
+		newServer.setName(ServerNameUtil.suggestNewName(servers));
+		pluginConfiguration.getProductServers(serverType).storeServer(newServer);
 
-        ServerNode child = ServerNodeFactory.getServerNode(serverType, newServer);
-        ServerTypeNode serverTypeNode = model.getServerTypeNode(serverType);
-        model.insertNodeInto(child, serverTypeNode, serverTypeNode.getChildCount());
-        model.reload();
+		ServerNode child = ServerNodeFactory.getServerNode(serverType, newServer);
+		ServerTypeNode serverTypeNode = model.getServerTypeNode(serverType, true);
+		model.insertNodeInto(child, serverTypeNode, serverTypeNode.getChildCount());
+		model.reload();
 
-        TreePath path = new TreePath(child.getPath());
-        serverTree.scrollPathToVisible(path);
-        serverTree.setSelectionPath(path);
-        serverTree.expandPath(path);
+		TreePath path = new TreePath(child.getPath());
+		serverTree.scrollPathToVisible(path);
+		serverTree.setSelectionPath(path);
+		serverTree.expandPath(path);
 
-        return newServer.getName();
-    }
+		return newServer.getName();
+	}
 
-    public void copyServer() {
+	public void copyServer() {
 /*
 		ServerBean newServer = new ServerBean();
 		newServer.setName(suggestCopyName(ConfigurationFactory.getConfiguration().getBambooConfiguration().getServers()));
@@ -99,105 +99,110 @@ public class ServerTreePanel extends JPanel implements TreeSelectionListener {
 		serverTree.updateUI();
 		return newServer.getName();
 */
-    }
+	}
 
 
-    public void removeServer() {
-        if (selectedNode != null) {
-            if (selectedNode instanceof ServerNode) {
-                int response = JOptionPane.showConfirmDialog(this,
-                        "Are you sure you want to delete the selected server?",
-                        "Confirm server delete",
-                        JOptionPane.YES_NO_OPTION,
-                        JOptionPane.QUESTION_MESSAGE,
-                        null);
+	public void removeServer() {
+		if (selectedNode != null) {
+			if (selectedNode instanceof ServerNode) {
+				int response = JOptionPane.showConfirmDialog(this,
+						"Are you sure you want to delete the selected server?",
+						"Confirm server delete",
+						JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null);
 
-                if (response != 0) {
-                    return;
-                }
-                TreePath path = new TreePath(((DefaultMutableTreeNode) selectedNode.getParent()).getPath());
-                this.pluginConfiguration.getProductServers(((ServerNode) selectedNode).getServerType()).removeServer(((ServerNode) selectedNode).getServer());
-                updateTreeConfiguration();
+				if (response != 0) {
+					return;
+				}
+				TreePath path = new TreePath(((DefaultMutableTreeNode) selectedNode.getParent()).getPath());
+				this.pluginConfiguration.getProductServers(((ServerNode) selectedNode).getServerType()).removeServer(((ServerNode) selectedNode).getServer());
+				updateTreeConfiguration();
 
-                serverTree.expandPath(path);
-            }
-        }
-    }
+				serverTree.expandPath(path);
+			}
+		}
+	}
 
-    public void setData(PluginConfiguration aPluginConfiguration) {
-        this.pluginConfiguration = aPluginConfiguration;
-        updateTreeConfiguration();
-    }
+	public void setData(PluginConfiguration aPluginConfiguration) {
+		this.pluginConfiguration = aPluginConfiguration;
+		updateTreeConfiguration();
+	}
 
-    private void updateServerTree(ServerType serverType) {
-        Collection<Server> servers = pluginConfiguration.getProductServers(serverType).getServers();
+	private void updateServerTree(ServerType serverType) {
+		Collection<Server> servers = pluginConfiguration.getProductServers(serverType).getServers();
 
-        ServerTypeNode serverTypeNode = model.getServerTypeNode(serverType);
-        serverTypeNode.removeAllChildren();
-        model.reload();
+		// !servers.isEmpty()
+		// if server list is empty, don't create server type node,
+		// otherwise create node - it would be required
+		ServerTypeNode serverTypeNode = model.getServerTypeNode(serverType, !servers.isEmpty());
+		if (serverTypeNode != null) {
+			serverTypeNode.removeAllChildren();
+		}
+		model.reload();
 
-        for (Server server : servers) {
-            ServerNode child = ServerNodeFactory.getServerNode(serverType, server);
+		for (Server server : servers) {
+			ServerNode child = ServerNodeFactory.getServerNode(serverType, server);
+			
+			model.insertNodeInto(child, serverTypeNode, serverTypeNode.getChildCount());
 
-            model.insertNodeInto(child, serverTypeNode, serverTypeNode.getChildCount());
+			if (firstServerNode == null) {
+				firstServerNode = child;
+			}
 
-            if (firstServerNode == null) {
-                firstServerNode = child;
-            }
+			if (selectedNode != null && selectedNode instanceof ServerNode) {
+				if (child.getServer().equals(((ServerNode) selectedNode).getServer())) {
+					newSelectedNode = child;
+				}
+			}
+		}
+	}
 
-            if (selectedNode != null && selectedNode instanceof ServerNode) {
-                if (child.getServer().equals(((ServerNode) selectedNode).getServer())) {
-                    newSelectedNode = child;
-                }
-            }
-        }
-    }
+	private void updateTreeConfiguration() {
+		firstServerNode = null;
+		updateServerTree(ServerType.BAMBOO_SERVER);
+		updateServerTree(ServerType.CRUCIBLE_SERVER);
 
-    private void updateTreeConfiguration() {
-        firstServerNode = null;
-        updateServerTree(ServerType.BAMBOO_SERVER);
-        updateServerTree(ServerType.CRUCIBLE_SERVER);
+		if (newSelectedNode != null) {
+			selectedNode = newSelectedNode;
+			TreePath path = new TreePath(selectedNode.getPath());
+			serverTree.scrollPathToVisible(path);
+			serverTree.setSelectionPath(path);
+			serverTree.expandPath(path);
+		} else {
+			selectedNode = null;
+			ConfigPanel.getInstance().getServerConfigPanel().showEmptyPanel();
+			if (firstServerNode != null) {
+				TreePath path = new TreePath(firstServerNode.getPath());
+				serverTree.scrollPathToVisible(path);
+				serverTree.setSelectionPath(path);
+				serverTree.expandPath(path);
+			}
+		}
+	}
 
-        if (newSelectedNode != null) {
-            selectedNode = newSelectedNode;
-            TreePath path = new TreePath(selectedNode.getPath());
-            serverTree.scrollPathToVisible(path);
-            serverTree.setSelectionPath(path);
-            serverTree.expandPath(path);
-        } else {
-            selectedNode = null;
-            ConfigPanel.getInstance().getServerConfigPanel().showEmptyPanel();
-            if (firstServerNode != null) {
-                TreePath path = new TreePath(firstServerNode.getPath());
-                serverTree.scrollPathToVisible(path);
-                serverTree.setSelectionPath(path);
-                serverTree.expandPath(path);
-            }
-        }
-    }
+	public void valueChanged(TreeSelectionEvent e) {
+		TreePath path = e.getNewLeadSelectionPath();
 
-    public void valueChanged(TreeSelectionEvent e) {
-        TreePath path = e.getNewLeadSelectionPath();
+		if (path != null) {
+			TreePath oldPath = e.getOldLeadSelectionPath();
+			if (oldPath != null) {
+				DefaultMutableTreeNode oldNode = (DefaultMutableTreeNode) oldPath.getLastPathComponent();
+				if (oldNode != null && oldNode instanceof ServerNode) {
+					ConfigPanel.getInstance().storeServer((ServerNode) oldNode);
+				}
+			}
+			selectedNode = (DefaultMutableTreeNode) path.getLastPathComponent();
+			if (selectedNode instanceof ServerNode) {
+				ConfigPanel.getInstance().getServerConfigPanel().editServer(((ServerNode) selectedNode).getServerType(), ((ServerNode) selectedNode).getServer());
+			} else {
+				ConfigPanel.getInstance().getServerConfigPanel().showEmptyPanel();
+			}
+		} else {
+			ConfigPanel.getInstance().getServerConfigPanel().showEmptyPanel();
+		}
 
-        if (path != null) {
-            TreePath oldPath = e.getOldLeadSelectionPath();
-            if (oldPath != null) {
-                DefaultMutableTreeNode oldNode = (DefaultMutableTreeNode) oldPath.getLastPathComponent();
-                if (oldNode != null && oldNode instanceof ServerNode) {
-                    ConfigPanel.getInstance().storeServer((ServerNode) oldNode);
-                }
-            }
-            selectedNode = (DefaultMutableTreeNode) path.getLastPathComponent();
-            if (selectedNode instanceof ServerNode) {
-                ConfigPanel.getInstance().getServerConfigPanel().editServer(((ServerNode) selectedNode).getServerType(), ((ServerNode) selectedNode).getServer());
-            } else {
-                ConfigPanel.getInstance().getServerConfigPanel().showEmptyPanel();
-            }
-        } else {
-            ConfigPanel.getInstance().getServerConfigPanel().showEmptyPanel();
-        }
-
-    }
+	}
 }
 
 
