c36c5b3c7a796ab48f43594ac20dfa0adec112b9
sginter <sginter@localhost>
2008-02-11 11:27:09 +0000

    PL-9 - title added to the crucible upload dialog

--- src/main/java/com/atlassian/theplugin/idea/crucible/CruciblePatchUploader.java
+++ src/main/java/com/atlassian/theplugin/idea/crucible/CruciblePatchUploader.java
@@ -2,14 +2,17 @@ package com.atlassian.theplugin.idea.crucible;
 
 public class CruciblePatchUploader implements Runnable {
 	private final String patch;
+	private final String commitMessage;
 
-	public CruciblePatchUploader(String patch) {
+	public CruciblePatchUploader(String commitMessage, String patch) {
+		this.commitMessage = commitMessage;
 		this.patch = patch;
 	}
 
 	public void run() {
 		final CruciblePatchUploadForm patchUploadForm = new CruciblePatchUploadForm();
 		patchUploadForm.setPatchPreview(patch);
+		patchUploadForm.setTitle(commitMessage);
 		patchUploadForm.show();
 	}
 }
