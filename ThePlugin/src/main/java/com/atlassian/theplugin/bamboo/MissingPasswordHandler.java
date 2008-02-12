7f346cbccd9f97f240127dc0138ea8d19446ea6b
sginter <sginter@localhost>
2008-02-12 11:09:02 +0000

    Reintroduce changes after Marek commited his without proper merge to top

--- ThePlugin/src/main/java/com/atlassian/theplugin/bamboo/MissingPasswordHandler.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/bamboo/MissingPasswordHandler.java
@@ -1,12 +1,12 @@
 package com.atlassian.theplugin.bamboo;
 
+import com.atlassian.theplugin.ServerType;
 import com.atlassian.theplugin.configuration.ConfigurationFactory;
 import com.atlassian.theplugin.configuration.Server;
 import com.atlassian.theplugin.configuration.ServerBean;
 import com.atlassian.theplugin.idea.PasswordDialog;
 import com.atlassian.theplugin.idea.PluginInfoUtil;
 import com.atlassian.theplugin.idea.ThePluginApplicationComponent;
-import com.atlassian.theplugin.ServerType;
 import com.intellij.openapi.application.ApplicationManager;
 
 import javax.swing.*;
@@ -14,11 +14,7 @@ import static javax.swing.JOptionPane.OK_CANCEL_OPTION;
 import static javax.swing.JOptionPane.PLAIN_MESSAGE;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Jacek
- * Date: 2008-01-25
- * Time: 11:20:12
- * To change this template use File | Settings | File Templates.
+ * Shows a dialog for each Bamboo server that has not the password set.
  */
 public class MissingPasswordHandler implements Runnable {
 
@@ -26,36 +22,33 @@ public class MissingPasswordHandler implements Runnable {
 
 	public void run() {
 
-		//ServerBean conf = (ServerBean) (ConfigurationFactory.getConfiguration().getBambooConfiguration().getServer());
-
 		if (!isDialogShown) {
 
 			isDialogShown = true;
 
 			for (Server server : ConfigurationFactory.getConfiguration().getProductServers(ServerType.BAMBOO_SERVER).getServers()) {
+				if (server.getIsConfigInitialized()) {
+					continue;
+				}
 				ServerBean serverBean = (ServerBean) server;
 				PasswordDialog dialog = new PasswordDialog(server);
 				dialog.setUserName(serverBean.getUsername());
 				dialog.pack();
 				JPanel panel = dialog.getPasswordPanel();
 
-				//WindowManager.getInstance().getAllFrames();
-
 				int answer = JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), panel,
-						PluginInfoUtil.getName(), OK_CANCEL_OPTION, PLAIN_MESSAGE);
+														   PluginInfoUtil.getName(), OK_CANCEL_OPTION, PLAIN_MESSAGE);
 
-				String password = "";
-				Boolean shouldPasswordBeStored = false;
 				if (answer == JOptionPane.OK_OPTION) {
-					password = dialog.getPasswordString();
-					shouldPasswordBeStored = dialog.getShouldPasswordBeStored();
+					String password = dialog.getPasswordString();
+					Boolean shouldPasswordBeStored = dialog.getShouldPasswordBeStored();
 					serverBean.setPasswordString(password, shouldPasswordBeStored);
 					serverBean.setUsername(dialog.getUserName());
 				} else {
 
 					JOptionPane.showMessageDialog(null,
-							"You can always change password by changing plugin settings (Preferences | IDE Settings | "
-									+ PluginInfoUtil.getName() + ")");
+												  "You can always change password by changing plugin settings (Preferences | IDE Settings | "
+														  + PluginInfoUtil.getName() + ")");
 				}
 				// so or so we assume that user provided password
 
