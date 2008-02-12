b4ca31cbf6f0d06ae58ee4d4f348e7733d6d6b3e
mcannon <mcannon@localhost>
2008-02-12 10:43:56 +0000

    Added a "Configure" link to get to plugin config easily. Especially useful for new installations (where there are no plans specified).
    Also, put the Atlassian Archiva maven repository first in the list so that Maven 2 runs a little faster.

--- ThePlugin/src/main/java/com/atlassian/theplugin/idea/ToolWindowBambooContent.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/ToolWindowBambooContent.java
@@ -3,11 +3,14 @@ package com.atlassian.theplugin.idea;
 import com.atlassian.theplugin.bamboo.BambooStatusDisplay;
 import com.atlassian.theplugin.bamboo.BuildStatus;
 import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.ProjectManager;
 
 import javax.swing.*;
-import javax.swing.event.HyperlinkListener;
 import javax.swing.event.HyperlinkEvent;
 import static javax.swing.event.HyperlinkEvent.EventType.ACTIVATED;
+import javax.swing.event.HyperlinkListener;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,8 +28,15 @@ public class ToolWindowBambooContent extends JEditorPane implements BambooStatus
 
             public void hyperlinkUpdate(HyperlinkEvent e) {
                 if (e.getEventType().equals(ACTIVATED)) {
-                    BrowserUtil.launchBrowser(e.getURL().toString());
-                }
+					if (e.getURL().toExternalForm().equals(ThePluginApplicationComponent.PLUGIN_CONFIG_URL))
+					{
+						ShowSettingsUtil.getInstance().editConfigurable(ProjectManager.getInstance().getDefaultProject(), ApplicationManager.getApplication().getComponent(ThePluginApplicationComponent.class));
+					}
+					else
+					{
+						BrowserUtil.launchBrowser(e.getURL().toString());
+					}
+				}
             }
         });
 	}
