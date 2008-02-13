a08e697936d6d2655128985fe817a6b4f45fa740
mwent <mwent@localhost>
2008-02-13 09:33:16 +0000

    PL-10

---
+++ src/main/java/com/atlassian/theplugin/idea/bamboo/ToolWindowBambooContent.java
@@ -0,0 +1,33 @@
+package com.atlassian.theplugin.idea.bamboo;
+
+import com.atlassian.theplugin.bamboo.BambooStatusDisplay;
+import com.atlassian.theplugin.bamboo.BuildStatus;
+import com.atlassian.theplugin.idea.GenericHyperlinkListener;
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.ProjectManager;
+
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import static javax.swing.event.HyperlinkEvent.EventType.ACTIVATED;
+import javax.swing.event.HyperlinkListener;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jacek
+ * Date: 2008-01-31
+ * Time: 17:36:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class ToolWindowBambooContent extends JEditorPane implements BambooStatusDisplay {
+	public ToolWindowBambooContent() {
+		setEditable(false);
+        setContentType("text/html");
+        addHyperlinkListener(new GenericHyperlinkListener());
+	}
+    
+    public void updateBambooStatus(BuildStatus generalBuildStatus, String htmlPage) {
+		this.setText(htmlPage);
+	}
+}
---
+++ src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleStatusChecker.java
@@ -0,0 +1,91 @@
+package com.atlassian.theplugin.idea.crucible;
+
+import com.atlassian.theplugin.ServerType;
+import com.atlassian.theplugin.crucible.CrucibleStatusListener;
+import com.atlassian.theplugin.crucible.RemoteReview;
+import com.atlassian.theplugin.crucible.CrucibleServerFactory;
+import com.atlassian.theplugin.crucible.HtmlCrucibleStatusListener;
+import com.atlassian.theplugin.crucible.api.CrucibleLoginException;
+import com.atlassian.theplugin.bamboo.BambooStatusListener;
+import com.atlassian.theplugin.bamboo.MissingPasswordHandler;
+import com.atlassian.theplugin.configuration.ConfigurationFactory;
+import com.atlassian.theplugin.configuration.Server;
+import com.atlassian.theplugin.configuration.ServerPasswordNotProvidedException;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.TimerTask;
+
+
+/**
+ * IDEA-specific class that uses {@link com.atlassian.theplugin.crucible.CrucibleServerFactory} to retrieve builds info and
+ * passes raw data to configured {@link com.atlassian.theplugin.crucible.CrucibleStatusListener}s.<p>
+ * <p/>
+ * Intended to be triggered by a {@link java.util.Timer} through the {@link #newTimerTask()}.<p>
+ * <p/>
+ * Thread safe.
+ */
+public class CrucibleStatusChecker {
+
+	private final List<CrucibleStatusListener> listenerList = new ArrayList<CrucibleStatusListener>();
+
+	public void registerListener(CrucibleStatusListener listener) {
+		synchronized (listenerList) {
+			listenerList.add(listener);
+		}
+	}
+
+	public void unregisterListener(CrucibleStatusListener listener) {
+		synchronized (listenerList) {
+			listenerList.remove(listener);
+		}
+	}
+
+	/**
+	 * DO NOT use that method in 'dispatching thread' of IDEA. It can block GUI for several seconds.
+	 */
+	private void doRun() {
+		// collect build info from each server
+		final Collection<RemoteReview> reviews = new ArrayList<RemoteReview>();
+		for (Server server : ConfigurationFactory.getConfiguration().getProductServers(ServerType.CRUCIBLE_SERVER).getEnabledServers()) {
+			try {
+				reviews.addAll(CrucibleServerFactory.getCrucibleServerFacade().getActiveReviewsForUser(server));
+			} catch (ServerPasswordNotProvidedException exception) {
+				ApplicationManager.getApplication().invokeLater(new MissingPasswordHandler(), ModalityState.defaultModalityState());
+			} catch (CrucibleLoginException e) {
+                // @todo
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+		// dispatch to the listeners
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				synchronized (listenerList) {
+					for (CrucibleStatusListener listener : listenerList) {
+						listener.updateReviews(reviews);
+					}
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create a new instance of {@link java.util.TimerTask} for {@link java.util.Timer} re-scheduling purposes.
+	 *
+	 * @return new instance of TimerTask
+	 */
+	public TimerTask newTimerTask() {
+		return new TimerTask() {
+			public void run() {
+				doRun();
+			}
+		};
+	}
+
+
+}
