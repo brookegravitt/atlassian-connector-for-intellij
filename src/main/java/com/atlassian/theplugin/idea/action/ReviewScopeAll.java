c4a501e8f2be535946ba82f95766e22e855089af
mwent <mwent@localhost>
2008-02-13 13:01:30 +0000

    Review scope combo box added

---
+++ src/main/java/com/atlassian/theplugin/idea/action/ReviewScopeAll.java
@@ -0,0 +1,10 @@
+package com.atlassian.theplugin.idea.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ReviewScopeAll extends AnAction {
+	public void actionPerformed(AnActionEvent event) {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+}
