2e616559d0fe21b27ad993c1d8c5c4f0f3fc950b
mcannon <mcannon@localhost>
2008-02-14 15:05:11 +0000

    Some code cleanups and niceties using IdeaHelper

--- src/main/java/com/atlassian/theplugin/configuration/PluginConfiguration.java
+++ src/main/java/com/atlassian/theplugin/configuration/PluginConfiguration.java
@@ -2,13 +2,6 @@ package com.atlassian.theplugin.configuration;
 
 import com.atlassian.theplugin.ServerType;
 
-/**
- * Created by IntelliJ IDEA.
- * User: sginter
- * Date: Jan 11, 2008
- * Time: 12:03:11 PM
- * To change this template use File | Settings | File Templates.
- */
 public interface PluginConfiguration {
 	boolean isPluginEnabled();
 	void setPluginEnabled(boolean value);
