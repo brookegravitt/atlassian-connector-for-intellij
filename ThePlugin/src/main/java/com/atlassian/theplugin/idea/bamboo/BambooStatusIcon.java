a94e98ff3347d208cce2bf5f78490bd5d7e3cd7d
mcannon <mcannon@localhost>
2008-02-14 16:30:56 +0000

    Switched to standard build icon and disabled tooltip on mouseover.

--- ThePlugin/src/main/java/com/atlassian/theplugin/idea/bamboo/BambooStatusIcon.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/bamboo/BambooStatusIcon.java
@@ -7,6 +7,7 @@ import com.atlassian.theplugin.idea.ThePluginProjectComponent;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.WindowManager;
+import com.intellij.ui.content.ContentManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,11 +16,11 @@ import java.awt.event.MouseEvent;
 
 public class BambooStatusIcon extends JLabel implements BambooStatusDisplay {
 
-	private static final Icon ICON_RED = IconLoader.getIcon("/icons/red-16.png");
-	private static final Icon ICON_GREEN = IconLoader.getIcon("/icons/green-16.png");
-	private static final Icon ICON_GREY = IconLoader.getIcon("/icons/grey-16.png");
+	private static final Icon ICON_RED = IconLoader.getIcon("/icons/icn_plan_failed.gif");
+	private static final Icon ICON_GREEN = IconLoader.getIcon("/icons/icn_plan_passed.gif");
+	private static final Icon ICON_GREY = IconLoader.getIcon("/icons/icn_plan_disabled.gif");
 
-	private final PluginStatusBarToolTip tooltip;
+//	private final PluginStatusBarToolTip tooltip;
 
 	/**
 	 * @param aProjectComponent reference to the project component
@@ -27,38 +28,42 @@ public class BambooStatusIcon extends JLabel implements BambooStatusDisplay {
     public BambooStatusIcon(final ThePluginProjectComponent aProjectComponent) {
 
 		// show tooltip on mouse over
-		tooltip = new PluginStatusBarToolTip(WindowManager.getInstance().getFrame(aProjectComponent.getProject()));
+//		tooltip = new PluginStatusBarToolTip(WindowManager.getInstance().getFrame(aProjectComponent.getProject()));
 
 		addMouseListener(new MouseAdapter() {
-			public void mouseEntered(MouseEvent e) {
+            /*
+            public void mouseEntered(MouseEvent e) {
 
 				tooltip.showToltip(
 						(int) MouseInfo.getPointerInfo().getLocation().getX(),
 						(int) MouseInfo.getPointerInfo().getLocation().getY());
 			}
+			*/
 
 			// show/hide toolbar on click
 			public void mouseClicked(MouseEvent e) {
 				ToolWindow toolWindow = aProjectComponent.getToolWindow();
-				toolWindow.activate(null);
-			}
+                ContentManager contentManager = toolWindow.getContentManager();
+                toolWindow.activate(null);
+                contentManager.setSelectedContent(contentManager.findContent("Bamboo"));
+            }
 		});
 
 	}
 
 
 	public void updateBambooStatus(BuildStatus status, String fullInfo) {
-
-		tooltip.setHtmlContent(fullInfo);
-
+//		tooltip.setHtmlContent(fullInfo);
 		switch (status) {
 			case BUILD_FAILED:
+                setToolTipText("Click to see failing builds.");
 				setIcon(ICON_RED);
-				break;
+                break;
 			case UNKNOWN:
 				setIcon(ICON_GREY);
 				break;
 			case BUILD_SUCCEED:
+                setToolTipText("All builds currently passing.");
 				setIcon(ICON_GREEN);
 				break;
 			default:
