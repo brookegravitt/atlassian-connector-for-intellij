49f7be316e0b73684267b2392300a9cc36cc3c5d
mcannon <mcannon@localhost>
2008-02-14 15:03:11 +0000

    Adding a simpler IdeaHelper class to collate commonly used methods / retrievers.

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/IdeaHelper.java
@@ -0,0 +1,29 @@
+package com.atlassian.theplugin.idea;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.application.ApplicationManager;
+
+/**
+ * Simple helper methods for the IDEA plugin
+ */
+public class IdeaHelper
+{
+    public static Project getCurrentProject(DataContext dataContext)
+    {
+        return DataKeys.PROJECT.getData(dataContext);
+    }
+
+    public static ToolWindow getToolWindow(Project p)
+    {
+        return ToolWindowManager.getInstance(p).getToolWindow(ThePluginProjectComponent.TOOL_WINDOW_NAME);
+    }
+
+    public static ThePluginApplicationComponent getAppComponent()
+    {
+        return ApplicationManager.getApplication().getComponent(ThePluginApplicationComponent.class);
+    }
+}
