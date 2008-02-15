146005f4451d7fb152075e912f75b84287f398ab
mcannon <mcannon@localhost>
2008-02-15 15:34:59 +0000

    Fixing checkstyle errors

--- src/test/java/com/atlassian/theplugin/configuration/ConfigurationBeanTest.java
+++ src/test/java/com/atlassian/theplugin/configuration/ConfigurationBeanTest.java
@@ -8,10 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collection;
 
-public class ConfigurationBeanTest extends TestCase
-{
-    public void testBean()
-    {
+public class ConfigurationBeanTest extends TestCase {
+    public void testBean() {
         ProductServerConfiguration config = new BambooConfigurationBean();
 
         assertTrue(config.getEnabledServers().isEmpty());
@@ -41,13 +39,12 @@ public class ConfigurationBeanTest extends TestCase
         config.removeServer(server);
         assertEquals(1, config.getServers().size());
         assertTrue(config.getServers().contains(server2));
-        
+
         // now check our persisted data is right
-        assertEquals(config.getServers(), ((BambooConfigurationBean)config).getServersData());
+        assertEquals(config.getServers(), ((BambooConfigurationBean) config).getServersData());
     }
 
-    public void testPersistence() throws Exception
-    {
+    public void testPersistence() throws Exception {
         // first, test a single ServerBean
         ServerBean server = new ServerBean();
         server.setUrlString("http://www.poland.pl");
@@ -67,17 +64,17 @@ public class ConfigurationBeanTest extends TestCase
         // now add the ServerBean to a BambooConfigurationBean
         BambooConfigurationBean bambooConfig = new BambooConfigurationBean();
         bambooConfig.storeServer(server);
-		e = XmlSerializer.serialize(bambooConfig);
+        e = XmlSerializer.serialize(bambooConfig);
         assertEquals(bambooConfig, XmlSerializer.deserialize(e, BambooConfigurationBean.class));
 
-		List<SubscribedPlanBean> plans2 = new ArrayList<SubscribedPlanBean>(plans);
-		ServerBean server2 = new ServerBean(server);
-		plans2.add(new SubscribedPlanBean("FOO-TEST2"));
-		server2.setSubscribedPlansData(plans2);
-		bambooConfig.storeServer(server2);
-		Collection<Server> servers = bambooConfig.getServers();
-		assertEquals(server2.getSubscribedPlans().size(), servers.iterator().next().getSubscribedPlans().size());
-		// now roll that up into the global configuration
+        List<SubscribedPlanBean> plans2 = new ArrayList<SubscribedPlanBean>(plans);
+        ServerBean server2 = new ServerBean(server);
+        plans2.add(new SubscribedPlanBean("FOO-TEST2"));
+        server2.setSubscribedPlansData(plans2);
+        bambooConfig.storeServer(server2);
+        Collection<Server> servers = bambooConfig.getServers();
+        assertEquals(server2.getSubscribedPlans().size(), servers.iterator().next().getSubscribedPlans().size());
+        // now roll that up into the global configuration
         PluginConfigurationBean config = new PluginConfigurationBean();
         config.setBambooConfigurationData(bambooConfig);
         e = XmlSerializer.serialize(config);
