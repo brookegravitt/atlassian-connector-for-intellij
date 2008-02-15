b6eac5bc8c75ef13309e352326e63d58ccc3a064
lguminski <lguminski@localhost>
2008-02-15 14:23:41 +0000

    fixed storing of subscribed plans

--- src/main/java/com/atlassian/theplugin/configuration/BambooConfigurationBean.java
+++ src/main/java/com/atlassian/theplugin/configuration/BambooConfigurationBean.java
@@ -1,5 +1,11 @@
 package com.atlassian.theplugin.configuration;
 
+import com.intellij.util.xmlb.annotations.Transient;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Bean storing information about Bamboo servers etc.<p>
  * The class serves both as a configuration provider for plugin logic and Bean for persistence.
@@ -8,6 +14,26 @@ public class BambooConfigurationBean extends AbstractConfigurationBean {
 	public BambooConfigurationBean() {
         super();
     }
+
+	@Override
+	@Transient
+	public void storeServer(Server server) {
+		ServerBean foundServer = (ServerBean) getServer(server);
+		if (foundServer == null) {
+			servers.add((ServerBean) server);
+		} else {
+			foundServer.setName(server.getName());
+			foundServer.setPasswordString(server.getPasswordString(), server.getShouldPasswordBeStored());
+			foundServer.setUrlString(server.getUrlString());
+			foundServer.setUserName(server.getUserName());
+			foundServer.setEnabled(server.getEnabled());
+			List<SubscribedPlanBean> subscribedPlans = new ArrayList<SubscribedPlanBean>();
+			for (SubscribedPlan subPlan : server.getSubscribedPlans()) {
+				subscribedPlans.add((SubscribedPlanBean) subPlan);
+			}
+			foundServer.setSubscribedPlansData(subscribedPlans);
+		}
+	}
 
 	public BambooConfigurationBean(ProductServerConfiguration cfg) {
 		super(cfg);
