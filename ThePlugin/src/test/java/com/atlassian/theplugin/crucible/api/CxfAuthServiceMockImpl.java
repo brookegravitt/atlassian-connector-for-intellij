74de0ee63f2a36c3880126e2d12996ed70a47842
mwent <mwent@localhost>
2008-02-15 10:40:19 +0000

    PL-9 unit tests for session

--- ThePlugin/src/test/java/com/atlassian/theplugin/crucible/api/CxfAuthServiceMockImpl.java
+++ ThePlugin/src/test/java/com/atlassian/theplugin/crucible/api/CxfAuthServiceMockImpl.java
@@ -2,12 +2,6 @@ package com.atlassian.theplugin.crucible.api;
 
 import com.atlassian.theplugin.crucible.api.soap.xfire.auth.RpcAuthServiceName;
 
-import javax.xml.ws.ResponseWrapper;
-import javax.xml.ws.RequestWrapper;
-import javax.jws.WebResult;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-
 /**
  * Created by IntelliJ IDEA.
  * User: Jacek
@@ -24,13 +18,7 @@ public class CxfAuthServiceMockImpl implements RpcAuthServiceName {
 	public static final String VALID_PASSWORD = "validPassword";
 	public static final String VALID_URL = "http://localhost:9000";
 
-	@ResponseWrapper(localName = "loginResponse", targetNamespace = "http://rpc.spi.crucible.atlassian.com/", className = "com.atlassian.theplugin.crucible.api.soap.xfire.auth.LoginResponse")
-	@RequestWrapper(localName = "login", targetNamespace = "http://rpc.spi.crucible.atlassian.com/", className = "com.atlassian.theplugin.crucible.api.soap.xfire.auth.Login")
-	@WebResult(name = "return", targetNamespace = "")
-	@WebMethod
-	public String login(@WebParam(name = "arg0", targetNamespace = "")
-	String userName, @WebParam(name = "arg1", targetNamespace = "")
-	String password) {
+	public String login(String userName, String password) {
 
 		if (userName.equals(INVALID_LOGIN) || password.equals(INVALID_PASSWORD)) {
 			return null;
@@ -41,11 +29,6 @@ public class CxfAuthServiceMockImpl implements RpcAuthServiceName {
 		}
 	}
 
-	@ResponseWrapper(localName = "logoutResponse", targetNamespace = "http://rpc.spi.crucible.atlassian.com/", className = "com.atlassian.theplugin.crucible.api.soap.xfire.auth.LogoutResponse")
-	@RequestWrapper(localName = "logout", targetNamespace = "http://rpc.spi.crucible.atlassian.com/", className = "com.atlassian.theplugin.crucible.api.soap.xfire.auth.Logout")
-	@WebMethod
-	public void logout(@WebParam(name = "arg0", targetNamespace = "")
-	String arg0) {
-		//To change body of implemented methods use File | Settings | File Templates.
+	public void logout(String arg0) {	
 	}
 }
