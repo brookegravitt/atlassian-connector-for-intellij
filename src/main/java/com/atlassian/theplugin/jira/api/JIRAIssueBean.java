8efd0b618214a46c573cbf0a4dba6450541bdb4a
mcannon <mcannon@localhost>
2008-02-15 13:59:26 +0000

    PL-51 - Adding initial JIRA API using XML-RPC and REST. Tests have about 60% coverage so far.

---
+++ src/main/java/com/atlassian/theplugin/jira/api/JIRAIssueBean.java
@@ -0,0 +1,89 @@
+package com.atlassian.theplugin.jira.api;
+
+import org.jdom.Element;
+
+public class JIRAIssueBean implements JIRAIssue {
+    String serverUrl;
+    String key;
+    String summary;
+    
+    public JIRAIssueBean()
+    {
+    }
+
+    public JIRAIssueBean(String serverUrl, Element e)
+    {
+        this.serverUrl = serverUrl;
+        this.summary = getTextSafely(e, "summary");
+        this.key = getTextSafely(e, "key");
+    }
+
+    private String getTextSafely(Element e, String name)
+    {
+        Element child = e.getChild(name);
+
+        if (child == null)
+            return null;
+
+        return child.getText();
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String getProjectUrl() {
+        return serverUrl + "/browse/" + getProjectKey();
+    }
+
+    public String getIssueUrl() {
+        return serverUrl + "/browse/" + getKey();
+    }
+
+    public String getProjectKey() {
+        return key.substring(0, key.indexOf("-"));
+    }
+    
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JIRAIssueBean that = (JIRAIssueBean) o;
+
+        if (key != null ? !key.equals(that.key) : that.key != null) return false;
+        if (serverUrl != null ? !serverUrl.equals(that.serverUrl) : that.serverUrl != null) return false;
+        if (summary != null ? !summary.equals(that.summary) : that.summary != null) return false;
+
+        return true;
+    }
+
+    public int hashCode()
+    {
+        int result;
+        result = (serverUrl != null ? serverUrl.hashCode() : 0);
+        result = 31 * result + (key != null ? key.hashCode() : 0);
+        result = 31 * result + (summary != null ? summary.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/atlassian/theplugin/jira/api/JIRARssClient.java
@@ -0,0 +1,109 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: amrk
+ * Date: 13/03/2004
+ * Time: 23:19:19
+ */
+package com.atlassian.theplugin.jira.api;
+
+import com.intellij.openapi.diagnostic.Logger;
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.*;
+
+public class JIRARssClient
+{
+    private static final Logger LOGGER = Logger.getInstance(JIRARssClient.class.getName());
+
+    private String serverUrl;
+    private String userName;
+    private String password;
+
+    public JIRARssClient(String url)
+    {
+        this.serverUrl = url;
+    }
+
+    public JIRARssClient(String url, String userName, String password)
+    {
+        this.serverUrl = url;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public List getAssignedIssues(String assignee) throws JIRAException
+    {
+        String url = serverUrl + "/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?resolution=-1&assignee=" + URLEncoder.encode(assignee) + "&sorter/field=updated&sorter/order=DESC&tempMax=100" + appendAuthentication();
+//        System.out.println("url = " + url);
+        try
+        {
+            Document doc = buildFeed(url);
+            Element root = doc.getRootElement();
+            Element channel = root.getChild("channel");
+            if (channel != null && !channel.getChildren("item").isEmpty())
+            {
+                return makeIssues(channel.getChildren("item"));
+            }
+
+            return Collections.EMPTY_LIST;
+        }
+        catch (Exception e)
+        {
+            throw new JIRAException(e.getMessage(), e);
+        }
+    }
+
+    private List makeIssues(List issueElements)
+    {
+        List<JIRAIssue> result = new ArrayList<JIRAIssue>(issueElements.size());
+        for (Iterator iterator = issueElements.iterator(); iterator.hasNext();)
+        {
+            result.add(new JIRAIssueBean(serverUrl, (Element) iterator.next()));
+        }
+        return result;
+    }
+
+    private String appendAuthentication()
+    {
+        if (userName != null)
+
+            return "&os_username=" + URLEncoder.encode(userName) + "&os_password=" + URLEncoder.encode(password);
+        return "";
+    }
+
+    protected Document buildFeed(String url) throws IOException, JDOMException {
+
+        // LOGGER.info("Refeshing issues from: " + url); // be careful about logging as we might leak the user's password - nasty!
+        String feedContent = getUrlContent(url);
+        SAXBuilder builder = new SAXBuilder();
+        builder.setValidation(false);
+        return builder.build(new StringReader(feedContent));
+    }
+
+    protected String getUrlContent(String url) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(getUrlAsStream(url)));
+
+        String inputLine;
+        StringBuffer buffer = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            buffer.append(inputLine);
+            buffer.append('\n');
+        }
+
+        in.close();
+
+        return buffer.toString();
+    }
+
+    // protected so that we can easily write tests by simply returning XML from a file instead of a URL!
+    protected InputStream getUrlAsStream(String url) throws IOException {
+        return new URL(url).openConnection().getInputStream();
+    }
+}
