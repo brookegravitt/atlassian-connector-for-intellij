2e58c2dfdf63f8c3fe9475c58c36f2d2dd1ceda1
mcannon <mcannon@localhost>
2008-02-15 10:20:25 +0000

    Fix more checkstyle failures.

--- ThePlugin/src/main/java/com/atlassian/theplugin/configuration/BambooConfigurationBean.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/configuration/BambooConfigurationBean.java
@@ -4,8 +4,7 @@ package com.atlassian.theplugin.configuration;
  * Bean storing information about Bamboo servers etc.<p>
  * The class serves both as a configuration provider for plugin logic and Bean for persistence.
  */
-public class BambooConfigurationBean extends AbstractConfigurationBean
-{
+public class BambooConfigurationBean extends AbstractConfigurationBean {
 	public BambooConfigurationBean() {
         super();
     }
