441885b31db654a70dfb4627c6e01ca139669da5
mcannon <mcannon@localhost>
2008-02-15 14:58:57 +0000

    PL-51 - Action to add a JIRA server and ConnectionTester interface to abstract connection testing

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/config/serverconfig/ConnectionTester.java
@@ -0,0 +1,15 @@
+package com.atlassian.theplugin.idea.config.serverconfig;
+
+import com.atlassian.theplugin.exception.ThePluginException;
+
+public interface ConnectionTester
+{
+    /**
+     * Test a connection. Throws exception on failure, nothing on success.
+     * @param username
+     * @param password
+     * @param server
+     * @throws ThePluginException If the connection failed.
+     */
+    public void testConnection(String username, String password, String server) throws ThePluginException;
+}
