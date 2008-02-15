932860fd463713f4d02426bc5efadffcc254e06f
mcannon <mcannon@localhost>
2008-02-15 15:42:22 +0000

    Fixing checkstyle errors

--- src/main/java/com/atlassian/theplugin/jira/JIRAServerFactory.java
+++ src/main/java/com/atlassian/theplugin/jira/JIRAServerFactory.java
@@ -3,21 +3,13 @@ package com.atlassian.theplugin.jira;
 import com.atlassian.theplugin.bamboo.BambooServerFacade;
 import com.atlassian.theplugin.bamboo.BambooServerFacadeImpl;
 
-/**
- * Created by IntelliJ IDEA.
- * User: sginter
- * Date: Jan 15, 2008
- * Time: 5:17:44 PM
- * To change this template use File | Settings | File Templates.
- */
-public final class JIRAServerFactory
-{
-	private static JIRAServerFacade facade = new JIRAServerFacadeImpl();
+public final class JIRAServerFactory {
+    private static JIRAServerFacade facade = new JIRAServerFacadeImpl();
 
-	private JIRAServerFactory() {
-	}
+    private JIRAServerFactory() {
+    }
 
-	public static JIRAServerFacade getJIRAServerFacade() {
-		return facade;
-	}
+    public static JIRAServerFacade getJIRAServerFacade() {
+        return facade;
+    }
 }
