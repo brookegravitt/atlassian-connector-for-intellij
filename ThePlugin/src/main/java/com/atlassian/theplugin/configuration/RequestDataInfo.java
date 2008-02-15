9cf618cce1bcf4e490ba4f479997a19b41984f41
mcannon <mcannon@localhost>
2008-02-15 08:24:13 +0000

    Added extra tests for Configuration. Small code changes to remove unused methods and cruft comments.

--- ThePlugin/src/main/java/com/atlassian/theplugin/configuration/RequestDataInfo.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/configuration/RequestDataInfo.java
@@ -2,13 +2,6 @@ package com.atlassian.theplugin.configuration;
 
 import java.util.Date;
 
-/**
- * Created by IntelliJ IDEA.
- * User: pmaruszak
- * Date: Jan 24, 2008
- * Time: 1:20:51 PM
- * To change this template use File | Settings | File Templates.
- */
 public class RequestDataInfo implements RequestData {
 	private Date pollingTime = new Date(0);
 
