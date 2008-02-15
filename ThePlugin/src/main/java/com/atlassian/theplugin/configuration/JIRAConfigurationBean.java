d155c5a057b1085800112f576fd2270d3f6fb5d0
mcannon <mcannon@localhost>
2008-02-15 15:11:30 +0000

    Fixing checkstyle errors

--- ThePlugin/src/main/java/com/atlassian/theplugin/configuration/JIRAConfigurationBean.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/configuration/JIRAConfigurationBean.java
@@ -4,13 +4,12 @@ package com.atlassian.theplugin.configuration;
  * Bean storing information about Bamboo servers etc.<p>
  * The class serves both as a configuration provider for plugin logic and Bean for persistence.
  */
-public class JIRAConfigurationBean extends AbstractConfigurationBean
-{
-	public JIRAConfigurationBean() {
+public class JIRAConfigurationBean extends AbstractConfigurationBean {
+    public JIRAConfigurationBean() {
         super();
     }
 
-	public JIRAConfigurationBean(ProductServerConfiguration cfg) {
-		super(cfg);
-	}
+    public JIRAConfigurationBean(ProductServerConfiguration cfg) {
+        super(cfg);
+    }
 }
