52067c29662159b663da97bb5400bee805f50ae8
mwent <mwent@localhost>
2008-02-18 09:11:19 +0000

    PL-9 unit tests for session

---
+++ src/main/java/com/atlassian/theplugin/crucible/api/PermId.java
@@ -0,0 +1,12 @@
+package com.atlassian.theplugin.crucible.api;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mwent
+ * Date: 2008-02-15
+ * Time: 13:43:40
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PermId {
+	String getId();
+}
