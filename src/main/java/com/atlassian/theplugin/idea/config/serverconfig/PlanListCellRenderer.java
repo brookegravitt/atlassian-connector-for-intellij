bdf186d8dd5740bd0364551b6b90c0cbace99ee9
mwent <mwent@localhost>
2008-03-05 10:43:27 +0000

    PL-145 rebuild plan form

--- src/main/java/com/atlassian/theplugin/idea/config/serverconfig/PlanListCellRenderer.java
+++ src/main/java/com/atlassian/theplugin/idea/config/serverconfig/PlanListCellRenderer.java
@@ -16,34 +16,37 @@ public class PlanListCellRenderer implements ListCellRenderer {
 	private static final Icon FAVOURITE_ON_ICON = IconLoader.getIcon("/icons/fav_on.gif");
 	private static final Icon FAVOURITE_OFF_ICON = IconLoader.getIcon("/icons/fav_off.gif");
 	private static final Icon DISABLED_ICON = IconLoader.getIcon("/icons/icn_plan_disabled-16.gif");
-		
+
 	public Component getListCellRendererComponent(JList list, Object value, int index,
 												  boolean isSelected, boolean cellHasFocus) {
-		BambooPlanItem pi = (BambooPlanItem) value;
-
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-		label = new JLabel();
-		checkBox = new JCheckBox(pi.getPlan().getPlanKey());
-		label.setIcon(pi.getPlan().isEnabled()
-				? (pi.getPlan().isFavourite() ? FAVOURITE_ON_ICON : FAVOURITE_OFF_ICON) : DISABLED_ICON);
-		checkBox.setText(pi.getPlan().getPlanKey());
-		checkBox.setSelected(pi.isSelected());
+		JLabel label = new JLabel();
 		panel.add(label);
-		panel.add(checkBox);
 
+		if (value instanceof BambooPlanItem) {
+			BambooPlanItem pi = (BambooPlanItem) value;
+			JCheckBox checkBox = new JCheckBox(pi.getPlan().getPlanKey());
+			label.setIcon(pi.getPlan().isEnabled() ? (pi.getPlan().isFavourite() ? FAVOURITE_ON_ICON : FAVOURITE_OFF_ICON) : DISABLED_ICON);
+			checkBox.setText(pi.getPlan().getPlanKey());
+			checkBox.setSelected(pi.isSelected());
+			panel.add(checkBox);
+
+			checkBox.setBackground(isSelected ? list.getSelectionBackground() : list.getBackground());
+			checkBox.setForeground(isSelected ? list.getSelectionForeground() : list.getForeground());
+
+			checkBox.setEnabled(list.isEnabled());
+			checkBox.setFont(list.getFont());
+			checkBox.setFocusPainted(false);
+
+			checkBox.setBorderPainted(true);
+			checkBox.setBorder(isSelected ? UIManager.getBorder("List.focusCellHighlightBorder") : NO_FOCUS_BORDER);
+		} else {
+			label.setText(value.toString());
+		}
 		panel.setBackground(isSelected ? list.getSelectionBackground() : list.getBackground());
 		panel.setForeground(isSelected ? list.getSelectionForeground() : list.getForeground());
-		checkBox.setBackground(isSelected ? list.getSelectionBackground() : list.getBackground());
-		checkBox.setForeground(isSelected ? list.getSelectionForeground() : list.getForeground());
-
 		panel.setEnabled(list.isEnabled());
-		checkBox.setEnabled(list.isEnabled());
-		checkBox.setFont(list.getFont());
-		checkBox.setFocusPainted(false);
-
-		checkBox.setBorderPainted(true);
-		checkBox.setBorder(isSelected ? UIManager.getBorder("List.focusCellHighlightBorder") : NO_FOCUS_BORDER);
 
 		return panel;
 	}
