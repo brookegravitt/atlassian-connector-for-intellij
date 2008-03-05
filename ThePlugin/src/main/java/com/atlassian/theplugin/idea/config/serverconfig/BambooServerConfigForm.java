ddd7c18bdd69f9d2fbd567cf45a069218bfd16d5
mwent <mwent@localhost>
2008-03-05 14:30:41 +0000

    PL-145 windows redesign

--- ThePlugin/src/main/java/com/atlassian/theplugin/idea/config/serverconfig/BambooServerConfigForm.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/config/serverconfig/BambooServerConfigForm.java
@@ -2,6 +2,7 @@ package com.atlassian.theplugin.idea.config.serverconfig;
 
 import com.atlassian.theplugin.bamboo.BambooServerFacade;
 import com.atlassian.theplugin.bamboo.api.BambooLoginException;
+import com.atlassian.theplugin.configuration.Server;
 import com.atlassian.theplugin.configuration.ServerBean;
 import com.atlassian.theplugin.exception.ThePluginException;
 import com.intellij.uiDesigner.core.GridConstraints;
@@ -31,7 +32,7 @@ public class BambooServerConfigForm extends JComponent implements ServerPanel {
 		$$$setupUI$$$();
 	}
 
-	public void setData(ServerBean aServer) {
+	public void setData(Server aServer) {
 		genericServerConfigForm.setData(aServer);
 		planList.setEnabled(!aServer.getUseFavourite());
 		planList.setData(aServer);
@@ -71,7 +72,7 @@ public class BambooServerConfigForm extends JComponent implements ServerPanel {
 				}
 			}
 		});
-		planList = new BambooPlansForm(bambooServerFacade);
+		planList = new BambooPlansForm(bambooServerFacade, genericServerConfigForm);
 	}
 
 	/**
@@ -84,14 +85,12 @@ public class BambooServerConfigForm extends JComponent implements ServerPanel {
 	private void $$$setupUI$$$() {
 		createUIComponents();
 		rootComponent = new JPanel();
-		rootComponent.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+		rootComponent.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
 		final JPanel panel1 = new JPanel();
 		panel1.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
 		rootComponent.add(panel1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-		panel1.add(genericServerConfigForm.$$$getRootComponent$$$(), new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_NORTH, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+		panel1.add(genericServerConfigForm.$$$getRootComponent$$$(), new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_NORTH, GridConstraints.FILL_HORIZONTAL, 1, 1, null, null, null, 0, false));
 		panel1.add(planList, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-		final Spacer spacer1 = new Spacer();
-		rootComponent.add(spacer1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
 	}
 
 	/**
