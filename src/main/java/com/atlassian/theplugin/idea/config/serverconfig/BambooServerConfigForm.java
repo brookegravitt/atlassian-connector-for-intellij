fd53cb9aa8745aebe477cbc065a8d5699783432e
mwent <mwent@localhost>
2008-03-05 12:50:49 +0000

    PL-145 windows redesign

--- src/main/java/com/atlassian/theplugin/idea/config/serverconfig/BambooServerConfigForm.java
+++ src/main/java/com/atlassian/theplugin/idea/config/serverconfig/BambooServerConfigForm.java
@@ -1,23 +1,16 @@
 package com.atlassian.theplugin.idea.config.serverconfig;
 
 import com.atlassian.theplugin.bamboo.BambooServerFacade;
+import com.atlassian.theplugin.bamboo.api.BambooLoginException;
 import com.atlassian.theplugin.configuration.ServerBean;
-import com.atlassian.theplugin.idea.IdeaHelper;
-import com.atlassian.theplugin.idea.config.BambooTestConnection;
-import com.atlassian.theplugin.util.Util;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
-import com.intellij.openapi.project.Project;
+import com.atlassian.theplugin.exception.ThePluginException;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import org.apache.log4j.Category;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import static java.lang.Thread.sleep;
 
 /**
  * Plugin configuration form.
@@ -27,51 +20,26 @@ public class BambooServerConfigForm extends JComponent implements ServerPanel {
 	private static final Category LOG = Category.getInstance(BambooServerConfigForm.class);
 
 	private JPanel rootComponent;
-	private JTextField serverName;
-	private JTextField serverUrl;
-	private JTextField username;
-	private JPasswordField password;
-	private JButton testConnection;
-	private JCheckBox chkPasswordRemember;
-	private JCheckBox cbEnabled;
-	private JPanel buildsPanel;
 
-	private transient ServerBean server;
 	private BambooPlansForm planList;
+	private GenericServerConfigForm genericServerConfigForm;
 	private final BambooServerFacade bambooServerFacade;
 
 	public BambooServerConfigForm(BambooServerFacade bambooServerFacadeInstance) {
 		this.bambooServerFacade = bambooServerFacadeInstance;
 
 		$$$setupUI$$$();
-		testConnection.addActionListener(new TestConnectionListener());
-
-	}
-
-	private void getPlansFromServer(ServerBean aServer) {
-		planList.setData(aServer);
 	}
 
 	public void setData(ServerBean aServer) {
-		this.server = new ServerBean(aServer);
-
-		serverName.setText(aServer.getName());
-		serverUrl.setText(aServer.getUrlString());
-		username.setText(aServer.getUserName());
-		chkPasswordRemember.setSelected(aServer.getShouldPasswordBeStored());
-		password.setText(aServer.getPasswordString());
-		cbEnabled.setSelected(aServer.getEnabled());
+		genericServerConfigForm.setData(aServer);
 		planList.setEnabled(!aServer.getUseFavourite());
-		getPlansFromServer(server);
+		planList.setData(aServer);
+
 	}
 
 	public ServerBean getData() {
-		serverUrl.setText(Util.addHttpPrefix(serverUrl.getText()));
-		server.setName(serverName.getText());
-		server.setUrlString(serverUrl.getText());
-		server.setUserName(username.getText());
-		server.setPasswordString(String.valueOf(password.getPassword()), chkPasswordRemember.isSelected());
-		server.setEnabled(cbEnabled.isSelected());
+		ServerBean server = genericServerConfigForm.getData();
 
 		ServerBean s = planList.getData();
 		server.setSubscribedPlansData(s.getSubscribedPlansData());
@@ -81,36 +49,7 @@ public class BambooServerConfigForm extends JComponent implements ServerPanel {
 	}
 
 	public boolean isModified() {
-		boolean isModified = false;
-
-		if (server != null) {
-			if (chkPasswordRemember.isSelected() != server.getShouldPasswordBeStored()) {
-				return true;
-			}
-			if (cbEnabled.isSelected() != server.getEnabled()) {
-				return true;
-			}
-			if (serverName.getText() != null
-					? !serverName.getText().equals(server.getName()) : server.getName() != null) {
-				return true;
-			}
-			if (serverUrl.getText() != null
-					? !serverUrl.getText().equals(server.getUrlString()) : server.getUrlString() != null) {
-				return true;
-			}
-			if (username.getText() != null
-					? !username.getText().equals(server.getUserName()) : server.getUserName() != null) {
-				return true;
-			}
-			String pass = String.valueOf(password.getPassword());
-			if (!pass.equals(server.getPasswordString())) {
-				return true;
-			}
-			if (planList.isModified()) {
-				return true;
-			}
-		}
-		return isModified;
+		return genericServerConfigForm.isModified() || planList.isModified();
 	}
 
 	public JComponent getRootComponent() {
@@ -122,6 +61,16 @@ public class BambooServerConfigForm extends JComponent implements ServerPanel {
 	}
 
 	private void createUIComponents() {
+		genericServerConfigForm = new GenericServerConfigForm(new ConnectionTester() {
+			public void testConnection(String username, String password, String server)
+					throws ThePluginException {
+				try {
+					bambooServerFacade.testServerConnection(server, username, password);
+				} catch (BambooLoginException e) {
+					throw new ThePluginException(e.getMessage());
+				}
+			}
+		});
 		planList = new BambooPlansForm(bambooServerFacade);
 	}
 
@@ -137,67 +86,12 @@ public class BambooServerConfigForm extends JComponent implements ServerPanel {
 		rootComponent = new JPanel();
 		rootComponent.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
 		final JPanel panel1 = new JPanel();
-		panel1.setLayout(new GridLayoutManager(7, 2, new Insets(0, 0, 0, 0), -1, -1));
-		rootComponent.add(panel1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-		serverName = new JTextField();
-		serverName.setText("");
-		panel1.add(serverName, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-		final JLabel label1 = new JLabel();
-		label1.setText("Server Name");
-		label1.setDisplayedMnemonic('S');
-		label1.setDisplayedMnemonicIndex(0);
-		panel1.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_EAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		serverUrl = new JTextField();
-		panel1.add(serverUrl, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-		username = new JTextField();
-		panel1.add(username, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-		password = new JPasswordField();
-		password.setText("");
-		panel1.add(password, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-		final JLabel label2 = new JLabel();
-		label2.setText("Server URL");
-		label2.setDisplayedMnemonic('U');
-		label2.setDisplayedMnemonicIndex(7);
-		panel1.add(label2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_EAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		final JLabel label3 = new JLabel();
-		label3.setText("User Name");
-		label3.setDisplayedMnemonic('N');
-		label3.setDisplayedMnemonicIndex(5);
-		panel1.add(label3, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_EAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		final JLabel label4 = new JLabel();
-		label4.setText("Password");
-		label4.setDisplayedMnemonic('P');
-		label4.setDisplayedMnemonicIndex(0);
-		panel1.add(label4, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_EAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		testConnection = new JButton();
-		testConnection.setText("Test Connection");
-		testConnection.setMnemonic('T');
-		testConnection.setDisplayedMnemonicIndex(0);
-		panel1.add(testConnection, new GridConstraints(5, 1, 1, 1, GridConstraints.ANCHOR_NORTHEAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		buildsPanel = new JPanel();
-		buildsPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-		panel1.add(buildsPanel, new GridConstraints(6, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-		buildsPanel.add(planList, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-		chkPasswordRemember = new JCheckBox();
-		chkPasswordRemember.setSelected(true);
-		chkPasswordRemember.setText("Remember password");
-		chkPasswordRemember.setMnemonic('R');
-		chkPasswordRemember.setDisplayedMnemonicIndex(0);
-		panel1.add(chkPasswordRemember, new GridConstraints(4, 1, 1, 1, GridConstraints.ANCHOR_EAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		final JPanel panel2 = new JPanel();
-		panel2.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-		rootComponent.add(panel2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		cbEnabled = new JCheckBox();
-		cbEnabled.setHorizontalAlignment(4);
-		cbEnabled.setHorizontalTextPosition(10);
-		cbEnabled.setText("Server Enabled");
-		cbEnabled.setMnemonic('E');
-		cbEnabled.setDisplayedMnemonicIndex(7);
-		panel2.add(cbEnabled, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		label1.setLabelFor(serverName);
-		label2.setLabelFor(serverUrl);
-		label3.setLabelFor(username);
-		label4.setLabelFor(password);
+		panel1.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+		rootComponent.add(panel1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+		panel1.add(genericServerConfigForm.$$$getRootComponent$$$(), new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_NORTH, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+		panel1.add(planList, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+		final Spacer spacer1 = new Spacer();
+		rootComponent.add(spacer1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
 	}
 
 	/**
@@ -205,56 +99,5 @@ public class BambooServerConfigForm extends JComponent implements ServerPanel {
 	 */
 	public JComponent $$$getRootComponent$$$() {
 		return rootComponent;
-	}
-
-	private class TestConnectionListener implements ActionListener {
-
-		public void actionPerformed(ActionEvent e) {
-
-			Task.Modal testConnectionTask =	new TestConnectionTask(IdeaHelper.getCurrentProject(), "Testing Connection", true);
-			testConnectionTask.setCancelText("Stop");
-			ProgressManager.getInstance().run(testConnectionTask);
-		}
-
-		private class TestConnectionTask extends Task.Modal {
-
-			private BambooTestConnection bambooTestConnection = null;
-			private static final int CHECK_CANCEL_INTERVAL = 500;	// miliseconds
-
-			public TestConnectionTask(Project currentProject, String title, boolean canBeCanceled) {
-
-				super(currentProject, title, canBeCanceled);
-
-				bambooTestConnection = new BambooTestConnection(
-						serverUrl.getText(), username.getText(), String.valueOf(password.getPassword()));
-			}
-
-			public void run(ProgressIndicator indicator) {
-
-				indicator.setText("Connecting...");
-				indicator.setFraction(0);
-				indicator.setIndeterminate(true);
-
-				bambooTestConnection.start();
-
-				//System.out.println("X: Connecting...");
-
-				while (bambooTestConnection.isRunning()) {
-					try {
-						if (indicator.isCanceled()) {
-							bambooTestConnection.setInterrupted();
-							//t.interrupt();
-							//System.out.println("X: Interupting...");
-							break;
-						} else {
-							sleep(CHECK_CANCEL_INTERVAL);
-						}
-					} catch (InterruptedException e) {
-						LOG.info(e.getMessage());
-						//System.out.println("X: Interupting2...");
-					}
-				}
-			}
-		}
 	}
 }
