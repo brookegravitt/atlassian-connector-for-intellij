bd7ecb9b83efaf024336deb7fc9f45f7d2931a67
jjaroczynski <jjaroczynski@localhost>
2008-03-06 15:51:04 +0000

    PL-113 added progress/cancel dialog to the 'Test Connection' in missing password dialog (only for Bamboo because there is no missing password dialog for crucible and JIRA)

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/LoginDataProvided.java
@@ -0,0 +1,7 @@
+package com.atlassian.theplugin;
+
+public interface LoginDataProvided {
+	String getServerUrl();
+	String getUserName();
+	String getPassword();
+}
--- ThePlugin/src/main/java/com/atlassian/theplugin/idea/PasswordDialog.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/PasswordDialog.java
@@ -1,25 +1,26 @@
 package com.atlassian.theplugin.idea;
 
+import com.atlassian.theplugin.LoginDataProvided;
 import com.atlassian.theplugin.bamboo.BambooServerFactory;
 import com.atlassian.theplugin.bamboo.api.BambooLoginException;
 import com.atlassian.theplugin.configuration.Server;
-import com.intellij.openapi.ui.Messages;
-import static com.intellij.openapi.ui.Messages.showMessageDialog;
-import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.atlassian.theplugin.exception.ThePluginException;
+import com.atlassian.theplugin.idea.config.serverconfig.ConnectionTester;
 import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
 
 import javax.swing.*;
-import java.awt.event.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class PasswordDialog extends JDialog {
+public class PasswordDialog extends JDialog implements LoginDataProvided {
 
 	private JPanel passwordPanel;
 	private JCheckBox chkRememberPassword;
-	private JPasswordField password;
+	private JPasswordField passwordField;
 	private JButton testConnectionButton;
 	private JLabel lblCommand;
-	private JTextField username;
+	private JTextField userName;
 	private transient Server server;
 
 	public PasswordDialog(final Server server) {
@@ -40,20 +41,17 @@ public class PasswordDialog extends JDialog {
 				onCancel();
 			}
 		}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-		testConnectionButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
+
+		testConnectionButton.addActionListener(new TestConnectionListener(new ConnectionTester() {
+			public void testConnection(String aUsername, String aPassword, String aServer) throws ThePluginException {
 				try {
 					BambooServerFactory.getBambooServerFacade().testServerConnection(
-							server.getUrlString(),
-							server.getUserName(),
-							getPasswordString());
-					showMessageDialog("Connected successfully", "Connection OK", Messages.getInformationIcon());
-				} catch (BambooLoginException e1) {
-					showMessageDialog(e1.getMessage(), "Connection Error" + server.getUrlString()
-							+ "-" + getPasswordString(), Messages.getErrorIcon());
+							aServer, aUsername, aPassword);
+				} catch (BambooLoginException e) {
+					throw new ThePluginException(e.getMessage());
 				}
 			}
-		});
+		}, this));
 	}
 
 	private void onCancel() {
@@ -61,12 +59,13 @@ public class PasswordDialog extends JDialog {
 		dispose();
 	}
 
+
 	public JPanel getPasswordPanel() {
 		return passwordPanel;
 	}
 
 	public String getPasswordString() {
-		return String.valueOf(password.getPassword());
+		return String.valueOf(passwordField.getPassword());
 	}
 
 	public Boolean getShouldPasswordBeStored() {
@@ -74,15 +73,23 @@ public class PasswordDialog extends JDialog {
 	}
 
 	public void setUserName(String anUsername) {
-		this.username.setText(anUsername);
+		this.userName.setText(anUsername);
 	}
 
 	public String getUserName() {
-		return this.username.getText();
+		return this.userName.getText();
 	}
 
 	private void createUIComponents() {
 		// TODO: place custom component creation code here
+	}
+
+	public String getServerUrl() {
+		return server.getUrlString();
+	}
+
+	public String getPassword() {
+		return getPasswordString();
 	}
 
 	{
@@ -105,8 +112,8 @@ public class PasswordDialog extends JDialog {
 		final JPanel panel1 = new JPanel();
 		panel1.setLayout(new GridLayoutManager(5, 2, new Insets(0, 0, 0, 0), -1, -1));
 		passwordPanel.add(panel1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-		password = new JPasswordField();
-		panel1.add(password, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+		passwordField = new JPasswordField();
+		panel1.add(passwordField, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
 		testConnectionButton = new JButton();
 		testConnectionButton.setText("Test connection");
 		testConnectionButton.setMnemonic('T');
@@ -126,10 +133,10 @@ public class PasswordDialog extends JDialog {
 		final JLabel label2 = new JLabel();
 		label2.setText("User Name");
 		panel1.add(label2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-		username = new JTextField();
-		panel1.add(username, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-		label1.setLabelFor(password);
-		label2.setLabelFor(username);
+		userName = new JTextField();
+		panel1.add(userName, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+		label1.setLabelFor(passwordField);
+		label2.setLabelFor(userName);
 	}
 
 	/**
