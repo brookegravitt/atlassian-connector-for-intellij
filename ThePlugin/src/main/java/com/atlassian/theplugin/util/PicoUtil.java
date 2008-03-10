18b64aca57b9141d43497599369ce99d7ed9c803
jjaroczynski <jjaroczynski@localhost>
2008-03-10 11:59:05 +0000

    PL-134 Crucible and Jira poll time added in configuration dialog

--- ThePlugin/src/main/java/com/atlassian/theplugin/util/PicoUtil.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/util/PicoUtil.java
@@ -1,17 +1,19 @@
 package com.atlassian.theplugin.util;
 
-import com.atlassian.theplugin.bamboo.BambooStatusChecker;
 import com.atlassian.theplugin.bamboo.BambooServerFacadeImpl;
+import com.atlassian.theplugin.bamboo.BambooStatusChecker;
 import com.atlassian.theplugin.configuration.PluginConfigurationBean;
+import com.atlassian.theplugin.crucible.CrucibleServerFacadeImpl;
 import com.atlassian.theplugin.idea.IdeaActionScheduler;
 import com.atlassian.theplugin.idea.NewVersionChecker;
 import com.atlassian.theplugin.idea.config.ConfigPanel;
 import com.atlassian.theplugin.idea.config.GeneralConfigPanel;
 import com.atlassian.theplugin.idea.config.serverconfig.BambooGeneralForm;
+import com.atlassian.theplugin.idea.config.serverconfig.JiraGeneralForm;
 import com.atlassian.theplugin.idea.config.serverconfig.ServerConfigPanel;
 import com.atlassian.theplugin.idea.config.serverconfig.ServerTreePanel;
+import com.atlassian.theplugin.idea.config.serverconfig.util.CrucibleGeneralForm;
 import com.atlassian.theplugin.idea.crucible.CrucibleStatusChecker;
-import com.atlassian.theplugin.crucible.CrucibleServerFacadeImpl;
 import org.picocontainer.MutablePicoContainer;
 
 public final class PicoUtil {
@@ -29,6 +31,8 @@ public final class PicoUtil {
 			ConfigPanel.class,
 			ServerConfigPanel.class,
 			BambooGeneralForm.class,
+			CrucibleGeneralForm.class,
+			JiraGeneralForm.class,
 			GeneralConfigPanel.class,
 			ServerTreePanel.class,
 			CrucibleServerFacadeImpl.class,
