ecd1b5d49b4172bc99c265f05387f1f54d89bf8f
mwent <mwent@localhost>
2008-03-17 15:50:26 +0000

    PL-173 all actions could take null as constant - check for null before check is icon is null

--- src/main/java/com/atlassian/theplugin/idea/action/jira/AbstractConstantComboAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/jira/AbstractConstantComboAction.java
@@ -15,44 +15,46 @@ import java.util.Iterator;
 import java.util.List;
 
 public abstract class AbstractConstantComboAction extends ComboBoxAction {
-    @NotNull
-    protected DefaultActionGroup createPopupActionGroup(JComponent jComponent) {
-        final ComboBoxButton comboBox;
-        if (!(jComponent instanceof ComboBoxButton)) {
-            throw new UnsupportedOperationException("This action can only be used as a combobox");
-        }
+	@NotNull
+	protected DefaultActionGroup createPopupActionGroup(JComponent jComponent) {
+		final ComboBoxButton comboBox;
+		if (!(jComponent instanceof ComboBoxButton)) {
+			throw new UnsupportedOperationException("This action can only be used as a combobox");
+		}
 
-        comboBox = (ComboBoxButton) jComponent;
-        DefaultActionGroup group = new DefaultActionGroup();
+		comboBox = (ComboBoxButton) jComponent;
+		DefaultActionGroup group = new DefaultActionGroup();
 
-        group.add(makeAction(comboBox, null));
-        ThePluginApplicationComponent app = IdeaHelper.getAppComponent();
-        JIRAServer server = app.getCurrentJIRAServer();
-        for (Iterator iterator = getValues(server).iterator(); iterator.hasNext();) {
-            JIRAConstant constant = (JIRAConstant) iterator.next();
-            group.add(makeAction(comboBox, constant));
-        }
-        return group;
-    }
+		group.add(makeAction(comboBox, null));
+		ThePluginApplicationComponent app = IdeaHelper.getAppComponent();
+		JIRAServer server = app.getCurrentJIRAServer();
+		for (Iterator iterator = getValues(server).iterator(); iterator.hasNext();) {
+			JIRAConstant constant = (JIRAConstant) iterator.next();
+			group.add(makeAction(comboBox, constant));
+		}
+		return group;
+	}
 
-    protected abstract List getValues(JIRAServer server);
+	protected abstract List getValues(JIRAServer server);
 
-    protected abstract String getDefaultText();
+	protected abstract String getDefaultText();
 
-    private AnAction makeAction(final ComboBoxButton comboBox, final JIRAConstant constant) {
-        return new AnAction(
-                (constant != null ? constant.getName() : getDefaultText()),
-                null,
-                (constant != null && constant.getIconUrl() != null ? new ImageIcon(constant.getIconUrl()) : null)
-        ) {
-            public void actionPerformed(AnActionEvent event) {
-                comboBox.setText(event.getPresentation().getText());
-                if (constant.getIconUrl() != null) {
-                    comboBox.setIcon(new ImageIcon(constant.getIconUrl()));
-                }
-                IdeaHelper.getJIRAToolWindowPanel(event).addQueryFragment(getDefaultText(), constant);
-                IdeaHelper.getJIRAToolWindowPanel(event).refreshIssues();
-            }
-        };
-    }
+	private AnAction makeAction(final ComboBoxButton comboBox, final JIRAConstant constant) {
+		return new AnAction(
+				(constant != null ? constant.getName() : getDefaultText()),
+				null,
+				(constant != null && constant.getIconUrl() != null ? new ImageIcon(constant.getIconUrl()) : null)
+		) {
+			public void actionPerformed(AnActionEvent event) {
+				comboBox.setText(event.getPresentation().getText());
+				if (constant != null) {
+					if (constant.getIconUrl() != null) {
+						comboBox.setIcon(new ImageIcon(constant.getIconUrl()));
+					}
+				}
+				IdeaHelper.getJIRAToolWindowPanel(event).addQueryFragment(getDefaultText(), constant);
+				IdeaHelper.getJIRAToolWindowPanel(event).refreshIssues();
+			}
+		};
+	}
 }
