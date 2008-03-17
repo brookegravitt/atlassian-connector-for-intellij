ec03fe9de4e950d89c359f78d6650ce98c7aebac
lguminski <lguminski@localhost>
2008-03-17 14:23:21 +0000

    PL-149 check now button added

---
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/autoupdate/ForwardToIconHandler.java
@@ -0,0 +1,32 @@
+package com.atlassian.theplugin.idea.autoupdate;
+
+import com.atlassian.theplugin.idea.ConfirmPluginUpdateHandler;
+import com.atlassian.theplugin.util.InfoServer;
+import com.atlassian.theplugin.configuration.PluginConfiguration;
+import com.atlassian.theplugin.exception.IncorrectVersionException;
+import com.atlassian.theplugin.exception.VersionServiceException;
+import com.atlassian.theplugin.exception.ThePluginException;
+import com.intellij.openapi.application.ApplicationManager;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lguminski
+ * Date: Mar 13, 2008
+ * Time: 12:17:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ForwardToIconHandler implements UpdateActionHandler {
+	private PluginConfiguration pluginConfiguration;
+
+	public ForwardToIconHandler(PluginConfiguration pluginConfiguration) {
+		this.pluginConfiguration = pluginConfiguration;
+	}
+
+	public void doAction(InfoServer.VersionInfo versionInfo) throws ThePluginException {
+		if (!versionInfo.getVersion().equals(pluginConfiguration.getRejectedUpgrade())) {
+			ConfirmPluginUpdateHandler handler = ConfirmPluginUpdateHandler.getInstance();
+			handler.setNewVersionInfo(versionInfo);
+			ApplicationManager.getApplication().invokeLater(handler);
+		}
+	}
+}
