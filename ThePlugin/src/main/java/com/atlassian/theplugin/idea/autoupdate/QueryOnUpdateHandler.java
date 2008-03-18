d5bb0ae880ae1bd1fadee5f8bfa7378b6bd62b17
jjaroczynski <jjaroczynski@localhost>
2008-03-18 13:16:37 +0000

    PL-137 problem with non-working tests fixed

--- ThePlugin/src/main/java/com/atlassian/theplugin/idea/autoupdate/QueryOnUpdateHandler.java
+++ ThePlugin/src/main/java/com/atlassian/theplugin/idea/autoupdate/QueryOnUpdateHandler.java
@@ -3,10 +3,10 @@ package com.atlassian.theplugin.idea.autoupdate;
 import com.atlassian.theplugin.configuration.PluginConfiguration;
 import com.atlassian.theplugin.exception.ThePluginException;
 import com.atlassian.theplugin.idea.IdeaHelper;
-import com.atlassian.theplugin.idea.autoupdate.PluginDownloader;
 import com.atlassian.theplugin.util.InfoServer;
 import com.atlassian.theplugin.util.PluginUtil;
 import com.atlassian.theplugin.util.Version;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 
@@ -20,14 +20,14 @@ import javax.swing.*;
  * To change this template use File | Settings | File Templates.
  */
 public class QueryOnUpdateHandler implements UpdateActionHandler {
-
+	private static final String DOWNLOAD_TITLE = "Downloading new " + PluginUtil.getName() + " plugin version ";
 	public QueryOnUpdateHandler(PluginConfiguration pluginConfiguration) {
 		this.pluginConfiguration = pluginConfiguration;
 	}
 
 	private PluginConfiguration pluginConfiguration;
 
-	public void doAction(InfoServer.VersionInfo versionInfo) throws ThePluginException {
+	public void doAction(final InfoServer.VersionInfo versionInfo) throws ThePluginException {
 		Version aVersion = versionInfo.getVersion();
 		String message = "New plugin version " + aVersion + " is available. "
 				+ "Your version is " + PluginUtil.getVersion()
@@ -42,7 +42,11 @@ public class QueryOnUpdateHandler implements UpdateActionHandler {
 			//Thread downloader = new Thread(new PluginDownloader(versionInfo, pluginConfiguration));
 			//downloader.start();
 
-			Task.Backgroundable downloader = new PluginDownloader(versionInfo, pluginConfiguration, IdeaHelper.getCurrentProject());
+			Task.Backgroundable downloader = new Task.Backgroundable(IdeaHelper.getCurrentProject(), DOWNLOAD_TITLE, false) {
+				public void run(ProgressIndicator indicator) {
+					new PluginDownloader(versionInfo, pluginConfiguration).run();
+				}
+			};
 
 			ProgressManager.getInstance().run(downloader);
 
