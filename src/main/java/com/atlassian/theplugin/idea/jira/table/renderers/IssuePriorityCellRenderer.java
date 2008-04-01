a460affc8c24b00dedc56033762c42aede8ebabd
mwent <mwent@localhost>
2008-04-01 09:26:51 +0000

    PL-213 PL-217

--- src/main/java/com/atlassian/theplugin/idea/jira/table/renderers/IssuePriorityCellRenderer.java
+++ src/main/java/com/atlassian/theplugin/idea/jira/table/renderers/IssuePriorityCellRenderer.java
@@ -1,32 +1,11 @@
 package com.atlassian.theplugin.idea.jira.table.renderers;
 
+import com.atlassian.theplugin.idea.jira.JiraIcon;
 import com.atlassian.theplugin.idea.jira.JiraIssueAdapter;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
-import java.awt.*;
 
-
-public class IssuePriorityCellRenderer extends DefaultTableCellRenderer {
-	public Component getTableCellRendererComponent(JTable jTable,
-												   Object o, boolean isSelected, boolean hasFocus, int i, int i1) {
-		Component c = super.getTableCellRendererComponent(jTable, o, isSelected, hasFocus, i, i1);
-		if (o instanceof JiraIssueAdapter) {
-			JiraIssueAdapter issueAdapter = (JiraIssueAdapter) o;
-			if (issueAdapter.getPriorityIcon() != null) {
-				((JLabel) c).setToolTipText(issueAdapter.getPriority());
-				if (issueAdapter.isUseIconDescription()) {
-					((JLabel) c).setText(issueAdapter.getPriority());
-				} else {
-					((JLabel) c).setText("");
-				}
-				((JLabel) c).setIcon(issueAdapter.getPriorityIcon());
-			} else {
-				((JLabel) c).setToolTipText(issueAdapter.getPriority());
-				((JLabel) c).setText(issueAdapter.getPriority());
-				((JLabel) c).setIcon(null);
-			}
-		}
-		return c;
+public class IssuePriorityCellRenderer extends AbstractIssueCellRenderer {
+	protected JiraIcon getJiraIcon(JiraIssueAdapter adapter) {
+		return adapter.getPriorityInfo();
 	}
 }
