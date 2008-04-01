73c04831eb011f8e26d8c072ceedcce97b5aa16f
jjaroczynski <jjaroczynski@localhost>
2008-04-01 15:10:01 +0000

    PL-228 shows progress animation when loading JIRA issues (still beta and throws exception sometimes - will be fixed)

---
+++ src/main/java/com/atlassian/theplugin/idea/ui/AnimatedProgressIcon.java
@@ -0,0 +1,51 @@
+package com.atlassian.theplugin.idea.ui;
+
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.util.ui.AsyncProcessIcon;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jacek
+ * Date: 2008-03-31
+ * Time: 15:33:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class AnimatedProgressIcon extends AsyncProcessIcon {
+	private Icon[] icons;
+	private Icon passiveIcon;
+
+	public AnimatedProgressIcon(@org.jetbrains.annotations.NonNls String name) {
+		super(name);
+
+		// comment that line if you want to use standard IDEA small progress circle
+		initCustomLook();
+	}
+
+	private void initCustomLook() {
+
+		loadIcons();
+
+		final int cycleLength = 640;	// whole animation single cycle lenght
+		final int cycleGap = 80;		// break after every single cycle (best 'cycleLenght / number of frames')
+
+		init(icons, passiveIcon, cycleLength, cycleGap, -1);	
+	}
+
+	private void loadIcons() {
+		icons = new Icon[]{
+			IconLoader.getIcon("/icons/progress/roller_1.png"),
+			IconLoader.getIcon("/icons/progress/roller_2.png"),
+			IconLoader.getIcon("/icons/progress/roller_3.png"),
+			IconLoader.getIcon("/icons/progress/roller_4.png"),
+			IconLoader.getIcon("/icons/progress/roller_5.png"),
+			IconLoader.getIcon("/icons/progress/roller_6.png"),
+			IconLoader.getIcon("/icons/progress/roller_7.png"),
+			IconLoader.getIcon("/icons/progress/roller_8.png"),
+		};
+
+		passiveIcon = IconLoader.getIcon("/icons/progress/roller_0.png");
+
+	}
+}
