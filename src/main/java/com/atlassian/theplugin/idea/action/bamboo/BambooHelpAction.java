da83849ab17ce96076ee704505854cc8521042a8
jgorycki <jgorycki@localhost>
2008-04-02 15:04:57 +0000

    PL-163 added proper help URLs to help actions

--- src/main/java/com/atlassian/theplugin/idea/action/bamboo/BambooHelpAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/bamboo/BambooHelpAction.java
@@ -1,11 +1,12 @@
 package com.atlassian.theplugin.idea.action.bamboo;
 
+import com.atlassian.theplugin.idea.Constants;
 import com.atlassian.theplugin.idea.action.HelpAction;
 
 public class BambooHelpAction extends HelpAction {
 
 	public BambooHelpAction() {
-		super("/BambooTab");
+		super(Constants.HELP_BAMBOO);
 	}
 
 }
--- src/main/java/com/atlassian/theplugin/idea/action/crucible/CrucibleHelpAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/crucible/CrucibleHelpAction.java
@@ -1,11 +1,12 @@
 package com.atlassian.theplugin.idea.action.crucible;
 
+import com.atlassian.theplugin.idea.Constants;
 import com.atlassian.theplugin.idea.action.HelpAction;
 
 public class CrucibleHelpAction extends HelpAction {
 
 	public CrucibleHelpAction() {
-		super("/CrucibleTab");
+		super(Constants.HELP_CRUCIBLE);
 	}
 
 }
