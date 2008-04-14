9dee81c265b1f5f81cb235b57323204a6640a7ff
jgorycki <jgorycki@localhost>
2008-04-14 15:09:01 +0000

    PL-250 - I want to be able to edit changeset comment when creating changeset from JIRA issue - plus added an option to either make the changelist active or not

--- src/main/java/com/atlassian/theplugin/idea/jira/ChangesetCreate.java
+++ src/main/java/com/atlassian/theplugin/idea/jira/ChangesetCreate.java
@@ -24,11 +24,13 @@ import java.awt.*;
 public class ChangesetCreate extends DialogWrapper {
 	private JPanel contentPane;
 	private JTextField changesetName;
+	private JTextArea changesetComment;
+	private JCheckBox isActive;
 
 	public ChangesetCreate(String issueKey) {
 		super(false);
 		init();
-		setTitle("ChangeList Name for " + issueKey);
+		setTitle("Create Changelist for " + issueKey);
 		changesetName.setDragEnabled(false);
 		getOKAction().putValue(Action.NAME, "Create");
 	}
@@ -38,8 +40,24 @@ public class ChangesetCreate extends DialogWrapper {
 		changesetName.setCaretPosition(0);
 	}
 
+	public void setChangestComment(String txt) {
+		changesetComment.setText(txt);
+	}
+
+	public void setActive(boolean active) {
+		isActive.setSelected(active);
+	}
+
 	public String getChangesetName() {
 		return changesetName.getText();
+	}
+
+	public String getChangesetComment() {
+		return changesetComment.getText();
+	}
+
+	public boolean isActive() {
+		return isActive.isSelected();
 	}
 
 	protected JComponent createCenterPanel() {
@@ -47,7 +65,11 @@ public class ChangesetCreate extends DialogWrapper {
 	}
 
 	public JComponent getPreferredFocusedComponent() {
-		return changesetName;
+		return changesetComment;
+	}
+
+	private void createUIComponents() {
+		// TODO: place custom component creation code here
 	}
 
 	{
@@ -67,20 +89,55 @@ public class ChangesetCreate extends DialogWrapper {
 	private void $$$setupUI$$$() {
 		contentPane = new JPanel();
 		contentPane.setLayout(new GridBagLayout());
-		contentPane.setMaximumSize(new Dimension(500, 52));
-		contentPane.setMinimumSize(new Dimension(500, 52));
+		contentPane.setMaximumSize(new Dimension(-1, -1));
+		contentPane.setMinimumSize(new Dimension(-1, -1));
 		contentPane.setOpaque(false);
-		contentPane.setPreferredSize(new Dimension(500, 52));
+		contentPane.setPreferredSize(new Dimension(500, 200));
 		contentPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12), null));
 		changesetName = new JTextField();
 		GridBagConstraints gbc;
 		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
+		gbc.gridx = 1;
 		gbc.gridy = 0;
 		gbc.weightx = 1.0;
 		gbc.anchor = GridBagConstraints.WEST;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		contentPane.add(changesetName, gbc);
+		final JScrollPane scrollPane1 = new JScrollPane();
+		gbc = new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.insets = new Insets(12, 0, 12, 0);
+		contentPane.add(scrollPane1, gbc);
+		changesetComment = new JTextArea();
+		scrollPane1.setViewportView(changesetComment);
+		final JLabel label1 = new JLabel();
+		label1.setText("Name:");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		contentPane.add(label1, gbc);
+		final JLabel label2 = new JLabel();
+		label2.setText("Comment:");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(0, 0, 0, 12);
+		contentPane.add(label2, gbc);
+		isActive = new JCheckBox();
+		isActive.setSelected(false);
+		isActive.setText("Make this changelist active");
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.gridwidth = 2;
+		gbc.anchor = GridBagConstraints.WEST;
+		contentPane.add(isActive, gbc);
 	}
 
 	/**
