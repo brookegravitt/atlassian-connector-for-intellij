db92b30864b355b9b53fadc28a4e67307687027a
jgorycki <jgorycki@localhost>
2008-04-25 10:52:35 +0000

    PL-244 - unused import removed

--- src/main/java/com/atlassian/theplugin/jira/api/JIRASession.java
+++ src/main/java/com/atlassian/theplugin/jira/api/JIRASession.java
@@ -18,7 +18,6 @@ package com.atlassian.theplugin.jira.api;
 
 import com.atlassian.theplugin.remoteapi.RemoteApiException;
 import com.atlassian.theplugin.remoteapi.RemoteApiLoginException;
-import com.atlassian.theplugin.configuration.Server;
 
 public interface JIRASession {
 	void login(String userName, String password) throws RemoteApiLoginException;
