da65244cbeee38a29d1f66378211d24a852518e0
pmaruszak <pmaruszak@localhost>
2008-05-06 14:42:01 +0000

    PL-224, During Eclipse implementation of this feature additional interface for HttpProxyDialog should be added

--- ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/util/HttpClientFactory.java
+++ ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/util/HttpClientFactory.java
@@ -16,11 +16,14 @@
 
 package com.atlassian.theplugin.commons.util;
 
+import com.atlassian.theplugin.exception.HttpProxySettingsException;
+import com.atlassian.theplugin.commons.thirdparty.apache.EasySSLProtocolSocketFactory;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.protocol.Protocol;
 import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
-import com.atlassian.theplugin.commons.thirdparty.apache.EasySSLProtocolSocketFactory;
 
 public final class HttpClientFactory {
 	private static MultiThreadedHttpConnectionManager connectionManager;
@@ -35,6 +38,7 @@ public final class HttpClientFactory {
 	private static int dataTimeout = DATA_TIMOUT;
 	private static int connectionTimout = CONNECTION_TIMOUT;
 	private static int connectionManagerTimeout = CONNECTION_MANAGER_TIMEOUT;
+	private static HttpConfigurableAdapter httpConfigurableAdapter;
 
 	static {
 		Protocol.registerProtocol("https", new Protocol(
@@ -43,13 +47,15 @@ public final class HttpClientFactory {
 		connectionManager.getParams().setConnectionTimeout(getConnectionTimeout());
 		connectionManager.getParams().setMaxTotalConnections(TOTAL_MAX_CONNECTIONS);
 		connectionManager.getParams().setDefaultMaxConnectionsPerHost(DEFAULT_MAX_CONNECTIONS_PER_HOST);
+		
 	}
 
 	///CLOVER:OFF
-	private HttpClientFactory() {		
+	private HttpClientFactory(HttpConfigurableAdapter httpConfigurableAdapter) {
+		this.httpConfigurableAdapter = httpConfigurableAdapter;
 	}
 
-	public static void setDataTimeout(int dataTimeout) {
+	protected static void setDataTimeout(int dataTimeout) {
 		HttpClientFactory.dataTimeout = dataTimeout;
 	}
 
@@ -62,12 +68,33 @@ public final class HttpClientFactory {
 	}
 ///CLOVER:ON
 
-	public static HttpClient getClient() {
+	public static HttpClient getClient() throws HttpProxySettingsException {
 		HttpClient httpClient = new HttpClient(connectionManager);
 		httpClient.getParams().setConnectionManagerTimeout(getConnectionManagerTimeout());
 		httpClient.getParams().setSoTimeout(getDataTimeout());
+
+		if (httpConfigurableAdapter != null) {
+			if (httpConfigurableAdapter.isUseHttpProxy()){
+				httpClient.getHostConfiguration().setProxy(httpConfigurableAdapter.getProxyHost(), httpConfigurableAdapter.getProxyPort());
+				if (httpConfigurableAdapter.isProxyAuthentication()) {
+
+					if (httpConfigurableAdapter.getPlainProxyPassword().length() == 0 &&
+							!httpConfigurableAdapter.isKeepProxyPassowrd()) {//ask user for proxy passowrd
+
+						throw new HttpProxySettingsException("HTTP Proxy password is incorrect");
+					}
+					httpClient.getState().setProxyCredentials(new AuthScope(httpConfigurableAdapter.getProxyHost(), httpConfigurableAdapter.getProxyPort()),
+							new UsernamePasswordCredentials(httpConfigurableAdapter.getProxyLogin(), httpConfigurableAdapter.getPlainProxyPassword()));
+
+
+
+				}
+			}
+		}
+
 		return httpClient;
 	}
+
 
 	private static int getConnectionManagerTimeout() {
 		return connectionManagerTimeout;
