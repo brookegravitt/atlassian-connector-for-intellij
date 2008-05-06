4a0d8dea842294baf02b7601e5e47a5e119f5497
jjaroczynski <jjaroczynski@localhost>
2008-05-06 10:57:52 +0000

    tests fix

---
+++ commons/src/test/java/com/atlassian/theplugin/EmptyTest.java
@@ -0,0 +1,16 @@
+package com.atlassian.theplugin;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jacek
+ * Date: 2008-05-06
+ * Time: 12:42:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class EmptyTest extends TestCase {
+	public void testEmpty() {
+		assertTrue(true);
+	}
+}
