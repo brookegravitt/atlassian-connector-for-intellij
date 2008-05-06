b170e99b711c9daca9467362aef8a90ee221b224
jjaroczynski <jjaroczynski@localhost>
2008-05-06 13:07:40 +0000

    tests fix

--- eclipse/src/main/java/com/atlassian/theplugin/eclipse/preferences/PreferencePage.java
+++ eclipse/src/main/java/com/atlassian/theplugin/eclipse/preferences/PreferencePage.java
@@ -36,23 +36,25 @@ public class PreferencePage
 	 * restore itself.
 	 */
 	public void createFieldEditors() {
-		addField(new DirectoryFieldEditor(PreferenceConstants.P_PATH, 
-				"&Directory preference:", getFieldEditorParent()));
-		addField(
-			new BooleanFieldEditor(
-				PreferenceConstants.P_BOOLEAN,
-				"&An example of a boolean preference",
-				getFieldEditorParent()));
-
-		addField(new RadioGroupFieldEditor(
-				PreferenceConstants.P_CHOICE,
-			"An example of a multiple-choice preference",
-			1,
-			new String[][] { { "&Choice 1", "choice1" }, {
-				"C&hoice 2", "choice2" }
-		}, getFieldEditorParent()));
-		addField(
-			new StringFieldEditor(PreferenceConstants.P_STRING, "A &text preference:", getFieldEditorParent()));
+//		addField(new DirectoryFieldEditor(PreferenceConstants.P_PATH, 
+//				"&Directory preference:", getFieldEditorParent()));
+//		addField(
+//			new BooleanFieldEditor(
+//				PreferenceConstants.P_BOOLEAN,
+//				"&An example of a boolean preference",
+//				getFieldEditorParent()));
+//
+//		addField(new RadioGroupFieldEditor(
+//				PreferenceConstants.P_CHOICE,
+//			"An example of a multiple-choice preference",
+//			1,
+//			new String[][] { { "&Choice 1", "choice1" }, {
+//				"C&hoice 2", "choice2" }
+//		}, getFieldEditorParent()));
+//		addField(
+//			new StringFieldEditor(PreferenceConstants.P_STRING, "A &text preference:", getFieldEditorParent()));
+		
+		addField(new StringFieldEditor("blabla", "Name:", getFieldEditorParent()));
 	}
 
 	/* (non-Javadoc)
