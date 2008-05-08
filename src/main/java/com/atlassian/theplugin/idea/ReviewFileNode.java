c3414390e729abf6e1b1a1ed5a48cb77b3d1dedf
mwent <mwent@localhost>
2008-05-08 11:26:39 +0000

    PL-21 idea.jar dependencies commented

--- src/main/java/com/atlassian/theplugin/idea/ReviewFileNode.java
+++ src/main/java/com/atlassian/theplugin/idea/ReviewFileNode.java
@@ -1,16 +1,10 @@
 package com.atlassian.theplugin.idea;
 
 import com.atlassian.theplugin.crucible.api.ReviewItemData;
-import com.intellij.ide.projectView.PresentationData;
-import com.intellij.ide.projectView.ViewSettings;
-import com.intellij.ide.projectView.impl.nodes.PsiFileNode;
-import com.intellij.openapi.editor.colors.EditorColors;
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiFile;
 
-public class ReviewFileNode extends PsiFileNode {
+public class ReviewFileNode /*extends PsiFileNode*/ {
 	ReviewItemData reviewItem;
-
+/*
 	protected ReviewFileNode(ReviewItemData reviewItem, Project project, PsiFile o, ViewSettings viewSettings) {
 		super(project, o, viewSettings);
 		this.reviewItem = reviewItem;
@@ -21,5 +15,5 @@ public class ReviewFileNode extends PsiFileNode {
 		presentationData.setPresentableText(presentationData.getPresentableText() + " (rev " + reviewItem.getFromRevision() + " -> " + reviewItem.getToRevision() + ")" );
         presentationData.setAttributesKey(EditorColors.TEXT_SEARCH_RESULT_ATTRIBUTES);
 	}
-
+*/
 }
