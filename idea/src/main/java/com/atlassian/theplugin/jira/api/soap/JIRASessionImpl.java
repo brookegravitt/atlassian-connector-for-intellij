811fa3764839597e11a11a33c4e44a918459887a
jgorycki <jgorycki@localhost>
2008-05-09 12:44:41 +0000

    changed login failer error text to match crucible's

--- idea/src/main/java/com/atlassian/theplugin/jira/api/soap/JIRASessionImpl.java
+++ idea/src/main/java/com/atlassian/theplugin/jira/api/soap/JIRASessionImpl.java
@@ -102,7 +102,7 @@ public class JIRASessionImpl implements JIRASession {
 		try {
 			token = service.login(userName, password);
 		} catch (RemoteAuthenticationException e) {
-			throw new RemoteApiLoginException("Invalid username or password");
+			throw new RemoteApiLoginException("authentiaction failed");
 		} catch (RemoteException e) {
 	   		throw new RemoteApiLoginException(e.toString());
 		}
