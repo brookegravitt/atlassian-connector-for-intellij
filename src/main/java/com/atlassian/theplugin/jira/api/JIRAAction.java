7b6b5b3323f459367f25ec9305f61dca1244a1a1
jgorycki <jgorycki@localhost>
2008-05-27 19:45:48 +0000

    PL-333 - Workflow actions in context menu for issues

---
+++ src/main/java/com/atlassian/theplugin/jira/api/JIRAAction.java
@@ -0,0 +1,6 @@
+package com.atlassian.theplugin.jira.api;
+
+import com.atlassian.theplugin.jira.api.JIRAQueryFragment;
+
+public interface JIRAAction extends JIRAQueryFragment {
+}
---
+++ src/main/java/com/atlassian/theplugin/jira/api/JIRAActionBean.java
@@ -0,0 +1,32 @@
+package com.atlassian.theplugin.jira.api;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class JIRAActionBean implements JIRAAction {
+    private String name;
+    private long id;
+
+    public JIRAActionBean(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String getQueryStringFragment() {
+        return "action=" + id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, String> getMap() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", getName());
+        map.put("id", Long.toString(id));
+        return map;
+    }
+}
