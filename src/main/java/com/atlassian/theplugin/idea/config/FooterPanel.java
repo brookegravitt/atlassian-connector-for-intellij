d72da764f2bf79b98976ca75acac49627a572a2b
jgorycki <jgorycki@localhost>
2008-05-27 08:33:11 +0000

    version 1.3.1 added

--- src/main/java/com/atlassian/theplugin/idea/config/FooterPanel.java
+++ src/main/java/com/atlassian/theplugin/idea/config/FooterPanel.java
@@ -63,6 +63,7 @@ public class FooterPanel extends JPanel {
 		versionMap.put("1.2.0", "10018");
 		versionMap.put("1.2.1", "10470");
 		versionMap.put("1.3.0", "10019");
+		versionMap.put("1.3.1", "10472");
 		versionMap.put("1.4.0", "10020");
 		versionMap.put("1.5.0", "10021");
 		versionMap.put("2.0.0", "10022");
