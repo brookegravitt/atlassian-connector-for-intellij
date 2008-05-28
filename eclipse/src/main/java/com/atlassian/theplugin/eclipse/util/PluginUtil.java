60d518ff62e57419e56b157a48113e95ddc6144d
jjaroczynski <jjaroczynski@localhost>
2008-05-28 13:36:21 +0000

    pl-301 icons in table

--- eclipse/src/main/java/com/atlassian/theplugin/eclipse/util/PluginUtil.java
+++ eclipse/src/main/java/com/atlassian/theplugin/eclipse/util/PluginUtil.java
@@ -1,10 +1,19 @@
 package com.atlassian.theplugin.eclipse.util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.swt.graphics.ImageLoader;
+
+import com.atlassian.theplugin.commons.bamboo.BuildStatus;
 import com.atlassian.theplugin.commons.util.Logger;
 import com.atlassian.theplugin.commons.util.LoggerImpl;
 
 public final class PluginUtil {
 	private static final String PLUGIN_NAME = "Attlasian IDE Eclipse plug-in";
+	private static ImageRegistry imageRegistry;
 
 	private PluginUtil() {
 	}
@@ -15,6 +24,19 @@ public final class PluginUtil {
 	
 	public static Logger getLogger() {
 		return LoggerImpl.getInstance();
+	}
+	
+	public static ImageRegistry getImageRegistry() {
+		if (imageRegistry == null) {
+			imageRegistry = new ImageRegistry();
+			
+			imageRegistry.put(BuildStatus.BUILD_SUCCEED.toString(), ImageDescriptor.createFromFile(PluginUtil.class, "/icons/bamboo_plan_passed.gif"));
+			imageRegistry.put(BuildStatus.BUILD_FAILED.toString(), ImageDescriptor.createFromFile(PluginUtil.class, "/icons/bamboo_plan_failed.gif"));
+			imageRegistry.put(BuildStatus.UNKNOWN.toString(), ImageDescriptor.createFromFile(PluginUtil.class, "icons/bamboo_plan_unknown.gif"));
+			
+		}
+		
+		return imageRegistry;
 	}
 	
 }
---
+++ eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/bamboo/BambooBuildAdapterEclipse.java
@@ -0,0 +1,13 @@
+package com.atlassian.theplugin.eclipse.view.bamboo;
+
+import com.atlassian.theplugin.commons.bamboo.BambooBuild;
+import com.atlassian.theplugin.commons.bamboo.BambooBuildAdapter;
+
+public class BambooBuildAdapterEclipse extends BambooBuildAdapter {
+
+
+	public BambooBuildAdapterEclipse(BambooBuild build) {
+		super(build);
+	}
+
+}
