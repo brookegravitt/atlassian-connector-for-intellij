bb9bbdf88c690991d7777ce090adbe909a65bfc1
jjaroczynski <jjaroczynski@localhost>
2008-05-29 09:21:37 +0000

    pl-301 added label and comment empty actions to the bamboo view toolbar

--- ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/bamboo/CommentBuildAction.java
+++ ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/bamboo/CommentBuildAction.java
@@ -3,39 +3,32 @@ package com.atlassian.theplugin.eclipse.view.bamboo;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.resource.ImageDescriptor;
 
-import java.lang.Thread;
-
-import com.atlassian.theplugin.commons.bamboo.BuildStatus;
 import com.atlassian.theplugin.eclipse.util.PluginUtil;
 
-public class RunBuildAction extends Action {
+public class CommentBuildAction extends Action {
 	
-	private static final String RUN_BUILD = "Run Build";
+	private static final String COMMENT_BUILD = "Comment Build";
 
 	@Override
 	public void run() {
 		super.run();
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 	@Override
 	public ImageDescriptor getImageDescriptor() {
 		// TODO use eclipse resource handling
-		return ImageDescriptor.createFromImage(PluginUtil.getImageRegistry().get(PluginUtil.BAMBOO_RUN));
+		return ImageDescriptor.createFromImage(PluginUtil.getImageRegistry().get(PluginUtil.BAMBOO_COMMENT));
 	}
 
 	@Override
 	public String getText() {
-		return RUN_BUILD;
+		return COMMENT_BUILD;
 	}
 
 	@Override
 	public String getToolTipText() {
-		return RUN_BUILD;
+		return COMMENT_BUILD;
 	}
 	
 	
