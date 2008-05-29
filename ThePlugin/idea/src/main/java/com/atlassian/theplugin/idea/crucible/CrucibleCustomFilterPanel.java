d444eda9dfbbd29c89904ec64a911259f1f18acc
pmaruszak <pmaruszak@localhost>
2008-05-29 09:04:46 +0000

    PL-279

---
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleCustomFilterPanel.java
@@ -0,0 +1,29 @@
+package com.atlassian.theplugin.idea.crucible;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: May 29, 2008
+ * Time: 9:11:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CrucibleCustomFilterPanel {
+	private JPanel panel1;
+	private JTextField filterTitle;
+	private JCheckBox draftCheckBox;
+	private JCheckBox pendingApprovalCheckBox;
+	private JCheckBox underReviewCheckBox;
+	private JCheckBox summarizeCheckBox;
+	private JCheckBox closedCheckBox;
+	private JCheckBox abandonedCheckBox;
+	private JCheckBox rejectedCheckBox;
+	private JCheckBox reviewNeedsFixingCheckBox;
+	private JComboBox projectComboBox;
+	private JComboBox authorComboBox;
+	private JComboBox moderatorComboBox;
+	private JComboBox creatorComboBox;
+	private JComboBox reviewerStatusComboBox;
+	private JComboBox reviewerComboBox;
+}
