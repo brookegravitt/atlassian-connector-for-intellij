9ebf9469e9f493ada6ede41bc6653148e01bb2b3
jjaroczynski <jjaroczynski@localhost>
2008-05-30 14:54:41 +0000

    pl-301 'comment' and 'label' actions for bamboo2 disabled (they were not implemented yet)

--- ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/bamboo/BambooToolWindow.java
+++ ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/bamboo/BambooToolWindow.java
@@ -87,8 +87,8 @@ public class BambooToolWindow extends ViewPart {
 	}
 	
 	public void enableBamboo2BuildActions() {
-		labelBuildAction.setEnabled(true);
-		commentBuildAction.setEnabled(true);
+		//labelBuildAction.setEnabled(true);
+		//commentBuildAction.setEnabled(true);
 		getViewSite().getActionBars().getToolBarManager().update(true);
 	}
 	
