c7642d0d755f18336754ad967045f0ba359a2f3a
jjaroczynski <jjaroczynski@localhost>
2008-05-30 14:28:55 +0000

    pl-301 * 'builds' field in configuration can store space separated builds' names (tooltip hint added also) * after clicking OK/Apply in configuration dialog the configuration is reloaded and bamboo checker restarted

--- eclipse/src/main/java/com/atlassian/theplugin/eclipse/EclipseActionScheduler.java
+++ eclipse/src/main/java/com/atlassian/theplugin/eclipse/EclipseActionScheduler.java
@@ -3,8 +3,11 @@
  */
 package com.atlassian.theplugin.eclipse;
 
+import org.eclipse.swt.SWTException;
+
 import com.atlassian.theplugin.commons.UIActionScheduler;
 import com.atlassian.theplugin.eclipse.preferences.Activator;
+import com.atlassian.theplugin.eclipse.util.PluginUtil;
 
 /**
  * @author Jacek
@@ -25,7 +28,13 @@ public final class EclipseActionScheduler implements UIActionScheduler {
 	 * @see com.atlassian.theplugin.commons.UIActionScheduler#invokeLater(java.lang.Runnable)
 	 */
 	public void invokeLater(Runnable runnable) {
-		Activator.getDefault().getWorkbench().getDisplay().asyncExec(runnable);
+		try {
+			Activator.getDefault().getWorkbench().getDisplay().asyncExec(runnable);
+		} catch (SWTException ex) {
+			PluginUtil.getLogger().warn(ex);
+		} catch (NullPointerException ex) {
+			PluginUtil.getLogger().warn(ex);
+		}
 	}
 
 }
