9ad72f3178d959df0c0b9178fa0cd2801b4145c5
mwent <mwent@localhost>
2008-05-30 10:59:01 +0000

    PL-279 - Crucible Project filters

---
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/ExpandAllAction.java
@@ -0,0 +1,11 @@
+package com.atlassian.theplugin.idea.action.crucible;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.atlassian.theplugin.idea.IdeaHelper;
+
+public class ExpandAllAction extends AnAction {
+    public void actionPerformed(AnActionEvent event) {
+        IdeaHelper.getCrucibleToolWindowPanel(event).expandAllPanels();
+    }
+}
