c917062e729ac3ca9d295d16c1ea971843c09feb
lguminski <lguminski@localhost>
2008-06-02 18:25:43 +0000

    some checkstyle errors fixed

--- src/main/java/com/atlassian/theplugin/idea/action/crucible/CrucibleShowCustomFilterAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/crucible/CrucibleShowCustomFilterAction.java
@@ -1,10 +1,9 @@
 package com.atlassian.theplugin.idea.action.crucible;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.atlassian.theplugin.idea.jira.JIRAToolWindowPanel;
 import com.atlassian.theplugin.idea.IdeaHelper;
 import com.atlassian.theplugin.idea.crucible.CrucibleTableToolWindowPanel;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 
 /**
  * Created by IntelliJ IDEA.
