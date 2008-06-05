c43f5014aa693b0b5d238c7b71904c0d96556424
mwent <mwent@localhost>
2008-06-05 15:53:41 +0000

    PL-279 - Crucible Project filters

--- idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/ViewFisheyeChangesetAction.java
+++ idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/ViewFisheyeChangesetAction.java
@@ -2,7 +2,6 @@ package com.atlassian.theplugin.idea.action.crucible;
 
 import com.atlassian.theplugin.commons.crucible.CrucibleServerFacadeImpl;
 import com.atlassian.theplugin.idea.crucible.CrucibleRevisionAddWorker;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.application.ApplicationManager;
@@ -11,7 +10,7 @@ import com.intellij.openapi.vcs.changes.ChangeList;
 import com.intellij.openapi.vcs.changes.Change;
 
 
-public class ViewFisheyeChangesetAction extends AnAction {
+public class ViewFisheyeChangesetAction extends Crucible16RepositoryAction {
     public void actionPerformed(AnActionEvent event) {
         final ChangeList[] changes = DataKeys.CHANGE_LISTS.getData(event.getDataContext());
 
@@ -32,3 +31,4 @@ public class ViewFisheyeChangesetAction extends AnAction {
         }
     }
 }
+
