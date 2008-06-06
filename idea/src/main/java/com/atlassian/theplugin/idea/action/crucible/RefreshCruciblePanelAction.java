3eca63fdabdf037d7793590b5402c7aab4cea78d
mwent <mwent@localhost>
2008-06-06 07:49:52 +0000

    PL-279 - Crucible Project filters

--- idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/RefreshCruciblePanelAction.java
+++ idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/RefreshCruciblePanelAction.java
@@ -25,48 +25,12 @@ import com.atlassian.theplugin.commons.util.Logger;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Jacek
- * Date: 2008-04-03
- * Time: 11:30:12
- * To change this template use File | Settings | File Templates.
- */
 public class RefreshCruciblePanelAction extends AnAction {
 	public void actionPerformed(AnActionEvent e) {
-		ThePluginProjectComponent projectComponent = IdeaHelper.getCurrentProjectComponent(e);
-
-		if (projectComponent != null) {
-			final CrucibleStatusChecker checker = projectComponent.getCrucibleStatusChecker();
-
-			if (checker.canSchedule()) {
-
-				final ProgressAnimationProvider animator =
-								IdeaHelper.getCrucibleToolWindowPanel(e).getProgressAnimation();
-
-				final Logger log = PluginUtil.getLogger();
-
-				new Thread(new Runnable() {
-					public void run() {
-
-						Thread t = new Thread(checker.newTimerTask(), "Manual Crucible panel refresh (checker)");
-
-						animator.startProgressAnimation();
-
-						t.start();
-						try {
-							t.join();
-						} catch (InterruptedException e) {
-							log.warn(e.toString());
-						} finally {
-							animator.stopProgressAnimation();
-						}
-
-
-					}
-				}, "Manual Crucible panel refresh").start();
-
-			}
-		}
-	}
+        ThePluginProjectComponent projectComponent = IdeaHelper.getCurrentProjectComponent(e);
+        if (projectComponent != null) {
+			CrucibleStatusChecker checker = projectComponent.getCrucibleStatusChecker();
+            IdeaHelper.getCrucibleToolWindowPanel(e).refreshReviews(checker);
+        }
+    }
 }
