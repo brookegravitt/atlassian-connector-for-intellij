188deb05d97bcc0a799f78754361a5a8f29a91c4
jjaroczynski <jjaroczynski@localhost>
2008-06-10 14:55:34 +0000

    ple-2 added tooltip like in Mylyn

---
+++ eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/popup/CompositeSynchImageDescriptor.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package com.atlassian.theplugin.eclipse.view.popup;
+
+import org.eclipse.jface.resource.CompositeImageDescriptor;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.Point;
+
+/**
+ * @author Mik Kersten
+ */
+public class CompositeSynchImageDescriptor extends CompositeImageDescriptor {
+
+	private ImageData base;
+
+	private ImageData background;
+
+	private boolean fillBackground;
+
+	protected Point size;
+
+	static int WIDTH;
+
+	public CompositeSynchImageDescriptor(ImageDescriptor icon, boolean fillBackground) {
+		this.base = getImageData(icon);
+		//this.background = getImageData(TasksUiImages.OVERLAY_SOLID_WHITE);
+		this.size = new Point(background.width, background.height);
+		this.fillBackground = fillBackground;
+	}
+
+	@Override
+	protected void drawCompositeImage(int width, int height) {
+		if (fillBackground) {
+			drawImage(background, 0, 0);
+		}
+		drawImage(base, 3, 2);
+	}
+
+	private ImageData getImageData(ImageDescriptor descriptor) {
+		ImageData data = descriptor.getImageData();
+		// see bug 51965: getImageData can return null
+		if (data == null) {
+			data = DEFAULT_IMAGE_DATA;
+		}
+		return data;
+	}
+
+	@Override
+	protected Point getSize() {
+		return new Point(size.x, size.y);
+	}
+}
