5a94beae4f5cdb0b8dd4a62023f9b6971d3aed0f
jjaroczynski <jjaroczynski@localhost>
2008-06-10 14:55:34 +0000

    ple-2 added tooltip like in Mylyn

---
+++ ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/popup/RepositoryTaskHandleUtil.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package com.atlassian.theplugin.eclipse.view.popup;
+
+/**
+ * @author Mik Kersten
+ */
+public class RepositoryTaskHandleUtil {
+
+	public static final String HANDLE_DELIM = "-";
+
+	private static final String MISSING_REPOSITORY = "norepository";
+
+	public static String getHandle(String repositoryUrl, String taskId) {
+		if (!isValidTaskId(taskId)) {
+			throw new RuntimeException("invalid handle for task, can not contain: " + HANDLE_DELIM + ", was: " + taskId);
+		}
+
+		if (repositoryUrl == null) {
+			return MISSING_REPOSITORY + HANDLE_DELIM + taskId;
+		} else {
+			return repositoryUrl + HANDLE_DELIM + taskId;
+		}
+	}
+
+	public static String getRepositoryUrl(String taskHandle) {
+		int index = taskHandle.lastIndexOf(RepositoryTaskHandleUtil.HANDLE_DELIM);
+		String url = null;
+		if (index != -1) {
+			url = taskHandle.substring(0, index);
+		}
+		return url;
+	}
+
+	public static String getTaskId(String taskHandle) {
+		int index = taskHandle.lastIndexOf(HANDLE_DELIM);
+		if (index != -1) {
+			String id = taskHandle.substring(index + 1);
+			return id;
+		}
+		return null;
+	}
+
+	public static boolean isValidTaskId(String taskId) {
+		return !taskId.contains(HANDLE_DELIM);
+	}
+
+}
