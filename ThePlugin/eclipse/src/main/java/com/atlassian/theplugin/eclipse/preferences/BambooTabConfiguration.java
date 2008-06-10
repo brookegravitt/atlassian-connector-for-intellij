7e6d2bbd0e3475fa734f82eb5a173136f60468ec
jjaroczynski <jjaroczynski@localhost>
2008-06-10 14:57:35 +0000

    ple-5 added configuration to store columns' order and width

---
+++ ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/preferences/BambooTabConfiguration.java
@@ -0,0 +1,19 @@
+package com.atlassian.theplugin.eclipse.preferences;
+
+import java.util.ArrayList;
+
+public class BambooTabConfiguration {
+
+	ArrayList<Integer> columnsOrder = new ArrayList<Integer>(0);
+
+	public ArrayList<Integer> getColumnsOrder() {
+		return columnsOrder;
+	}
+
+	public void setColumnsOrder(ArrayList<Integer> columnsOrder) {
+		this.columnsOrder = columnsOrder;
+	}
+	
+	
+	
+}
