8ac6990994dd45e14cfb6b49f1f91fb08f7e2ad5
pmaruszak <pmaruszak@localhost>
2008-06-12 22:48:27 +0000

    Bottom Crucible window wth tree panel

---
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleReviewTreeModel.java
@@ -0,0 +1,40 @@
+package com.atlassian.theplugin.idea.crucible;
+
+import com.atlassian.theplugin.commons.crucible.api.model.ReviewItem;
+
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Jun 10, 2008
+ * Time: 3:06:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CrucibleReviewTreeModel extends DefaultTreeModel {
+
+	static final long serialVersionUID = 1631701743528670523L;
+
+	public CrucibleReviewTreeModel(CrucibleTreeRootNode root) {
+		super(root);
+	}
+
+	public ReviewItemDataNode getReviewItemDataNode(ReviewItem reviewItem, boolean addIfMissing) {
+		for (int i = 0; i < root.getChildCount(); ++i) {
+			ReviewItemDataNode reviewItemDataNode = (ReviewItemDataNode) root.getChildAt(i);
+			if (reviewItemDataNode.getReviewItem() == reviewItem) {
+				return reviewItemDataNode;
+			}
+		}
+		if (addIfMissing) {
+			final ReviewItemDataNode child = new ReviewItemDataNode(reviewItem);
+			insertNodeInto(child, (DefaultMutableTreeNode) root, root.getChildCount());
+			this.nodeChanged(root);
+			return child;
+		}
+		return null;
+	}
+
+}
+
---
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/ReviewItemDataNode.java
@@ -0,0 +1,65 @@
+package com.atlassian.theplugin.idea.crucible;
+
+import com.atlassian.theplugin.commons.crucible.api.model.ReviewItem;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Jun 10, 2008
+ * Time: 3:08:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReviewItemDataNode extends DefaultMutableTreeNode {
+	static final long serialVersionUID = -1192703287399203290L;
+
+	private ReviewItem reviewItem;
+
+	public ReviewItemDataNode(ReviewItem aReviewItemData) {
+		this.reviewItem = aReviewItemData;
+	}
+
+	public ReviewItem getReviewItem() {
+		return reviewItem;
+	}
+
+	public void setReviewItem(ReviewItem reviewItem) {
+		this.reviewItem = reviewItem;
+	}
+
+	public String toString() {
+		if (reviewItem.getFromPath().length() > 0 && reviewItem.getToPath().length() > 0){
+			return reviewItem.getFromPath() + " (mod)";
+
+		} else if (reviewItem.getFromPath().length() > 0) {
+			return reviewItem.getFromPath() + " (new)";
+		} else if (reviewItem.getToPath().length() > 0) {
+			return reviewItem.getToPath() + " (del)";
+		} else {
+			return "unknown";
+		}		
+	}
+
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		ReviewItemDataNode that = (ReviewItemDataNode) o;
+
+		if (reviewItem.equals(that.reviewItem)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public int hashCode() {
+		return reviewItem.hashCode();
+	}
+}
+
