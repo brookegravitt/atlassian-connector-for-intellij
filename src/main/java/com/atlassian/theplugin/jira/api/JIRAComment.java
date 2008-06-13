5e5f71a55c368d6a94e2683891572da8d27fe734
jgorycki <jgorycki@localhost>
2008-06-13 14:40:04 +0000

    PL-40 - An in IDE view of the issue, focussing on stack traces and related code pieces

--- src/main/java/com/atlassian/theplugin/jira/api/JIRAComment.java
+++ src/main/java/com/atlassian/theplugin/jira/api/JIRAComment.java
@@ -1,7 +1,10 @@
 package com.atlassian.theplugin.jira.api;
 
+import java.util.Calendar;
+
 public interface JIRAComment {
 	String getId();
 	String getAuthor();
 	String getBody();
+	Calendar getCreationDate();
 }
