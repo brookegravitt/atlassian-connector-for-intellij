3f1aee2b8eb315b1acf51e088a912a3de03c05a7
jgorycki <jgorycki@localhost>
2008-06-16 15:39:44 +0000

    PL-417 - JIRA - show JIRA view in the editor panel, showing and allowing creation of comments

---
+++ idea/src/main/java/com/atlassian/theplugin/idea/action/jira/editor/CollapseCommentsAction.java
@@ -0,0 +1,9 @@
+package com.atlassian.theplugin.idea.action.jira.editor;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class CollapseCommentsAction extends AnAction {
+	public void actionPerformed(AnActionEvent event) {
+	}
+}
---
+++ idea/src/main/java/com/atlassian/theplugin/idea/action/jira/editor/RefreshCommentsAction.java
@@ -0,0 +1,9 @@
+package com.atlassian.theplugin.idea.action.jira.editor;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class RefreshCommentsAction extends AnAction {
+	public void actionPerformed(AnActionEvent event) {
+	}
+}
