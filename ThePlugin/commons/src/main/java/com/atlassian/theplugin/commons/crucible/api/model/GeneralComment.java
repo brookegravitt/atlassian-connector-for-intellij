21352ee54e5778c3319140667abd2c0ed6aa1233
lguminski <lguminski@localhost>
2008-06-18 22:36:01 +0000

    PL-282  UI functionality refactored

--- ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/crucible/api/model/GeneralComment.java
+++ ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/crucible/api/model/GeneralComment.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,7 @@ import java.util.Map;
 public interface GeneralComment {
 	PermId getPermId();
 
-    String getMessage();
+	String getMessage();
 
 	boolean isDraft();
 
@@ -41,5 +41,42 @@ public interface GeneralComment {
 
 	List<GeneralComment> getReplies();
 
-    Map<String, CustomField> getCustomFields();    
+	Map<String, CustomField> getCustomFields();
+
+	STATE getState();
+
+	public enum STATE {
+		DRAFT {
+			public String GetColorString() {
+				return "#FFD415";
+			}},
+		REVIEW {
+			public String GetColorString() {
+				return "green";
+			}},
+		DEFECT_RAISED {
+			public String GetColorString() {
+				return "#FFD415";
+			}
+			public String toString() {
+				return "DEFECT";
+			}
+		},
+		DEFECT_APPROVED {
+			public String GetColorString() {
+				return DEFECT_RAISED.GetColorString();
+			}
+
+			public String toString() {
+				return "DEFECT APROVED";
+			}
+		},
+		DELETED {
+			public String GetColorString() {
+				return "black";
+			}
+		};
+
+		public abstract String GetColorString();
+	}
 }
