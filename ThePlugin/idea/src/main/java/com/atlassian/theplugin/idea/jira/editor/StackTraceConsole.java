a8a77de0ead8228ce973da9112d758d23e9ddfa7
jgorycki <jgorycki@localhost>
2008-06-18 11:40:09 +0000

    PL-40 - An in IDE view of the issue, focussing on stack traces and related code pieces

---
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/jira/editor/StackTraceConsole.java
@@ -0,0 +1,73 @@
+package com.atlassian.theplugin.idea.jira.editor;
+
+import com.atlassian.theplugin.jira.api.JIRAIssue;
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
+import com.intellij.execution.filters.TextConsoleBuilder;
+import com.intellij.execution.ui.ConsoleViewContentType;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.ui.content.Content;
+import com.intellij.peer.PeerFactory;
+
+import java.util.HashMap;
+
+public final class StackTraceConsole {
+
+	private static final String TOOL_WINDOW_TITLE = "JIRA Stack Traces";
+
+	private static StackTraceConsole instance = new StackTraceConsole();
+
+	private HashMap<String, ConsoleView> consoleMap = new HashMap<String, ConsoleView>();
+
+	private StackTraceConsole() {
+	}
+
+	public static StackTraceConsole getInstance() {
+		return instance;
+	}
+
+	public void print(JIRAIssue issue, String key, String txt) {
+		ConsoleView console = setupConsole(issue.getKey(), key);
+		if (console != null) {
+			console.clear();
+			console.print(txt, ConsoleViewContentType.NORMAL_OUTPUT);
+		}
+	}
+
+	private ConsoleView setupConsole(String issueKey, String origin) {
+		ConsoleView console;
+		String contentKey = issueKey + " " + origin;
+
+		ToolWindowManager twm = ToolWindowManager.getInstance(IdeaHelper.getCurrentProject());
+		ToolWindow consoleToolWindow = twm.getToolWindow(TOOL_WINDOW_TITLE);
+		if (consoleToolWindow == null) {
+			consoleToolWindow = twm.registerToolWindow(TOOL_WINDOW_TITLE, true, ToolWindowAnchor.BOTTOM);
+		}
+
+		Content content = consoleToolWindow.getContentManager().findContent(contentKey);
+
+		if (content != null) {
+			console = consoleMap.get(contentKey);
+		} else {
+			TextConsoleBuilderFactory factory = TextConsoleBuilderFactory.getInstance();
+			TextConsoleBuilder builder = factory.createBuilder(IdeaHelper.getCurrentProject());
+			console = builder.getConsole();
+			consoleMap.remove(contentKey);
+			consoleMap.put(contentKey, console);
+
+			PeerFactory peerFactory = PeerFactory.getInstance();
+			content = peerFactory.getContentFactory().createContent(console.getComponent(), contentKey, true);
+			content.setIcon(IconLoader.getIcon("/icons/tab_jira.png"));
+			content.putUserData(com.intellij.openapi.wm.ToolWindow.SHOW_CONTENT_ICON, Boolean.TRUE);
+			consoleToolWindow.getContentManager().addContent(content);
+		}
+		consoleToolWindow.getContentManager().setSelectedContent(content);
+		consoleToolWindow.show(null);
+
+		return console;
+	}
+}
