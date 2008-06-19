3f52bcda049d60fa279465a2b35c259be6934485
jgorycki <jgorycki@localhost>
2008-06-19 10:20:59 +0000

    PL-424 - implement heuristics to detect if a comment or decription actually contains a stack trace and only show "Analyse stack trace" link if this is the case

---
+++ src/main/java/com/atlassian/theplugin/idea/jira/editor/StackTraceDetector.java
@@ -0,0 +1,21 @@
+package com.atlassian.theplugin.idea.jira.editor;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public final class StackTraceDetector {
+
+	private final static String STACK_LINE_PATTERN = "\\(.+\\.java:\\d+\\)";
+
+	private StackTraceDetector() {
+	}
+
+	public static boolean containsStackTrace(String txt) {
+		Pattern p = Pattern.compile(STACK_LINE_PATTERN);
+		Matcher m = p.matcher(txt);
+		if (m.find()) {
+			return true;
+		}
+		return false;
+	}
+}
