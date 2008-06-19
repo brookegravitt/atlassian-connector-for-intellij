c6db115f082f8b17a4caee55a4eebefe92603ec8
lguminski <lguminski@localhost>
2008-06-19 02:22:48 +0000

    PL-282  genera comment UI improved

--- src/main/java/com/atlassian/theplugin/idea/crucible/comments/CrucibleReviewActionListener.java
+++ src/main/java/com/atlassian/theplugin/idea/crucible/comments/CrucibleReviewActionListener.java
@@ -1,6 +1,7 @@
 package com.atlassian.theplugin.idea.crucible.comments;
 
 import com.atlassian.theplugin.commons.crucible.api.model.ReviewItem;
+import com.atlassian.theplugin.commons.crucible.api.model.GeneralComment;
 import com.atlassian.theplugin.idea.crucible.ReviewDataInfoAdapter;
 
 /**
@@ -11,11 +12,12 @@ import com.atlassian.theplugin.idea.crucible.ReviewDataInfoAdapter;
  * To change this template use File | Settings | File Templates.
  */
 public interface CrucibleReviewActionListener {
+	public static final CrucibleReviewActionListener I_WANT_THIS_MESSAGE_BACK = null;
 	/**
 	 * A method ivoked by a background thread when a new review needs to be focused
-	 * @param reviewItem
+	 * @param reviewDataInfoAdapter
 	 */
-	void focusOnReview(ReviewDataInfoAdapter reviewItem);
+	void focusOnReview(ReviewDataInfoAdapter reviewDataInfoAdapter);
 
 	/**
 	 * A method ivoked by a background thread when a new file within a review needs to be focused
@@ -23,4 +25,8 @@ public interface CrucibleReviewActionListener {
 	 * @param reviewItem
 	 */
 	void focusOnFile(ReviewDataInfoAdapter reviewDataInfoAdapter, ReviewItem reviewItem);
+
+	void focusOnGeneralComment(ReviewDataInfoAdapter reviewDataInfoAdapter, GeneralComment comment);
+
+	void focusOnGeneralCommentReply(ReviewDataInfoAdapter reviewDataInfoAdapter, GeneralComment comment);
 }
---
+++ src/main/java/com/atlassian/theplugin/idea/crucible/events/FocusOnGeneralCommentEvent.java
@@ -0,0 +1,27 @@
+package com.atlassian.theplugin.idea.crucible.events;
+
+import com.atlassian.theplugin.idea.crucible.comments.CrucibleReviewActionListener;
+import com.atlassian.theplugin.idea.crucible.ReviewDataInfoAdapter;
+import com.atlassian.theplugin.commons.crucible.api.model.GeneralComment;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lguminski
+ * Date: Jun 18, 2008
+ * Time: 10:48:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FocusOnGeneralCommentEvent extends CrucibleEvent {
+	private ReviewDataInfoAdapter reviewDataInfoAdapter;
+	private GeneralComment comment;
+
+	public FocusOnGeneralCommentEvent(CrucibleReviewActionListener caller, ReviewDataInfoAdapter reviewDataInfoAdapter, GeneralComment generalComment) {
+		super(caller);
+		this.reviewDataInfoAdapter = reviewDataInfoAdapter;
+		this.comment = generalComment;
+	}
+
+	protected void notify(CrucibleReviewActionListener listener) {
+		listener.focusOnGeneralComment(reviewDataInfoAdapter, comment);
+	}
+}
