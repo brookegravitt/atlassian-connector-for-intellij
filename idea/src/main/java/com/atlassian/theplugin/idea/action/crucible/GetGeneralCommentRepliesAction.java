a04740ccb4f34cd78fe587e804008002208672ac
lguminski <lguminski@localhost>
2008-06-20 13:22:21 +0000

    PL-282 action handlers added and interface improved

---
+++ idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/GetGeneralCommentRepliesAction.java
@@ -0,0 +1,28 @@
+package com.atlassian.theplugin.idea.action.crucible;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.atlassian.theplugin.commons.crucible.api.model.GeneralComment;
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.idea.crucible.events.FocusOnGeneralCommentReplyEvent;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lguminski
+ * Date: Jun 20, 2008
+ * Time: 2:42:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GetGeneralCommentRepliesAction extends TableSelectedAction {
+
+	protected void itemSelected(Object row) {
+//		GeneralComment selectedComment = (GeneralComment) row;
+//			IdeaHelper.getCurrentReviewActionEventBroker().trigger(
+//					new FocusOnGeneralCommentReplyEvent(
+//							this,
+//							reviewDataInfoAdapter,
+//							selectedComment
+//					)
+//			);
+	}
+}
