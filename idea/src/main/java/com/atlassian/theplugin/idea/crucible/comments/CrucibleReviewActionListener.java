0e6bc337a40129a4fc084d0aa1170f8efc35c6ac
lguminski <lguminski@localhost>
2008-06-24 09:09:42 +0000

    PL-282 some types of crucible events introduced "FocusOn" and "Show"

--- idea/src/main/java/com/atlassian/theplugin/idea/crucible/comments/CrucibleReviewActionListener.java
+++ idea/src/main/java/com/atlassian/theplugin/idea/crucible/comments/CrucibleReviewActionListener.java
@@ -16,17 +16,9 @@ import java.util.Collection;
  */
 public interface CrucibleReviewActionListener {
 	public static final CrucibleReviewActionListener I_WANT_THIS_MESSAGE_BACK = null;
-	/**
-	 * A method ivoked by a background thread when a new review needs to be focused
-	 * @param reviewDataInfoAdapter
-	 */
+
 	void focusOnReview(ReviewDataInfoAdapter reviewDataInfoAdapter);
 
-	/**
-	 * A method ivoked by a background thread when a new file within a review needs to be focused
-	 * @param reviewDataInfoAdapter
-	 * @param reviewItem
-	 */
 	void focusOnFile(ReviewDataInfoAdapter reviewDataInfoAdapter, ReviewItem reviewItem);
 
 	void focusOnGeneralComment(ReviewDataInfoAdapter reviewDataInfoAdapter, GeneralComment comment);
@@ -37,4 +29,25 @@ public interface CrucibleReviewActionListener {
 								 Collection<VersionedComment> versionedComments, VersionedComment versionedComment);
 
 	void focusOnVersionedCommentReply(ReviewDataInfoAdapter reviewDataInfoAdapter, GeneralComment comment);
+	/**
+	 * A method ivoked by a background thread when a new review needs to be shown
+	 * @param reviewDataInfoAdapter
+	 */
+	void showReview(ReviewDataInfoAdapter reviewDataInfoAdapter);
+
+	/**
+	 * A method ivoked by a background thread when a new file within a review needs to be shown
+	 * @param reviewDataInfoAdapter
+	 * @param reviewItem
+	 */
+	void showReviewedFileItem(ReviewDataInfoAdapter reviewDataInfoAdapter, ReviewItem reviewItem);
+
+	void showGeneralComment(ReviewDataInfoAdapter reviewDataInfoAdapter, GeneralComment comment);
+
+	void showGeneralCommentReply(ReviewDataInfoAdapter reviewDataInfoAdapter, GeneralComment comment);
+
+	void showVersionedComment(ReviewDataInfoAdapter reviewDataInfoAdapter, ReviewItem reviewItem,
+								 Collection<VersionedComment> versionedComments, VersionedComment versionedComment);
+
+	void showVersionedCommentReply(ReviewDataInfoAdapter reviewDataInfoAdapter, GeneralComment comment);
 }
