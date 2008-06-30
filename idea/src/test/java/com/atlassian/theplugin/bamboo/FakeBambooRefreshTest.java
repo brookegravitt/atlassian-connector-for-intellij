b431169b5d0bb6595dace5ae82ac16d7199e0391
pmaruszak <pmaruszak@localhost>
2008-06-30 10:59:51 +0000

    PL-399, added some debug informatiohn to Bamboo and Crucible Checkers

---
+++ idea/src/test/java/com/atlassian/theplugin/bamboo/FakeBambooRefreshTest.java
@@ -0,0 +1,18 @@
+package com.atlassian.theplugin.bamboo;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Jun 27, 2008
+ * Time: 8:55:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FakeBambooRefreshTest extends TestCase {
+	public void testRefreshButton(){
+		fail();
+		//assertTrue(true);
+		
+	}
+}
