67643be3e3a76d0c227d89bff533e4296f90a3b5
jjaroczynski <jjaroczynski@localhost>
2008-06-30 15:15:56 +0000

    PLE-21 bamboo plans are listed and marked according to configuration. saving not working yet

--- ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/preferences/PreferencePageServers.java
+++ ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/preferences/PreferencePageServers.java
@@ -17,9 +17,31 @@
 package com.atlassian.theplugin.eclipse.preferences;
 
 
-import org.eclipse.jface.preference.*;
-import org.eclipse.ui.IWorkbenchPreferencePage;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.Preferences.IPropertyChangeListener;
+import org.eclipse.core.runtime.Preferences.PropertyChangeEvent;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import com.atlassian.theplugin.commons.ServerType;
+import com.atlassian.theplugin.commons.bamboo.BambooBuild;
+import com.atlassian.theplugin.commons.bamboo.BambooPlan;
+import com.atlassian.theplugin.commons.bamboo.BambooServerFacade;
+import com.atlassian.theplugin.commons.bamboo.BambooServerFacadeImpl;
+import com.atlassian.theplugin.commons.configuration.ServerBean;
+import com.atlassian.theplugin.commons.exception.ServerPasswordNotProvidedException;
+import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
+import com.atlassian.theplugin.eclipse.EclipseActionScheduler;
+import com.atlassian.theplugin.eclipse.util.PluginUtil;
 
 /**
  * This class represents a preference page that
@@ -61,8 +83,60 @@ public class PreferencePageServers
 		builds.getLabelControl(getFieldEditorParent()).setToolTipText("Space separated build names");
 		addField(builds);
 		
-		addField(new MyTableFieldEditor("BUILDSXXX", "Builds:", getFieldEditorParent()));
+		final MyTableFieldEditor myTableFieldEditor = new MyTableFieldEditor("BUILDSXXX", "Builds:", getFieldEditorParent());
+		addField(myTableFieldEditor);
+//		Activator.getDefault().getPluginPreferences().addPropertyChangeListener(new IPropertyChangeListener() {
+//			public void propertyChange(PropertyChangeEvent event) {
+//				if (event.getProperty().equals(PreferenceConstants.BAMBOO_URL)
+//			}
+//		});
 		
+		// TODO add refresh button to retrieve plan keys manually (e.g. in case server has changed)
+		Job planListJob = new Job("Atlassian Bamboo") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+
+				BambooServerFacade bambooFacade = BambooServerFacadeImpl.getInstance(PluginUtil.getLogger());
+				
+				Collection<ServerBean> servers = Activator.getDefault().getPluginConfiguration().getBambooConfigurationData().getServersData();
+				Iterator<ServerBean> iterator = servers.iterator();
+				
+				// we take only first server right now
+				if (iterator.hasNext()) {
+					Collection<BambooPlan> plans = new ArrayList<BambooPlan>(0);
+					try {
+						plans = bambooFacade.getPlanList(iterator.next());
+					} catch (ServerPasswordNotProvidedException e1) {
+						e1.printStackTrace();
+					} catch (RemoteApiException e1) {
+						e1.printStackTrace();
+					}
+				
+					StringBuffer plansString = new StringBuffer();
+					for (BambooPlan plan : plans) {
+						plansString.append(plan.getPlanKey());
+						plansString.append(" ");
+					}
+					//System.out.println(plansString.toString());
+					// myTableFieldEditor.setStringValue(plans.toString());
+					
+					final Collection<BambooPlan> allPlans = plans;
+					
+					EclipseActionScheduler.getInstance().invokeLater(new Runnable() {
+						public void run() {
+							myTableFieldEditor.setPlans(allPlans);
+						}
+					});
+					
+				}
+				
+			
+				return Status.OK_STATUS;
+			}
+		};
+		
+		planListJob.schedule();
 	}
 
 	/* (non-Javadoc)
