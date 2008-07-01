a06827bf4a1bc6466aa5626ff64d06d4fb8e14eb
jgorycki <jgorycki@localhost>
2008-07-01 11:38:49 +0000

    PL-432 - adjusted to Pawel's liking (mostly :))

---
+++ idea/src/main/java/com/atlassian/theplugin/idea/action/jira/editor/EditorAnalyseStackTraceAction.java
@@ -0,0 +1,24 @@
+package com.atlassian.theplugin.idea.action.jira.editor;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.atlassian.theplugin.idea.jira.editor.ThePluginJIRAEditorComponent;
+
+public class EditorAnalyseStackTraceAction extends AnAction {
+	public void actionPerformed(AnActionEvent event) {
+		ThePluginJIRAEditorComponent.JIRAFileEditor editor =
+				ThePluginJIRAEditorComponent.getEditorByKey(event.getPlace());
+		if (editor != null) {
+			editor.analyzeDescriptionStackTrace();
+		}
+	}
+
+	public void update(AnActionEvent event) {
+		ThePluginJIRAEditorComponent.JIRAFileEditor editor =
+				ThePluginJIRAEditorComponent.getEditorByKey(event.getPlace());
+		if (editor != null) {
+			event.getPresentation().setEnabled(editor.hasStackTraceInDescription());
+		}
+		super.update(event);
+	}
+}
