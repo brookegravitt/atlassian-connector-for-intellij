e290c11ee8e8979c527822b817786bc050c9faf2
lguminski <lguminski@localhost>
2008-07-03 00:08:36 +0000

    PL-326 ui refined

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/ui/CollapsiblePanel.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/ui/CollapsiblePanel.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,8 @@
 
 package com.atlassian.theplugin.idea.ui;
 
-import com.intellij.util.ui.UIUtil;
 import com.intellij.openapi.util.IconLoader;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.ActionGroup;
-import com.intellij.openapi.actionSystem.ActionToolbar;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NonNls;
 
 import javax.swing.*;
@@ -30,318 +27,321 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class CollapsiblePanel extends JPanel {
-  private JButton myToggleCollapseButton;
-  private JComponent myContent;
-  private boolean myIsCollapsed;
-  private final Collection<CollapsingListener> myListeners = new ArrayList<CollapsingListener>();
-  private boolean myIsInitialized = false;
-  private  Icon myExpandIcon;
-  private  Icon myCollapseIcon;
-  private JLabel myTitleLabel;
-  private JPanel contentPanel;
-  private JPanel labelPanel;
+	private JButton myToggleCollapseButton;
+	private JComponent myContent;
+	private boolean myIsCollapsed;
+	private final Collection<CollapsingListener> myListeners = new ArrayList<CollapsingListener>();
+	private boolean myIsInitialized = false;
+	private Icon myExpandIcon;
+	private Icon myCollapseIcon;
+	private JLabel myTitleLabel;
+	private JPanel contentPanel;
+	private JPanel labelPanel;
 
-  public static final KeyStroke LEFT_KEY_STROKE = KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0);
-  public static final KeyStroke RIGHT_KEY_STROKE = KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0);
-  @NonNls
-  public static final String EXPAND = "expand";
-  @NonNls public static final String COLLAPSE = "collapse";
-
-
-  public CollapsiblePanel(boolean collapseButtonAtLeft,
-                          boolean isCollapsed, Icon collapseIcon, Icon expandIcon,
-                          String title){
-	  super(new GridBagLayout());
-	  setupComponents(expandIcon, collapseIcon, title, collapseButtonAtLeft, isCollapsed);
-  }
-
-  public CollapsiblePanel(JComponent content, boolean collapseButtonAtLeft,
-                          boolean isCollapsed, Icon collapseIcon, Icon expandIcon,
-                          String title) {
-    super(new GridBagLayout());
-	setupComponents(expandIcon, collapseIcon, title, collapseButtonAtLeft, isCollapsed);
-	setContent(content);
-  }
+	public static final KeyStroke LEFT_KEY_STROKE = KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0);
+	public static final KeyStroke RIGHT_KEY_STROKE = KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0);
+	@NonNls
+	public static final String EXPAND = "expand";
+	@NonNls
+	public static final String COLLAPSE = "collapse";
 
 
-  public CollapsiblePanel(boolean collapseButtonAtLeft,
-                          boolean isCollapsed, String title) {
-    super(new GridBagLayout());
-	Icon collapseIcon = IconLoader.findIcon("/icons/navigate_down_10.gif");
-	Icon expandIcon = IconLoader.findIcon("/icons/navigate_right_10.gif");
-	setupComponents(expandIcon, collapseIcon, title, collapseButtonAtLeft, isCollapsed);
-  }
+	public CollapsiblePanel(boolean collapseButtonAtLeft,
+							boolean isCollapsed, Icon collapseIcon, Icon expandIcon,
+							String title) {
+		super(new GridBagLayout());
+		setupComponents(expandIcon, collapseIcon, title, collapseButtonAtLeft, isCollapsed);
+	}
 
-  public void setTitle(String title){
-	  myTitleLabel.setText(title);
-  }
-
-
-  private Dimension getButtonDimension() {
-    if (myExpandIcon == null) {
-      return new Dimension(9, 9);
-    } else {
-      return new Dimension((myExpandIcon.getIconWidth() > myCollapseIcon.getIconWidth() ? myExpandIcon.getIconWidth() : myCollapseIcon.getIconWidth()),
-			  				myExpandIcon.getIconHeight() > myCollapseIcon.getIconHeight() ? myExpandIcon.getIconHeight() : myCollapseIcon.getIconHeight());
-    }
-  }
-
-  public CollapsiblePanel(JComponent content, boolean collapseButtonAtLeft) {
-    this(content, collapseButtonAtLeft, false, null, null, null);
-  }
-
-  protected void setCollapsed(boolean collapse) {
-    try {
-
-	  if (collapse) {
-        if (myIsInitialized) {
-			remove(contentPanel);
+	public CollapsiblePanel(JComponent content, boolean collapseButtonAtLeft,
+							boolean isCollapsed, Icon collapseIcon, Icon expandIcon,
+							String title) {
+		super(new GridBagLayout());
+		setupComponents(expandIcon, collapseIcon, title, collapseButtonAtLeft, isCollapsed);
+		setContent(content);
+	}
 
 
-		  if (contentPanel != null) {
-			  contentPanel.setVisible(false);
-			  myContent.setVisible(false);
-		  }
-		  labelPanel.requestFocusInWindow();
-		  }
+	public CollapsiblePanel(boolean collapseButtonAtLeft,
+							boolean isCollapsed, String title) {
+		super(new GridBagLayout());
+		Icon collapseIcon = IconLoader.findIcon("/icons/navigate_down_10.gif");
+		Icon expandIcon = IconLoader.findIcon("/icons/navigate_right_10.gif");
+		setupComponents(expandIcon, collapseIcon, title, collapseButtonAtLeft, isCollapsed);
+	}
 
-	  } else {
-	  	if (myContent != null) {
-			add(contentPanel,
-				new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-									   new Insets(3, 0, 0, 0), 0, 0));
-			  contentPanel.setVisible(true);
-			  myContent.setVisible(true);
-
-		  }
-
-  	  }
-	  myIsCollapsed = collapse;
+	public void setTitle(String title) {
+		myTitleLabel.setText(title);
+	}
 
 
-      revalidate();
-	  repaint();
+	private Dimension getButtonDimension() {
+		if (myExpandIcon == null) {
+			return new Dimension(9, 9);
+		} else {
+			return new Dimension((myExpandIcon.getIconWidth() > myCollapseIcon.getIconWidth() ? myExpandIcon.getIconWidth() : myCollapseIcon.getIconWidth()),
+					myExpandIcon.getIconHeight() > myCollapseIcon.getIconHeight() ? myExpandIcon.getIconHeight() : myCollapseIcon.getIconHeight());
+		}
+	}
 
-	  Icon icon = getIcon();
-      if (icon != null) {
+	public CollapsiblePanel(JComponent content, boolean collapseButtonAtLeft) {
+		this(content, collapseButtonAtLeft, false, null, null, null);
+	}
 
-		myToggleCollapseButton.setIcon(icon);
-        myToggleCollapseButton.setBorder(null);
-        myToggleCollapseButton.setBorderPainted(false);
-        myToggleCollapseButton.setToolTipText(getToggleButtonToolTipText());
-	  }
+	protected void setCollapsed(boolean collapse) {
+		try {
 
-
-	  if (collapse) {
-        setFocused(true);
-        setSelected(true);
-
-	  } else if (myContent != null) {
-        myContent.requestFocusInWindow();
-      }
-
+			if (collapse) {
+				if (myIsInitialized) {
+					remove(contentPanel);
 
 
-	  notifyListners();
-
-      revalidate();
-      repaint();
-	} finally {
-      myIsInitialized = true;
-    }
-  }
-
-  private String getToggleButtonToolTipText() {
-    if (myIsCollapsed) {
-      return "Collapsed";
-    } else {
-      return "Expanded";
-    }
-  }
-
-  private Icon getIcon() {
-    if (myIsCollapsed) {
-      return myExpandIcon;
-    } else {
-      return myCollapseIcon;
-    }
-  }
-
-  private void notifyListners() {
-    CollapsingListener[] listeners = myListeners.toArray(new CollapsingListener[myListeners.size()]);
-    for (CollapsingListener listener : listeners) {
-      listener.onCollapsingChanged(this, isCollapsed());
-    }
-  }
-
-  public void setContent(JComponent content){
-	  setBackground(content.getBackground());
-	  this.myContent = content;
-	  contentPanel.setBackground(content.getBackground());
-	  contentPanel.add(content, BorderLayout.CENTER);
-  }
-
- private void setupComponents(Icon expandIcon, Icon collapseIcon,
-							   String title, boolean collapseButtonAtLeft,
-  							   boolean isCollapsed){
-
-	contentPanel = new JPanel(new BorderLayout());
-
-	this.myToggleCollapseButton = new JButton();
-	this.myExpandIcon = expandIcon;
-	this.myCollapseIcon = collapseIcon;
-
-	final Dimension buttonDimension = getButtonDimension();
-
-
-
-	myToggleCollapseButton.setOpaque(false);
-    myToggleCollapseButton.setBorderPainted(false);
-
-	myToggleCollapseButton.setSize(buttonDimension);
-    myToggleCollapseButton.setPreferredSize(buttonDimension);
-    myToggleCollapseButton.setMinimumSize(buttonDimension);
-    myToggleCollapseButton.setMaximumSize(buttonDimension);
-
-    myToggleCollapseButton.setFocusable(true);
-
-
-	myToggleCollapseButton.getActionMap().put(COLLAPSE, new AbstractAction() {
-      public void actionPerformed(ActionEvent e) {
-        collapse();
-      }
-    });
-
-    myToggleCollapseButton.getActionMap().put(EXPAND, new AbstractAction() {
-      public void actionPerformed(ActionEvent e) {
-        expand();
-      }
-    });
-
-    myToggleCollapseButton.getInputMap().put(LEFT_KEY_STROKE, COLLAPSE);
-    myToggleCollapseButton.getInputMap().put(RIGHT_KEY_STROKE, EXPAND);
-
-	 myToggleCollapseButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        setCollapsed(!myIsCollapsed);
-      }
-    });
-
-    final int iconAnchor = collapseButtonAtLeft ? GridBagConstraints.WEST : GridBagConstraints.EAST;
-	labelPanel = new JPanel(new BorderLayout(20,0));
-
-	labelPanel.add(myToggleCollapseButton, BorderLayout.LINE_START);
-	labelPanel.setBackground(UIUtil.getTableSelectionBackground());
-    if (title != null) {
-
-	  myTitleLabel = new JLabel(title);
-	  myTitleLabel.setFont(UIUtil.getLabelFont().deriveFont(Font.BOLD));
-
-	  labelPanel.add(myTitleLabel, BorderLayout.CENTER);
-	  revalidate();
-	  repaint();
-
-		myTitleLabel.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) { // on double click, just open the issue
-				if (myIsCollapsed) {
-					expand();
-				} else {
-					collapse();
+					if (contentPanel != null) {
+						contentPanel.setVisible(false);
+						myContent.setVisible(false);
+					}
+					labelPanel.requestFocusInWindow();
 				}
+
+			} else {
+				if (myContent != null) {
+					add(contentPanel,
+							new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+									new Insets(3, 0, 0, 0), 0, 0));
+					contentPanel.setVisible(true);
+					myContent.setVisible(true);
+
+				}
+
+			}
+			myIsCollapsed = collapse;
+
+
+			revalidate();
+			repaint();
+
+			Icon icon = getIcon();
+			if (icon != null) {
+
+				myToggleCollapseButton.setIcon(icon);
+				myToggleCollapseButton.setBorder(null);
+				myToggleCollapseButton.setBorderPainted(false);
+				myToggleCollapseButton.setToolTipText(getToggleButtonToolTipText());
+			}
+
+
+			if (collapse) {
+				setFocused(true);
+				setSelected(true);
+
+			} else if (myContent != null) {
+				myContent.requestFocusInWindow();
+			}
+
+
+			notifyListners();
+
+			revalidate();
+			repaint();
+		} finally {
+			myIsInitialized = true;
+		}
+	}
+
+	private String getToggleButtonToolTipText() {
+		if (myIsCollapsed) {
+			return "Collapsed";
+		} else {
+			return "Expanded";
+		}
+	}
+
+	private Icon getIcon() {
+		if (myIsCollapsed) {
+			return myExpandIcon;
+		} else {
+			return myCollapseIcon;
+		}
+	}
+
+	private void notifyListners() {
+		CollapsingListener[] listeners = myListeners.toArray(new CollapsingListener[myListeners.size()]);
+		for (CollapsingListener listener : listeners) {
+			listener.onCollapsingChanged(this, isCollapsed());
+		}
+	}
+
+	public JComponent getContent() {
+		return myContent;
+	}
+
+	public void setContent(JComponent content) {
+		setBackground(content.getBackground());
+		this.myContent = content;
+		contentPanel.setBackground(content.getBackground());
+		contentPanel.add(content, BorderLayout.CENTER);
+	}
+
+	private void setupComponents(Icon expandIcon, Icon collapseIcon,
+								 String title, boolean collapseButtonAtLeft,
+								 boolean isCollapsed) {
+
+		contentPanel = new JPanel(new BorderLayout());
+
+		this.myToggleCollapseButton = new JButton();
+		this.myExpandIcon = expandIcon;
+		this.myCollapseIcon = collapseIcon;
+
+		final Dimension buttonDimension = getButtonDimension();
+
+
+		myToggleCollapseButton.setOpaque(false);
+		myToggleCollapseButton.setBorderPainted(false);
+
+		myToggleCollapseButton.setSize(buttonDimension);
+		myToggleCollapseButton.setPreferredSize(buttonDimension);
+		myToggleCollapseButton.setMinimumSize(buttonDimension);
+		myToggleCollapseButton.setMaximumSize(buttonDimension);
+
+		myToggleCollapseButton.setFocusable(true);
+
+
+		myToggleCollapseButton.getActionMap().put(COLLAPSE, new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				collapse();
 			}
 		});
+
+		myToggleCollapseButton.getActionMap().put(EXPAND, new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				expand();
+			}
+		});
+
+		myToggleCollapseButton.getInputMap().put(LEFT_KEY_STROKE, COLLAPSE);
+		myToggleCollapseButton.getInputMap().put(RIGHT_KEY_STROKE, EXPAND);
+
+		myToggleCollapseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setCollapsed(!myIsCollapsed);
+			}
+		});
+
+		final int iconAnchor = collapseButtonAtLeft ? GridBagConstraints.WEST : GridBagConstraints.EAST;
+		labelPanel = new JPanel(new BorderLayout(20, 0));
+
+		labelPanel.add(myToggleCollapseButton, BorderLayout.LINE_START);
+		labelPanel.setBackground(UIUtil.getTableSelectionBackground());
+		if (title != null) {
+
+			myTitleLabel = new JLabel(title);
+			myTitleLabel.setFont(UIUtil.getLabelFont().deriveFont(Font.BOLD));
+
+			labelPanel.add(myTitleLabel, BorderLayout.CENTER);
+			revalidate();
+			repaint();
+
+			myTitleLabel.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent e) { // on double click, just open the issue
+					if (myIsCollapsed) {
+						expand();
+					} else {
+						collapse();
+					}
+				}
+			});
+
+		}
+
+
+		add(labelPanel,
+				new GridBagConstraints(0, 0, 1, 1, 1, 0.0,
+						iconAnchor,
+						GridBagConstraints.HORIZONTAL,
+						new Insets(1, collapseButtonAtLeft ? 0 : 1, 0, collapseButtonAtLeft ? 1 : 0), 0,
+						0));
+
+		myIsCollapsed = isCollapsed;
+		setCollapsed(isCollapsed);
 
 	}
 
 
-	add(labelPanel,
-        new GridBagConstraints(0, 0, 1, 1, 1, 0.0,
-                               iconAnchor,
-                               GridBagConstraints.HORIZONTAL,
-                               new Insets(1, collapseButtonAtLeft ? 0 : 1, 0, collapseButtonAtLeft ? 1 : 0), 0,
-                               0));
+	public void addCollapsingListener(CollapsingListener listener) {
+		myListeners.add(listener);
+	}
 
-	myIsCollapsed = isCollapsed;
-	setCollapsed(isCollapsed);
+	public void removeCollapsingListener(CollapsingListener listener) {
+		myListeners.remove(listener);
+	}
 
-  }
+	public boolean isCollapsed() {
+		return myIsCollapsed;
+	}
 
+	public void expand() {
+		if (myIsCollapsed) {
+			setCollapsed(false);
+		}
+	}
 
-  public void addCollapsingListener(CollapsingListener listener) {
-    myListeners.add(listener);
-  }
+	public void collapse() {
+		if (!myIsCollapsed) {
+			setCollapsed(true);
+		}
+	}
 
-  public void removeCollapsingListener(CollapsingListener listener) {
-    myListeners.remove(listener);
-  }
+	public void setFocused(boolean focused) {
+		myToggleCollapseButton.requestFocusInWindow();
+	}
 
-  public boolean isCollapsed() {
-    return myIsCollapsed;
-  }
+	public void setSelected(boolean selected) {
+		myToggleCollapseButton.setSelected(selected);
+	}
 
-  public void expand() {
-    if (myIsCollapsed) {
-      setCollapsed(false);
-    }
-  }
+	public ActionMap getCollapsibleActionMap() {
+		return myToggleCollapseButton.getActionMap();
+	}
 
-  public void collapse() {
-    if (!myIsCollapsed) {
-      setCollapsed(true);
-    }
-  }
+	public InputMap getCollapsibleInputMap() {
+		return myToggleCollapseButton.getInputMap();
+	}
 
-  public void setFocused(boolean focused) {
-    myToggleCollapseButton.requestFocusInWindow();
-  }
+	protected void paintComponent(Graphics g) {
+		updatePanel();
+		super.paintComponent(g);
+	}
 
-  public void setSelected(boolean selected) {
-	myToggleCollapseButton.setSelected(selected);
-  }
+	private void updatePanel() {
+		setBackground(UIUtil.getTableSelectionBackground());
+		//contentPanel.setPreferredSize(getCustomPreferredSize());
 
-  public ActionMap getCollapsibleActionMap() {
-    return myToggleCollapseButton.getActionMap();
-  }
+	}
 
-  public InputMap getCollapsibleInputMap() {
-    return myToggleCollapseButton.getInputMap();
-  }
-
-  protected void paintComponent(Graphics g) {
-    updatePanel();
-    super.paintComponent(g);
-  }
-
-  private void updatePanel() {
-      setBackground(UIUtil.getTableSelectionBackground());
-	  //contentPanel.setPreferredSize(getCustomPreferredSize());
-	  
-  }
-
-  protected void paintChildren(Graphics g) {
-    if (myTitleLabel != null) {
-      updateTitle();
-    }
+	protected void paintChildren(Graphics g) {
+		if (myTitleLabel != null) {
+			updateTitle();
+		}
 
 
-    updateToggleButton();
+		updateToggleButton();
 
-    super.paintChildren(g);
-  }
+		super.paintChildren(g);
+	}
 
-  private void updateToggleButton() {
-      myToggleCollapseButton.setBackground(UIUtil.getTableSelectionBackground());
+	private void updateToggleButton() {
+		myToggleCollapseButton.setBackground(UIUtil.getTableSelectionBackground());
 
-  }
+	}
 
-  private void updateTitle() {
+	private void updateTitle() {
 
-      myTitleLabel.setForeground(UIUtil.getTableSelectionForeground());
-      myTitleLabel.setBackground(UIUtil.getTableSelectionBackground());
-  }
+		myTitleLabel.setForeground(UIUtil.getTableSelectionForeground());
+		myTitleLabel.setBackground(UIUtil.getTableSelectionBackground());
+	}
 
-  private boolean paintAsSelected() {
-    return myToggleCollapseButton.hasFocus() && isCollapsed();
-  }
+	private boolean paintAsSelected() {
+		return myToggleCollapseButton.hasFocus() && isCollapsed();
+	}
 
 }
