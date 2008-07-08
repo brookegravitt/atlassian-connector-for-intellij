9bfed01c6c45536d7931b585f23cad105d986d32
jgorycki <jgorycki@localhost>
2008-07-08 15:20:45 +0000

    PL-14 - second approximation

---
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/bamboo/ExpandTestsAction.java
@@ -0,0 +1,12 @@
+package com.atlassian.theplugin.idea.action.bamboo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.atlassian.theplugin.idea.bamboo.TestResultsToolWindow;
+
+public class ExpandTestsAction extends AnAction {
+	public void actionPerformed(AnActionEvent event) {
+		TestResultsToolWindow.Expandable exp = TestResultsToolWindow.getExpandable(event.getPlace());
+		exp.expand();
+	}
+}
