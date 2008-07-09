cc00d2cc392537df712b739d6007dd9fa6dad47b
jgorycki <jgorycki@localhost>
2008-07-09 13:04:57 +0000

    PL-14 - done - w00t!

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/bamboo/ToggleAllTestsAction.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/bamboo/ToggleAllTestsAction.java
@@ -1,12 +1,20 @@
 package com.atlassian.theplugin.idea.action.bamboo;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
 import com.atlassian.theplugin.idea.bamboo.TestResultsToolWindow;
 
-public class ToggleAllTestsAction extends AnAction {
-    public void actionPerformed(AnActionEvent event) {
-        TestResultsToolWindow.TestTree exp = TestResultsToolWindow.getTestTree(event.getPlace());
-        exp.toggleAll();
-    }
+public class ToggleAllTestsAction extends ToggleAction {
+	public boolean isSelected(AnActionEvent event) {
+		TestResultsToolWindow.TestTree tree = TestResultsToolWindow.getTestTree(event.getPlace());
+		if (tree == null) {
+			return !TestResultsToolWindow.TestTree.PASSED_TESTS_VISIBLE_DEFAULT;
+		}
+		return !tree.isPassedTestsVisible();
+	}
+
+	public void setSelected(AnActionEvent event, boolean b) {
+		TestResultsToolWindow.TestTree tree = TestResultsToolWindow.getTestTree(event.getPlace());
+		tree.setPassedTestsVisible(!b);
+	}
 }
