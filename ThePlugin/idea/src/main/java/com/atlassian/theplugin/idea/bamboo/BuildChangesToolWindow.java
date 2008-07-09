8ad4c309ba2095a4e31d5f00c238b678ddf6c7b3
jgorycki <jgorycki@localhost>
2008-07-09 19:55:18 +0000

    PL-17 - temporary commit, work in progress - this does not work yet

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/bamboo/BuildChangesToolWindow.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/bamboo/BuildChangesToolWindow.java
@@ -32,11 +32,10 @@ import com.atlassian.theplugin.idea.Constants;
 import javax.swing.*;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeModelListener;
 import javax.swing.tree.*;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.LinkedHashMap;
-import java.util.HashMap;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.*;
@@ -95,15 +94,6 @@ public final class BuildChangesToolWindow {
 		public CommitDetailsPanel(String name, final List<Commit> commits) {
 			super();
 			setLayout(new GridBagLayout());
-//			GridBagConstraints gbc = new GridBagConstraints();
-//			gbc.gridx = 0;
-//			gbc.gridy = 0;
-//			gbc.weightx = 1.0;
-//			gbc.weighty = 1.0;
-//			gbc.fill = GridBagConstraints.BOTH;
-
-
-
 
 			Splitter split = new Splitter(false, SPLIT_RATIO);
 			split.setShowDividerControls(true);
@@ -178,12 +168,111 @@ public final class BuildChangesToolWindow {
 			gbc1.weighty = 1.0;
 			gbc1.fill = GridBagConstraints.BOTH;
 
-			consolePanel.add(new JPanel(), gbc1);
+			consolePanel.add(new JScrollPane(createFileTree(commits.get(0).getFiles())), gbc1);
 
 			split.setSecondComponent(consolePanel);
 
 			add(split, gbc);
 		}
 
-	}
+        private class FileTreeModel implements TreeModel {
+
+            private class FileNode extends DefaultMutableTreeNode {
+                public Map<String, FileNode> children;
+
+                public FileNode(String fullName) {
+                    super(fullName);
+                    children = new HashMap<String, FileNode>();
+                }
+
+                public void addChild(String fullName, FileNode child) {
+                    if (!children.containsKey(fullName)) {
+                        children.put(fullName, child);
+                        add(child);
+                    }
+                }
+
+                public boolean hasNode(String fullName) {
+                    return children.containsKey(fullName);
+                }
+
+                public FileNode getNode(String fullName) {
+                    return children.get(fullName);
+                }
+
+                public String toString() {
+                    String s = super.toString();
+                    int idx = s.lastIndexOf('/');
+                    if (idx == -1) {
+                        return s;
+                    }
+                    return s.substring(idx + 1);
+                }
+            }
+
+            private FileNode root;
+
+            public FileTreeModel(List<CommitFile> files) {
+                root = new FileNode("/");
+                for (CommitFile f : files) {
+                    addFile(f);
+                }
+            }
+
+            public void addFile(CommitFile file) {
+                int idx = 1;
+                String fileName = file.getFileName();
+                FileNode node = root;
+                do {
+                    int newIdx = file.getFileName().indexOf('/', idx);
+                    if (newIdx != -1) {
+                        String newNodeName = fileName.substring(1, newIdx);
+                        if (!node.hasNode(newNodeName)) {
+                            FileNode newNode = new FileNode(fileName.substring(1, newIdx));
+                            node.addChild(fileName.substring(1, newIdx), newNode);
+                            node = newNode;
+                        } else {
+                            node = node.getNode(newNodeName);
+                        }
+                    }
+                    idx = newIdx + 1;
+                } while (idx > 0);
+            }
+
+            public Object getRoot() {
+                return root;
+            }
+
+            public Object getChild(Object parent, int index) {
+                return ((FileNode) parent).getChildAt(index);
+            }
+
+            public int getChildCount(Object parent) {
+                return ((FileNode) parent).children.size();
+            }
+
+            public boolean isLeaf(Object node) {
+                return ((FileNode) node).children.size() == 0;
+            }
+
+            public void valueForPathChanged(TreePath path, Object newValue) {
+            }
+
+            public int getIndexOfChild(Object parent, Object child) {
+                return ((FileNode) parent).getIndex((FileNode) child);
+            }
+
+            public void addTreeModelListener(TreeModelListener l) {
+            }
+
+            public void removeTreeModelListener(TreeModelListener l) {
+            }
+        }
+
+        private JTree createFileTree(List<CommitFile> files) {
+            JTree tree = new JTree(new FileTreeModel(files));
+            tree.setRootVisible(false);
+            return tree;
+        }
+    }
 }
