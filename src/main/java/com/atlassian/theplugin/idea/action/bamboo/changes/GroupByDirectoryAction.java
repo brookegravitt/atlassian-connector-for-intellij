eccef481c4c085322c76cd9966ab7785cc59b9ad
jgorycki <jgorycki@localhost>
2008-07-11 13:49:50 +0000

    PL-17 - flat tree view added

--- src/main/java/com/atlassian/theplugin/idea/action/bamboo/changes/GroupByDirectoryAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/bamboo/changes/GroupByDirectoryAction.java
@@ -1,14 +1,20 @@
 package com.atlassian.theplugin.idea.action.bamboo.changes;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import com.atlassian.theplugin.idea.bamboo.BuildChangesToolWindow;
 
-public class GroupByDirectoryAction extends AnAction {
-	public void actionPerformed(AnActionEvent event) {
+public class GroupByDirectoryAction extends ToggleAction {
+	public boolean isSelected(AnActionEvent event) {
+		BuildChangesToolWindow.ChangesTree tree = BuildChangesToolWindow.getChangesTree(event.getPlace());
+		if (tree == null) {
+			return !BuildChangesToolWindow.ChangesTree.GROUP_BY_DIRECTORY_DEFAULT;
+		}
+		return tree.isGroupByDirectory();
 	}
 
-	public void update(AnActionEvent event) {
-		event.getPresentation().setEnabled(false);
-		super.update(event);
+	public void setSelected(AnActionEvent event, boolean b) {
+		BuildChangesToolWindow.ChangesTree tree = BuildChangesToolWindow.getChangesTree(event.getPlace());
+		tree.setGroupByDirectory(b);
 	}
 }
