29c7912ac18e97cd5adab8075dba7ddb23cb92a3
lguminski <lguminski@localhost>
2008-07-11 13:00:46 +0000

    PL-17 ui code refactoring

---
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/ui/tree/AtlassianTreeModel.java
@@ -0,0 +1,19 @@
+package com.atlassian.theplugin.idea.ui.tree;
+
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lguminski
+ * Date: Jul 11, 2008
+ * Time: 1:31:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AtlassianTreeModel extends DefaultTreeModel {
+
+	public AtlassianTreeModel(AtlassianTreeNode root) {
+		super(root);
+	}
+}
