334f66cc9697c2554e65ad6c6fe8d001779a88f4
jjaroczynski <jjaroczynski@localhost>
2008-07-16 12:54:14 +0000

    PLE-39 bamboo icon visible in status line

---
+++ ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/bamboo/BambooStatusBar.java
@@ -0,0 +1,56 @@
+package com.atlassian.theplugin.eclipse.view.bamboo;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.texteditor.StatusLineContributionItem;
+
+import com.atlassian.theplugin.commons.bamboo.BambooPopupInfo;
+import com.atlassian.theplugin.commons.bamboo.BambooStatusDisplay;
+import com.atlassian.theplugin.commons.bamboo.BuildStatus;
+import com.atlassian.theplugin.eclipse.preferences.Activator;
+import com.atlassian.theplugin.eclipse.util.PluginUtil;
+
+
+public class BambooStatusBar extends StatusLineContributionItem implements BambooStatusDisplay {
+	
+	public BambooStatusBar() {
+		super(Activator.PLUGIN_ID + ".statusline");
+
+		setImage(PluginUtil.getImageRegistry().get(PluginUtil.ICON_BAMBOO_UNKNOWN));
+		//setToolTipText("PAZU");
+		
+		setActionHandler(new Action() {
+			public void run() {
+				try {
+					Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().
+						showView(Activator.PLUGIN_ID + ".viewmain");
+				} catch (PartInitException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	@Override
+	public void updateBambooStatus(BuildStatus generalBuildStatus,
+			BambooPopupInfo info) {
+		switch(generalBuildStatus) {
+		case BUILD_FAILED:
+			setImage(PluginUtil.getImageRegistry().get(PluginUtil.ICON_BAMBOO_FAILED));
+			setToolTipText("Some builds failed. Click to see details.");
+			break;
+		case BUILD_SUCCEED:
+			setImage(PluginUtil.getImageRegistry().get(PluginUtil.ICON_BAMBOO_SUCCEEDED));
+			setToolTipText("All builds currently passing.");
+			break;
+		case UNKNOWN:
+		default:
+			setImage(PluginUtil.getImageRegistry().get(PluginUtil.ICON_BAMBOO_UNKNOWN));
+			setToolTipText("");
+			break;
+		}
+		
+		
+	}
+}
