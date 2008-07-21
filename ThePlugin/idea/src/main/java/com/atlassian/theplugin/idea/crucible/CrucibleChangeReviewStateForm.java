9ef65a36dfe0a15ef87ab3ac47ee0386bc4f2158
mwent <mwent@localhost>
2008-07-21 18:04:04 +0000

    PL-21 - Post-commit reviews in IDE

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleChangeReviewStateForm.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleChangeReviewStateForm.java
@@ -22,6 +22,7 @@ import com.atlassian.theplugin.commons.crucible.api.model.Action;
 import com.atlassian.theplugin.commons.crucible.api.model.Review;
 import com.atlassian.theplugin.commons.exception.ServerPasswordNotProvidedException;
 import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
+import com.atlassian.theplugin.idea.IdeaHelper;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import static com.intellij.openapi.ui.Messages.showMessageDialog;
@@ -100,12 +101,6 @@ public class CrucibleChangeReviewStateForm extends DialogWrapper {
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(2, 2, new Insets(1, 1, 1, 1), -1, -1));
         rootComponent.add(panel1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-        final JLabel label1 = new JLabel();
-        label1.setText("Server:");
-        panel1.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final JLabel label2 = new JLabel();
-        label2.setText("Repository:");
-        panel1.add(label2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
         rootComponent.add(spacer1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
     }
@@ -179,17 +174,17 @@ public class CrucibleChangeReviewStateForm extends DialogWrapper {
                     crucibleServerFacade.recoverReview(review.getServer(), review.getPermId());
                     break;
             }
-
+            IdeaHelper.getAppComponent().rescheduleStatusCheckers(true);
         } catch (RemoteApiException e) {
             showMessageDialog(e.getMessage(),
                     "Error changing review state: " + review.getServer().getUrlString(), Messages.getErrorIcon());
         } catch (ServerPasswordNotProvidedException e) {
             showMessageDialog(e.getMessage(), "Error changing review state: " + review.getServer().getUrlString(), Messages.getErrorIcon());
         }
+
         super.doOKAction();
     }
 
-    private void createUIComponents
-            () {
+    private void createUIComponents() {
     }
 }
