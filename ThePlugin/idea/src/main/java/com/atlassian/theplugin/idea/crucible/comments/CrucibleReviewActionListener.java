a0b3c5af94a316749fadefbb5263fbb1c4b07f2e
lguminski <lguminski@localhost>
2008-07-21 15:24:36 +0000

    PL-303 cleanup of crucible events

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/comments/CrucibleReviewActionListener.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/comments/CrucibleReviewActionListener.java
@@ -18,26 +18,6 @@ public class CrucibleReviewActionListener {
 	public static final CrucibleReviewActionListener I_WANT_THIS_MESSAGE_BACK = null;
 	public static final CrucibleReviewActionListener I_DONT_CARE = null;
 
-	public void focusOnReview(ReviewData reviewData) {
-	}
-
-	public void focusOnFile(ReviewData reviewData, CrucibleFileInfo reviewItem) {
-	}
-
-    //@todo wtf general comment
-    public void focusOnGeneralComment(ReviewData reviewData) {
-	}
-
-	public void focusOnGeneralCommentReply(ReviewData reviewData, GeneralComment comment) {
-	}
-
-	public void focusOnVersionedComment(ReviewData reviewData, CrucibleFileInfo reviewItem,
-								 Collection<VersionedComment> versionedComments, VersionedComment versionedComment) {
-	}
-
-	public void focusOnVersionedCommentReply(ReviewData reviewData, GeneralComment comment) {
-	}
-
 	/**
 	 * A method ivoked by a background thread when a new review needs to be shown
 	 *
@@ -46,25 +26,4 @@ public class CrucibleReviewActionListener {
 	public void showReview(ReviewData reviewData) {
 	}
 
-	/**
-	 * A method ivoked by a background thread when a new file within a review needs to be shown
-	 *
-	 * @param reviewData
-	 * @param reviewItem
-	 */
-	public void showReviewedFileItem(ReviewData reviewData, CrucibleFileInfo reviewItem) {
-	}
-
-	public void showGeneralComment(ReviewData reviewData, GeneralComment comment) {
-	}
-
-	public void showGeneralCommentReply(ReviewData reviewData, GeneralComment comment) {
-	}
-
-	public void showVersionedComment(ReviewData reviewData, CrucibleFileInfo reviewItem,
-							  Collection<VersionedComment> versionedComments, VersionedComment versionedComment) {
-	}
-
-	public void showVersionedCommentReply(ReviewData reviewData, GeneralComment comment) {
-	}
 }
