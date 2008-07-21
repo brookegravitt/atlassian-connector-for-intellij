22c705deecf8cc42a1a6edf2f9b6b1a6de9403c9
lguminski <lguminski@localhost>
2008-07-21 10:30:54 +0000

    PL-303 ui refined

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/ui/tree/comment/FileNameNode.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/ui/tree/comment/FileNameNode.java
@@ -3,6 +3,11 @@ package com.atlassian.theplugin.idea.ui.tree.comment;
 import com.atlassian.theplugin.idea.ui.tree.AtlassianTreeNode;
 import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFileInfo;
 import com.atlassian.theplugin.util.CommentPanelBuilder;
+import com.intellij.ui.ColoredTreeCellRenderer;
+import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.ui.SimpleColoredComponent;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.CellConstraints;
 
 import javax.swing.tree.TreeCellRenderer;
 import javax.swing.*;
@@ -33,13 +38,27 @@ public class FileNameNode extends AtlassianTreeNode {
 	}
 
 	private static class MyRenderer implements TreeCellRenderer {
+
 		public Component getTreeCellRendererComponent(JTree tree, Object value, boolean isSelected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+			JPanel panel = new JPanel(new FormLayout("4dlu, left:pref:grow, 4dlu", "4dlu, pref:grow, 4dlu"));
+			SimpleColoredComponent component = new SimpleColoredComponent();
+//			component.setFont(component.getFont().deriveFont(component.getFont().getSize() + 1));
 			FileNameNode node = (FileNameNode) value;
-			JPanel panel = new JPanel(new BorderLayout());
-			panel.add(new JLabel(node.getFile().getFileDescriptor().getName()));
-			panel.setBackground(Color.white);
+			CrucibleFileInfo file = node.getFile();
+			component.append(file.getFileDescriptor().getUrl(), SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES);
+
+			StringBuilder txt = new StringBuilder();
+			txt.append(" (rev: ");
+			txt.append(file.getOldFileDescriptor().getRevision());
+			txt.append("-");
+			txt.append(file.getFileDescriptor().getRevision());
+			txt.append(")");
+			component.append(txt.toString(), SimpleTextAttributes.GRAY_ITALIC_ATTRIBUTES);
+			panel.add(component, new CellConstraints(2, 2));
+			
+
 			panel.setBorder(isSelected ? UIManager.getBorder("List.focusCellHighlightBorder")
-					: BorderFactory.createEmptyBorder(1,1,1,1));
+					: BorderFactory.createEmptyBorder(1, 1, 1, 1));
 			return panel;
 
 		}
