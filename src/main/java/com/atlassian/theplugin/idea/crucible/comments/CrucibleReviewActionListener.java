1409a1e4a385bcdbaa1f37021ac7462eb1490ae4
mwent <mwent@localhost>
2008-07-22 06:57:01 +0000

    PL-21 - checkstyle

--- src/main/java/com/atlassian/theplugin/idea/crucible/comments/CrucibleReviewActionListener.java
+++ src/main/java/com/atlassian/theplugin/idea/crucible/comments/CrucibleReviewActionListener.java
@@ -1,11 +1,7 @@
 package com.atlassian.theplugin.idea.crucible.comments;
 
-import com.atlassian.theplugin.commons.crucible.api.model.GeneralComment;
-import com.atlassian.theplugin.commons.crucible.api.model.VersionedComment;
 import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFileInfo;
 import com.atlassian.theplugin.idea.crucible.ReviewData;
-
-import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,20 +11,20 @@ import java.util.Collection;
  * To change this template use File | Settings | File Templates.
  */
 public class CrucibleReviewActionListener {
-	public static final CrucibleReviewActionListener I_WANT_THIS_MESSAGE_BACK = null;
-	public static final CrucibleReviewActionListener ANONYMOUS = null;
+    public static final CrucibleReviewActionListener I_WANT_THIS_MESSAGE_BACK = null;
+    public static final CrucibleReviewActionListener ANONYMOUS = null;
 
-	/**
-	 * A method ivoked by a background thread when a new review needs to be shown
-	 *
-	 * @param reviewData
-	 */
-	public void showReview(ReviewData reviewData) {
-	}
+    /**
+     * A method ivoked by a background thread when a new review needs to be shown
+     *
+     * @param reviewData
+     */
+    public void showReview(ReviewData reviewData) {
+    }
 
-	public void focusOnGeneralComments(ReviewData review) {
-	}
+    public void focusOnGeneralComments(ReviewData review) {
+    }
 
-	public void focusOnFileComments(ReviewData review, CrucibleFileInfo file) {
-	}
+    public void focusOnFileComments(ReviewData review, CrucibleFileInfo file) {
+    }
 }
