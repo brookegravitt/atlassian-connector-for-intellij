edd9a41ea66bb86ee8dd3bedba257fb27b2ca8b5
lguminski <lguminski@localhost>
2008-07-22 09:29:51 +0000

    license headers added

--- ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/popup/NotificationPopupColors.java
+++ ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/view/popup/NotificationPopupColors.java
@@ -1,26 +1,18 @@
-/*   **********************************************************************  **
- **   Copyright notice                                                       **
- **                                                                          **
- **   (c) 2005-2006 RSSOwl Development Team                                  **
- **   http://www.rssowl.org/                                                 **
- **                                                                          **
- **   All rights reserved                                                    **
- **                                                                          **
- **   This program and the accompanying materials are made available under   **
- **   the terms of the Eclipse Public License v1.0 which accompanies this    **
- **   distribution, and is available at:                                     **
- **   http://www.rssowl.org/legal/epl-v10.html                               **
- **                                                                          **
- **   A copy is found in the file epl-v10.html and important notices to the  **
- **   license from the team is found in the textfile LICENSE.txt distributed **
- **   in this package.                                                       **
- **                                                                          **
- **   This copyright notice MUST APPEAR in all copies of the file!           **
- **                                                                          **
- **   Contributors:                                                          **
- **     RSSOwl Development Team - initial API and implementation             **
- **                                                                          **
- **  **********************************************************************  */
+/**
+ * Copyright (C) 2008 Atlassian
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.atlassian.theplugin.eclipse.view.popup;
 
