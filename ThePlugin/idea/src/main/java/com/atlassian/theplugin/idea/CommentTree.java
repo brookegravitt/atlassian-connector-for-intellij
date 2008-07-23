ece975cc48fec807faab4fd34967373dc3779de8
lguminski <lguminski@localhost>
2008-07-23 10:02:26 +0000

    PL-303 showFile event added + some ui refinements

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/CommentTree.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/CommentTree.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -64,23 +64,7 @@ public class CommentTree extends AtlassianTree {
 	}
 
 	private void registerUI() {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				CommentTree.this.setUI(new BasicWideNodeTreeUI());
-			}
-		});
-	}
-
-	public void focusOnNode(AtlassianTreeNode node) {
-		if (node == null) {
-			return;
-		}
-		for (int i = 0; i < getRowCount(); i++) {
-			if (((AtlassianTreeNode) getPathForRow(i).getLastPathComponent()).equals(node)) {
-				this.setSelectionRow(i);
-				this.scrollRowToVisible(i);
-			}
-		}
+		CommentTree.this.setUI(new BasicWideNodeTreeUI());
 	}
 
 	/**
