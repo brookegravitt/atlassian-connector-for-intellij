7cdd0c5ca897e874608348400332dac5f6e6a563
wseliga <wseliga@localhost>
2008-07-23 15:05:46 +0000

    PL-467 first attempt to working copies of files coming from Bamboo

--- src/main/java/com/atlassian/theplugin/idea/ui/tree/file/CrucibleFileNode.java
+++ src/main/java/com/atlassian/theplugin/idea/ui/tree/file/CrucibleFileNode.java
@@ -1,30 +1,28 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ package com.atlassian.theplugin.idea.ui.tree.file;
 
-package com.atlassian.theplugin.idea.ui.tree.file;
-
-import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFileInfo;
 import com.atlassian.theplugin.commons.crucible.ValueNotYetInitialized;
-import com.atlassian.theplugin.commons.remoteapi.rest.AbstractHttpSession;
-import com.atlassian.theplugin.idea.crucible.ReviewData;
+import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFileInfo;
 import com.atlassian.theplugin.idea.ui.tree.AtlassianClickAction;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import com.intellij.ui.SimpleTextAttributes;
-import com.intellij.openapi.fileTypes.FileTypeManager;
-import com.intellij.openapi.fileTypes.FileType;
+import org.apache.commons.io.FilenameUtils;
 
 import javax.swing.*;
 import javax.swing.tree.TreeCellRenderer;
@@ -40,14 +38,12 @@ import java.awt.*;
 public class CrucibleFileNode extends FileNode {
 
 	private CrucibleFileInfo fileInfo;
-	private ReviewData review;
-	private static final ColoredTreeCellRenderer MY_RENDERER = new CrucibleFileNodeRenderer();
+    private static final ColoredTreeCellRenderer MY_RENDERER = new CrucibleFileNodeRenderer();
 
-	public CrucibleFileNode(CrucibleFileInfo file, ReviewData review, AtlassianClickAction action) {
-		super(AbstractHttpSession.getLastComponentFromUrl(file.getFileDescriptor().getUrl()), action);
+	public CrucibleFileNode(CrucibleFileInfo file, AtlassianClickAction action) {
+		super(FilenameUtils.getName(file.getFileDescriptor().getUrl()), action);
 		this.fileInfo = file;
-		this.review = review;
-	}
+    }
 
 	@Override
 	public TreeCellRenderer getTreeCellRenderer() {
