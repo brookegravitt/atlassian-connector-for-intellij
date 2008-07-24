857565728518642e1267608447f50cada6bbe1a4
lguminski <lguminski@localhost>
2008-07-24 04:02:00 +0000

    PL-303 events added

---
+++ src/main/java/com/atlassian/theplugin/idea/crucible/events/GeneralCommentReplyAdded.java
@@ -0,0 +1,29 @@
+package com.atlassian.theplugin.idea.crucible.events;
+
+import com.atlassian.theplugin.idea.crucible.ReviewData;
+import com.atlassian.theplugin.idea.crucible.comments.CrucibleReviewActionListener;
+import com.atlassian.theplugin.commons.crucible.api.model.GeneralComment;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lguminski
+ * Date: Jul 24, 2008
+ * Time: 5:25:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GeneralCommentReplyAdded extends CrucibleEvent {
+	private ReviewData review;
+	private GeneralComment parentComment;
+	private GeneralComment comment;
+
+	public GeneralCommentReplyAdded(CrucibleReviewActionListener caller, ReviewData review, GeneralComment parentComment, GeneralComment comment) {
+		super(caller);
+		this.review = review;
+		this.parentComment = parentComment;
+		this.comment = comment;
+	}
+
+	protected void notify(CrucibleReviewActionListener listener) {
+		listener.createdGeneralCommentReply(review, parentComment, comment);
+	}
+}
