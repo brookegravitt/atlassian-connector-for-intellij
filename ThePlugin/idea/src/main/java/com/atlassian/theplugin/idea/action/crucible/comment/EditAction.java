1e841edba62a1e2f38a14a9464ec3e88473d6fd0
lguminski <lguminski@localhost>
2008-07-25 11:27:36 +0000

    PL-303 toolbar moved on top of CommentTreePanel

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/EditAction.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/EditAction.java
@@ -1,17 +1,21 @@
-package com.atlassian.theplugin.idea.action.crucible.comment.file;
+package com.atlassian.theplugin.idea.action.crucible.comment;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+
+import java.awt.*;
 
 /**
  * Created by IntelliJ IDEA.
  * User: lguminski
  * Date: Jul 23, 2008
- * Time: 3:58:21 PM
+ * Time: 3:49:59 PM
  * To change this template use File | Settings | File Templates.
  */
-public class RemoveAction extends AnAction {
+public class EditAction extends AnAction {
 	public void actionPerformed(AnActionEvent e) {
-		//To change body of implemented methods use File | Settings | File Templates.
+		Component component = DataKeys.CONTEXT_COMPONENT.getData(e.getDataContext());
+		
 	}
 }
