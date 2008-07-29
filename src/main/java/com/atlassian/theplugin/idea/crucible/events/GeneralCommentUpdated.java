f05f3420310b13e05cf5e201fa864c13b2c32e1d
lguminski <lguminski@localhost>
2008-07-29 16:18:04 +0000

    PL-303 checksyle warning removed

--- src/main/java/com/atlassian/theplugin/idea/crucible/events/GeneralCommentUpdated.java
+++ src/main/java/com/atlassian/theplugin/idea/crucible/events/GeneralCommentUpdated.java
@@ -1,9 +1,8 @@
 package com.atlassian.theplugin.idea.crucible.events;
 
+import com.atlassian.theplugin.commons.crucible.api.model.GeneralComment;
 import com.atlassian.theplugin.idea.crucible.ReviewData;
 import com.atlassian.theplugin.idea.crucible.comments.CrucibleReviewActionListener;
-import com.atlassian.theplugin.idea.CrucibleBottomToolWindowPanel;
-import com.atlassian.theplugin.commons.crucible.api.model.GeneralComment;
 
 /**
  * Created by IntelliJ IDEA.
