31545d21aef694be42ec07ee542cfa00d5d58e59
lguminski <lguminski@localhost>
2008-08-11 08:39:45 +0000

    PL-303 keeping state of filtering in crucible trees and some refactoring

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/tree/ModelProvider.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/crucible/tree/ModelProvider.java
@@ -2,7 +2,6 @@ package com.atlassian.theplugin.idea.crucible.tree;
 
 import com.atlassian.theplugin.idea.ui.tree.AtlassianClickAction;
 import com.atlassian.theplugin.idea.ui.tree.AtlassianTreeModel;
-import com.atlassian.theplugin.idea.ui.tree.Filter;
 import com.atlassian.theplugin.idea.ui.tree.file.FolderNode;
 
 /**
@@ -13,22 +12,11 @@ import com.atlassian.theplugin.idea.ui.tree.file.FolderNode;
  * To change this template use File | Settings | File Templates.
  */
 public abstract class ModelProvider {
-	private Filter filter = Filter.ALL;
 
 	public abstract AtlassianTreeModel getModel(final AtlassianTreeWithToolbar.STATE state);
-	public void setFilter(Filter filter) {
-		this.filter = filter;
-	}
-
-	public Filter getFilter() {
-		return filter;
-	}
-
-	public Filter getNextState(final Filter filter) {
-		return Filter.ALL;
-	}
 
 	public static final ModelProvider EMPTY_MODEL_PROVIDER = new ModelProvider() {
+
         private AtlassianTreeModel model = new AtlassianTreeModel(new FolderNode("/", AtlassianClickAction.EMPTY_ACTION));
 
         public AtlassianTreeModel getModel(final AtlassianTreeWithToolbar.STATE state) {
