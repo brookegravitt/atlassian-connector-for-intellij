366df0280ef92b3d1c73d3ac7c66c6171f8ce34f
mwent <mwent@localhost>
2008-08-14 09:31:00 +0000

    PL-21 - Post-commit reviews in IDE - NPE bugs on adding comments

--- ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/crucible/api/model/CommitType.java
+++ ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/crucible/api/model/CommitType.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,5 +17,5 @@
 package com.atlassian.theplugin.commons.crucible.api.model;
 
 public enum CommitType {
-	Added, Deleted, Modified, Moved, Unknown
+	Added, Deleted, Modified, Moved, Copied, Unknown
 }
