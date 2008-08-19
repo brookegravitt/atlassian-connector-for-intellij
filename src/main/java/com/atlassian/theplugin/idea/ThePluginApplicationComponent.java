a4e777618f210af7ccbcff3a7699ea161486e1f0
wseliga <wseliga@localhost>
2008-08-19 13:59:18 +0000

    PL-45 fixing various bugs around configuration (introduced after refactoring)

--- src/main/java/com/atlassian/theplugin/idea/ThePluginApplicationComponent.java
+++ src/main/java/com/atlassian/theplugin/idea/ThePluginApplicationComponent.java
@@ -38,8 +38,6 @@ import com.intellij.openapi.extensions.AreaPicoContainer;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.IconLoader;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
