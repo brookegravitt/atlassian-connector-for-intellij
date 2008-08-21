a578fed40c5facd99622928b9a71c6bd27007f33
pmaruszak <pmaruszak@localhost>
2008-08-21 09:51:46 +0000

    "Configure" icon from tool bar now opens project configuration instead of global one.

--- src/main/java/com/atlassian/theplugin/idea/action/ShowProjectSettingsAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/ShowProjectSettingsAction.java
@@ -17,18 +17,24 @@
 package com.atlassian.theplugin.idea.action;
 
 import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.idea.ProjectConfigurationComponent;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
 
 /**
  * Simple action to show the settings for the plugin.
  */
-public class ShowSettingsAction extends AnAction {
+public class ShowProjectSettingsAction extends AnAction {
 	@Override
 	public void actionPerformed(AnActionEvent event) {
-		ShowSettingsUtil.getInstance().editConfigurable(
-				IdeaHelper.getCurrentProject(event.getDataContext()),				
-                IdeaHelper.getAppComponent());
+		Project project = IdeaHelper.getCurrentProject(event);
+		if (project != null) {
+			Configurable component = project.getComponent(ProjectConfigurationComponent.class);
+			ShowSettingsUtil.getInstance().editConfigurable(project,component);		
+		}
 	}
+
 }
