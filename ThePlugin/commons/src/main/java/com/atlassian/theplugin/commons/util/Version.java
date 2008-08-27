ca36cee860514233758aebc171ad27389ca03b28
pmaruszak <pmaruszak@localhost>
2008-08-27 10:26:24 +0000

    PL-522, merged to 2.0.0

--- ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/util/Version.java
+++ ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/util/Version.java
@@ -43,11 +43,11 @@ public class Version implements Serializable {
 	private transient VersionNumber versionNumber;
 	private transient Integer buildNo;
 
-	private String version = null;
+	private final String version;
 
 	public Version(String version) throws IncorrectVersionException {
-		super();
-		setVersion(version);
+		this.version = version;
+		parseVersionString(version);
 	}
 
 	public Version() throws IncorrectVersionException {
@@ -58,24 +58,24 @@ public class Version implements Serializable {
 		return version;
 	}
 
-	public void setVersion(String version) throws IncorrectVersionException {
-		this.version = version;
+	private void parseVersionString(String version) throws IncorrectVersionException {
 		if (!version.equals(SPECIAL_DEV_VERSION)) {
-			tokenize();
+			tokenize(version.toUpperCase());
 		}
 	}
 
 
-	private static final String PATTERN = "^(\\d+)\\.(\\d+)\\.(\\d+)((-(SNAPSHOT))?+), SVN:(\\d+)$";
-	//private static final String PATTERN = "^(\\d+)\\.(\\d+)\\.(\\d+)((-(ALPHA|BETA|SNAPSHOT))?+), SVN:(\\d+)$";
+	//private static final String PATTERN = "^(\\d+)\\.(\\d+)\\.(\\d+)((-(SNAPSHOT))?+), SVN:(\\d+)$";
+	private static final String PATTERN = "^(\\d+)\\.(\\d+)\\.(\\d+)((-(ALPHA|BETA|SNAPSHOT))?+)((-(\\d+))?+), SVN:(\\d+)$";
 	private static final int MAJOR_TOKEN_GRP = 1;
 	private static final int MINOR_TOKEN_GRP = 2;
 	private static final int MICRO_TOKEN_GRP = 3;
 	private static final int ALPHANUM_TOKEN_GRP = 6;
-	private static final int BUILD_TOKEN_GRP = 7;
+	private static final int ALPHANUM_VERSION_GRP = 9;
+	private static final int BUILD_TOKEN_GRP = 10;
 
-	private void tokenize() throws IncorrectVersionException {
-		Scanner s = new Scanner(version);
+	private void tokenize(final String aVersion) throws IncorrectVersionException {
+		Scanner s = new Scanner(aVersion);
 		s.findInLine(PATTERN);
 		try {
 			MatchResult result = s.match();
@@ -83,15 +83,17 @@ public class Version implements Serializable {
 					Integer.valueOf(result.group(MAJOR_TOKEN_GRP)),
 					Integer.valueOf(result.group(MINOR_TOKEN_GRP)),
 					Integer.valueOf(result.group(MICRO_TOKEN_GRP)),
-					result.group(ALPHANUM_TOKEN_GRP)
+					result.group(ALPHANUM_TOKEN_GRP),
+					result.group(ALPHANUM_VERSION_GRP)
 			);
 			buildNo = Integer.valueOf(result.group(BUILD_TOKEN_GRP));
 		} catch (IllegalStateException ex) {
-			throw new IncorrectVersionException("Version (" + version + ") does not match pattern (\"" + PATTERN
+			throw new IncorrectVersionException("Version (" + aVersion + ") does not match pattern (\"" + PATTERN
 					+ "\")", ex);
 		}
 	}
 
+	@Override
 	public boolean equals(Object that) {
 		if (this == that) {
 			return true;
@@ -109,6 +111,7 @@ public class Version implements Serializable {
 		return true;
 	}
 
+	@Override
 	public int hashCode() {
 		return (version != null ? version.hashCode() : 0);
 	}
@@ -123,6 +126,9 @@ public class Version implements Serializable {
 	}
 
 	public boolean greater(Version other) {
+		if (other == null) {
+			return false;
+		}
 		if (other.version.equals(SPECIAL_DEV_VERSION)) {
 			return false;
 		}
@@ -133,6 +139,7 @@ public class Version implements Serializable {
 			return getBuildNo() > other.getBuildNo();
 		}
 		return this.getVersionNumber().greater(other.getVersionNumber());
+
 	}
 
 	private VersionNumber getVersionNumber() {
@@ -145,23 +152,28 @@ public class Version implements Serializable {
 		private int minor;
 		private int micro;
 		private AlphaNum alphaNum;
+		private int alphaNumValue = 0;
 		private static final int PRIME = 31;
 
 
 		public enum AlphaNum {
-			ALPHA, BETA, SNAPSHOT, NONE;
+			SNAPSHOT, ALPHA, BETA, NONE;			
 		}
 
-		public VersionNumber(int major, int minor, int micro, String alphaNum) throws IncorrectVersionException {
+		public VersionNumber(int major, int minor, int micro, String alphaNum, String alphaNumValue) throws IncorrectVersionException {
 			this.major = major;
-
 			this.minor = minor;
-			this.micro = micro;
+			this.micro = micro;			
+
 			if (alphaNum == null) {
 				this.alphaNum = AlphaNum.NONE;
 			} else {
 				try {
 					this.alphaNum = AlphaNum.valueOf(alphaNum);
+					
+					if (alphaNumValue != null) {
+						this.alphaNumValue = Integer.valueOf(alphaNumValue);
+					}
 				} catch (IllegalArgumentException ex) {
 					throw new IncorrectVersionException("Unknown version alphanum: " + alphaNum);
 				}
@@ -169,6 +181,10 @@ public class Version implements Serializable {
 		}
 
 		public boolean greater(VersionNumber other) {
+			if (other == null) {
+				return false;
+			}
+			
 			if (major > other.major) {
 				return true;
 			} else {
@@ -179,7 +195,12 @@ public class Version implements Serializable {
 						return true;
 					} else {
 						if (major == other.major && minor == other.minor && micro == other.micro) {
-							return alphaNum.ordinal() > other.alphaNum.ordinal();
+							if (alphaNum.ordinal() == other.alphaNum.ordinal()) {
+								
+								return alphaNumValue > other.alphaNumValue;
+							} else {
+								return (alphaNum.ordinal() > other.alphaNum.ordinal());
+							}
 						}
 					}
 				}
@@ -187,6 +208,7 @@ public class Version implements Serializable {
 			return false;
 		}
 
+		@Override
 		public boolean equals(Object o) {
 			if (this == o) {
 				return true;
@@ -210,9 +232,15 @@ public class Version implements Serializable {
 				return false;
 			}
 
+			if (alphaNumValue != that.alphaNumValue) {
+				return false;
+			}
+
+
 			return true;
 		}
 
+		@Override
 		public int hashCode() {
 			int result;
 			result = major;
