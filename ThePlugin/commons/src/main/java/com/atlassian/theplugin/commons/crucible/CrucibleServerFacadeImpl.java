4465248df6f401130150932d0a2ebfc5de00aa8c
jgorycki <jgorycki@localhost>
2008-09-04 11:47:36 +0000

    PL-581 - Should explicitly tell me that the plugin does not work with CRU <1.6

--- ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/crucible/CrucibleServerFacadeImpl.java
+++ ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/crucible/CrucibleServerFacadeImpl.java
@@ -24,7 +24,6 @@ import com.atlassian.theplugin.commons.crucible.api.model.*;
 import com.atlassian.theplugin.commons.crucible.api.rest.CrucibleSessionImpl;
 import com.atlassian.theplugin.commons.exception.ServerPasswordNotProvidedException;
 import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
-import com.atlassian.theplugin.commons.remoteapi.RemoteApiLoginException;
 
 import java.util.HashMap;
 import java.util.List;
