1fe4ce7d40e637a2bbb4e742e5624ac893115773
wseliga <wseliga@localhost>
2008-09-17 17:06:55 +0000

    PL-467, PL-532, PL-551 polishing Bamboo Show Change Files logic and UI behaviour, several keyboard shortcuts added, refactored selection handling on AtlassianTable, implemented Show Diff in Bamboo Changed Files

--- ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/bamboo/api/BambooSessionImpl.java
+++ ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/bamboo/api/BambooSessionImpl.java
@@ -116,7 +116,8 @@ public class BambooSessionImpl extends AbstractHttpSession implements BambooSess
                 throw new RemoteApiLoginException("Server did not return any authentication token");
             }
             if (elements.size() != 1) {
-                throw new RemoteApiLoginException("Server returned unexpected number of authentication tokens (" + elements.size() + ")");
+                throw new RemoteApiLoginException("Server returned unexpected number of authentication tokens ("
+						+ elements.size() + ")");
             }
             this.authToken = elements.get(0).getText();
         } catch (MalformedURLException e) {
