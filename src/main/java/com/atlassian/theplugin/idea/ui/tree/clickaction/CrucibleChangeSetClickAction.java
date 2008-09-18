5eb677d1a7192b283aed2ea6b2ff47307cb75276
jgorycki <jgorycki@localhost>
2008-09-18 12:23:51 +0000

    PL-607 - tree items made to work after being updated

---
+++ src/main/java/com/atlassian/theplugin/idea/ui/tree/clickaction/CrucibleChangeSetClickAction.java
@@ -0,0 +1,32 @@
+package com.atlassian.theplugin.idea.ui.tree.clickaction;
+
+import com.atlassian.theplugin.idea.ui.tree.AtlassianClickAction;
+import com.atlassian.theplugin.idea.ui.tree.AtlassianTreeNode;
+import com.atlassian.theplugin.idea.crucible.comments.ReviewActionEventBroker;
+import com.atlassian.theplugin.idea.crucible.comments.CrucibleReviewActionListener;
+import com.atlassian.theplugin.idea.crucible.events.FocusOnGeneralComments;
+import com.atlassian.theplugin.idea.crucible.ReviewData;
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.intellij.openapi.project.Project;
+
+public class CrucibleChangeSetClickAction implements AtlassianClickAction {
+
+	private Project project;
+	private ReviewData review;
+
+	public CrucibleChangeSetClickAction(Project project, ReviewData review) {
+		this.project = project;
+		this.review = review;
+	}
+
+	public void execute(AtlassianTreeNode node, int noOfClicks) {
+		switch (noOfClicks) {
+			case 1:
+			case 2:
+				ReviewActionEventBroker broker = IdeaHelper.getReviewActionEventBroker(project);
+				broker.trigger(new FocusOnGeneralComments(CrucibleReviewActionListener.ANONYMOUS, review));
+				break;
+			default:
+		}
+	}
+}
