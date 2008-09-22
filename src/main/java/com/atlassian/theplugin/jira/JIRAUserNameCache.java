5e231e859a18c57db4a4b7e41ba8c0c68375cbf6
jgorycki <jgorycki@localhost>
2008-09-22 13:36:43 +0000

    PL-592 - Show user names instead of login names in JIRA comment fields

---
+++ src/main/java/com/atlassian/theplugin/jira/JIRAUserNameCache.java
@@ -0,0 +1,37 @@
+package com.atlassian.theplugin.jira;
+
+import com.atlassian.theplugin.jira.api.JIRAUserBean;
+import com.atlassian.theplugin.jira.api.JIRAException;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public final class JIRAUserNameCache {
+
+	private Map<JIRAServer, Map<String, JIRAUserBean>> serverMap = new HashMap<JIRAServer, Map<String, JIRAUserBean>>();
+	private JIRAServerFacade facade;
+
+	private JIRAUserNameCache() {
+		facade = JIRAServerFacadeImpl.getInstance();
+	}
+
+	private static JIRAUserNameCache instance = new JIRAUserNameCache();
+
+	public static JIRAUserNameCache getInstance() {
+		return instance;
+	}
+
+	public JIRAUserBean getUser(JIRAServer server, String userId) throws JIRAException {
+		Map<String, JIRAUserBean> userMap = serverMap.get(server);
+		if (userMap == null) {
+			userMap = new HashMap<String, JIRAUserBean>();
+			serverMap.put(server, userMap);
+		}
+		JIRAUserBean user = userMap.get(userId);
+		if (user == null) {
+			user = facade.getUser(server.getServer(), userId);
+			userMap.put(userId, user);
+		}
+		return user;
+	}
+}
