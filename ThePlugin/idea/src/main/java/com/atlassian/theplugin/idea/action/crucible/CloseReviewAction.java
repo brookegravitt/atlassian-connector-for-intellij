ab3be3e5d2b8cf24908245c6e7131de04abbb413
wseliga <wseliga@localhost>
2008-09-23 13:13:24 +0000

    PL-636 refactored code around CrucibleChangeReviewStateForm and made text field really editable (also from UI L&F) NONE fixed bug with invalid display of issue moderator in Crucible Review Window

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/CloseReviewAction.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/crucible/CloseReviewAction.java
@@ -20,7 +20,8 @@ import com.atlassian.theplugin.commons.crucible.api.model.Action;
 
 
 public class CloseReviewAction extends AbstractTransitionReviewAction {
-    protected Action getRequestedTransition() {
+    @Override
+	protected Action getRequestedTransition() {
         return Action.CLOSE;
     }
 }
