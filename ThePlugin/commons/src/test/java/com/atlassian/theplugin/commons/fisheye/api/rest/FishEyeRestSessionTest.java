afffe70dbddf637bb51bedca38ec5df597cb18db
wseliga <wseliga@localhost>
2008-10-08 15:28:47 +0000

    PL-630, PL-631, PL-5 FishEye session under dev

---
+++ ThePlugin/commons/src/test/java/com/atlassian/theplugin/commons/fisheye/api/rest/FishEyeRestSessionTest.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2008 Atlassian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atlassian.theplugin.commons.fisheye.api.rest;
+
+import junit.framework.TestCase;
+
+/**
+ * FishEyeRestSession Tester.
+ *
+ * @author wseliga
+ */
+public class FishEyeRestSessionTest extends TestCase {
+
+    @Override
+	public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+	public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void xtestAdjustHttpHeader() {
+        //TODO: wseliga implement it
+        fail("unimplemented");
+    }
+
+    public void xtestPreprocessResult() {
+        //TODO: wseliga implement it
+        fail("unimplemented");
+    }
+
+    public void xtestLogin() {
+        //TODO: wseliga implement it
+        fail("unimplemented");
+    }
+
+    public void xtestLogout() {
+        //TODO: wseliga implement it
+        fail("unimplemented");
+    }
+
+    public void xtestGetDocument() {
+        //TODO: wseliga implement it
+        fail("unimplemented");
+    }
+
+    public void xtestGetLastModified() {
+        //TODO: wseliga implement it
+        fail("unimplemented");
+    }
+
+    public void xtestGetEtag() {
+        //TODO: wseliga implement it
+        fail("unimplemented");
+    }
+
+
+}
