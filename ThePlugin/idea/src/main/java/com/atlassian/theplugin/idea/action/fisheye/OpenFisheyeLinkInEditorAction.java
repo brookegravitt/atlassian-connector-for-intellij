80d5996baef509f48a262a759212b7b086c298f3
wseliga <wseliga@localhost>
2008-10-09 15:09:18 +0000

    NONE removing invalid imports, polishing code after Marek's Fedex ;)

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/fisheye/OpenFisheyeLinkInEditorAction.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/fisheye/OpenFisheyeLinkInEditorAction.java
@@ -1,23 +1,20 @@
 package com.atlassian.theplugin.idea.action.fisheye;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiFile;
 import com.atlassian.theplugin.idea.IdeaHelper;
 import com.atlassian.theplugin.idea.VcsIdeaHelper;
 import com.atlassian.theplugin.idea.fisheye.SourceCodeLinkParser;
 import com.atlassian.theplugin.util.CodeNavigationUtil;
-import com.gargoylesoftware.htmlunit.TextUtil;
-
-import java.util.Scanner;
-import java.util.regex.MatchResult;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
 
 public class OpenFisheyeLinkInEditorAction extends AnAction {
+	@Override
 	public void actionPerformed(final AnActionEvent event) {
 		Project project = IdeaHelper.getCurrentProject(event);
 		if (project != null) {
@@ -28,15 +25,17 @@ public class OpenFisheyeLinkInEditorAction extends AnAction {
 				if (parser.getPath() != null) {
 					PsiFile psiFile = CodeNavigationUtil.guessCorrespondingPsiFile(project, parser.getPath());
 					if (psiFile != null) {
+						final VirtualFile virtualFile = psiFile.getVirtualFile();
+						if (virtualFile == null) {
+							Messages.showErrorDialog(project, "No corresponding file found for PSI element", "Unable to open file");
+							return;
+						}
 						if (parser.getRevision() != null) {
-							VcsIdeaHelper.openFile(project, psiFile.getVirtualFile(), parser.getRevision(), parser.getLine(), 0,
-									null);
+							VcsIdeaHelper.openFile(project, virtualFile, parser.getRevision(), parser.getLine(), 0, null);
 						} else {
 							FileEditorManager fem = FileEditorManager.getInstance(project);
-							OpenFileDescriptor desc = new OpenFileDescriptor(project, psiFile.getVirtualFile(), 0, 0);
-							if (desc != null) {
-								fem.openTextEditor(desc, true);
-							}
+							OpenFileDescriptor desc = new OpenFileDescriptor(project, virtualFile, 0, 0);
+							fem.openTextEditor(desc, true);
 						}
 					} else {
 						Messages.showErrorDialog(project, "File not found in the project", "Unable to open file");
@@ -49,6 +48,7 @@ public class OpenFisheyeLinkInEditorAction extends AnAction {
 
 	}
 
+	@Override
 	public void update(final AnActionEvent event) {
 		if (IdeaHelper.getCurrentProject(event) == null) {
 			event.getPresentation().setEnabled(false);
