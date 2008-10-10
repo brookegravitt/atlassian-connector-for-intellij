9b07e7debfa8f9c0d2cd64a34231b00b28219fb4
jgorycki <jgorycki@localhost>
2008-10-10 11:26:46 +0000

    PL-682 - Measure time from starting progress on an issue till time is reported

--- src/main/java/com/atlassian/theplugin/idea/action/bamboo/DebugFailedTestsAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/bamboo/DebugFailedTestsAction.java
@@ -9,7 +9,7 @@ public class DebugFailedTestsAction extends AnAction {
 	public void actionPerformed(AnActionEvent e) {
 		TestResultsToolWindow window = IdeaHelper.getProjectComponent(e, TestResultsToolWindow.class);
 		if (window != null) {
-			window.runFailedTests(true);
+			window.runFailedTests(e, true);
 		}
 	}
 
