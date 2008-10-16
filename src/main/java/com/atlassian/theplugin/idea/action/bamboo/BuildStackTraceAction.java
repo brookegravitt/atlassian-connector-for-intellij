b9b180665872ea1da325c5e43433cf7459518382
pmaruszak <pmaruszak@localhost>
2008-10-16 13:09:14 +0000

    PL-612, Added action description to "Show Failed Tests" - "Show Failed Tests. Failed plan is not selected or you are not connected to Bamboo >= 2.x"

--- src/main/java/com/atlassian/theplugin/idea/action/bamboo/BuildStackTraceAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/bamboo/BuildStackTraceAction.java
@@ -37,7 +37,7 @@ public class BuildStackTraceAction extends AnAction {
 		if (tw != null) {
 			enabled = tw.canShowFailedTests();
 			if (!enabled) {
-				event.getPresentation().setText("Show Failed Tests. Select failed plan or you are not connected to Bamboo >= 2.x");
+				event.getPresentation().setText("Show Failed Tests. Failed plan is not selected or you are not connected to Bamboo >= 2.x");
 			} else {
 				event.getPresentation().setText("Show Failed Tests");
 			}
