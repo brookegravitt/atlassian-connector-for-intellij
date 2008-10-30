c31eb9aee17f85b7dc5b33a81648726b3ac2ce78
jgorycki <jgorycki@localhost>
2008-10-30 11:15:44 +0000

    new version numbers

--- src/main/java/com/atlassian/theplugin/idea/BugReporting.java
+++ src/main/java/com/atlassian/theplugin/idea/BugReporting.java
@@ -58,6 +58,8 @@ public final class BugReporting {
         versionMap.put("2.0.0-beta-3", "10593");
 		versionMap.put("2.0.0-beta-4", "10632");
 		versionMap.put("2.0.0-beta-5", "10644");
+		versionMap.put("2.0.0-beta-6", "10672");
+		versionMap.put("2.0.0-beta-7", "10680");
 	}
 
     private static final int MAX_URI_LENGTH = 4096;
