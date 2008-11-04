284aa432649b507b38578ccceba5396b799e8ddc
jgorycki <jgorycki@localhost>
2008-11-04 13:07:29 +0000

    JIRA Issue List Model listener

---
+++ src/main/java/com/atlassian/theplugin/jira/model/JIRAIssueListModel.java
@@ -0,0 +1,15 @@
+package com.atlassian.theplugin.jira.model;
+
+import com.atlassian.theplugin.jira.api.JIRAIssue;
+
+import java.util.List;
+
+public interface JIRAIssueListModel {
+	void clear();
+	void addIssue(JIRAIssue issue);
+	void addIssues(List<JIRAIssue> issues);
+	List<JIRAIssue> getIssues();
+	void notifyListeners();
+	void addModelListener(JIRAIssueListModelListener listener);
+	void removeModelListener(JIRAIssueListModelListener listener);
+}
