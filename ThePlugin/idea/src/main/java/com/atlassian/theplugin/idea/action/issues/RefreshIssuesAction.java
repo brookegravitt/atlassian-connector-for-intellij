94aadf3c1b49dddf9c403f8530778d9d3ae7621a
wseliga <wseliga@localhost>
2008-11-06 20:33:56 +0000

    NONE fixing checkstyle issues

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/issues/RefreshIssuesAction.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/issues/RefreshIssuesAction.java
@@ -1,22 +1,15 @@
 package com.atlassian.theplugin.idea.action.issues;
 
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.idea.jira.IssuesToolWindowPanel;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.progress.Task;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.atlassian.theplugin.idea.jira.IssuesToolWindowPanel;
-import com.atlassian.theplugin.idea.IdeaHelper;
-import com.atlassian.theplugin.commons.cfg.JiraServerCfg;
-import com.atlassian.theplugin.cfg.CfgUtil;
-import com.atlassian.theplugin.jira.api.JIRAException;
-
-import java.util.Collection;
 
 /**
  * User: pmaruszak
  */
 public class RefreshIssuesAction extends AnAction {
+	@Override
 	public void actionPerformed(final AnActionEvent e) {
 		final IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(e);
 		panel.refreshIssues();
