da9e127b67e566fbeee61561f3915d32deec3566
pmaruszak <pmaruszak@localhost>
2008-11-06 22:11:48 +0000

    PL-801, filter tree

---
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAFilterTree.java
@@ -0,0 +1,73 @@
+package com.atlassian.theplugin.idea.jira.tree;
+
+import com.atlassian.theplugin.commons.cfg.JiraServerCfg;
+import com.atlassian.theplugin.jira.model.JIRAFilterListModel;
+import com.atlassian.theplugin.jira.model.JIRAFilterListModelListener;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeCellRenderer;
+import javax.swing.tree.TreeSelectionModel;
+import java.awt.*;
+
+/**
+ * User: pmaruszak
+ */
+public class JIRAFilterTree extends JTree implements JIRAFilterListModelListener {
+	private static final ServerTreeRenderer MY_RENDERER = new ServerTreeRenderer();
+
+	JIRAFilterListModel listModel;
+	private DefaultTreeModel treeModel;
+
+	public void modelChanged(final JIRAFilterListModel listModel) {
+		createServerNodes((DefaultMutableTreeNode)treeModel.getRoot());
+	}
+
+	public JIRAFilterTree(JIRAFilterListModel listModel){
+		final DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Root");
+
+		treeModel = new DefaultTreeModel(rootNode);
+
+		setModel(treeModel);
+		setRootVisible(false);
+		expandRow(0);
+		this.listModel = listModel;
+		if (listModel != null){
+			listModel.addModelListener(this);
+			createServerNodes((DefaultMutableTreeNode)treeModel.getRoot());
+			
+		}
+
+		getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		setCellRenderer(MY_RENDERER);
+	}
+
+	private void createServerNodes(DefaultMutableTreeNode rootNode){
+
+		if (listModel == null) {
+			return;
+		}
+
+		for (JiraServerCfg server: listModel.getJIRAServers()){
+			JIRAServerTreeNode serverNode = new JIRAServerTreeNode(server);
+			rootNode.add(serverNode);
+		}
+	}
+
+	private static class ServerTreeRenderer implements TreeCellRenderer {
+
+		public Component getTreeCellRendererComponent(final JTree jTree, final Object o, final boolean b, final boolean b1,
+				final boolean b2, final int i, final boolean b3) {
+
+			if (o instanceof JIRAServerTreeNode) {
+				JIRAServerTreeNode node = (JIRAServerTreeNode) o;
+				return new JLabel(node.getNodeName());
+			} else {
+				return new JLabel("Root");
+			}
+		}
+	}
+
+
+}
