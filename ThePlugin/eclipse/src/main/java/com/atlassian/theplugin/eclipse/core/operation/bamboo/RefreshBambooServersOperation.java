75fa24c891469d81d68e0136eaa3aa82e44d087a
wseliga <wseliga@localhost>
2008-11-06 00:41:13 +0000

    NONE checkstyle fix

--- ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/core/operation/bamboo/RefreshBambooServersOperation.java
+++ ThePlugin/eclipse/src/main/java/com/atlassian/theplugin/eclipse/core/operation/bamboo/RefreshBambooServersOperation.java
@@ -1,14 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2005-2006 Polarion Software.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Alexander Gurov - Initial API and implementation
- *******************************************************************************/
-
 package com.atlassian.theplugin.eclipse.core.operation.bamboo;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -19,11 +8,6 @@ import com.atlassian.theplugin.eclipse.ui.bamboo.BambooServersView;
 import com.atlassian.theplugin.eclipse.ui.bamboo.BambooTreeViewer;
 import com.atlassian.theplugin.eclipse.ui.bamboo.IBambooTreeNode;
 
-/**
- * Refresh repository location in the repository tree operation
- * 
- * @author Alexander Gurov
- */
 public class RefreshBambooServersOperation extends AbstractNonLockingOperation {
 	protected Object [] nodes;
 	protected boolean deep;
@@ -51,7 +35,7 @@ public class RefreshBambooServersOperation extends AbstractNonLockingOperation {
 					BambooServersView.refresh(current, new BambooTreeViewer.IRefreshVisitor() {
 						public void visit(Object data) {
 							if (data instanceof IBambooTreeNode && RefreshBambooServersOperation.this.deep) {
-								((IBambooTreeNode)data).refresh();
+								((IBambooTreeNode) data).refresh();
 							}
 						}
 					});
