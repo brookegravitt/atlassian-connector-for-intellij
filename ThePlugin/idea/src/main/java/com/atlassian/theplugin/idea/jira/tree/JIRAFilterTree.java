cb244ced39ceeb991c53c940d55aa7d04f3235a0
pmaruszak <pmaruszak@localhost>
2008-11-10 13:24:52 +0000

    PL-801, left tree problem with rendering fixed

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAFilterTree.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAFilterTree.java
@@ -9,10 +9,7 @@ import com.atlassian.theplugin.jira.model.JIRAFilterListModelListener;
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeCellRenderer;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeSelectionModel;
+import javax.swing.tree.*;
 import java.awt.*;
 
 /**
@@ -20,38 +17,44 @@ import java.awt.*;
  */
 public class JIRAFilterTree extends JTree implements JIRAFilterListModelListener {
 
+	private static final ServerTreeRenderer MY_RENDERER = new ServerTreeRenderer();
+	private DefaultTreeModel treeModel;
+
 	public JIRAFilterTree(final JIRAFilterListModel listModel) {
-		final DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Root");
+		listModel.addModelListener(this);
+		reCreateTree(listModel);
+	}
+
+	private void reCreateTree(final JIRAFilterListModel listModel) {
+		DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode();
 		treeModel = new DefaultTreeModel(rootNode);
 
 		setModel(treeModel);
+
 		setShowsRootHandles(true);
 		setRootVisible(false);
 		if (listModel != null) {
-			listModel.addModelListener(this);
+
+			getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+			setCellRenderer(MY_RENDERER);
+			getSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
+
+				public void valueChanged(final TreeSelectionEvent event) {
+					final TreePath selectionPath = getSelectionModel().getSelectionPath();
+
+					if (selectionPath != null && selectionPath.getLastPathComponent() != null) {
+						((JIRAAbstractTreeNode) selectionPath.getLastPathComponent()).onSelect();
+					}
+				}
+			});
+
 			createServerNodes(listModel, (DefaultMutableTreeNode) treeModel.getRoot());
-			treeModel.nodeStructureChanged(rootNode);
+
+
 
 		}
-
-		getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-		setCellRenderer(MY_RENDERER);
-		expandAll();
-		getSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
-
-			public void valueChanged(final TreeSelectionEvent event) {
-				((JIRAAbstractTreeNode) getSelectionModel().getSelectionPath().getLastPathComponent()).onSelect();
-			}
-		});
+		treeModel.nodeStructureChanged(rootNode);
 	}
-
-	private static final ServerTreeRenderer MY_RENDERER = new ServerTreeRenderer();
-
-	//private JIRAFilterListModel listModel;
-	private DefaultTreeModel treeModel;
-//	public void setListModel(final JIRAFilterListModel listModel) {
-
-//	}
 
 	public void expandAll() {
 		for (int i = 0; i < this.getRowCount(); i++) {
@@ -66,18 +69,15 @@ public class JIRAFilterTree extends JTree implements JIRAFilterListModelListener
 	}
 
 	public void modelChanged(JIRAFilterListModel listModel) {
-		createServerNodes(listModel, (DefaultMutableTreeNode) treeModel.getRoot());
-		treeModel.nodeStructureChanged((DefaultMutableTreeNode) treeModel.getRoot());
+		removeAll();
+		reCreateTree(listModel);
 		expandAll();
 	}
 
 	public void selectedSavedFilter(final JiraServerCfg jiraServer, final JIRASavedFilter savedFilter) {
-		//To change body of implemented methods use File | Settings | File Templates.
 	}
 
-	public void selectedManualFilter(final JiraServerCfg jiraServer,
-	                                 final java.util.List<JIRAQueryFragment> manualFilter) {
-		//To change body of implemented methods use File | Settings | File Templates.
+	public void selectedManualFilter(final JiraServerCfg jiraServer, final java.util.List<JIRAQueryFragment> manualFilter) {
 	}
 
 	private void createServerNodes(JIRAFilterListModel listModel, DefaultMutableTreeNode rootNode) {
@@ -88,15 +88,13 @@ public class JIRAFilterTree extends JTree implements JIRAFilterListModelListener
 
 		for (JiraServerCfg server : listModel.getJIRAServers()) {
 			JIRAServerTreeNode serverNode = new JIRAServerTreeNode(listModel, server);
-			rootNode.add(serverNode);
 			createFilterNodes(server, serverNode, listModel);
+			rootNode.add(serverNode);
 		}
 	}
 
-
-	private void createFilterNodes(JiraServerCfg jiraServer, DefaultMutableTreeNode node,
-	                               JIRAFilterListModel listModel) {
-		for (JIRASavedFilter savedFilter : listModel.getSavedFilters(jiraServer)) {
+	private void createFilterNodes(JiraServerCfg jiraServer, DefaultMutableTreeNode node, JIRAFilterListModel listModel){
+		for (JIRASavedFilter savedFilter : listModel.getSavedFilters(jiraServer)){
 			node.add(new JIRASavedFilterTreeNode(listModel, savedFilter));
 		}
 		//@todo refactor to separate class
@@ -112,7 +110,7 @@ public class JIRAFilterTree extends JTree implements JIRAFilterListModelListener
 				JIRAAbstractTreeNode node = (JIRAAbstractTreeNode) value;
 				return node.getRenderer(null, selected, expanded, hasFocus);
 			} else {
-				return new JLabel("Root");
+				return super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
 			}
 		}
 	}
--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRASavedFilterTreeNode.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRASavedFilterTreeNode.java
@@ -3,9 +3,6 @@ package com.atlassian.theplugin.idea.jira.tree;
 import com.atlassian.theplugin.jira.api.JIRASavedFilter;
 import com.atlassian.theplugin.jira.model.JIRAFilterListModel;
 import com.intellij.openapi.util.IconLoader;
-import com.intellij.ui.SimpleColoredComponent;
-import com.intellij.ui.SimpleTextAttributes;
-import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
 
@@ -16,7 +13,6 @@ public class JIRASavedFilterTreeNode extends JIRAAbstractTreeNode {
 	private static final Icon JIRA_FILTER_ICON = IconLoader.getIcon("/actions/showSource.png");
 	private JIRASavedFilter savedFilter;
 	private JIRAFilterListModel listModel;
-
 
 	public JIRASavedFilterTreeNode(final JIRAFilterListModel listModel, final JIRASavedFilter savedFilter) {
 		this.listModel = listModel;
@@ -29,14 +25,9 @@ public class JIRASavedFilterTreeNode extends JIRAAbstractTreeNode {
 
 	public JComponent getRenderer(final JComponent c, final boolean selected,
 	                              final boolean expanded, final boolean hasFocus) {
-		SimpleColoredComponent component = new SimpleColoredComponent();
-		component.append(savedFilter.getName(), SimpleTextAttributes.REGULAR_ATTRIBUTES);
-
-		component.setIcon(JIRA_FILTER_ICON);
-		component.setOpaque(true);
-		component.setBackground(selected ? UIUtil.getTreeSelectionBackground() : UIUtil.getTreeTextBackground());
-		component.setForeground(selected ? UIUtil.getTreeSelectionForeground() : UIUtil.getTreeTextForeground());
-		return component;
+		JLabel label = new JLabel(savedFilter.getName(), JIRA_FILTER_ICON, SwingUtilities.HORIZONTAL);
+		
+		return label;
 	}
 
 	public void onSelect() {
