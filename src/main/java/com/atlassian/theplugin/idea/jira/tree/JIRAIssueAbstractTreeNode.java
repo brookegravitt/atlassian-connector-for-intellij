e768e1ea5ecb209fda9d5a2ff736ade71706f79f
pmaruszak <pmaruszak@localhost>
2008-11-10 13:24:52 +0000

    PL-801, left tree problem with rendering fixed

--- src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAIssueAbstractTreeNode.java
+++
@@ -1,9 +0,0 @@
-package com.atlassian.theplugin.idea.jira.tree;
-
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.*;
-
-public abstract class JIRAIssueAbstractTreeNode extends DefaultMutableTreeNode {
-	public abstract String toString();
-	public abstract JComponent getRenderer(JComponent c, boolean selected, boolean expanded, boolean hasFocus);
-}
--- src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAServerTreeNode.java
+++ src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAServerTreeNode.java
@@ -3,9 +3,6 @@ package com.atlassian.theplugin.idea.jira.tree;
 import com.atlassian.theplugin.commons.cfg.JiraServerCfg;
 import com.atlassian.theplugin.jira.model.JIRAFilterListModel;
 import com.intellij.openapi.util.IconLoader;
-import com.intellij.ui.SimpleColoredComponent;
-import com.intellij.ui.SimpleTextAttributes;
-import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
 
@@ -27,14 +24,8 @@ public class JIRAServerTreeNode extends JIRAAbstractTreeNode {
 	}
 
 	public JComponent getRenderer(final JComponent c, final boolean selected, final boolean expanded, final boolean hasFocus) {
-		SimpleColoredComponent component = new SimpleColoredComponent();
-		component.append(jiraServer.getName(), SimpleTextAttributes.REGULAR_ATTRIBUTES);
-
-		component.setBackground(selected ? UIUtil.getTreeSelectionBackground() : UIUtil.getTreeTextBackground());
-		component.setForeground(selected ? UIUtil.getTreeSelectionForeground() : UIUtil.getTreeTextForeground());
-		component.setIcon(JIRA_SERVER_ICON);
-		
-		return component;
+		final JLabel label = new JLabel(jiraServer.getName(), JIRA_SERVER_ICON, SwingUtilities.HORIZONTAL);		
+		return label;
 	}
 
 	public JiraServerCfg getJiraServer() {
