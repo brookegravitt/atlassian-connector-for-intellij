2b7e821eda5c8449ef23200eff6b56e732e728cf
mwent <mwent@localhost>
2008-11-10 16:05:19 +0000

    PL-765 Bamboo actions cleanup

--- ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/bamboo/ViewBuildAction.java
+++ ThePlugin/idea/src/main/java/com/atlassian/theplugin/idea/action/bamboo/ViewBuildAction.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,11 +17,10 @@
 package com.atlassian.theplugin.idea.action.bamboo;
 
 import com.atlassian.theplugin.idea.IdeaHelper;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
-public class ViewBuildAction extends AnAction {
-    public void actionPerformed(AnActionEvent event) {
-        IdeaHelper.getBambooToolWindowPanel(event).viewBuild();
-    }
+public class ViewBuildAction extends AbstractBambooBuildAction {
+	public void actionPerformed(AnActionEvent event) {
+		IdeaHelper.getBambooToolWindowPanel(event).viewBuild();
+	}
 }
