3fff0d17ed37d4a7f7c7fe4a2c1c095269a387b1
wseliga <wseliga@localhost>
2008-11-16 22:00:26 +0000

    NONE moving commons to a separate SVN tree

--- ThePlugin/commons/src/main/java/com/atlassian/theplugin/commons/crucible/api/model/CrucibleUserCache.java
+++
@@ -1,8 +0,0 @@
-package com.atlassian.theplugin.commons.crucible.api.model;
-
-import com.atlassian.theplugin.commons.cfg.CrucibleServerCfg;
-
-public interface CrucibleUserCache {
-	User getUser(CrucibleServerCfg server, String userId, boolean fetchIfNotExist);
-	void addUser(CrucibleServerCfg server, User user);
-}
