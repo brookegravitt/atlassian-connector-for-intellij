e06ecd56df024dea9d3f3bab240128f4c6ab610f
jjaroczynski <jjaroczynski@localhost>
2008-11-18 11:42:45 +0000

    PL-847 unified status bar

--- src/main/java/com/atlassian/theplugin/idea/jira/MessageScrollPane.java
+++ src/main/java/com/atlassian/theplugin/idea/jira/MessageScrollPane.java
@@ -1,9 +1,6 @@
 package com.atlassian.theplugin.idea.jira;
 
-import com.atlassian.theplugin.commons.bamboo.StausIconBambooListener;
-import com.atlassian.theplugin.idea.GenericHyperlinkListener;
 import com.intellij.ui.HyperlinkLabel;
-import thirdparty.javaworld.ClasspathHTMLEditorKit;
 
 import javax.swing.*;
 import javax.swing.event.HyperlinkListener;
@@ -14,36 +11,22 @@ import java.awt.*;
  */
 public class MessageScrollPane extends JPanel implements MessageStatusDisplay {
 
-	private JScrollPane scroll;
 	private JPanel statusPanel;
-	private JLabel statusLabel;
 	private HyperlinkLabel getMoreIssues;
-	private static final Color FAIL_COLOR = new Color(255, 201, 201);
+	private static final Color FAIL_COLOR = new Color(255, 100, 100);
+	private final Color defaultColor = this.getBackground();
 
 	protected static final Dimension ED_PANE_MINE_SIZE = new Dimension(200, 200);
-
-	private JEditorPane pane = new JEditorPane();
-	//List<MessageStatusDisplay> listeners = new ArrayList<MessageStatusDisplay>();
+	private JLabel pane = new JLabel();
 
 	public MessageScrollPane(String initialText) {
-		scroll = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		statusPanel = new JPanel();
 		statusPanel.setLayout(new FlowLayout());
-		statusLabel = new JLabel("No issues loaded");
-		statusPanel.add(statusLabel);
 		getMoreIssues = new HyperlinkLabel("Get more issues...");
 		statusPanel.add(getMoreIssues);
 
-		scroll.setWheelScrollingEnabled(true);
-
-		pane.setEditorKit(new ClasspathHTMLEditorKit());
-		pane.setEditable(false);
-        pane.setContentType("text/html");
-        pane.addHyperlinkListener(new GenericHyperlinkListener());
 		pane.setMinimumSize(ED_PANE_MINE_SIZE);
-
-		add(pane);
-		scroll.setViewportView(pane);
+		pane.setOpaque(false);
 
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
@@ -51,7 +34,7 @@ public class MessageScrollPane extends JPanel implements MessageStatusDisplay {
 		gbc.gridy = 0;
 		gbc.weightx = 1.0;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(scroll, gbc);
+		add(pane, gbc);
 
 		gbc.gridx = 1;
 		gbc.weightx = 0.0;
@@ -60,23 +43,16 @@ public class MessageScrollPane extends JPanel implements MessageStatusDisplay {
 		setMessage(initialText);
 	}
 
-	private String wrapBody(String s) {
-		return "<html>" + StausIconBambooListener.BODY_WITH_STYLE + s + "</body></html>";
-
-	}
-
 	public void setMessage(String message) {
-		pane.setText(wrapBody(message));
-		pane.setBackground(Color.WHITE);
+		pane.setText(" " + message);
+		pane.setBackground(defaultColor);
+		setBackground(defaultColor);
 	}
 
 	public void setMessage(String msg, boolean isError) {
-		pane.setBackground(isError ? FAIL_COLOR : Color.WHITE);
-		pane.setText(wrapBody("<table width=\"100%\"><tr><td colspan=\"2\">" + msg + "</td></tr></table>"));
-	}
-
-	public void setStatus(String message) {
-		statusLabel.setText(message);
+		pane.setBackground(isError ? FAIL_COLOR : defaultColor);
+		setBackground(isError ? FAIL_COLOR : defaultColor);
+		pane.setText(" " + msg);
 	}
 
 	public void addMoreListener(HyperlinkListener listener) {
