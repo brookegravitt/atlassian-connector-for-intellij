fcec0f6affca0414347dc934019bccdd4d61df55
wseliga <wseliga@localhost>
2008-11-20 16:08:17 +0000

    PL-868 fixed behaviour for Mac, refactored action

--- src/main/java/com/atlassian/theplugin/idea/action/issues/CreateChangeListAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/issues/CreateChangeListAction.java
@@ -1,39 +1,35 @@
 package com.atlassian.theplugin.idea.action.issues;
 
+import com.atlassian.theplugin.idea.Constants;
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.idea.jira.IssuesToolWindowPanel;
+import com.atlassian.theplugin.jira.api.JIRAIssue;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
-import com.atlassian.theplugin.idea.jira.IssuesToolWindowPanel;
-import com.atlassian.theplugin.idea.IdeaHelper;
-import com.atlassian.theplugin.jira.api.JIRAIssue;
-import com.atlassian.theplugin.jira.model.JIRAIssueListModelBuilder;
-import com.atlassian.theplugin.jira.model.JIRAIssueListModelBuilderImpl;
 
 public class CreateChangeListAction extends AnAction {
-	public void actionPerformed(AnActionEvent e) {
-		IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(e);
-		if (panel != null) {
-			panel.createChangeListAction();
+	@Override
+	public void actionPerformed(AnActionEvent anActionEvent) {
+		final IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(anActionEvent);
+		final JIRAIssue issue = anActionEvent.getData(Constants.ISSUE_KEY);
+		if (panel != null && issue != null) {
+			panel.createChangeListAction(issue);
 		}
 	}
 
+	@Override
 	public void update(AnActionEvent anActionEvent) {
 	    super.update(anActionEvent);
 
-		JIRAIssueListModelBuilder builder = IdeaHelper.getProjectComponent(
-				IdeaHelper.getCurrentProject(anActionEvent), JIRAIssueListModelBuilderImpl.class);
-		if (builder == null || builder.getModel() == null) {
-			anActionEvent.getPresentation().setEnabled(false);
-			return;
-		}
-		JIRAIssue issue = builder.getModel().getSelectedIssue();
+		final JIRAIssue issue = anActionEvent.getData(Constants.ISSUE_KEY);
+		anActionEvent.getPresentation().setEnabled(issue != null);
 
 	    if (issue != null) {
 	        String changeListName = issue.getKey() + " - " + issue.getSummary();
-
-	        Project project = DataKeys.PROJECT.getData(anActionEvent.getDataContext());
+			final Project project = anActionEvent.getData(DataKeys.PROJECT);
 	        if (ChangeListManager.getInstance(project).findChangeList(changeListName) == null) {
 	            anActionEvent.getPresentation().setText("Create ChangeList");
 	        } else {
