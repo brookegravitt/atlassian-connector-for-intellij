7af595243f1397ec77f667280a86acc827cd330e
jgorycki <jgorycki@localhost>
2008-11-26 10:09:53 +0000

    PL-864 - in the issues view, the group by type is nice, still it would be better if the sub-tasks are grouped by it's super task.

--- src/main/java/com/atlassian/theplugin/idea/action/issues/JIRAAbstractAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/issues/JIRAAbstractAction.java
@@ -1,7 +1,5 @@
 package com.atlassian.theplugin.idea.action.issues;
 
-import com.atlassian.theplugin.idea.IdeaHelper;
-import com.atlassian.theplugin.jira.model.*;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
@@ -17,15 +15,9 @@ public abstract class JIRAAbstractAction extends AnAction {
 	@Override
 	public final void update(AnActionEvent event) {
 		super.update(event);
-		JIRAIssueListModelBuilder issueBuilder =
-				IdeaHelper.getProjectComponent(event, JIRAIssueListModelBuilderImpl.class);
-		JIRAFilterListBuilder filterBuilder = IdeaHelper.getProjectComponent(event, JIRAFilterListBuilder.class);
-		JIRAServerModel jiraServerModel = IdeaHelper.getProjectComponent(event, JIRAServerModelImpl.class);
 
-		boolean enabled = (issueBuilder != null && !issueBuilder.isModelFrozen()) 
-						&& (filterBuilder != null && !filterBuilder.isModelFrozen())
-						&& (jiraServerModel != null && !jiraServerModel.isModelFrozen());
-		event.getPresentation().setEnabled(enabled);
+		boolean enabled = ModelFreezeUpdater.getStateAndSetPresentationEnabled(event);
+		
 		if (enabled) {
 
 			onUpdate(event);
