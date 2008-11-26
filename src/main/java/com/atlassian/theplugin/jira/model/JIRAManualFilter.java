f1d7c3f2a537f312e06a41485b60ee60684d1713
pmaruszak <pmaruszak@localhost>
2008-11-26 15:39:44 +0000

    PL-740, remembers selection of filter after refresh

--- src/main/java/com/atlassian/theplugin/jira/model/JIRAManualFilter.java
+++ src/main/java/com/atlassian/theplugin/jira/model/JIRAManualFilter.java
@@ -34,7 +34,9 @@ public final class JIRAManualFilter {
 		}
 
 	}
+
 	private List<JIRAQueryFragment> queryFragment;
+
 	private String name;
 
 	JIRAManualFilter(final String name, List<JIRAQueryFragment> queryFragment) {
@@ -118,5 +120,30 @@ public final class JIRAManualFilter {
 		}
 	}
 
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
 
+		JIRAManualFilter that = (JIRAManualFilter) o;
+
+		if (name != null ? !name.equals(that.name) : that.name != null) {
+			return false;
+		}
+		if (queryFragment != null ? !queryFragment.equals(that.queryFragment) : that.queryFragment != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public int hashCode() {
+		int result;
+		result = (queryFragment != null ? queryFragment.hashCode() : 0);
+		result = 31 * result + (name != null ? name.hashCode() : 0);
+		return result;
+	}
 }
