5dcdeb3484fbcd3e28f644b371d56b1387db966d
pmaruszak <pmaruszak@localhost>
2008-11-26 11:07:23 +0000

    PL-740,  filter tree elements are renderer fix

--- src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAManualFilterTreeNode.java
+++ src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAManualFilterTreeNode.java
@@ -2,17 +2,13 @@ package com.atlassian.theplugin.idea.jira.tree;
 
 import com.atlassian.theplugin.jira.model.JIRAFilterListModel;
 import com.atlassian.theplugin.jira.model.JIRAManualFilter;
-import com.intellij.openapi.util.IconLoader;
-import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**
  * User: pmaruszak
  */
-public class JIRAManualFilterTreeNode extends JIRAAbstractTreeNode {
-	private static final Icon JIRA_FILTER_ICON = IconLoader.getIcon("/actions/showViewer.png");
+public class JIRAManualFilterTreeNode extends JIRAAbstractTreeNode {	
 	private JIRAManualFilter manualFilter;
 	private JIRAFilterListModel listModel;
 
@@ -28,21 +24,7 @@ public class JIRAManualFilterTreeNode extends JIRAAbstractTreeNode {
 
 	public JComponent getRenderer(final JComponent c, final boolean selected,
 	                              final boolean expanded, final boolean hasFocus) {
-
-
-		Color bgColor = selected ? UIUtil.getTreeSelectionBackground() : UIUtil.getTreeTextBackground();
-		Color fgColor = selected ? UIUtil.getTreeSelectionForeground() : UIUtil.getTreeTextForeground();
-
-		fgColor = c.isEnabled() ? fgColor : UIUtil.getInactiveTextColor();
-
-		JLabel label = new JLabel("Custom filter", JIRA_FILTER_ICON, SwingUtilities.LEADING);
-		label.setForeground(fgColor);
-		label.setDisabledIcon(JIRA_FILTER_ICON);
-		label.setBackground(bgColor);		
-
-		label.setEnabled(c.isEnabled());
-		label.setOpaque(true);
-		return label;
+		return new JLabel("Incorrect renderer");
 	}
 
 	public void onSelect() {
