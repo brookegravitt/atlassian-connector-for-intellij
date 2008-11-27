7dbac0024927d233ffff86fea9edfd4151c581dc
jgorycki <jgorycki@localhost>
2008-11-27 11:34:57 +0000

    some misc fixes

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/RefreshIssuesAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/RefreshIssuesAction.java
@@ -2,7 +2,10 @@ package com.atlassian.theplugin.idea.action.issues;
 
 import com.atlassian.theplugin.idea.IdeaHelper;
 import com.atlassian.theplugin.idea.jira.IssuesToolWindowPanel;
+import com.atlassian.theplugin.jira.model.JIRAIssueListModelBuilder;
+import com.atlassian.theplugin.jira.model.JIRAIssueListModelBuilderImpl;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
 
 /**
  * User: pmaruszak
@@ -17,6 +20,9 @@ public class RefreshIssuesAction extends JIRAAbstractAction {
 	}
 
 	public void onUpdate(AnActionEvent event) {
-
+		final Project project = IdeaHelper.getCurrentProject(event.getDataContext());
+		JIRAIssueListModelBuilder builder = IdeaHelper.getProjectComponent(project, JIRAIssueListModelBuilderImpl.class);
+		boolean enabled = builder != null && builder.getServer() != null;
+		event.getPresentation().setEnabled(enabled);
 	}
 }
