873d36e174017a739aa6c55460b53f9cbf5247bb
jgorycki <jgorycki@localhost>
2008-11-27 11:34:57 +0000

    some misc fixes

--- src/main/java/com/atlassian/theplugin/idea/action/issues/CreateIssueAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/issues/CreateIssueAction.java
@@ -2,7 +2,10 @@ package com.atlassian.theplugin.idea.action.issues;
 
 import com.atlassian.theplugin.idea.IdeaHelper;
 import com.atlassian.theplugin.idea.jira.IssuesToolWindowPanel;
+import com.atlassian.theplugin.jira.model.JIRAIssueListModelBuilder;
+import com.atlassian.theplugin.jira.model.JIRAIssueListModelBuilderImpl;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
 
 public class CreateIssueAction extends JIRAAbstractAction {
 	public void actionPerformed(AnActionEvent e) {
@@ -13,9 +16,9 @@ public class CreateIssueAction extends JIRAAbstractAction {
 	}
 
 	public void onUpdate(AnActionEvent event) {
-//		Project project = IdeaHelper.getCurrentProject(event.getDataContext());
-//		JIRAIssueListModelBuilder builder = IdeaHelper.getProjectComponent(project, JIRAIssueListModelBuilderImpl.class);
-//		boolean enabled = builder != null && builder.getServer() != null;
-//		event.getPresentation().setEnabled(enabled);enabled
+		final Project project = IdeaHelper.getCurrentProject(event);
+		JIRAIssueListModelBuilder builder = IdeaHelper.getProjectComponent(project, JIRAIssueListModelBuilderImpl.class);
+		boolean enabled = builder != null && builder.getServer() != null;
+		event.getPresentation().setEnabled(enabled);
 	}
 }
