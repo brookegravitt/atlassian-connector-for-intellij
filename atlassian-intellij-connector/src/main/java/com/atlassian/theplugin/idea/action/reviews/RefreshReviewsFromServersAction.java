540b2294d2d813a2bc3c139bd2947c355200648b
jjaroczynski <jjaroczynski@localhost>
2008-12-08 16:23:18 +0000

    PL-906: refresh implemented

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/reviews/RefreshReviewsFromServersAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/reviews/RefreshReviewsFromServersAction.java
@@ -1,5 +1,7 @@
 package com.atlassian.theplugin.idea.action.reviews;
 
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.idea.crucible.ReviewsToolWindowPanel;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
@@ -7,8 +9,13 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
  * User: pmaruszak
  */
 public class RefreshReviewsFromServersAction extends AnAction {
-	public void actionPerformed(AnActionEvent event) {
-		
+
+	@Override
+	public void actionPerformed(final AnActionEvent e) {
+		final ReviewsToolWindowPanel panel = IdeaHelper.getReviewsToolWindowPanel(e);
+		if (panel != null) {
+			panel.refresh();
+		}
+
 	}
-		
 }
