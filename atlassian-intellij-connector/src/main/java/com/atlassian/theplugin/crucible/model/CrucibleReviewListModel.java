c38cb184076f43e4365e011de74481bfae8b37f7
pmaruszak <pmaruszak@localhost>
2008-12-17 15:28:45 +0000

    PL-962, style fixes

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/crucible/model/CrucibleReviewListModel.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/crucible/model/CrucibleReviewListModel.java
@@ -34,7 +34,8 @@ public interface CrucibleReviewListModel {
 	void removeAll();
 	void addListener(CrucibleReviewListModelListener listener);
 	void removeListener(CrucibleReviewListModelListener listener);
-	void updateReviews(final long epoch, final Map<CrucibleFilter, ReviewNotificationBean> reviews, final boolean sendNotifications);
+	void updateReviews(final long epoch, final Map<CrucibleFilter, ReviewNotificationBean> reviews, 
+					   final boolean sendNotifications);
 	ReviewAdapter getSelectedReview();
 	void setSelectedReview(ReviewAdapter review);
 	long getEpoch();
