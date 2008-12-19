3f9cd0e4b391b92f7562647426177ee88125f7ec
mwent <mwent@localhost>
2008-12-19 06:36:13 +0000

    PL-962

--- src/main/java/com/atlassian/theplugin/crucible/model/ReviewListModelBuilder.java
+++ src/main/java/com/atlassian/theplugin/crucible/model/ReviewListModelBuilder.java
@@ -1,7 +1,13 @@
 package com.atlassian.theplugin.crucible.model;
 
+import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFilter;
+import com.atlassian.theplugin.idea.crucible.ReviewNotificationBean;
+
+import java.util.Map;
+
 public interface ReviewListModelBuilder {
-	void getReviewsFromServer(final CrucibleReviewListModel crucibleReviewListModel,
-							  final UpdateReason updateReason,
-							  final long epoch);
+	Map<CrucibleFilter, ReviewNotificationBean> getReviewsFromServer(
+			final CrucibleReviewListModel crucibleReviewListModel,
+			final UpdateReason updateReason,
+			final long epoch) throws InterruptedException;
 }
