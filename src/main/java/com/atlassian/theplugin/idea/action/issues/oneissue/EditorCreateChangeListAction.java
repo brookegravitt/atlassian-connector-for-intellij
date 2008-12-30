e761bcc4c6ddbd90fc50fc267b9d3a281126ee96
jgorycki <jgorycki@localhost>
2008-12-30 16:21:50 +0000

    PL-978, PL-979, PL-980

---
+++ src/main/java/com/atlassian/theplugin/idea/action/issues/oneissue/EditorCreateChangeListAction.java
@@ -0,0 +1,24 @@
+package com.atlassian.theplugin.idea.action.issues.oneissue;
+
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.idea.jira.IssuesToolWindowPanel;
+import com.atlassian.theplugin.idea.jira.IssueToolWindow;
+import com.atlassian.theplugin.jira.api.JIRAIssue;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * User: jgorycki
+ * Date: Dec 30, 2008
+ * Time: 4:11:01 PM
+ */
+public class EditorCreateChangeListAction extends AnAction {
+	@Override
+	public void actionPerformed(AnActionEvent e) {
+		final IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(e);
+		JIRAIssue issue = IssueToolWindow.getIssue(e.getPlace());
+		if (panel != null && issue != null) {
+			panel.createChangeListAction(issue);
+		}
+	}
+}
