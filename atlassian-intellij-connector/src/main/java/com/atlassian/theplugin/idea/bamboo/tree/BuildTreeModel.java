6b96d38b57df882890b85ad33c4fd646e9877a03
jjaroczynski <jjaroczynski@localhost>
2009-01-06 12:56:25 +0000

    PL-787: build tree model skeleton

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/bamboo/tree/BuildTreeModel.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/bamboo/tree/BuildTreeModel.java
@@ -15,10 +15,144 @@
  */
 package com.atlassian.theplugin.idea.bamboo.tree;
 
-import javax.swing.table.DefaultTableModel;
+import com.atlassian.theplugin.idea.bamboo.BambooBuildAdapterIdea;
+import com.atlassian.theplugin.idea.bamboo.BuildGroupBy;
+import com.atlassian.theplugin.idea.bamboo.BuildModel;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * @author Jacek Jaroczynski
  */
-public class BuildTreeModel extends DefaultTableModel {
+public class BuildTreeModel extends DefaultTreeModel {
+
+	BuildModel buildModel = new BuildModel();
+
+	private BuildGroupBy groupBy = BuildGroupBy.NONE;
+
+	private BuildNodeManipulator generalNodeManipulator;
+	private BuildNodeManipulator stateNodeManipulator;
+	private BuildNodeManipulator serverNodeManipulator;
+	private BuildNodeManipulator authorNodeManipulator;
+	private BuildNodeManipulator projectNodeManipulator;
+
+	public BuildTreeModel() {
+		super(new DefaultMutableTreeNode());
+
+		// todo add group by node manipulators
+		generalNodeManipulator = new GeneralBuildNodeManipulator(buildModel, getRoot());
+		stateNodeManipulator = new GeneralBuildNodeManipulator(buildModel, getRoot());//StateNodeManipulator(buildModel, getRoot());
+		serverNodeManipulator = new GeneralBuildNodeManipulator(buildModel, getRoot());//new ServerNodeManipulator(buildModel, getRoot());
+		authorNodeManipulator = new GeneralBuildNodeManipulator(buildModel, getRoot());//AuthorNodeManipulator(buildModel, getRoot());
+		projectNodeManipulator = new GeneralBuildNodeManipulator(buildModel, getRoot());//ProjectNodeManipulator(buildModel, getRoot());
+	}
+
+	/**
+	 * Sets groupBy field used to group the tree and triggers tree to rebuild
+	 * Only tree should use that method.
+	 * @param aGroupBy
+	 */
+	public void groupBy(BuildGroupBy aGroupBy) {
+		this.groupBy = aGroupBy;
+
+		// clear entire tree
+		getRoot().removeAllChildren();
+
+		// redraw tree
+		nodeStructureChanged(getRoot());
+	}
+
+	/**
+	 * Simple setter (does not trigger tree to rebuild)
+	 * @param groupBy
+	 */
+	public void setGroupBy(BuildGroupBy groupBy) {
+		this.groupBy = groupBy;
+	}
+
+	/*
+	Override TreeModel methods
+	 */
+
+	@Override
+	public DefaultMutableTreeNode getRoot() {
+		return (DefaultMutableTreeNode) super.getRoot();
+	}
+
+	@Override
+	public Object getChild(Object parent, int index) {
+
+		switch (groupBy) {
+			case AUTHOR:
+				return authorNodeManipulator.getChild(parent, index);
+			case PROJECT:
+				return projectNodeManipulator.getChild(parent, index);
+			case SERVER:
+				return serverNodeManipulator.getChild(parent, index);
+			case STATE:
+				return stateNodeManipulator.getChild(parent, index);
+			case NONE:
+			default:
+				return generalNodeManipulator.getChild(parent, index);
+		}
+	}
+
+	@Override
+	public int getChildCount(Object parent) {
+
+		switch (groupBy) {
+			case AUTHOR:
+				return authorNodeManipulator.getChildCount(parent);
+			case PROJECT:
+				return projectNodeManipulator.getChildCount(parent);
+			case SERVER:
+				return serverNodeManipulator.getChildCount(parent);
+			case STATE:
+				return stateNodeManipulator.getChildCount(parent);
+			case NONE:
+			default:
+				return generalNodeManipulator.getChildCount(parent);
+		}
+	}
+
+	@Override
+	public boolean isLeaf(Object node) {
+		if (node == getRoot()) {
+//				|| node instanceof CrucibleReviewStateTreeNode
+//				|| node instanceof CrucibleReviewServerTreeNode
+//				|| node instanceof CrucibleReviewAuthorTreeNode
+//				|| node instanceof CrucibleReviewProjectTreeNode) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public void valueForPathChanged(TreePath path, Object newValue) {
+		System.out.println("valueForPathChanged");
+	}
+
+	@Override
+	// todo add group by handling if necessary
+	public int getIndexOfChild(Object parent, Object child) {
+		if (parent == getRoot()) {
+			if (child instanceof BuildTreeNode) {
+				BambooBuildAdapterIdea build = ((BuildTreeNode) child).getBuild();
+				return new ArrayList<BambooBuildAdapterIdea>(buildModel.getBuilds()).indexOf(build);
+			}
+		}
+
+		return -1;
+	}
+
+	public void setBuilds(Collection<BambooBuildAdapterIdea> builds) {
+		buildModel.setBuils(builds);
+
+		// todo refresh tree
+	}
 }
