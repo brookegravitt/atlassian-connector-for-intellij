1ab7f45e9483d5ba77fee20c38ae6bec05ea299d
jjaroczynski <jjaroczynski@localhost>
2009-01-09 18:05:52 +0000

    PL-787: * label and comment actions * details panel available only for bamboo2

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/bamboo/onebuild/LabelBuildAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/bamboo/onebuild/LabelBuildAction.java
@@ -1,24 +1,23 @@
 package com.atlassian.theplugin.idea.action.bamboo.onebuild;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.atlassian.theplugin.idea.bamboo.BambooTableToolWindowPanel;
-import com.atlassian.theplugin.idea.bamboo.BuildToolWindow;
-import com.atlassian.theplugin.idea.IdeaHelper;
 
 /**
  * User: jgorycki
  * Date: Jan 7, 2009
  * Time: 2:23:23 PM
  */
-public class LabelBuildAction extends AnAction {
-	public void actionPerformed(AnActionEvent e) {
-		BambooTableToolWindowPanel panel = IdeaHelper.getBambooToolWindowPanel(e);
-		if (panel != null) {
-			BuildToolWindow btw = IdeaHelper.getBuildToolWindow(e);
-			if (btw != null) {
-				panel.openLabelDialog(btw.getBuild(e.getPlace()));
-			}
+public class LabelBuildAction extends AbstractBuildDetailsAction {
+	@Override
+	public void actionPerformed(AnActionEvent event) {
+		labelBuild(event);
+	}
+
+	@Override
+	public void update(final AnActionEvent event) {
+		super.update(event);
+		if (getBuild(event) != null && !getBuild(event).isBamboo2()) {
+			event.getPresentation().setEnabled(false);
 		}
 	}
 }
