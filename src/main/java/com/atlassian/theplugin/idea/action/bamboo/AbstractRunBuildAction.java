f9b87feebf6c9ba28b22982311f80570d72ebe6c
jjaroczynski <jjaroczynski@localhost>
2009-01-09 14:35:46 +0000

    PL-787: run build action in the new builds panel (action removed from the old panel)

---
+++ src/main/java/com/atlassian/theplugin/idea/action/bamboo/AbstractRunBuildAction.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) 2008 Atlassian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atlassian.theplugin.idea.action.bamboo;
+
+import com.atlassian.theplugin.commons.bamboo.BambooServerFacadeImpl;
+import com.atlassian.theplugin.commons.exception.ServerPasswordNotProvidedException;
+import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.idea.bamboo.BambooBuildAdapterIdea;
+import com.atlassian.theplugin.util.PluginUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+/**
+ * @author Jacek Jaroczynski
+ */
+public abstract class AbstractRunBuildAction extends AnAction {
+
+	@Override
+	public void actionPerformed(AnActionEvent event) {
+
+		BambooBuildAdapterIdea build = getBuild(event);
+		Project project = IdeaHelper.getCurrentProject(event);
+
+		if (build != null && project != null) {
+			executeBuild(project, build);
+		}
+	}
+
+	@Override
+	public void update(AnActionEvent event) {
+		super.update(event);
+		BambooBuildAdapterIdea build = getBuild(event);
+		if (build != null && build.getEnabled() && build.getBuildKey() != null && build.getBuildNumber() != null) {
+			event.getPresentation().setEnabled(true);
+		} else {
+			event.getPresentation().setEnabled(false);
+		}
+	}
+
+	protected void executeBuild(final Project project, @NotNull final BambooBuildAdapterIdea build) {
+
+		Task.Backgroundable executeTask = new Task.Backgroundable(project, "Starting Build", false) {
+			@Override
+			public void run(final ProgressIndicator indicator) {
+
+				try {
+					setStatusMessageUIThread(project, "Starting build on plan: " + build.getBuildKey());
+					BambooServerFacadeImpl.getInstance(PluginUtil.getLogger()).
+							executeBuild(build.getServer(), build.getBuildKey());
+					setStatusMessageUIThread(project, "Build executed on plan: " + build.getBuildKey());
+				} catch (ServerPasswordNotProvidedException e) {
+					setStatusMessageUIThread(project, "Build not executed: Password not provided for server");
+				} catch (RemoteApiException e) {
+					setStatusMessageUIThread(project, "Build not executed: " + e.getMessage());
+				}
+			}
+		};
+
+		ProgressManager.getInstance().run(executeTask);
+	}
+
+	private void setStatusMessageUIThread(final Project project, final String message) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				setStatusMessage(project, message);
+			}
+		});
+	}
+
+	protected abstract BambooBuildAdapterIdea getBuild(final AnActionEvent event);
+
+	protected abstract void setStatusMessage(final Project project, final String message);
+}
+
