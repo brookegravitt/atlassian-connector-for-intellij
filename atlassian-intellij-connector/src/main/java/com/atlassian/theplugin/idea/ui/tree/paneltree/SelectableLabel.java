dd4d1e37082863bfa05f04ffa4193743fd4fc91e
jgorycki <jgorycki@localhost>
2009-01-12 12:24:35 +0000

    PL-983 - Build Panel: Changes Tab

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/ui/tree/paneltree/SelectableLabel.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/ui/tree/paneltree/SelectableLabel.java
@@ -12,21 +12,27 @@ import java.awt.*;
  */
 
 public final class SelectableLabel extends JLabel {
+	private final boolean enabled;
+	private final int height;
+
 	public SelectableLabel(boolean selected, boolean enabled, String text, int height) {
 		this(selected, enabled, text, null, SwingConstants.LEADING, height);
 	}
 
-	public SelectableLabel(boolean selected, boolean enabled, String text, int height, boolean bold) {
-		this(selected, enabled, text, null, SwingConstants.LEADING, height, bold);
+	public SelectableLabel(boolean selected, boolean enabled, String text,
+						   int height, boolean bold, boolean doSizeAdjustment) {
+		this(selected, enabled, text, null, SwingConstants.LEADING, height, bold, doSizeAdjustment);
 	}
 
 	public SelectableLabel(boolean selected, boolean enabled, String text, Icon icon, int alignment, int height) {
-		this(selected, enabled, text, icon, alignment, height, false);
+		this(selected, enabled, text, icon, alignment, height, false, true);
 	}
 
 	public SelectableLabel(boolean selected, boolean enabled, String text,
-						   Icon icon, int alignment, int height, boolean bold) {
+						   Icon icon, int alignment, int height, boolean bold, boolean doSizeAdjustment) {
 		super(text, SwingConstants.LEADING);
+		this.enabled = enabled;
+		this.height = height;
 
 		if (bold) {
 			setFont(getFont().deriveFont(Font.BOLD));
@@ -38,14 +44,25 @@ public final class SelectableLabel extends JLabel {
 		}
 
 		setEnabled(enabled);
+		setSelected(selected);
+
+		if (doSizeAdjustment) {
+			adjustPreferredSize();
+		}
+		
+		setHorizontalTextPosition(alignment);
+		setOpaque(true);
+	}
+
+	public void setSelected(boolean selected) {
 		Color fgColor = selected ? UIUtil.getTreeSelectionForeground() : UIUtil.getTreeTextForeground();
 		fgColor = enabled ? fgColor : UIUtil.getInactiveTextColor();
-
-		setHorizontalTextPosition(alignment);
-		setPreferredSize(new Dimension((int) getPreferredSize().getWidth(), height));
-		setOpaque(true);
 		setBackground(selected ? UIUtil.getTreeSelectionBackground() : UIUtil.getTreeTextBackground());
 		setForeground(fgColor);
+	}
+
+	public void adjustPreferredSize() {
+		setPreferredSize(new Dimension((int) getPreferredSize().getWidth(), height));
 	}
 }
 
