2c7551b3b676c121fd3cd6b6f105777ab0f285e7
jgorycki <jgorycki@localhost>
2009-01-14 15:10:08 +0000

    PL-1045 - Very slow filtering of reviews in Reviews tab

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/tree/node/CrucibleReviewTreeNode.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/tree/node/CrucibleReviewTreeNode.java
@@ -28,10 +28,12 @@ public class CrucibleReviewTreeNode extends ReviewTreeNode {
 
 	private final ReviewAdapter review;
 	private static final int MAX_LENGTH = 1000;
-
+	private RendererPanel renderer;
+	
 	public CrucibleReviewTreeNode(ReviewAdapter review) {
 		super(review.getPermId().getId(), null, null);
 		this.review = review;
+		renderer = new RendererPanel();
 	}
 
 	@Override
@@ -44,76 +46,106 @@ public class CrucibleReviewTreeNode extends ReviewTreeNode {
 		return review.getPermId().getId() + ": " + review.getName();
 	}
 
+	private class RendererPanel extends JPanel {
+		private SelectableLabel key;
+		private SelectableLabel summary;
+		private SelectableLabel state;
+		private SelectableLabel author;
+		private SelectableLabel created;
+		private JPanel padding;
+
+		private RendererPanel() {
+			super(new GridBagLayout());
+
+			setBackground(UIUtil.getTreeTextBackground());
+
+			GridBagConstraints gbc = new GridBagConstraints();
+			gbc.gridx = 0;
+			gbc.gridy = 0;
+			gbc.weightx = 0.0;
+			gbc.fill = GridBagConstraints.NONE;
+
+			gbc.insets = new Insets(0, 0, 0, 0);
+			key = new SelectableLabel(true, true, review.getPermId().getId() + ": ", ICON_HEIGHT);
+			add(key, gbc);
+
+			gbc.gridx++;
+			gbc.weightx = 1.0;
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			summary = new SelectableLabel(true, true, review.getName(), ICON_HEIGHT);
+			add(summary, gbc);
+
+			gbc.gridx++;
+			gbc.weightx = 0.0;
+			gbc.fill = GridBagConstraints.NONE;
+			state = new SelectableLabel(true, true, review.getState().value(), null,
+					SwingConstants.LEADING, ICON_HEIGHT);
+			setFixedComponentSize(state, STATUS_LABEL_WIDTH, ICON_HEIGHT);
+			add(state, gbc);
+
+			gbc.gridx++;
+			gbc.weightx = 0.0;
+			gbc.insets = new Insets(0, 0, 0, 0);
+			author = new SelectableLabel(true, true, review.getAuthor().getDisplayName(), null,
+					SwingConstants.LEADING, ICON_HEIGHT);
+			setFixedComponentSize(author, AUTHOR_LABEL_WIDTH, ICON_HEIGHT);
+			add(author, gbc);
+
+			DateFormat dfo = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+			String t = dfo.format(review.getCreateDate());
+			gbc.gridx++;
+			gbc.weightx = 0.0;
+			created = new SelectableLabel(true, true, t, null, SwingConstants.LEADING, ICON_HEIGHT);
+			created.setHorizontalAlignment(SwingConstants.RIGHT);
+			Dimension minDimension = created.getPreferredSize();
+			minDimension.setSize(
+					Math.max(PluginUtil.getDateWidth(created, dfo), minDimension.getWidth()), minDimension.getHeight());
+			setFixedComponentSize(created, minDimension.width, ICON_HEIGHT);
+			add(created, gbc);
+
+			padding = new JPanel();
+			gbc.gridx++;
+			gbc.weightx = 0.0;
+			gbc.fill = GridBagConstraints.NONE;
+			padding.setPreferredSize(new Dimension(RIGHT_PADDING, ICON_HEIGHT));
+			padding.setMinimumSize(new Dimension(RIGHT_PADDING, ICON_HEIGHT));
+			padding.setMaximumSize(new Dimension(RIGHT_PADDING, ICON_HEIGHT));
+			padding.setOpaque(true);
+			add(padding, gbc);
+
+			// now black magic here: 2-pass creation of multiline tooltip, with maximum width of MAX_TOOLTIP_WIDTH
+			final JToolTip jToolTip = createToolTip();
+			jToolTip.setTipText(buildTolltip(0));
+			final int prefWidth = jToolTip.getPreferredSize().width;
+			int width = prefWidth > MAX_TOOLTIP_WIDTH ? MAX_TOOLTIP_WIDTH : 0;
+			setToolTipText(buildTolltip(width));
+		}
+
+		public void setParameters(boolean selected, boolean enabled) {
+			key.setSelected(selected);
+			key.setEnabled(enabled);
+
+			summary.setSelected(selected);
+			summary.setEnabled(enabled);
+
+			state.setSelected(selected);
+			state.setEnabled(enabled);
+
+			author.setSelected(selected);
+			author.setEnabled(enabled);
+
+			created.setSelected(selected);
+			created.setEnabled(enabled);
+
+			padding.setBackground(selected ? UIUtil.getTreeSelectionBackground() : UIUtil.getTreeTextBackground());
+			padding.setForeground(selected ? UIUtil.getTreeSelectionForeground() : UIUtil.getTreeTextForeground());
+		}
+	}
+
 	@Override
 	public JComponent getRenderer(JComponent c, boolean selected, boolean expanded, boolean hasFocus) {
-		boolean enabled = c.isEnabled();
-
-		JPanel p = new JPanel();
-		p.setBackground(UIUtil.getTreeTextBackground());
-		p.setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.weightx = 0.0;
-		gbc.fill = GridBagConstraints.NONE;
-
-		gbc.insets = new Insets(0, 0, 0, 0);
-		JLabel key = new SelectableLabel(selected, enabled, review.getPermId().getId() + ": ", ICON_HEIGHT);
-		p.add(key, gbc);
-
-		gbc.gridx++;
-		gbc.weightx = 1.0;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		JLabel summary = new SelectableLabel(selected, enabled, review.getName(), ICON_HEIGHT);
-		p.add(summary, gbc);
-
-		gbc.gridx++;
-		gbc.weightx = 0.0;
-		gbc.fill = GridBagConstraints.NONE;
-		JLabel state = new SelectableLabel(selected, enabled, review.getState().value(), null,
-				SwingConstants.LEADING, ICON_HEIGHT);
-		setFixedComponentSize(state, STATUS_LABEL_WIDTH, ICON_HEIGHT);
-		p.add(state, gbc);
-
-		gbc.gridx++;
-		gbc.weightx = 0.0;
-		gbc.insets = new Insets(0, 0, 0, 0);
-		JLabel author = new SelectableLabel(selected, enabled, review.getAuthor().getDisplayName(), null,
-				SwingConstants.LEADING, ICON_HEIGHT);
-		setFixedComponentSize(author, AUTHOR_LABEL_WIDTH, ICON_HEIGHT);
-		p.add(author, gbc);
-
-		DateFormat dfo = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
-		String t = dfo.format(review.getCreateDate());
-		gbc.gridx++;
-        gbc.weightx = 0.0;
-		JLabel created = new SelectableLabel(selected, enabled, t, null, SwingConstants.LEADING, ICON_HEIGHT);
-		created.setHorizontalAlignment(SwingConstants.RIGHT);
-		Dimension minDimension = created.getPreferredSize();
-		minDimension.setSize(
-				Math.max(PluginUtil.getDateWidth(created, dfo), minDimension.getWidth()), minDimension.getHeight());
-		setFixedComponentSize(created, minDimension.width, ICON_HEIGHT);
-		p.add(created, gbc);
-
-		JPanel padding = new JPanel();
-        gbc.gridx++;
-        gbc.weightx = 0.0;
-		gbc.fill = GridBagConstraints.NONE;
-        padding.setPreferredSize(new Dimension(RIGHT_PADDING, ICON_HEIGHT));
-        padding.setMinimumSize(new Dimension(RIGHT_PADDING, ICON_HEIGHT));
-        padding.setMaximumSize(new Dimension(RIGHT_PADDING, ICON_HEIGHT));
-		padding.setBackground(selected ? UIUtil.getTreeSelectionBackground() : UIUtil.getTreeTextBackground());
-		padding.setForeground(selected ? UIUtil.getTreeSelectionForeground() : UIUtil.getTreeTextForeground());
-        padding.setOpaque(true);
-        p.add(padding, gbc);
-
-		// now black magic here: 2-pass creation of multiline tooltip, with maximum width of MAX_TOOLTIP_WIDTH  
-		final JToolTip jToolTip = p.createToolTip();
-		jToolTip.setTipText(buildTolltip(0));
-		final int prefWidth = jToolTip.getPreferredSize().width;
-		int width = prefWidth > MAX_TOOLTIP_WIDTH ? MAX_TOOLTIP_WIDTH : 0;
-		p.setToolTipText(buildTolltip(width));
-		return p;
+		renderer.setParameters(selected, c.isEnabled());
+		return renderer;
 	}
 
 	private String buildTolltip(int width) {
