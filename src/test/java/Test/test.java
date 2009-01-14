f3f17cb9eb014b4fa8fe2092c0f86961633ca8a0
pmaruszak <pmaruszak@localhost>
2009-01-14 14:25:53 +0000

    testing PL-773, 2

---
+++ src/test/java/Test/test.java
@@ -0,0 +1,11 @@
+package Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Jan 14, 2009
+ * Time: 1:44:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
