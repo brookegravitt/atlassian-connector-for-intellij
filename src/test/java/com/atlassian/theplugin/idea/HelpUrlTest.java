17ddac399b55128254be0e08a64d03b4353078e7
wseliga <wseliga@localhost>
2009-01-15 11:03:04 +0000

    NONE avoiding offending words in the source code

--- src/test/java/com/atlassian/theplugin/idea/HelpUrlTest.java
+++ src/test/java/com/atlassian/theplugin/idea/HelpUrlTest.java
@@ -27,6 +27,6 @@ public class HelpUrlTest extends TestCase {
 	}
 
 	public void testGetBadHelpTopic() {
-		assertNull(HelpUrl.getHelpUrl("dupa"));
+		assertNull(HelpUrl.getHelpUrl("theplugin.someinvalid.nonexsiting.key"));
 	}
 }
