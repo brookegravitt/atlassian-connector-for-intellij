b7d1516e3084ec6783c3dbff9638f4f016c6cb28
mwent <mwent@localhost>
2009-01-16 20:58:35 +0000

    PL-1057 - Opening review causes error intermittently

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/VcsIdeaHelper.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/VcsIdeaHelper.java
@@ -370,17 +370,6 @@ public final class VcsIdeaHelper {
 		boolean shouldNavigate();
 	}
 
-	public interface OpenDiffAction {
-		/**
-		 * Open file view based on two file revisions
-		 * Will be always invoked in UI thread
-		 *
-		 * @param displayFile   descriptor of the main file to display
-		 * @param referenceFile reference file (e.g. "from file" in diff)
-		 */
-		void run(OpenFileDescriptor displayFile, VirtualFile referenceFile, CommitType commitType);
-	}
-
 	private static class FetchingTwoFilesTask extends Task.Backgroundable {
 		private OpenFileDescriptor displayDescriptor;
 		private VirtualFile referenceVirtualFile;
