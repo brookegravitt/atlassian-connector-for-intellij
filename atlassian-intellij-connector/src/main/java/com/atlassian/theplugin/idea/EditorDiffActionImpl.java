66c50963124788d34e84b91b79b95af5c0bcd49f
mwent <mwent@localhost>
2009-01-16 20:58:35 +0000

    PL-1057 - Opening review causes error intermittently

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/EditorDiffActionImpl.java
@@ -0,0 +1,56 @@
+package com.atlassian.theplugin.idea;
+
+import com.atlassian.theplugin.commons.crucible.api.model.CommitType;
+import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFileInfo;
+import com.atlassian.theplugin.commons.crucible.api.model.ReviewAdapter;
+import com.atlassian.theplugin.idea.crucible.ChangeViewer;
+import com.atlassian.theplugin.idea.crucible.CommentHighlighter;
+import com.intellij.openapi.diff.FileContent;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public class EditorDiffActionImpl implements OpenDiffAction {
+	private final Project project;
+	private final ReviewAdapter review;
+	private final CrucibleFileInfo reviewItem;
+
+	public EditorDiffActionImpl(@NotNull final Project project,
+			@NotNull final ReviewAdapter review,
+			@NotNull final CrucibleFileInfo reviewItem) {
+		this.project = project;
+		this.review = review;
+		this.reviewItem = reviewItem;
+	}
+
+	public void run(OpenFileDescriptor displayFile, VirtualFile referenceFile, CommitType commitType) {
+		FileEditorManager fem = FileEditorManager.getInstance(project);
+		Editor editor = fem.openTextEditor(displayFile, true);
+		if (editor == null) {
+			return;
+		}
+		switch (commitType) {
+			case Moved:
+			case Copied:
+			case Modified:
+				final Document displayDocument = new FileContent(project, displayFile.getFile())
+						.getDocument();
+				final Document referenceDocument = new FileContent(project, referenceFile).getDocument();
+				ChangeViewer.highlightChangesInEditor(project, /*editor, */referenceDocument, displayDocument
+						, reviewItem.getOldFileDescriptor().getRevision()
+						, reviewItem.getFileDescriptor().getRevision());
+				break;
+			case Added:
+				break;
+			case Deleted:
+				break;
+			default:
+				break;
+		}
+		CommentHighlighter.highlightCommentsInEditor(project, editor, review, reviewItem, displayFile);
+	}
+}
--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleEditorFactoryListener.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleEditorFactoryListener.java
@@ -1,28 +1,23 @@
 package com.atlassian.theplugin.idea.crucible;
 
 import com.atlassian.theplugin.commons.crucible.ValueNotYetInitialized;
-import com.atlassian.theplugin.commons.crucible.api.model.CommitType;
 import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFileInfo;
 import com.atlassian.theplugin.commons.crucible.api.model.ReviewAdapter;
 import com.atlassian.theplugin.commons.crucible.api.model.VersionedComment;
 import com.atlassian.theplugin.crucible.model.CrucibleReviewListModel;
+import com.atlassian.theplugin.idea.EditorDiffActionImpl;
 import com.atlassian.theplugin.idea.VcsIdeaHelper;
 import com.atlassian.theplugin.util.CodeNavigationUtil;
-import com.intellij.openapi.diff.FileContent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.event.EditorFactoryEvent;
 import com.intellij.openapi.editor.event.EditorFactoryListener;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
-import java.util.Set;
 
 public class CrucibleEditorFactoryListener implements EditorFactoryListener {
 	private final Project project;
@@ -35,27 +30,24 @@ public class CrucibleEditorFactoryListener implements EditorFactoryListener {
 	}
 
 	public void editorCreated(final EditorFactoryEvent editorFactoryEvent) {
-		Collection<ReviewAdapter> reviews = crucibleReviewListModel.getOpenInIdeReviews();
-		if (!reviews.isEmpty()) {
-			for (ReviewAdapter review : reviews) {
-				try {
-					Set<CrucibleFileInfo> files = review.getFiles();
-					for (CrucibleFileInfo fileInfo : files) {
-						PsiFile f = CodeNavigationUtil
-								.guessCorrespondingPsiFile(project, fileInfo.getFileDescriptor().getName());
-						if (f != null && f.getVirtualFile() != null) {
-							Document doc = editorFactoryEvent.getEditor().getDocument();
-							VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(doc);
-							if (virtualFile != null) {
-								if (virtualFile.getPath().equals(f.getVirtualFile().getPath())) {
-									showVirtualFileWithComments(project, editorFactoryEvent.getEditor(), virtualFile, review,
-											fileInfo);
-								}
-							}
+		Document doc = editorFactoryEvent.getEditor().getDocument();
+		VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(doc);
+		if (virtualFile != null) {
+			Collection<ReviewAdapter> reviews = crucibleReviewListModel.getOpenInIdeReviews();
+			if (!reviews.isEmpty()) {
+				for (ReviewAdapter review : reviews) {
+					try {
+						CrucibleFileInfo crucibleFile = CodeNavigationUtil
+								.getBestMatchingCrucibleFileInfo(virtualFile.getPath(), review.getFiles());
+						if (crucibleFile != null) {
+							showVirtualFileWithComments(project, editorFactoryEvent.getEditor(), virtualFile,
+									review,
+									crucibleFile);
+
 						}
+					} catch (ValueNotYetInitialized valueNotYetInitialized) {
+						// don't do anything - should not happen, but even if happens - we don't want to break file opening
 					}
-				} catch (ValueNotYetInitialized valueNotYetInitialized) {
-					// don't do anything - should not happen, but even if happens - we don't want to break file opening
 				}
 			}
 		}
@@ -84,36 +76,6 @@ public class CrucibleEditorFactoryListener implements EditorFactoryListener {
 				, reviewItem.getCommitType()
 				, line
 				, 1
-				, new VcsIdeaHelper.OpenDiffAction() {
-
-					public void run(OpenFileDescriptor displayFile, VirtualFile referenceFile, CommitType commitType) {
-						if (referenceFile == null && reviewItem.getCommitType() == CommitType.Modified) {
-							Messages.showErrorDialog(project,
-									"Cannot fetch " + reviewItem.getOldFileDescriptor().getAbsoluteUrl()
-											+ ".\nAnnotated file cannot be displayed.", "Error");
-							return;
-						}
-
-						switch (commitType) {
-							case Moved:
-							case Copied:
-							case Modified:
-								final Document displayDocument = new FileContent(project, displayFile.getFile())
-										.getDocument();
-								final Document referenceDocument = new FileContent(project, referenceFile).getDocument();
-								ChangeViewer.highlightChangesInEditor(project, /*editor, */referenceDocument, displayDocument
-										, reviewItem.getOldFileDescriptor().getRevision()
-										, reviewItem.getFileDescriptor().getRevision());
-								break;
-							case Added:
-								break;
-							case Deleted:
-								break;
-							default:
-								break;
-						}
-						CommentHighlighter.highlightCommentsInEditor(project, editor, review, reviewItem, displayFile);
-					}
-				});
+				, new EditorDiffActionImpl(project, review, reviewItem));
 	}
 }
