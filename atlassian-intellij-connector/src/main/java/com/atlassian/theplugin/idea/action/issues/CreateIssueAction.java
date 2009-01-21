a8ddad530acfdd13c49b3a4d15d57c30cb2e017f
wseliga <wseliga@localhost>
2009-01-21 16:50:07 +0000

    PL-827 implemented

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/CreateIssueAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/CreateIssueAction.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 
 public class CreateIssueAction extends JIRAAbstractAction {
+	@Override
 	public void actionPerformed(AnActionEvent e) {
 		IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(e);
 		if (panel != null) {
@@ -15,6 +16,7 @@ public class CreateIssueAction extends JIRAAbstractAction {
 		}
 	}
 
+	@Override
 	public void onUpdate(AnActionEvent event) {
 		final Project project = IdeaHelper.getCurrentProject(event);
 		JIRAIssueListModelBuilder builder = IdeaHelper.getProjectComponent(project, JIRAIssueListModelBuilderImpl.class);
