354da1154cd1d05c29a1efc4575f0eff7192a343
mwent <mwent@localhost>
2009-02-03 12:36:10 +0000

    PL-1084 - Project Configuration - Provide link from project settings to ide settings and vice versa

--- src/main/java/com/atlassian/theplugin/idea/action/ShowProjectSettingsAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/ShowProjectSettingsAction.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,11 +29,16 @@ import com.intellij.openapi.project.Project;
  */
 public class ShowProjectSettingsAction extends AnAction {
 	@Override
+	public void update(final AnActionEvent event) {
+		event.getPresentation().setEnabled(IdeaHelper.getCurrentProject(event) != null);
+	}
+
+	@Override
 	public void actionPerformed(AnActionEvent event) {
 		Project project = IdeaHelper.getCurrentProject(event);
 		if (project != null) {
 			Configurable component = project.getComponent(ProjectConfigurationComponent.class);
-			ShowSettingsUtil.getInstance().editConfigurable(project, component);		
+			ShowSettingsUtil.getInstance().editConfigurable(project, component);
 		}
 	}
 
