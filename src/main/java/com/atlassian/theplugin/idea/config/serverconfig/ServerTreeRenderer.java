a867e86e50319823fb7059f78e28a58a9c18e324
pmaruszak <pmaruszak@localhost>
2009-02-03 15:59:18 +0000

    PL-1086

--- src/main/java/com/atlassian/theplugin/idea/config/serverconfig/ServerTreeRenderer.java
+++ src/main/java/com/atlassian/theplugin/idea/config/serverconfig/ServerTreeRenderer.java
@@ -16,6 +16,7 @@
 
 package com.atlassian.theplugin.idea.config.serverconfig;
 
+import com.atlassian.theplugin.idea.config.serverconfig.model.ServerInfoNode;
 import com.atlassian.theplugin.idea.config.serverconfig.model.ServerNode;
 import com.atlassian.theplugin.idea.config.serverconfig.model.ServerTypeNode;
 import com.intellij.openapi.util.IconLoader;
@@ -37,6 +38,7 @@ public class ServerTreeRenderer extends DefaultTreeCellRenderer {
 	private static Icon fisheyeServerDisabledIcon;
 	private static Icon fisheyeServerEnabledIcon;
 	private static Icon fisheyeServersIcon;
+	private static Icon infoIcon;
 
 	static {
 		bambooServersIcon = IconLoader.getIcon("/icons/bamboo-blue-16.png");
@@ -51,6 +53,7 @@ public class ServerTreeRenderer extends DefaultTreeCellRenderer {
 		fisheyeServersIcon = IconLoader.getIcon("/icons/fisheye-16.png");
 		fisheyeServerEnabledIcon = IconLoader.getIcon("/icons/fisheye-16.png");
 		fisheyeServerDisabledIcon = IconLoader.getIcon("/icons/fisheye-16-disabled.png");
+		infoIcon = IconLoader.getIcon("/actions/help.png");
 	}
 
 	@Override
@@ -79,6 +82,34 @@ public class ServerTreeRenderer extends DefaultTreeCellRenderer {
 
 			}
         }
+
+		if (value instanceof ServerInfoNode) {
+			final ServerInfoNode serverInfoNode = (ServerInfoNode) value;
+			String labelText = "";
+			label = new JLabel();
+			label.setIcon(infoIcon);
+
+			switch (serverInfoNode.getServerType()) {
+				case BAMBOO_SERVER:
+
+					labelText = "Find out more about Bamboo";
+					break;
+				case CRUCIBLE_SERVER:
+					labelText = "Find out more about Crucible";
+					break;
+				case JIRA_SERVER:
+					labelText = "Find out more about JIRA";
+					break;
+				case FISHEYE_SERVER:
+					labelText = "Find out more about FishEye";
+					break;
+				default:
+					break;
+
+			}
+			label.setFont(label.getFont().deriveFont(Font.ITALIC));
+			label.setText("<html><a href=\"" + serverInfoNode.getServerType().getInfoUrl() + "\">" + labelText + "</a></html>");
+		}
 
         if (value instanceof ServerNode) {
             final ServerNode serverNode = (ServerNode) value;
