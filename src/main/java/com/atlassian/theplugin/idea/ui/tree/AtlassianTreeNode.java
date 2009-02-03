301785a1148075550fa9b3f8890c19580c8c8c68
mwent <mwent@localhost>
2009-02-03 18:54:40 +0000

    PL-1091 filter fixed

--- src/main/java/com/atlassian/theplugin/idea/ui/tree/AtlassianTreeNode.java
+++ src/main/java/com/atlassian/theplugin/idea/ui/tree/AtlassianTreeNode.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,12 +48,17 @@ public abstract class AtlassianTreeNode extends DefaultMutableTreeNode implement
 	public AtlassianTreeNode filter(final Filter filter) {
 		AtlassianTreeNode result = null;
 		if (filter.isValid(this)) {
-			result = getClone();
+			AtlassianTreeNode tempResult = getClone();
+			boolean foundValidChild = false;
 			for (int i = 0; i < getChildCount(); i++) {
 				AtlassianTreeNode child = getChildAt(i).filter(filter);
 				if (child != null) {
-					result.addNode(child);
+					foundValidChild = true;
+					tempResult.addNode(child);
 				}
+			}
+			if (foundValidChild || getChildCount() == 0) {
+				result = tempResult;
 			}
 		}
 		return result;
@@ -72,7 +77,7 @@ public abstract class AtlassianTreeNode extends DefaultMutableTreeNode implement
 
 		@Override
 		public AtlassianTreeNode getClone() {
-			return AtlassianTreeNode.EMPTY_NODE;  
+			return AtlassianTreeNode.EMPTY_NODE;
 		}
 
 		public int compareTo(Object o) {
