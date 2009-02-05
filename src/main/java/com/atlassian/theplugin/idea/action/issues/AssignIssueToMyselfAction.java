db5d92fd0c03f083431f4e1d7cbfa87c1a8afa5f
mwent <mwent@localhost>
2009-02-05 12:13:31 +0000

    PL-1065 - Show all operations in issue panel

--- src/main/java/com/atlassian/theplugin/idea/action/issues/AssignIssueToMyselfAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/issues/AssignIssueToMyselfAction.java
@@ -1,17 +1,20 @@
 package com.atlassian.theplugin.idea.action.issues;
 
+import com.atlassian.theplugin.idea.Constants;
 import com.atlassian.theplugin.idea.IdeaHelper;
 import com.atlassian.theplugin.idea.jira.IssuesToolWindowPanel;
+import com.atlassian.theplugin.jira.api.JIRAIssue;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
 public class AssignIssueToMyselfAction extends JIRAAbstractAction {
 	public void actionPerformed(AnActionEvent anActionEvent) {
-	    IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(anActionEvent);
-		if (panel != null) {
-			panel.assignIssueToMyself();
+		final JIRAIssue issue = anActionEvent.getData(Constants.ISSUE_KEY);
+		IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(anActionEvent);
+		if (issue != null && panel != null) {
+			panel.assignIssueToMyself(issue);
 		}
 	}
 
-	public void onUpdate(AnActionEvent event) {		
+	public void onUpdate(AnActionEvent event) {
 	}
 }
