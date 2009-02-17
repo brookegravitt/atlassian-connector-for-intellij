02c2ddab659608f5ee00993e6d4cda792d51b8d7
jjaroczynski <jjaroczynski@localhost>
2009-02-17 14:56:57 +0000

    PL-1204: NotNull assertion fixed

--- src/main/java/com/atlassian/theplugin/idea/config/serverconfig/GenericServerConfigForm.java
+++ src/main/java/com/atlassian/theplugin/idea/config/serverconfig/GenericServerConfigForm.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -56,13 +56,14 @@ public class GenericServerConfigForm implements TestConnectionProcessor {
 	public GenericServerConfigForm(final Project project, final Connector tester) {
 
 		$$$setupUI$$$();
-		testConnection.addActionListener(new TestConnectionListener(project, tester, new TestConnectionListener.ServerCfgProvider() {
-			public ServerCfg getServerCfg() {
-				synchronized (GenericServerConfigForm.this) {
-					return serverCfg;
-				}
-			}
-		}, this));
+		testConnection
+				.addActionListener(new TestConnectionListener(project, tester, new TestConnectionListener.ServerCfgProvider() {
+					public ServerCfg getServerCfg() {
+						synchronized (GenericServerConfigForm.this) {
+							return serverCfg;
+						}
+					}
+				}, this));
 		serverUrl.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusLost(FocusEvent e) {
