1ed196a86d9c68fd4bdb5b3e9dd61e9fd61b9748
jjaroczynski <jjaroczynski@localhost>
2009-02-24 17:24:08 +0000

    PL-1179: actions in issue details tool window are disabled if there is no enabled server (still need to fix 'workflow' and 'open in browser' actions)

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/ModelFreezeUpdater.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/ModelFreezeUpdater.java
@@ -1,8 +1,8 @@
 package com.atlassian.theplugin.idea.action.issues;
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.atlassian.theplugin.jira.model.*;
 import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.jira.model.*;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 
 /**
  * User: jgorycki
@@ -10,7 +10,8 @@ import com.atlassian.theplugin.idea.IdeaHelper;
  * Time: 10:49:40 AM
  */
 public final class ModelFreezeUpdater {
-	private ModelFreezeUpdater() { }
+	private ModelFreezeUpdater() {
+	}
 
 	public static boolean getStateAndSetPresentationEnabled(AnActionEvent event) {
 		JIRAIssueListModelBuilder issueBuilder =
@@ -18,12 +19,12 @@ public final class ModelFreezeUpdater {
 		JIRAFilterListBuilder filterBuilder = IdeaHelper.getProjectComponent(event, JIRAFilterListBuilder.class);
 		JIRAServerModel jiraServerModel = IdeaHelper.getProjectComponent(event, JIRAServerModelImpl.class);
 
-		boolean enabled = (issueBuilder != null && !issueBuilder.isModelFrozen())
-						&& (filterBuilder != null && !filterBuilder.isModelFrozen())
-						&& (jiraServerModel != null && !jiraServerModel.isModelFrozen());
-		
-		event.getPresentation().setEnabled(enabled);
+		return (issueBuilder != null && !issueBuilder.isModelFrozen())
+				&& (filterBuilder != null && !filterBuilder.isModelFrozen())
+				&& (jiraServerModel != null && !jiraServerModel.isModelFrozen());
 
-		return enabled;
+//		event.getPresentation().setEnabled(enabled);
+
+//		return enabled;
 	}
 }
