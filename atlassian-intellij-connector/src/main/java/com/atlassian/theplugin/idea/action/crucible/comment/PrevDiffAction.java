29070dc03ebbb1ad374e79fcb285962b417d9bd7
jgorycki <jgorycki@localhost>
2009-03-03 15:14:27 +0000

    PL-1232 PL-1233

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/PrevDiffAction.java
@@ -0,0 +1,15 @@
+package com.atlassian.theplugin.idea.action.crucible.comment;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * User: jgorycki
+ * Date: Mar 3, 2009
+ * Time: 3:25:26 PM
+ */
+public class PrevDiffAction extends AnAction {
+	public void actionPerformed(AnActionEvent event) {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+}
