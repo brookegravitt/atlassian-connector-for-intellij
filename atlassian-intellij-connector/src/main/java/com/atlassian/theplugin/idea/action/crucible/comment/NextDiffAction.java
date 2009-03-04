753fce052ce8b5f83d78cbcf3356c94fa3b8621c
jgorycki <jgorycki@localhost>
2009-03-04 15:06:57 +0000

    PL-1233 - Go to next and previous change in review

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/NextDiffAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/NextDiffAction.java
@@ -1,15 +1,61 @@
 package com.atlassian.theplugin.idea.action.crucible.comment;
 
-import com.intellij.openapi.actionSystem.AnAction;
+import com.atlassian.theplugin.idea.ui.tree.AtlassianTree;
+import com.atlassian.theplugin.idea.ui.tree.AtlassianTreeNode;
+import com.atlassian.theplugin.idea.ui.tree.file.CrucibleFileNode;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.vcs.ex.Range;
 
 /**
  * User: jgorycki
  * Date: Mar 3, 2009
  * Time: 3:25:21 PM
  */
-public class NextDiffAction extends AnAction {
-	public void actionPerformed(AnActionEvent event) {
-		//To change body of implemented methods use File | Settings | File Templates.
+public class NextDiffAction extends AbstractDiffNavigationAction {
+
+	public void actionPerformed(final AnActionEvent e) {
+		AtlassianTree tree = (AtlassianTree) getTree(e);
+		CrucibleFileNode node = getNextFileNode(tree, getSelectedNode(e), true);
+		if (tree != null) {
+			if (node != null) {
+				selectNode(tree, node);
+				Editor ed = getEditorForNode(node);
+				if (ed == null) {
+					openFileNode(e, node, false);
+				} else {
+					Range r = getNextRange(ed);
+					if (r != null) {
+						selectNextDiff(node);
+					} else {
+						node = getNextFileNode(tree, getSelectedNode(e), false);
+						if (node != null) {
+							selectNode(tree, node);
+							openFileNode(e, node, false);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public void update(final AnActionEvent e) {
+
+		boolean enabled = false;
+
+		AtlassianTreeNode node = getSelectedNode(e);
+		CrucibleFileNode fileNode = getNextFileNode((AtlassianTree) getTree(e), node, true);
+		if (fileNode != null) {
+			Editor ed = getEditorForNode(fileNode);
+			if (ed != null) {
+				Range r = getNextRange(ed);
+				enabled = r != null;
+			}
+			if (!enabled) {
+				fileNode = getNextFileNode((AtlassianTree) getTree(e), node, false);
+				enabled = fileNode != null;
+			}
+		}
+		e.getPresentation().setEnabled(enabled);
 	}
 }
--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/PrevDiffAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/PrevDiffAction.java
@@ -1,15 +1,61 @@
 package com.atlassian.theplugin.idea.action.crucible.comment;
 
-import com.intellij.openapi.actionSystem.AnAction;
+import com.atlassian.theplugin.idea.ui.tree.AtlassianTree;
+import com.atlassian.theplugin.idea.ui.tree.AtlassianTreeNode;
+import com.atlassian.theplugin.idea.ui.tree.file.CrucibleFileNode;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.vcs.ex.Range;
 
 /**
  * User: jgorycki
  * Date: Mar 3, 2009
  * Time: 3:25:26 PM
  */
-public class PrevDiffAction extends AnAction {
-	public void actionPerformed(AnActionEvent event) {
-		//To change body of implemented methods use File | Settings | File Templates.
+public class PrevDiffAction extends AbstractDiffNavigationAction {
+
+	public void actionPerformed(final AnActionEvent e) {
+		AtlassianTree tree = (AtlassianTree) getTree(e);
+		CrucibleFileNode node = getPrevFileNode(tree, getSelectedNode(e), true);
+		if (tree != null) {
+			if (node != null) {
+				selectNode(tree, node);
+				Editor ed = getEditorForNode(node);
+				if (ed == null) {
+					openFileNode(e, node, true);
+				} else {
+					Range r = getPrevRange(ed);
+					if (r != null) {
+						selectPrevDiff(node);
+					} else {
+						node = getPrevFileNode(tree, getSelectedNode(e), false);
+						if (node != null) {
+							selectNode(tree, node);
+							openFileNode(e, node, true);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public void update(final AnActionEvent e) {
+
+		boolean enabled = false;
+
+		AtlassianTreeNode node = getSelectedNode(e);
+		CrucibleFileNode fileNode = getPrevFileNode((AtlassianTree) getTree(e), node, true);
+		if (fileNode != null) {
+			Editor ed = getEditorForNode(fileNode);
+			if (ed != null) {
+				Range r = getPrevRange(ed);
+				enabled = r != null;
+			}
+			if (!enabled) {
+				fileNode = getPrevFileNode((AtlassianTree) getTree(e), node, false);
+				enabled = fileNode != null;
+			}
+		}
+		e.getPresentation().setEnabled(enabled);
 	}
 }
