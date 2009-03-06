550b0cd4791d11aed6848c1a1bc23ce08aadb0d8
jjaroczynski <jjaroczynski@localhost>
2009-03-06 16:09:11 +0000

    PL-1231: review quick search (find local reviews and merge with server reviews, store servers selection in search dialog) under dev

--- src/main/java/com/atlassian/theplugin/crucible/model/CrucibleReviewListModel.java
+++ src/main/java/com/atlassian/theplugin/crucible/model/CrucibleReviewListModel.java
@@ -19,7 +19,6 @@ import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFilter;
 import com.atlassian.theplugin.commons.crucible.api.model.ReviewAdapter;
 import com.atlassian.theplugin.commons.crucible.api.model.notification.CrucibleNotification;
 import com.atlassian.theplugin.idea.crucible.ReviewNotificationBean;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.List;
@@ -44,10 +43,10 @@ public interface CrucibleReviewListModel {
 	List<CrucibleNotification> updateReviews(final long epoch, final Map<CrucibleFilter, ReviewNotificationBean> reviews,
 			final UpdateReason updateReason);
 
-	@Nullable
-	ReviewAdapter getSelectedReview();
+//	@Nullable
+//	ReviewAdapter getSelectedReview();
 
-	void setSelectedReview(ReviewAdapter review);
+//	void setSelectedReview(ReviewAdapter review);
 
 	Collection<ReviewAdapter> getOpenInIdeReviews();
 
