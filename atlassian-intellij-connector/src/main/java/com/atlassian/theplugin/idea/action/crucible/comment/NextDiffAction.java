12123814113bdfd385db7a279cef1d3f36e787b9
jgorycki <jgorycki@localhost>
2009-03-10 10:45:17 +0000

    PL-1233 - Go to next and previous change in review

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/NextDiffAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/crucible/comment/NextDiffAction.java
@@ -3,9 +3,8 @@ package com.atlassian.theplugin.idea.action.crucible.comment;
 import com.atlassian.theplugin.idea.ui.tree.AtlassianTree;
 import com.atlassian.theplugin.idea.ui.tree.AtlassianTreeNode;
 import com.atlassian.theplugin.idea.ui.tree.file.CrucibleFileNode;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.vcs.ex.Range;
 
@@ -18,49 +17,17 @@ import javax.swing.*;
  */
 public class NextDiffAction extends AbstractDiffNavigationAction {
 
-	public void actionPerformed(final AnActionEvent e) {
-		AtlassianTree tree = (AtlassianTree) getTree(e);
-		if (tree != null) {
-			CrucibleFileNode node = getNextFileNode(tree, getSelectedNode(e), true);
-			if (node != null) {
-				selectNode(tree, node);
-				Editor ed = getEditorForNode(node);
-				if (ed == null) {
-					openFileNode(e, node, false);
-				} else {
-					Range r = getNextRange(ed);
-					if (r != null) {
-						openFileAndSelectRange(e, node, r);
-					} else {
-						node = getNextFileNode(tree, getSelectedNode(e), false);
-						if (node != null) {
-							selectNode(tree, node);
-							openFileNode(e, node, false);
-						}
-					}
-				}
-			}
-		}
+	protected Range getSubsequentRange(Editor ed) {
+		return getNextRange(ed);
 	}
 
-	protected void updateForTree(final AnActionEvent e) {
+	protected CrucibleFileNode getSubsequentFileNode(AtlassianTree tree,
+													 AtlassianTreeNode selectedNode, boolean alsoThis) {
+		return getNextFileNode(tree, selectedNode, alsoThis);
+	}
 
-		boolean enabled = false;
-
-		AtlassianTreeNode node = getSelectedNode(e);
-		CrucibleFileNode fileNode = getNextFileNode((AtlassianTree) getTree(e), node, true);
-		if (fileNode != null) {
-			Editor ed = getEditorForNode(fileNode);
-			if (ed != null) {
-				Range r = getNextRange(ed);
-				enabled = r != null;
-			}
-			if (!enabled) {
-				fileNode = getNextFileNode((AtlassianTree) getTree(e), node, false);
-				enabled = fileNode != null;
-			}
-		}
-		e.getPresentation().setEnabled(enabled);
+	protected boolean wantLastNode() {
+		return false;
 	}
 
 	public void registerShortcutsInEditor(Editor editor) {
