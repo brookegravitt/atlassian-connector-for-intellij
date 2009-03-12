5b08c2d92e6ad5466a64b5e39ea835dc8f42f601
mwent <mwent@localhost>
2009-03-12 11:48:21 +0000

    custom field labels handling

--- src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleReviewMetricsCombos.java
+++ src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleReviewMetricsCombos.java
@@ -1,11 +1,17 @@
 package com.atlassian.theplugin.idea.crucible;
 
-import com.atlassian.theplugin.commons.crucible.api.model.*;
+import com.atlassian.theplugin.commons.crucible.api.model.Comment;
+import com.atlassian.theplugin.commons.crucible.api.model.CommentBean;
+import com.atlassian.theplugin.commons.crucible.api.model.CustomField;
+import com.atlassian.theplugin.commons.crucible.api.model.CustomFieldBean;
+import com.atlassian.theplugin.commons.crucible.api.model.CustomFieldDef;
+import com.atlassian.theplugin.commons.crucible.api.model.CustomFieldValue;
+import com.atlassian.theplugin.commons.util.MiscUtil;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: jgorycki
@@ -15,28 +21,19 @@ import java.util.*;
 public class CrucibleReviewMetricsCombos {
 
 	private Map<String, JComboBox> combos = new HashMap<String, JComboBox>();
-	private final ReviewAdapter review;
+
 	private final CommentBean comment;
 	private final JPanel parent;
 
-	public CrucibleReviewMetricsCombos(ReviewAdapter review, CommentBean comment,
-									   Collection<CustomFieldDef> metrics, JPanel parent) {
-		this.review = review;
+	public CrucibleReviewMetricsCombos(CommentBean comment,
+			Collection<CustomFieldDef> metrics, JPanel parent) {
 		this.comment = comment;
 		this.parent = parent;
 		for (CustomFieldDef metric : metrics) {
 			final JLabel label = new JLabel(metric.getLabel());
 			final JComboBox combo = new JComboBox();
+			combo.setModel(new CustomFieldComboBoxModel(comment, metric));
 			final String metricName = metric.getLabel();
-			combo.addItem("Select " + metricName);
-			for (CustomFieldValue value : metric.getValues()) {
-				combo.addItem(value.getName());
-			}
-			combo.addActionListener(new ActionListener() {
-				public void actionPerformed(final ActionEvent event) {
-					setMetricField(combo, metricName);
-				}
-			});
 			combos.put(metricName, combo);
 			parent.add(label);
 			parent.add(combo);
@@ -53,35 +50,92 @@ public class CrucibleReviewMetricsCombos {
 		parent.setVisible(visible);
 	}
 
-	private void setMetricField(JComboBox combo, String field) {
-		if (comment == null) {
-			return;
+	private class CustomFieldValueWrapper extends CustomFieldValue {
+		private CustomFieldValueWrapper(final String name, final Object value) {
+			super(name, value);
 		}
-		CustomField oldCf = comment.getCustomFields().get(field);
-		if (oldCf != null) {
-			comment.getCustomFields().remove(oldCf);
-		}
-		if (combo.getSelectedIndex() > 0) {
-			CustomFieldBean cf = getCustomFieldFromCombo(combo);
-			comment.getCustomFields().put(field, cf);
+
+		@Override
+		public String toString() {
+			return getName();
 		}
 	}
 
-	private CustomFieldBean getCustomFieldFromCombo(JComboBox combo) {
-		CustomFieldBean cf = new CustomFieldBean();
-		cf.setConfigVersion(review.getMetricsVersion());
-		cf.setValue((String) combo.getSelectedItem());
-		return cf;
-	}
+	private class CustomFieldComboBoxModel extends AbstractListModel implements ComboBoxModel {
+		private Collection<CustomFieldValueWrapper> data;
+		private final Comment comment;
+		private CustomFieldDef customFieldDef;
+		private CustomFieldValueWrapper NONE = new CustomFieldValueWrapper("None", "none");
 
-//	public Collection<CustomField> getCustomFields() {
-//		List<CustomField> fields = new ArrayList<CustomField>();
-//		for (JComboBox combo : combos.values()) {
-//			if (combo.getSelectedIndex() > 0) {
-//				CustomFieldBean cf = getCustomFieldFromCombo(combo);
-//				fields.add(cf);
-//			}
-//		}
-//		return fields;
-//	}
+		private CustomFieldComboBoxModel(Comment comment, CustomFieldDef customFieldDef) {
+			this.comment = comment;
+			this.customFieldDef = customFieldDef;
+		}
+
+		private Collection<CustomFieldValueWrapper> getCustomFields() {
+			if (data == null) {
+				data = MiscUtil.buildArrayList();
+				for (CustomFieldValue value : customFieldDef.getValues()) {
+					data.add(new CustomFieldValueWrapper(value.getName(), value.getValue()));
+				}
+			}
+			return data;
+		}
+
+		public void setSelectedItem(final Object anItem) {
+			final Object selectedItem = getSelectedItem();
+			if (selectedItem != null && !selectedItem.equals(anItem) || selectedItem == null && anItem != null) {
+				if (anItem != null) {
+					CustomFieldValue item = (CustomFieldValue) anItem;
+					setMetricField(customFieldDef, item);
+				} else {
+					setMetricField(customFieldDef, NONE);
+				}
+				fireContentsChanged(this, -1, -1);
+			}
+		}
+
+		private void setMetricField(CustomFieldDef field, CustomFieldValue value) {
+			if (comment == null) {
+				return;
+			}
+			CustomField oldCf = comment.getCustomFields().get(customFieldDef.getName());
+			if (oldCf != null) {
+				comment.getCustomFields().remove(customFieldDef.getName());
+			}
+			if (!value.getName().equals(NONE.getName())) {
+				CustomFieldBean newField = new CustomFieldBean();
+				newField.setConfigVersion(field.getConfigVersion());
+				newField.setValue(value.getName());
+				comment.getCustomFields().put(field.getName(), newField);
+			}
+		}
+
+		public Object getSelectedItem() {
+			CustomField v = comment.getCustomFields().get(customFieldDef.getName());
+			if (v != null) {
+				return new CustomFieldValueWrapper(v.getValue(), v.getValue());
+			}
+			return NONE;
+		}
+
+		public Object getElementAt(final int index) {
+			if (index == 0) {
+				return NONE;
+			}
+			int i = 1;
+			for (CustomFieldValue value : getCustomFields()) {
+				if (i == index) {
+					return value;
+				}
+				i++;
+			}
+			return null;
+		}
+
+		public int getSize() {
+			return getCustomFields().size() + 1;
+		}
+
+	}
 }
