e226bfe3d40c800d7b12e15bd9dfd9e703cc376a
mwent <mwent@localhost>
2009-03-20 14:09:45 +0000

    PL-735 - Fully support working with pre-commit reviews

--- src/main/java/com/atlassian/theplugin/idea/action/crucible/PreCommitReviewActionNoChangeSelected.java
+++ src/main/java/com/atlassian/theplugin/idea/action/crucible/PreCommitReviewActionNoChangeSelected.java
@@ -3,7 +3,7 @@ package com.atlassian.theplugin.idea.action.crucible;
 import com.atlassian.theplugin.commons.crucible.CrucibleServerFacadeImpl;
 import com.atlassian.theplugin.idea.IdeaHelper;
 import com.atlassian.theplugin.idea.action.reviews.AbstractCrucibleToolbarAction;
-import com.atlassian.theplugin.idea.crucible.CrucibleCreatePreCommitNoChangeUploadReviewForm;
+import com.atlassian.theplugin.idea.crucible.CrucibleCreatePreCommitUploadReviewForm;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.project.Project;
@@ -15,7 +15,7 @@ public class PreCommitReviewActionNoChangeSelected extends AbstractCrucibleToolb
 		if (project == null) {
 			return;
 		}
-		new CrucibleCreatePreCommitNoChangeUploadReviewForm(project, CrucibleServerFacadeImpl.getInstance(),
+		new CrucibleCreatePreCommitUploadReviewForm(project, CrucibleServerFacadeImpl.getInstance(), null,
 				IdeaHelper.getCfgManager()).show();
 	}
 }
