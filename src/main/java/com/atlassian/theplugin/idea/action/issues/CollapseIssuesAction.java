10edcc0cf4949ed0303087273ea502c34a0be824
jjaroczynski <jjaroczynski@localhost>
2009-03-25 15:55:48 +0000

    PL-1238: col/exp enabled for recenlty open issues list

--- src/main/java/com/atlassian/theplugin/idea/action/issues/CollapseIssuesAction.java
+++ src/main/java/com/atlassian/theplugin/idea/action/issues/CollapseIssuesAction.java
@@ -10,9 +10,20 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 public class CollapseIssuesAction extends JIRAAbstractAction {
 	public void actionPerformed(final AnActionEvent e) {
 		final IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(e);
-		panel.collapseAllRightTreeNodes();
+		if (panel != null) {
+			panel.collapseAllRightTreeNodes();
+		}
 	}
 
-	public void onUpdate(AnActionEvent event) {		
+	public void onUpdate(AnActionEvent event) {
+		IssuesToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(event);
+		boolean enabled = panel != null && (panel.getSelectedServer() != null || panel.isRecentlyOpenFilterSelected());
+		event.getPresentation().setEnabled(enabled);
+	}
+
+	public void onUpdate(final AnActionEvent event, final boolean enabled) {
+		if (ModelFreezeUpdater.getState(event)) {
+			onUpdate(event);
+		}
 	}
 }
