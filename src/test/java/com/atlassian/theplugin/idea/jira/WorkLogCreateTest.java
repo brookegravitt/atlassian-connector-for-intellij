264e8193a675826fa5be6bdb15438e738841bcb8
jgorycki <jgorycki@localhost>
2009-03-26 08:05:24 +0000

    PL-1289 - User can select an issue as the active issue

--- src/test/java/com/atlassian/theplugin/idea/jira/WorkLogCreateTest.java
+++ src/test/java/com/atlassian/theplugin/idea/jira/WorkLogCreateTest.java
@@ -31,35 +31,35 @@ public class WorkLogCreateTest extends TestCase {
     }
 
     public void testGetFormatedDurationStringForNow() {
-		String res = WorkLogCreateAndMaybeDeactivate.getFormatedDurationString(new Date());
+		String res = WorkLogCreateAndMaybeDeactivateDialog.getFormatedDurationString(new Date());
 	    assertTrue(res.length() == 0);
     }
 
 	public void testGetFormatedDurationStringForHourAgo() {
 		Date d = new Date();
 		d.setTime(d.getTime() - 1000 * 3600);
-		String res = WorkLogCreateAndMaybeDeactivate.getFormatedDurationString(d);
+		String res = WorkLogCreateAndMaybeDeactivateDialog.getFormatedDurationString(d);
 		assertEquals(res, "1h");
 	}
 
 	public void testGetFormatedDurationStringForHalfAnHourAgo() {
 		Date d = new Date();
 		d.setTime(d.getTime() - 1000 * 1800);
-		String res = WorkLogCreateAndMaybeDeactivate.getFormatedDurationString(d);
+		String res = WorkLogCreateAndMaybeDeactivateDialog.getFormatedDurationString(d);
 		assertEquals(res, "30m");
 	}
 
 	public void testGetFormatedDurationStringForTwoAndAHalfAnHourAgo() {
 		Date d = new Date();
 		d.setTime(d.getTime() - 1000 * (1800 + 2 * 3600));
-		String res = WorkLogCreateAndMaybeDeactivate.getFormatedDurationString(d);
+		String res = WorkLogCreateAndMaybeDeactivateDialog.getFormatedDurationString(d);
 		assertEquals(res, "2h 30m");
 	}
 
 	public void testGetFormatedDurationStringForTooLongTimeAgo() {
 		Date d = new Date();
 		d.setTime(d.getTime() - 1000 * 3600 * 24);
-		String res = WorkLogCreateAndMaybeDeactivate.getFormatedDurationString(d);
+		String res = WorkLogCreateAndMaybeDeactivateDialog.getFormatedDurationString(d);
 		assertTrue(res.length() == 0);
 	}
 
