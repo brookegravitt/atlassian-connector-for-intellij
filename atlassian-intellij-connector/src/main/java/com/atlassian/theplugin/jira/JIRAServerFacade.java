bffae1897bb37beaaeb6e3c7f74b80597e71b10f
pmaruszak <pmaruszak@localhost>
2009-03-27 15:34:54 +0000

    PL-1289 - User can select an issue as the active issue

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/jira/JIRAServerFacade.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/jira/JIRAServerFacade.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,24 +24,24 @@ import java.util.Calendar;
 import java.util.List;
 
 public interface JIRAServerFacade extends ProductServerFacade {
-    List<JIRAIssue> getIssues(JiraServerCfg server, List<JIRAQueryFragment> query,
-				   String sort,
-				   String sortOrder,
-				   int start,
-				   int size) throws JIRAException;
+	List<JIRAIssue> getIssues(JiraServerCfg server, List<JIRAQueryFragment> query,
+			String sort,
+			String sortOrder,
+			int start,
+			int size) throws JIRAException;
 
 	List<JIRAIssue> getSavedFilterIssues(JiraServerCfg server,
-							  List<JIRAQueryFragment> query,
-							  String sort,
-							  String sortOrder,
-							  int start,
-							  int size) throws JIRAException;
+			List<JIRAQueryFragment> query,
+			String sort,
+			String sortOrder,
+			int start,
+			int size) throws JIRAException;
 
 	List<JIRAProject> getProjects(JiraServerCfg server) throws JIRAException;
 
-    List<JIRAConstant> getIssueTypes(JiraServerCfg server) throws JIRAException;
-    
-    List<JIRAConstant> getStatuses(JiraServerCfg server) throws JIRAException;
+	List<JIRAConstant> getIssueTypes(JiraServerCfg server) throws JIRAException;
+
+	List<JIRAConstant> getStatuses(JiraServerCfg server) throws JIRAException;
 
 	List<JIRAConstant> getIssueTypesForProject(JiraServerCfg server, String project) throws JIRAException;
 
@@ -55,24 +55,24 @@ public interface JIRAServerFacade extends ProductServerFacade {
 
 	List<JIRAResolutionBean> getResolutions(JiraServerCfg server) throws JIRAException;
 
-    List<JIRAAction> getAvailableActions(JiraServerCfg server, JIRAIssue issue) throws JIRAException;
+	List<JIRAAction> getAvailableActions(JiraServerCfg server, JIRAIssue issue) throws JIRAException;
 
 	List<JIRAActionField> getFieldsForAction(JiraServerCfg server, JIRAIssue issue, JIRAAction action) throws JIRAException;
 
 	void progressWorkflowAction(JiraServerCfg server, JIRAIssue issue, JIRAAction action) throws JIRAException;
 
-	void addComment(JiraServerCfg server, JIRAIssue issue, String comment) throws JIRAException;
+	void addComment(JiraServerCfg server, String issueKey, String comment) throws JIRAException;
 
-    JIRAIssue createIssue(JiraServerCfg server, JIRAIssue issue) throws JIRAException;
+	JIRAIssue createIssue(JiraServerCfg server, JIRAIssue issue) throws JIRAException;
 
 	JIRAIssue getIssueUpdate(JiraServerCfg server, JIRAIssue issue) throws JIRAException;
-	
+
 	JIRAIssue getIssue(JiraServerCfg server, String key) throws JIRAException;
 
 	JIRAIssue getIssueDetails(JiraServerCfg server, JIRAIssue issue) throws JIRAException;
 
 	void logWork(JiraServerCfg server, JIRAIssue issue, String timeSpent, Calendar startDate,
-				 String comment, boolean updateEstimate, String newEstimate)
+			String comment, boolean updateEstimate, String newEstimate)
 			throws JIRAException;
 
 	void setAssignee(JiraServerCfg server, JIRAIssue issue, String assignee) throws JIRAException;
