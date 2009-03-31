5904669ba0890dc8b472fdccbea6f91f28c4995c
pmaruszak <pmaruszak@localhost>
2009-03-31 10:52:49 +0000

    PL-1289 - User can select an issue as the active issue

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/ActivateJiraIssueAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/ActivateJiraIssueAction.java
@@ -16,14 +16,11 @@
 package com.atlassian.theplugin.idea.action.issues.activetoolbar;
 
 import com.atlassian.theplugin.commons.cfg.JiraServerCfg;
-import com.atlassian.theplugin.configuration.JiraWorkspaceConfiguration;
-import com.atlassian.theplugin.idea.Constants;
 import com.atlassian.theplugin.idea.IdeaHelper;
 import com.atlassian.theplugin.jira.api.JIRAIssue;
 import com.atlassian.theplugin.jira.model.ActiveJiraIssue;
 import com.atlassian.theplugin.jira.model.ActiveJiraIssueBean;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import org.joda.time.DateTime;
@@ -95,24 +92,4 @@ public class ActivateJiraIssueAction extends AbstractActiveJiraIssueAction {
 		}
 	}
 
-	private static void registerToolbar(AnActionEvent event) {
-		ActionManager aManager = ActionManager.getInstance();
-		ActionGroup newActionGroup = (ActionGroup) aManager.getAction(Constants.ACTIVE_TOOLBAR_NAME);
-		DefaultActionGroup mainToolBar = (DefaultActionGroup) aManager.getAction("MainToolBar");
-
-		if (newActionGroup != null && mainToolBar != null) {
-			AnAction[] children = mainToolBar.getChildren(event);
-			for (AnAction child : children) {
-			}
-			mainToolBar.add(newActionGroup);
-		}
-	}
-
-	public static void showToolbar(final Project project) {
-		final JiraWorkspaceConfiguration conf = IdeaHelper.getProjectComponent(project, JiraWorkspaceConfiguration.class);
-
-		if (conf != null && conf.getActiveJiraIssue() != null) {
-			//registerToolbar();
-		}
-	}
 }
