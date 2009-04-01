08a9ed9d9c7b92c9a1e3a5b97ace90bacd80519a
jjaroczynski <jjaroczynski@localhost>
2009-04-01 05:36:52 +0000

    PL-1323: line wrap fix

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/ChangesetCreate.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/ChangesetCreate.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -113,6 +113,8 @@ public class ChangesetCreate extends DialogWrapper {
 		gbc.insets = new Insets(12, 0, 12, 0);
 		contentPane.add(scrollPane1, gbc);
 		changesetComment = new JTextArea();
+		changesetComment.setLineWrap(true);
+		changesetComment.setWrapStyleWord(true);
 		scrollPane1.setViewportView(changesetComment);
 		final JLabel label1 = new JLabel();
 		label1.setText("Name:");
