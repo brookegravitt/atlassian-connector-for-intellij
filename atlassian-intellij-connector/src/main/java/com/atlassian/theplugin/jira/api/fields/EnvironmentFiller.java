177694f4ac33a529f4f04a92b57b8ed66a411b71
jgorycki <jgorycki@localhost>
2009-04-06 15:09:17 +0000

    PL-1251 - Allow issue workflow actions with input fields

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/jira/api/fields/EnvironmentFiller.java
@@ -0,0 +1,9 @@
+package com.atlassian.theplugin.jira.api.fields;
+
+/**
+ * User: jgorycki
+ * Date: Apr 6, 2009
+ * Time: 4:33:46 PM
+ */
+public class EnvironmentFiller extends DefaultEmptyFieldFiller {
+}
