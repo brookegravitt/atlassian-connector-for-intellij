801088102ba7b81997f64751aba66871f3fec573
jgorycki <jgorycki@localhost>
2009-04-06 10:19:58 +0000

    PL-1251 - Allow issue workflow actions with input fields

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/controls/IssueResolutionComboBox.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/controls/IssueResolutionComboBox.java
@@ -4,6 +4,7 @@ import com.atlassian.theplugin.commons.cfg.JiraServerCfg;
 import com.atlassian.theplugin.jira.api.JIRAException;
 import com.atlassian.theplugin.jira.api.JIRAIssue;
 import com.atlassian.theplugin.jira.api.JIRAResolutionBean;
+import com.atlassian.theplugin.jira.api.JIRAActionField;
 import com.atlassian.theplugin.jira.model.JIRAServerModel;
 import com.atlassian.theplugin.util.PluginUtil;
 import com.intellij.util.ui.UIUtil;
@@ -17,7 +18,7 @@ import java.util.List;
  * Date: Apr 6, 2009
  * Time: 11:41:06 AM
  */
-public class IssueResolutionComboBox extends JComboBox {
+public class IssueResolutionComboBox extends JComboBox implements ActionFieldEditor {
 	private DefaultComboBoxModel model;
 	private boolean initialized;
 
@@ -63,10 +64,12 @@ public class IssueResolutionComboBox extends JComboBox {
 		t.start();
 	}
 
-	public JIRAResolutionBean getSelectedResolution() {
+	public JIRAActionField getEditedFieldValue(JIRAActionField field) {
 		if (!initialized) {
 			return null;
 		}
-		return (JIRAResolutionBean) getSelectedItem();
+		JIRAResolutionBean res = (JIRAResolutionBean) getSelectedItem();
+		field.addValue(Long.valueOf(res.getId()).toString());
+		return field;
 	}
 }
