0ed2c98533f71568be742227d91af0cdeaa9b9e5
jgorycki <jgorycki@localhost>
2009-04-07 08:59:11 +0000

    PL-1251 - Allow issue workflow actions with input fields

--- src/main/java/com/atlassian/theplugin/jira/api/fields/TimeTrackingFiller.java
+++ src/main/java/com/atlassian/theplugin/jira/api/fields/TimeTrackingFiller.java
@@ -1,9 +1,16 @@
 package com.atlassian.theplugin.jira.api.fields;
 
+import com.atlassian.theplugin.jira.api.JIRAIssue;
+
+import java.util.List;
+
 /**
  * User: jgorycki
  * Date: Apr 6, 2009
  * Time: 4:30:12 PM
  */
-public class TimeTrackingFiller extends DefaultEmptyFieldFiller {
+public class TimeTrackingFiller implements FieldFiller {
+	public List<String> getFieldValues(String field, JIRAIssue detailedIssue) {
+		return null;
+	}
 }
