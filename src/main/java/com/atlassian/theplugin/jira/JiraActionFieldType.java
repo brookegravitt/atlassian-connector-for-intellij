3e161920dab7939ed4c4ee290e7a7c99e1389ce8
jgorycki <jgorycki@localhost>
2009-04-07 11:50:11 +0000

    PL-1251 - Allow issue workflow actions with input fields

--- src/main/java/com/atlassian/theplugin/jira/JiraActionFieldType.java
+++ src/main/java/com/atlassian/theplugin/jira/JiraActionFieldType.java
@@ -7,10 +7,7 @@ import com.atlassian.theplugin.jira.api.JIRAIssue;
 import com.atlassian.theplugin.jira.api.fields.*;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * User: jgorycki
@@ -37,10 +34,12 @@ public final class JiraActionFieldType {
 	}
 
 	private static final class WidgetTypeAndFieldFiller {
+		private final int sortOrder;
 		private FieldFiller filler;
 		private WidgetType widgetType;
 
-		private WidgetTypeAndFieldFiller(WidgetType widgetType, FieldFiller filler) {
+		private WidgetTypeAndFieldFiller(WidgetType widgetType, int sortOrder, FieldFiller filler) {
+			this.sortOrder = sortOrder;
 			this.filler = filler;
 			this.widgetType = widgetType;
 		}
@@ -52,25 +51,31 @@ public final class JiraActionFieldType {
 		public WidgetType getWidgetType() {
 			return widgetType;
 		}
+
+		public int getSortOrder() {
+			return sortOrder;
+		}
 	}
 
 	private static Map<String, WidgetTypeAndFieldFiller> typeMap = new HashMap<String, WidgetTypeAndFieldFiller>();
 	private static CustomFieldFiller customFieldFiller = new CustomFieldFiller();
 
 	static {
-		typeMap.put("resolution", new WidgetTypeAndFieldFiller(WidgetType.RESOLUTION, new ResolutionFiller()));
-		typeMap.put("fixVersions", new WidgetTypeAndFieldFiller(WidgetType.FIX_VERSIONS, new FixVersionsFiller()));
-		typeMap.put("versions", new WidgetTypeAndFieldFiller(WidgetType.VERSIONS, new AffectsVersionsFiller()));
-		typeMap.put("components", new WidgetTypeAndFieldFiller(WidgetType.COMPONENTS, new ComponentsFiller()));
-		typeMap.put("description", new WidgetTypeAndFieldFiller(WidgetType.DESCRIPTION, new DescriptionFiller()));
-		typeMap.put("duedate", new WidgetTypeAndFieldFiller(WidgetType.CALENDAR, new DueDateFiller()));
-		typeMap.put("environment", new WidgetTypeAndFieldFiller(WidgetType.ENVIRONMENT, new EnvironmentFiller()));
-		typeMap.put("issuetype", new WidgetTypeAndFieldFiller(WidgetType.ISSUE_TYPE, new IssueTypeFiller()));
-		typeMap.put("priority", new WidgetTypeAndFieldFiller(WidgetType.PRIORITY, new PriorityFiller()));
-		typeMap.put("reporter", new WidgetTypeAndFieldFiller(WidgetType.REPORTER, new ReporterFiller()));
-		typeMap.put("summary", new WidgetTypeAndFieldFiller(WidgetType.SUMMARY, new SummaryFiller()));
-		typeMap.put("timetracking", new WidgetTypeAndFieldFiller(WidgetType.TIME_SPENT, new TimeTrackingFiller()));
-		typeMap.put("assignee", new WidgetTypeAndFieldFiller(WidgetType.ASSIGNEE, new AssigneeFiller()));
+		int i = 0;
+
+		typeMap.put("summary", new WidgetTypeAndFieldFiller(WidgetType.SUMMARY, ++i, new SummaryFiller()));
+		typeMap.put("resolution", new WidgetTypeAndFieldFiller(WidgetType.RESOLUTION, ++i, new ResolutionFiller()));
+		typeMap.put("issuetype", new WidgetTypeAndFieldFiller(WidgetType.ISSUE_TYPE, ++i, new IssueTypeFiller()));
+		typeMap.put("priority", new WidgetTypeAndFieldFiller(WidgetType.PRIORITY, ++i, new PriorityFiller()));
+		typeMap.put("duedate", new WidgetTypeAndFieldFiller(WidgetType.CALENDAR, ++i, new DueDateFiller()));
+		typeMap.put("components", new WidgetTypeAndFieldFiller(WidgetType.COMPONENTS, ++i, new ComponentsFiller()));
+		typeMap.put("versions", new WidgetTypeAndFieldFiller(WidgetType.VERSIONS, ++i, new AffectsVersionsFiller()));
+		typeMap.put("fixVersions", new WidgetTypeAndFieldFiller(WidgetType.FIX_VERSIONS, ++i, new FixVersionsFiller()));
+		typeMap.put("assignee", new WidgetTypeAndFieldFiller(WidgetType.ASSIGNEE, ++i, new AssigneeFiller()));
+		typeMap.put("reporter", new WidgetTypeAndFieldFiller(WidgetType.REPORTER, ++i, new ReporterFiller()));
+		typeMap.put("environment", new WidgetTypeAndFieldFiller(WidgetType.ENVIRONMENT, ++i, new EnvironmentFiller()));
+		typeMap.put("description", new WidgetTypeAndFieldFiller(WidgetType.DESCRIPTION, ++i, new DescriptionFiller()));
+		typeMap.put("timetracking", new WidgetTypeAndFieldFiller(WidgetType.TIME_SPENT, ++i, new TimeTrackingFiller()));
 	}
 
 	private JiraActionFieldType() {
@@ -112,5 +117,19 @@ public final class JiraActionFieldType {
 			result.setValues(customFieldFiller.getFieldValues(field.getFieldId(), issue));
 		}
 		return result;
+	}
+
+	public static Collection<JIRAActionField> sortFieldList(List<JIRAActionField> fieldList) {
+		int customFieldOffset = typeMap.size();
+		Map<Integer, JIRAActionField> sorted = new TreeMap<Integer, JIRAActionField>();
+		for (JIRAActionField field : fieldList) {
+			if (typeMap.containsKey(field.getFieldId())) {
+				sorted.put(typeMap.get(field.getFieldId()).getSortOrder(), field);
+			} else {
+				sorted.put(customFieldOffset++, field);
+			}
+		}
+
+		return sorted.values();
 	}
 }
