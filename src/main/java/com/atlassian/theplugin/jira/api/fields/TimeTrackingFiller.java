9fa7679351d8085d080ce504d9d1d8c35b7fa2f9
jjaroczynski <jjaroczynski@localhost>
2009-04-16 16:48:58 +0000

    PL-1366: timetracking field added

--- src/main/java/com/atlassian/theplugin/jira/api/fields/TimeTrackingFiller.java
+++ src/main/java/com/atlassian/theplugin/jira/api/fields/TimeTrackingFiller.java
@@ -2,6 +2,7 @@ package com.atlassian.theplugin.jira.api.fields;
 
 import com.atlassian.theplugin.jira.api.JIRAIssue;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,6 +12,22 @@ import java.util.List;
  */
 public class TimeTrackingFiller implements FieldFiller {
 	public List<String> getFieldValues(String field, JIRAIssue detailedIssue) {
-		return null;
+
+		if (detailedIssue.getTimeSpent() == null) {
+			return Arrays.asList(translate(detailedIssue.getOriginalEstimate()));
+		}
+
+		return Arrays.asList(translate(detailedIssue.getRemainingEstimate()));
+	}
+
+	private String translate(String displayValue) {
+		if (displayValue != null) {
+			displayValue = displayValue.replaceAll(" week", "w");
+			displayValue = displayValue.replaceAll(" day", "d");
+			displayValue = displayValue.replaceAll(" hour", "h");
+			displayValue = displayValue.replaceAll(" minute", "m");
+			displayValue = displayValue.replaceAll(",", "");
+		}
+		return displayValue;
 	}
 }
