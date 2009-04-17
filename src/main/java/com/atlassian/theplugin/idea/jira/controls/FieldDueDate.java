f776d1d80f9b925344d502a1b39e804ef847c2e3
jjaroczynski <jjaroczynski@localhost>
2009-04-17 15:15:28 +0000

    PL-1385: date picker control added

--- src/main/java/com/atlassian/theplugin/idea/jira/controls/FieldDueDate.java
+++ src/main/java/com/atlassian/theplugin/idea/jira/controls/FieldDueDate.java
@@ -16,9 +16,21 @@
 package com.atlassian.theplugin.idea.jira.controls;
 
 import com.atlassian.theplugin.jira.api.JIRAActionField;
+import com.atlassian.theplugin.util.PluginUtil;
+import com.intellij.openapi.ui.DialogWrapper;
+import net.sf.nachocalendar.CalendarFactory;
+import net.sf.nachocalendar.components.CalendarPanel;
+import net.sf.nachocalendar.model.DateSelectionModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * @author Jacek Jaroczynski
@@ -36,6 +48,31 @@ public class FieldDueDate extends JPanel implements ActionFieldEditor {
 		textField = new FieldTextField(date, field);
 		add(textField);
 		add(Box.createRigidArea(new Dimension(BOX_WIDTH, 0)));
+		final JButton button = new JButton("Select a Date");
+		add(button);
+
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent event) {
+
+				SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MMM/yy", Locale.US);
+				Date dueDate = null;
+
+				try {
+					dueDate = dateFormatter.parse(textField.getText());
+				} catch (ParseException e) {
+					PluginUtil.getLogger().info("Wrong date format [" + textField.getText() + "]. Using TODAY", e);
+					dueDate = new Date();
+				}
+
+				DatePicker datePicker = new DatePicker("Select Due Date", dueDate);
+				if (datePicker.getExitCode() == DialogWrapper.OK_EXIT_CODE) {
+					SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMM/yy", Locale.US);
+					textField.setText(dateFormat.format(datePicker.getSelectedDate()));
+				}
+
+
+			}
+		});
 
 	}
 
@@ -49,5 +86,61 @@ public class FieldDueDate extends JPanel implements ActionFieldEditor {
 
 	public String getFieldName() {
 		return textField.getFieldName();
+	}
+
+	private class DatePicker extends DialogWrapper {
+
+		private JPanel panel = new JPanel();
+
+		private CalendarPanel calendar;
+
+		DatePicker(final String title, final Date now) {
+			super(false);
+			init();
+			Calendar nowcal = Calendar.getInstance();
+
+			calendar = CalendarFactory.createCalendarPanel(1);
+			calendar.setSelectionMode(DateSelectionModel.SINGLE_SELECTION);
+
+			nowcal.setTime(now);
+			nowcal.set(Calendar.HOUR_OF_DAY, 0);
+			nowcal.set(Calendar.MINUTE, 0);
+			nowcal.set(Calendar.SECOND, 0);
+			nowcal.set(Calendar.MILLISECOND, 0);
+
+			Date nowZeroZero = nowcal.getTime();
+
+			calendar.setDate(nowZeroZero);
+			calendar.setValue(nowZeroZero);
+
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(now);
+
+			setTitle(title);
+
+			panel.setLayout(new GridBagLayout());
+			GridBagConstraints gbc = new GridBagConstraints();
+
+			gbc.anchor = GridBagConstraints.CENTER;
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.gridx = 0;
+			gbc.gridy = 0;
+			gbc.gridwidth = 2;
+			gbc.weightx = 1;
+			panel.add(new JLabel("Day", SwingConstants.CENTER), gbc);
+			gbc.gridy = 1;
+			panel.add(calendar, gbc);
+
+			show();
+		}
+
+		@Override
+		protected JComponent createCenterPanel() {
+			return panel;
+		}
+
+		public Date getSelectedDate() {
+			return (Date) calendar.getValue();
+		}
 	}
 }
