d676d5182fb613a851e2f5e613e4d0cd546cf27b
jgorycki <jgorycki@localhost>
2009-04-20 09:41:55 +0000

    test

--- atlassian-intellij-connector/atlassian-ide-plugin.xml/src/main/java/com/atlassian/theplugin/ConnectionWrapper.java
+++ atlassian-intellij-connector/atlassian-ide-plugin.xml/src/main/java/com/atlassian/theplugin/ConnectionWrapper.java
@@ -28,7 +28,7 @@ public class ConnectionWrapper extends Thread {
 	private Exception exception;
 
 	public Exception getException() {
-		return exception;aaaaaa fdfdfddggghh                   ggffgfg
+		return exception;aaaaaa fdfdfddggghh                   ggffgfg  vgfg
 	}
 
 	public String getErrorMessage() {
