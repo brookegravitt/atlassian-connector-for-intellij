970673617f20789551325ff96669711bdce322dc
jjaroczynski <jjaroczynski@localhost>
2009-04-22 16:18:22 +0000

    PL-1377 - Add icons to the active issue dropdown: * show only available issues in dropdown * show tooltip for dropdown with issue summary

--- src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/ActiveJiraIssueComboBox.java
+++ src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/ActiveJiraIssueComboBox.java
@@ -15,9 +15,9 @@
  */
 package com.atlassian.theplugin.idea.action.issues.activetoolbar;
 
-import com.atlassian.theplugin.configuration.IssueRecentlyOpenBean;
-import com.atlassian.theplugin.configuration.JiraWorkspaceConfiguration;
+import com.atlassian.theplugin.cache.RecentlyOpenIssuesCache;
 import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.jira.api.JIRAIssue;
 import com.atlassian.theplugin.jira.model.ActiveJiraIssue;
 import com.atlassian.theplugin.jira.model.ActiveJiraIssueBean;
 import com.intellij.ide.DataManager;
@@ -37,35 +37,48 @@ import javax.swing.*;
 public class ActiveJiraIssueComboBox extends ComboBoxAction {
 	static final Icon JIRA_ICON = IconLoader.getIcon("/icons/jira-blue-16.png");
 	static final Icon JIRA_ICON_DISABLED = IconLoader.getIcon("/icons/jira-grey-16.png");
+	private static final int SUMMARY_LENGHT = 50;
 
 	public void update(final AnActionEvent event) {
 		ActiveJiraIssue activeIssue = ActiveIssueUtils.getActiveJiraIssue(event);
 		String text = "No active issue";
+		String summary = "";
 		if (activeIssue != null) {
 			text = activeIssue.getIssueKey();
+
+			RecentlyOpenIssuesCache cache = IdeaHelper.getProjectComponent(event, RecentlyOpenIssuesCache.class);
+			JIRAIssue issue = cache.getLoadedRecenltyOpenIssue(activeIssue.getIssueKey(), activeIssue.getServerId());
+
+			if (issue != null) {
+				summary = issue.getSummary();
+				if (summary.length() > SUMMARY_LENGHT) {
+					summary = summary.substring(0, SUMMARY_LENGHT) + "...";
+				}
+			}
 		}
 
 		event.getPresentation().setText(text);
+		event.getPresentation().setDescription(text + ": " + summary);
 //		event.getPresentation().setIcon(JIRA_ICON);
 		super.update(event);
 	}
 
 	@NotNull
 	protected DefaultActionGroup createPopupActionGroup(final JComponent jComponent) {
-		final Project currentProject = IdeaHelper
+		final Project project = IdeaHelper
 				.getCurrentProject(DataManager.getInstance().getDataContext(jComponent));
 
 
 		DefaultActionGroup group = new DefaultActionGroup("Issues to activate", true);
-		final JiraWorkspaceConfiguration conf = IdeaHelper
-				.getProjectComponent(currentProject, JiraWorkspaceConfiguration.class);
-		final ActiveJiraIssue activeIssue = ActiveIssueUtils.getActiveJiraIssue(currentProject);
-		if (conf != null) {
-			for (IssueRecentlyOpenBean issue : conf.getRecentlyOpenIssues()) {
-				if (activeIssue == null || !issue.getIssueKey().equals(activeIssue.getIssueKey())) {
-					ActiveJiraIssue newActiveIsse = new ActiveJiraIssueBean(issue.getServerId(), issue.getIssueKey(),
-							new DateTime());
-					group.add(new ActivateIssueItemAction(newActiveIsse, currentProject));
+//		final JiraWorkspaceConfiguration conf = IdeaHelper .getProjectComponent(project, JiraWorkspaceConfiguration.class);
+		final RecentlyOpenIssuesCache cache = IdeaHelper.getProjectComponent(project, RecentlyOpenIssuesCache.class);
+		final ActiveJiraIssue activeIssue = ActiveIssueUtils.getActiveJiraIssue(project);
+		if (cache != null) {
+			for (JIRAIssue issue : cache.getLoadedRecenltyOpenIssues()) {
+				if (activeIssue == null || !issue.getKey().equals(activeIssue.getIssueKey())) {
+					ActiveJiraIssue newActiveIsse = new ActiveJiraIssueBean(issue.getServer().getServerId().toString(),
+							issue.getKey(), new DateTime());
+					group.add(new ActivateIssueItemAction(newActiveIsse, project));
 				}
 			}
 		}
