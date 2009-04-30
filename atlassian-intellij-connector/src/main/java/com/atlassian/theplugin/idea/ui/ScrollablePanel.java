94e7b4d56fd551b2f3b35a84ea099c64024167c6
jgorycki <jgorycki@localhost>
2009-04-30 07:21:56 +0000

    PL-1430 - Scrollbar increment block is too small

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/ui/ScrollablePanel.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/ui/ScrollablePanel.java
@@ -10,18 +10,20 @@ import java.awt.*;
  */
 public class ScrollablePanel extends JPanel implements Scrollable {
 	public static final int A_LOT = 100000;
+    // I say it is a decent approximation of how tall a font character is. I am too lazy to actually measure it
+    private static final int LINE_SCROLL_INCREMENT = 20;
 
-	// cheating obviously but this seems to do the right thing, so whatever :)
+    // cheating obviously but this seems to do the right thing, so whatever :)
 	public Dimension getPreferredScrollableViewportSize() {
 		return new Dimension(1, A_LOT);
 	}
 
 	public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
-		return 1;
+		return LINE_SCROLL_INCREMENT;
 	}
 
 	public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
-		return 1;
+		return LINE_SCROLL_INCREMENT;
 	}
 
 	public boolean getScrollableTracksViewportWidth() {
