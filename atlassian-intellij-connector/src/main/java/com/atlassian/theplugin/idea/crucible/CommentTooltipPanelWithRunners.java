88ab3273efbb2c6b35ac9891702c873cd18f49e2
jgorycki <jgorycki@localhost>
2009-05-06 09:42:52 +0000

    PL-1443 - Use new popup tooltip window for all review comment inputs

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/CommentTooltipPanelWithRunners.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/CommentTooltipPanelWithRunners.java
@@ -6,7 +6,6 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Date;
