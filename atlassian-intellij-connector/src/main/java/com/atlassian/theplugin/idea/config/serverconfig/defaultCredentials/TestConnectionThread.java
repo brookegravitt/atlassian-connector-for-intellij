3f2c6d639093d329cfb429360e0f3bd4b5bfc8c6
pmaruszak <pmaruszak@localhost>
2009-05-08 06:54:57 +0000

    PL-1426 - Default Credentials functionality problems

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/config/serverconfig/defaultCredentials/TestConnectionThread.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) 2008 Atlassian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atlassian.theplugin.idea.config.serverconfig.defaultCredentials;
+
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.project.Project;
+import com.atlassian.theplugin.idea.config.TestDefaultCredentials;
+import com.atlassian.theplugin.idea.config.serverconfig.ProductConnector;
+import com.atlassian.theplugin.idea.TestConnectionProcessor;
+import com.atlassian.theplugin.ConnectionWrapper;
+import com.atlassian.theplugin.util.PluginUtil;
+import com.atlassian.theplugin.commons.bamboo.BambooServerFacadeImpl;
+import com.atlassian.theplugin.commons.crucible.CrucibleServerFacadeImpl;
+import com.atlassian.theplugin.jira.JIRAServerFacadeImpl;
+
+import java.util.Set;
+
+/**
+ * User: pmaruszak
+ */
+public class TestConnectionThread extends Thread {
+	private static final int CHECK_CANCEL_INTERVAL = 500;
+	private final TestConnectionProcessor testConnectionProcessor;
+	private final ServerDataExt server;
+
+	public TestConnectionThread(@org.jetbrains.annotations.NotNull String s, TestConnectionProcessor testConnectionProcessor,
+			ServerDataExt server) {
+		super(s);
+		this.testConnectionProcessor = testConnectionProcessor;
+		this.server = server;
+	}
+
+	@Override
+	public void run() {
+			ProductConnector productConnector;
+			ConnectionWrapper testConnector;
+			switch (server.getServerType()) {
+				case JIRA_SERVER:
+					productConnector = new ProductConnector(JIRAServerFacadeImpl.getInstance());
+					break;
+
+				case BAMBOO_SERVER:
+					productConnector = new ProductConnector(BambooServerFacadeImpl.getInstance(PluginUtil.getLogger()));
+					break;
+
+				case CRUCIBLE_SERVER:
+				case FISHEYE_SERVER:
+					productConnector = new ProductConnector(CrucibleServerFacadeImpl.getInstance());
+					break;
+
+				default:
+					return;
+
+			}
+			testConnector = new ConnectionWrapper(productConnector, server.getServerData(), "testing connection");
+
+			testConnector.start();
+			while (testConnector.getConnectionState() == ConnectionWrapper.ConnectionState.NOT_FINISHED) {
+				try {
+					if (false) {
+						testConnector.setInterrupted();
+						//t.interrupt();
+						break;
+					} else {
+						java.lang.Thread.sleep(CHECK_CANCEL_INTERVAL);
+					}
+				} catch (InterruptedException e) {
+					//log.info(e.getMessage());
+				}
+			}
+
+			if (testConnector.getConnectionState() == ConnectionWrapper.ConnectionState.SUCCEEDED) {
+				testConnectionProcessor.onSuccess();
+			} else {
+				testConnectionProcessor.onError(testConnector.getErrorMessage());
+			}
+
+		}
+
+
+}
