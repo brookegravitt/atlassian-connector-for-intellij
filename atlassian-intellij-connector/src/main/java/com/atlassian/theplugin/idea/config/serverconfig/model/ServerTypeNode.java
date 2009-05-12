e84e5ab72f4a3d4a1c208c81076819b8f30d9054
jjaroczynski <jjaroczynski@localhost>
2009-05-12 09:36:49 +0000

    default credentials testing improvement

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/config/serverconfig/model/ServerTypeNode.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/config/serverconfig/model/ServerTypeNode.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,7 +28,7 @@ import javax.swing.tree.DefaultMutableTreeNode;
  */
 public class ServerTypeNode extends DefaultMutableTreeNode {
 	static final long serialVersionUID = -1192703287399203299L;
-	
+
 	private ServerType serverType;
 
 	public ServerTypeNode(ServerType aServerType) {
@@ -44,7 +44,7 @@ public class ServerTypeNode extends DefaultMutableTreeNode {
 	}
 
 	public String toString() {
-		return serverType.toString();
+		return serverType.toString() + " Servers";
 	}
 
 	public boolean equals(Object o) {
