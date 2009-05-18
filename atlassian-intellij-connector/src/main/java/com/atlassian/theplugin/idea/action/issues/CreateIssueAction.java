1a865656fe2d2795780d4b5960347fcb19174626
jgorycki <jgorycki@localhost>
2009-05-18 13:30:48 +0000

    PL-1480 - Make create JIRA hotkey available globally

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/CreateIssueAction.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/CreateIssueAction.java
@@ -3,8 +3,9 @@ package com.atlassian.theplugin.idea.action.issues;
 import com.atlassian.theplugin.idea.IdeaHelper;
 import com.atlassian.theplugin.idea.jira.IssueListToolWindowPanel;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.AnAction;
 
-public class CreateIssueAction extends JIRAAbstractAction {
+public class CreateIssueAction extends AnAction {
 	@Override
 	public void actionPerformed(AnActionEvent e) {
 		IssueListToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(e);
@@ -14,7 +15,7 @@ public class CreateIssueAction extends JIRAAbstractAction {
 	}
 
 	@Override
-	public void onUpdate(AnActionEvent event) {
+	public void update(AnActionEvent event) {
 		IssueListToolWindowPanel panel = IdeaHelper.getIssuesToolWindowPanel(event);
 
 		boolean enabled = panel != null && panel.getSelectedServer() != null;
