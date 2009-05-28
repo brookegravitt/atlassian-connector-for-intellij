6c081233bea8b3ba52fb96ff615e04875d25c1b7
jjaroczynski <jjaroczynski@localhost>
2009-05-28 11:11:41 +0000

    PL-1517 - I get an empty tree when sorting bamboo view by Server

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/configuration/BambooProjectConfiguration.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/configuration/BambooProjectConfiguration.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,24 +20,23 @@ package com.atlassian.theplugin.configuration;
  * @author Jacek Jaroczynski
  */
 public class BambooProjectConfiguration {
-	private ProjectToolWindowTableConfiguration tableConfiguration =
-			new ProjectToolWindowTableConfiguration();
+	//	private ProjectToolWindowTableConfiguration tableConfiguration = new ProjectToolWindowTableConfiguration();
 	private BambooViewConfigurationBean view = new BambooViewConfigurationBean();
 
 	public BambooProjectConfiguration() {
 
 	}
 
-	public ProjectToolWindowTableConfiguration getTableConfiguration() {
-		return tableConfiguration;
-	}
-
-	public void setTableConfiguration(ProjectToolWindowTableConfiguration tableConfiguration) {
-		this.tableConfiguration = tableConfiguration;
-	}
+//	public ProjectToolWindowTableConfiguration getTableConfiguration() {
+//		return tableConfiguration;
+//	}
+//
+//	public void setTableConfiguration(ProjectToolWindowTableConfiguration tableConfiguration) {
+//		this.tableConfiguration = tableConfiguration;
+//	}
 
 	public void copyConfiguration(BambooProjectConfiguration bambooConfiguration) {
-		tableConfiguration.copyConfiguration(bambooConfiguration.getTableConfiguration());
+//		tableConfiguration.copyConfiguration(bambooConfiguration.getTableConfiguration());
 		view.copyConfiguration(bambooConfiguration.getView());
 	}
 
