af021ab7b9a7267dfaf95cf584f3dccb19be965f
jjaroczynski <jjaroczynski@localhost>
2009-06-17 15:12:38 +0000

    PL-1537 - Refactor CfgManager: remove CfgManager dependencies

---
+++ atlassian-intellij-connector/src/test/java/com/atlassian/theplugin/cfg/CfgManagerNotificationTest.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (C) 2008 Atlassian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atlassian.theplugin.cfg;
+
+import com.atlassian.theplugin.commons.cfg.*;
+import com.atlassian.theplugin.commons.util.MiscUtil;
+import com.atlassian.theplugin.idea.config.ProjectCfgManagerImpl;
+import com.spartez.util.junit3.IAction;
+import com.spartez.util.junit3.TestUtil;
+import junit.framework.TestCase;
+import org.easymock.EasyMock;
+
+/**
+ * @author Jacek Jaroczynski
+ */
+public class CfgManagerNotificationTest extends TestCase {
+
+	//	private CfgManager cfgManager;
+	private ProjectCfgManagerImpl projectCfgManager;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+//		cfgManager = new CfgManagerImpl();
+		projectCfgManager = new ProjectCfgManagerImpl(null, new CfgManagerImpl(), null);
+		populateServerCfgs();
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	private final BambooServerCfg bamboo1 = new BambooServerCfg("bamboo1", new ServerId());
+	private final JiraServerCfg jira1 = new JiraServerCfg("jira1", new ServerId());
+
+	private void populateServerCfgs() {
+//		cfgManager.addGlobalServer(bamboo);
+//		cfgManager.addGlobalServer(jira);
+
+		projectCfgManager.addProjectSpecificServer(bamboo1);
+		projectCfgManager.addProjectSpecificServer(jira1);
+	}
+
+
+	public void testlNotifications() {
+
+		final ProjectConfiguration emptyCfg = ProjectConfiguration.emptyConfiguration();
+		ConfigurationListener project1Listener = EasyMock.createMock(ConfigurationListener.class);
+
+		// record
+		project1Listener.configurationUpdated(emptyCfg);
+		project1Listener.serverRemoved(bamboo1);
+		project1Listener.serverRemoved(jira1);
+		project1Listener.bambooServersChanged(emptyCfg);
+		project1Listener.jiraServersChanged(emptyCfg);
+
+		// test
+		EasyMock.replay(project1Listener);
+		projectCfgManager.addProjectConfigurationListener(project1Listener);
+
+//		cfgManager.updateGlobalConfiguration(new GlobalConfiguration());
+		projectCfgManager.updateProjectConfiguration(emptyCfg);
+
+		EasyMock.verify(project1Listener);
+		EasyMock.reset(project1Listener);
+
+//		cfgManager.updateGlobalConfiguration(new GlobalConfiguration());
+
+		projectCfgManager.removeProjectConfigurationListener(project1Listener);
+		final ProjectConfiguration nonEmptyCfg = new ProjectConfiguration(MiscUtil.<ServerCfg>buildArrayList(bamboo1));
+
+		// record
+
+		// test
+		EasyMock.replay(project1Listener);
+//		cfgManager.updateGlobalConfiguration(new GlobalConfiguration());
+		projectCfgManager.updateProjectConfiguration(nonEmptyCfg);
+		EasyMock.verify(project1Listener);
+	}
+
+
+	public void testAddListener() {
+		TestUtil.assertThrows(IllegalArgumentException.class, new IAction() {
+			public void run() throws Throwable {
+				projectCfgManager.addProjectConfigurationListener(null);
+			}
+		});
+	}
+
+}
