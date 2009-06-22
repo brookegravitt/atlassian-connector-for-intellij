e951abfef512441579d8261fb8c67f8959cff45b
jgorycki <jgorycki@localhost>
2009-06-22 11:41:41 +0000

    PL-1629 - Exception when opening the project

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/IdeaHelper.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/IdeaHelper.java
@@ -210,16 +210,12 @@ public final class IdeaHelper {
 	}
 
 	public static <T> T getProjectComponent(final AnActionEvent event, final Class<T> clazz) {
-		final Project project = getCurrentProject(event);
-		if (project == null) {
-			return null;
-		}
-		return getProjectComponent(project, clazz);
+		return getProjectComponent(getCurrentProject(event), clazz);
 	}
 
 	@Nullable
 	public static <T> T getProjectComponent(final Project project, final Class<T> clazz) {
-		if (project == null) {
+		if (project == null || project.isDisposed()) {
 			return null;
 		}
 		return clazz.cast(project.getPicoContainer().getComponentInstanceOfType(clazz));
