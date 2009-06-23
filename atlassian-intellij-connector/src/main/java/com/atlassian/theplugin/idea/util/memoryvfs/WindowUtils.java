b1a2cb44deb171d745396d11ac2b1ba5351b5df5
pmaruszak <pmaruszak@localhost>
2009-06-23 09:23:33 +0000

    PL-1561 reverting changes

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/util/memoryvfs/WindowUtils.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/util/memoryvfs/WindowUtils.java
@@ -6,24 +6,6 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-/**
- * Created by IntelliJ IDEA.
- * User: pmaruszak
- * Date: Jun 22, 2009
- * Time: 11:36:49 AM
- * To change this template use File | Settings | File Templates.
- */
-public class WindowUtils {
-    /**
-     * Set the component to have focus when the given window is first shown.
-     *
-     * @param window    the window
-     * @param component the component to have focus
-     */
-    public static void setInitialFocusComponent(Window window, Component component) {
-        window.addWindowListener(new InitialFocusSetter(component));
-    }
-}
 class InitialFocusSetter extends WindowAdapter {
     private Component component;
     public InitialFocusSetter(Component component) {
