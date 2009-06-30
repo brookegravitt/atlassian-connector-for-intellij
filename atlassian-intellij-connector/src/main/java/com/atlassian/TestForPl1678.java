44d6422e7472582cf45a14e2a9a879137b020334
jgorycki <jgorycki@localhost>
2009-06-30 10:25:05 +0000

    test PL-1678

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/TestForPl1678.java
@@ -0,0 +1,11 @@
+package com.atlassian;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kalamon
+ * Date: Jun 30, 2009
+ * Time: 12:23:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestForPl1678 {
+}
