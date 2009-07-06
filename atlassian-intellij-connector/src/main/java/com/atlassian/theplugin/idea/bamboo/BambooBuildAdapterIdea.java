38ffe3ba08b877d036c9984210a99d400106f76c
jjaroczynski <jjaroczynski@localhost>
2009-07-06 13:51:46 +0000

    PL-1596: bamboo build in progress

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/bamboo/BambooBuildAdapterIdea.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/bamboo/BambooBuildAdapterIdea.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2008 Atlassian
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@ import com.atlassian.theplugin.commons.bamboo.BambooBuild;
 import com.atlassian.theplugin.commons.bamboo.BambooBuildAdapter;
 import com.atlassian.theplugin.commons.bamboo.BuildStatus;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.util.ui.AsyncProcessIcon;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -31,11 +32,15 @@ public class BambooBuildAdapterIdea extends BambooBuildAdapter {
 	private static final Icon ICON_GREEN = IconLoader.getIcon("/icons/icn_plan_passed.gif");
 	private static final Icon ICON_GREY = IconLoader.getIcon("/icons/icn_plan_disabled.gif");
 
+	private static final Icon ICON_IS_IN_QUEUE = IconLoader.getIcon("/icons/bamboo_building.gif");
+	private static final Icon ICON_IS_BUILDING = IconLoader.getIcon("/icons/cup.png");
+
 	private static final Icon ICON_MY_BUILD_RED = IconLoader.getIcon("/actions/lightning.png");
 	private static final Icon ICON_MY_BUILD_GREEN = IconLoader.getIcon("/icons/lightning_green.png");
 
 	public BambooBuildAdapterIdea(BambooBuild build) {
 		super(build);
+
 	}
 
 	@Nullable
@@ -52,6 +57,13 @@ public class BambooBuildAdapterIdea extends BambooBuildAdapter {
 	@NotNull
 	public Icon getIcon() {
 		if (build.getEnabled()) {
+
+			if (build.isBuilding()) {
+				return ICON_IS_BUILDING;
+			} else if (build.isInQueue()) {
+				return ICON_IS_IN_QUEUE;
+			}
+
 			switch (getStatus()) {
 				case FAILURE:
 					return ICON_RED;
@@ -83,10 +95,34 @@ public class BambooBuildAdapterIdea extends BambooBuildAdapter {
 		return AdjustedBuildStatus.DISABLED;
 	}
 
-
 	public boolean areActionsAllowed() {
 		final AdjustedBuildStatus buildStatus = getAdjustedStatus();
 		return buildStatus != AdjustedBuildStatus.UNKNOWN && buildStatus != AdjustedBuildStatus.DISABLED;
 	}
 
+	public class BuildInProgressIcon extends AsyncProcessIcon {
+
+		private Icon[] icons;
+		private Icon passiveIcon;
+		private static final int CYCLE_LENGTH = 600; // whole animation single cycle lenght
+		private static final int CYCLE_GAP = 60; // break after every single cycle (best 'cycleLenght / number of frames')
+
+		public BuildInProgressIcon(@org.jetbrains.annotations.NonNls String name) {
+			super(name);
+
+			// comment that line if you want to use standard IDEA small progress circle
+//			initCustomLook();
+		}
+
+		private void initCustomLook() {
+			loadIcons();
+			init(icons, passiveIcon, CYCLE_LENGTH, CYCLE_GAP, -1);
+		}
+
+		private void loadIcons() {
+			icons = new Icon[]{ICON_GREEN, ICON_GREY};
+
+			passiveIcon = ICON_RED;
+		}
+	}
 }
