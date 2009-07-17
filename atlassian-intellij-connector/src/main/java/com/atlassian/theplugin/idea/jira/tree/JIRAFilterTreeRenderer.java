61a54f4e8fc4b0ac68ca0ab2af6352b0a7bffe9a
pmaruszak <pmaruszak@localhost>
2009-07-17 10:32:29 +0000

    PL-1590 - Context menu for filters in Issues

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAFilterTreeRenderer.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/tree/JIRAFilterTreeRenderer.java
@@ -19,85 +19,84 @@ import java.util.Map;
  * User: pmaruszak
  */
 public class JIRAFilterTreeRenderer extends DefaultTreeCellRenderer {
-	private static final Icon JIRA_MANUAL_FILTER_ICON = IconLoader.getIcon("/actions/showViewer.png");
-	private static final Icon JIRA_SAVED_FILTER_ICON = IconLoader.getIcon("/actions/showSource.png");
+    private static final Icon JIRA_MANUAL_FILTER_ICON = IconLoader.getIcon("/actions/showViewer.png");
+    private static final Icon JIRA_SAVED_FILTER_ICON = IconLoader.getIcon("/actions/showSource.png");
     private static final String TOOLTOP_FOOTER_HTML = "<hr style=\"height: '1'; text-align: 'left'; "
             + "color: 'black'; width: '100%'\">"
             + "<p style=\"font-size:'90%'; color:'grey'\">right click on filter node to edit</p>";
 
-	@Override
-	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected,
-			boolean expanded, boolean leaf, int row, boolean hasFocus) {
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected,
+                                                  boolean expanded, boolean leaf, int row, boolean hasFocus) {
 
-		JComponent c = (JComponent) super.getTreeCellRendererComponent(
-				tree, value, selected, expanded, leaf, row, hasFocus);
-         setToolTipText(null);
-        
-		if (value instanceof JIRAManualFilterTreeNode && c instanceof JLabel) {
+        JComponent c = (JComponent) super.getTreeCellRendererComponent(
+                tree, value, selected, expanded, leaf, row, hasFocus);
+        setToolTipText(null);
+
+        if (value instanceof JIRAManualFilterTreeNode && c instanceof JLabel) {
             final JIRAManualFilterTreeNode filterTreeNode = (JIRAManualFilterTreeNode) value;
             setToolTipText(createManualFilterToolTipText(filterTreeNode.getManualFilter(),
                     filterTreeNode.getJiraServerCfg()));
             ((JLabel) c).setIcon(JIRA_MANUAL_FILTER_ICON);
             return c;
             //return ((JIRAManualFilterTreeNode) value).getRenderer(c, selected, expanded, hasFocus);
-		}
+        }
 
-		if (value instanceof JIRASavedFilterTreeNode && c instanceof JLabel) {
+        if (value instanceof JIRASavedFilterTreeNode && c instanceof JLabel) {
 
-			((JLabel) c).setIcon(JIRA_SAVED_FILTER_ICON);
-			return c;
-		}
+            ((JLabel) c).setIcon(JIRA_SAVED_FILTER_ICON);
+            return c;
+        }
 
-		if (value instanceof JiraRecentlyOpenTreeNode && c instanceof JLabel) {
-			((JLabel) c).setIcon(JIRA_SAVED_FILTER_ICON);
-			return c;
-		}
+        if (value instanceof JiraRecentlyOpenTreeNode && c instanceof JLabel) {
+            ((JLabel) c).setIcon(JIRA_SAVED_FILTER_ICON);
+            return c;
+        }
 
-		if (value instanceof AbstractTreeNode) {
-			return ((AbstractTreeNode) value).getRenderer(c, selected, expanded, hasFocus);
-		}
-		return c;
-	}
+        if (value instanceof AbstractTreeNode) {
+            return ((AbstractTreeNode) value).getRenderer(c, selected, expanded, hasFocus);
+        }
+        return c;
+    }
 
 
-     private String createManualFilterToolTipText(JIRAManualFilter manualFilter, ServerData server) {
+    private String createManualFilterToolTipText(JIRAManualFilter manualFilter, ServerData server) {
 
         Collection<Entry> entries = MiscUtil.buildArrayList();
         Map<JIRAManualFilter.QueryElement, ArrayList<String>> map = manualFilter.groupBy(true);
-		for (JIRAManualFilter.QueryElement element : map.keySet()) {
-			entries.add(new Entry(element.getName(), StringUtils.join(map.get(element), ", ")));
-		}
+        for (JIRAManualFilter.QueryElement element : map.keySet()) {
+            entries.add(new Entry(element.getName(), StringUtils.join(map.get(element), ", ")));
+        }
 
 
-		if (entries.size() == 0) {
-			// get also 'any' values
-			map = manualFilter.groupBy(false);
-			for (JIRAManualFilter.QueryElement element : map.keySet()) {
-				entries.add(new Entry(element.getName(), StringUtils.join(map.get(element), ", ")));
-			}
-		}
-        StringBuffer sb = new StringBuffer();
         if (entries.size() == 0) {
-			sb.append("<html>");
+            // get also 'any' values
+            map = manualFilter.groupBy(false);
+            for (JIRAManualFilter.QueryElement element : map.keySet()) {
+                entries.add(new Entry(element.getName(), StringUtils.join(map.get(element), ", ")));
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+
+        if (entries.size() == 0) {
+
             sb.append("No Custom Filter Defined");
             sb.append(TOOLTOP_FOOTER_HTML);
-            sb.append("</html>");
-            return sb.toString();
-		}
+        } else {
 
-
-        sb.append("<html><table>");
-        sb.append("<tr><td><font color=blue><b>").append(server.getName()).append("</b></td><td></td>");
-        for (Entry entry : entries) {
-            sb.append("<tr><td><b>").append(entry.getLabel()).append(":")
-                    .append("</b></td><td>");
-            if (entry.isError()) {
-                sb.append("<font color=red>");
+            sb.append("<html><table>");
+            sb.append("<tr><td><font color=blue><b>").append(server.getName()).append("</b></td><td></td>");
+            for (Entry entry : entries) {
+                sb.append("<tr><td><b>").append(entry.getLabel()).append(":")
+                        .append("</b></td><td>");
+                if (entry.isError()) {
+                    sb.append("<font color=red>");
+                }
+                sb.append(entry.getValue()).append("</td></tr>");
             }
-
-            sb.append(entry.getValue()).append("</td></tr>");
+            sb.append("</table>");
         }
-        sb.append("</table>");
         sb.append(TOOLTOP_FOOTER_HTML);
         sb.append("</html>");
         return sb.toString();
