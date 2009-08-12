e74cbb990ebbc6a0b5980b8fae389dfd1a215d1d
wseliga <wseliga@localhost>
2009-08-12 15:05:02 +0000

    ACC-44 passing also IntelliJ version

--- atlassian-intellij-connector/src/main/java/com/atlassian/connector/intellij/remoteapi/IntelliJHttpSessionCallback.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/connector/intellij/remoteapi/IntelliJHttpSessionCallback.java
@@ -4,6 +4,7 @@ import com.atlassian.connector.commons.api.ConnectionCfg;
 import com.atlassian.connector.intellij.util.HttpClientFactory;
 import com.atlassian.theplugin.commons.exception.HttpProxySettingsException;
 import com.atlassian.theplugin.commons.remoteapi.rest.HttpSessionCallbackImpl;
+import com.atlassian.theplugin.idea.BugReporting;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 
@@ -11,10 +12,11 @@ import org.apache.commons.httpclient.params.HttpMethodParams;
  * @author Wojciech Seliga
  */
 public class IntelliJHttpSessionCallback extends HttpSessionCallbackImpl {
+	private static final String USER_AGENT = "Atlassian Connector for IntelliJ, version " + BugReporting.getVersionString();
 
 	public HttpClient getHttpClient(ConnectionCfg server) throws HttpProxySettingsException {
 		final HttpClient client = HttpClientFactory.getClient();
-		client.getParams().setParameter(HttpMethodParams.USER_AGENT, "Atlassian Connector for IntelliJ");
+		client.getParams().setParameter(HttpMethodParams.USER_AGENT, USER_AGENT);
 		return client;
 	}
 
