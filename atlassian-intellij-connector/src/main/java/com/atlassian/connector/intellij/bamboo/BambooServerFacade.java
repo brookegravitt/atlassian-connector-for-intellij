700b9054ce0fc5e6474815c22f3afae056c5e1d2
jgorycki <jgorycki@localhost>
2009-08-19 10:31:41 +0000

    PL-1598 - Show JIRA issues in build

--- atlassian-intellij-connector/src/main/java/com/atlassian/connector/intellij/bamboo/BambooServerFacade.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/connector/intellij/bamboo/BambooServerFacade.java
@@ -25,7 +25,6 @@ import com.atlassian.theplugin.commons.cfg.SubscribedPlan;
 import com.atlassian.theplugin.commons.exception.ServerPasswordNotProvidedException;
 import com.atlassian.theplugin.commons.remoteapi.ProductServerFacade;
 import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
-import com.atlassian.connector.commons.api.ConnectionCfg;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
