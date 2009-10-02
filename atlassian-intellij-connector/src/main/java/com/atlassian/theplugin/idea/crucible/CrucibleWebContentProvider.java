bc111ff579ade4869a239c49e467a02d1276788b
jjaroczynski <jjaroczynski@localhost>
2009-10-02 15:04:16 +0000

    PLE-615 https://studio.atlassian.com/browse/PLE-615 added possibility to open and compare pre-commit changed files

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleWebContentProvider.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleWebContentProvider.java
@@ -23,6 +23,7 @@ import com.atlassian.theplugin.commons.VersionedVirtualFile;
 import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFileInfo;
 import com.atlassian.theplugin.commons.exception.ServerPasswordNotProvidedException;
 import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
+import com.atlassian.theplugin.commons.util.UrlUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.vfs.VcsVirtualFile;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -50,14 +51,9 @@ public class CrucibleWebContentProvider extends IdeaReviewFileContentProvider {
             if (contentUrl == null) {
                 return null;
             }
-            String[] serverTokens = serverUrl.split("/");
-            String[] contentTokens = contentUrl.split("/");
-
-            if (serverTokens.length > 0 && contentTokens.length > 0) {
-                if (serverTokens[serverTokens.length - 1].equals(contentTokens[0])) {
-                    contentUrl = contentUrl.substring(contentTokens[0].length(), contentUrl.length());
-                }
-            }
+            
+            contentUrl = UrlUtil.adjustUrlPath(contentUrl, serverUrl);
+            
             byte[] content = IntelliJCrucibleServerFacade.getInstance().getFileContent(review.getServerData(), contentUrl);
 
             VirtualFile file = new VcsVirtualFile(versionedVirtualFile.getUrl(), content,
