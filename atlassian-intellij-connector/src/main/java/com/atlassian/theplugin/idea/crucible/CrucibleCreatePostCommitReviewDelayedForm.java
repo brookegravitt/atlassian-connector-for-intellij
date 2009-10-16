df7cc317356736f41a5596c857355e04ea7029a8
pmaruszak <pmaruszak@localhost>
2009-10-16 14:38:28 +0000

    Fixing problem with creating review without files

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleCreatePostCommitReviewDelayedForm.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/crucible/CrucibleCreatePostCommitReviewDelayedForm.java
@@ -27,13 +27,14 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.*;
+import com.intellij.openapi.vcs.AbstractVcsHelper;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ChangeList;
+import com.intellij.openapi.vcs.changes.committed.CommittedChangesCache;
 import com.intellij.openapi.vcs.versionBrowser.ChangeBrowserSettings;
 import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.vcsUtil.VcsUtil;
+import com.intellij.util.Consumer;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -42,121 +43,126 @@ import java.util.Collection;
 import java.util.List;
 
 public class CrucibleCreatePostCommitReviewDelayedForm extends AbstractCrucibleCreatePostCommitReviewForm {
-	private boolean doCreateReview = false;
-	List<CommittedChangeList> list;
-	private CrucibleConfigurationBean cruciblePluginConfig;
-	private Collection<VirtualFile> virtualFiles;
+    private boolean doCreateReview = false;
+    List<CommittedChangeList> list;
+    private CrucibleConfigurationBean cruciblePluginConfig;
+    private Collection<VirtualFile> virtualFiles;
 
-	public CrucibleCreatePostCommitReviewDelayedForm(
-			final Project project,
-			final CrucibleServerFacade crucibleServerFacade,
-			@NotNull final ProjectCfgManagerImpl projectCfgManager,
-			CrucibleConfigurationBean cruciblePluginConfig,
-			String title,
-			Collection<VirtualFile> virtualFiles) {
+    public CrucibleCreatePostCommitReviewDelayedForm(
+            final Project project,
+            final CrucibleServerFacade crucibleServerFacade,
+            @NotNull final ProjectCfgManagerImpl projectCfgManager,
+            CrucibleConfigurationBean cruciblePluginConfig,
+            String title,
+            Collection<VirtualFile> virtualFiles) {
 
-		super(project, crucibleServerFacade, title, projectCfgManager);
-		this.cruciblePluginConfig = cruciblePluginConfig;
-		this.virtualFiles = virtualFiles;
-		setCustomComponent(null);
-		setReviewCreationTimeout(cruciblePluginConfig.getReviewCreationTimeout());
-		pack();
-	}
+        super(project, crucibleServerFacade, title, projectCfgManager);
+        this.cruciblePluginConfig = cruciblePluginConfig;
+        this.virtualFiles = virtualFiles;
+        setCustomComponent(null);
+        setReviewCreationTimeout(cruciblePluginConfig.getReviewCreationTimeout());
+        pack();
+    }
 
-	@Override
-	protected void doOKAction() {
-		doCreateReview = true;
-		doCancelAction();
-	}
+    @Override
+    protected void doOKAction() {
+        doCreateReview = true;
+        doCancelAction();
+    }
 
-	@Override
-	protected ReviewAdapter createReview(final ServerData server, final ReviewProvider reviewProvider)
-			throws RemoteApiException, ServerPasswordNotProvidedException {
-		return createReviewImpl(server, reviewProvider, getChanges());
-	}
+    @Override
+    protected ReviewAdapter createReview(final ServerData server, final ReviewProvider reviewProvider)
+            throws RemoteApiException, ServerPasswordNotProvidedException {
+        return createReviewImpl(server, reviewProvider, getChanges());
+    }
 
-	private ChangeList[] getChanges() {
-		if (list.size() > 0) {
-			for (CommittedChangeList committedChangeList : list) {
-				if (isMyCommittedChangeList(committedChangeList)) {
-					ChangeList[] chlist = new ChangeList[1];
-					chlist[0] = committedChangeList;
-					return chlist;
-				}
-			}
-		}
-		return null;
-	}
+    private ChangeList[] getChanges() {
+        if (list.size() > 0) {
+            for (CommittedChangeList committedChangeList : list) {
+                if (isMyCommittedChangeList(committedChangeList)) {
+                    ChangeList[] chlist = new ChangeList[1];
+                    chlist[0] = committedChangeList;
+                    return chlist;
+                }
+            }
+        }
+        return null;
+    }
 
-	private boolean isMyCommittedChangeList(CommittedChangeList committedChangeList) {
-		int verifiedChangesCnt = 0;
-		for (VirtualFile virtualFile : virtualFiles) {
-			for (Change change : committedChangeList.getChanges()) {
-				if (change.affectsFile(new File(virtualFile.getPath()))) {
-					++verifiedChangesCnt;
-					break;
-				}
-			}
-		}
-		return verifiedChangesCnt == virtualFiles.size();
-	}
+    private boolean isMyCommittedChangeList(CommittedChangeList committedChangeList) {
+        int verifiedChangesCnt = 0;
+        for (VirtualFile virtualFile : virtualFiles) {
+            for (Change change : committedChangeList.getChanges()) {
+                if (change.affectsFile(new File(virtualFile.getPath()))) {
+                    ++verifiedChangesCnt;
+                    break;
+                }
+            }
+        }
+        return verifiedChangesCnt == virtualFiles.size();
+    }
 
-	public void startReviewCreation() {
-		if (doCreateReview) {
-			doCreateReview = false;
-			final Task.Backgroundable task = new ChangesRefreshTask();
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
+    public void startReviewCreation() {
+        if (doCreateReview) {
+            doCreateReview = false;
+            final Task.Backgroundable task = new ChangesRefreshTask();
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
                     LoggerImpl.getInstance().info("CrucibleCreatePostCommitReviewDelayedForm.startReviewCreation() - starting ChangesRefreshTask");
-					ProgressManager.getInstance().run(task);
-				}
-			});
-		} else {
+                    ProgressManager.getInstance().run(task);
+                }
+            });
+        } else {
             LoggerImpl.getInstance().info("CrucibleCreatePostCommitReviewDelayedForm.startReviewCreation() - doCreateReview == false");
         }
-	}
+    }
 
-	private class ChangesRefreshTask extends Task.Backgroundable {
-		private static final int REVISIONS_NUMBER = 30;
+    private class ChangesRefreshTask extends Task.Backgroundable {
+        private static final int REVISIONS_NUMBER = 30;
 
-		public ChangesRefreshTask() {
-			super(project, "Fetching recent commits");
-		}
+        public ChangesRefreshTask() {
+            super(project, "Fetching recent commits");
+        }
 
-		public void run(@NotNull ProgressIndicator progressIndicator) {
-			final VirtualFile baseDir = project.getBaseDir();
-			if (baseDir == null) {
-				throw new RuntimeException("Cannot determine base directory of the project");
-			}
-			ProjectLevelVcsManager mgr = ProjectLevelVcsManager.getInstance(project);
-			AbstractVcs abstractVcs = mgr.getVcsFor(baseDir);
-			if (abstractVcs != null) {
-                LoggerImpl.getInstance().info("ChangesRefreshTask.run() - getting committed changes provider");
-				@SuppressWarnings("unchecked")
-				final CachingCommittedChangesProvider<CommittedChangeList, ChangeBrowserSettings> committedChangesProvider
-						= abstractVcs.getCachingCommittedChangesProvider();
-				if (committedChangesProvider == null) {
-					throw new RuntimeException("Cannot determine VCS support for the project");
-				}
-				ChangeBrowserSettings changeBrowserSettings = new ChangeBrowserSettings();
-				RepositoryLocation repositoryLocation = committedChangesProvider
-						.getLocationFor(VcsUtil.getFilePath(baseDir.getPath()));
-				try {
-                    LoggerImpl.getInstance().info("ChangesRefreshTask.run() - committedChangesProvider.getCommittedChanges()");
-					list = committedChangesProvider.getCommittedChanges(
-							changeBrowserSettings, repositoryLocation, REVISIONS_NUMBER);
-				} catch (VcsException e) {
-					LoggerImpl.getInstance().error(e);
-				}
-			} else {
-                LoggerImpl.getInstance().info("ChangesRefreshTask.run() - vcs is null");
+        public void run(@NotNull ProgressIndicator progressIndicator) {
+            final VirtualFile baseDir = project.getBaseDir();
+            if (baseDir == null) {
+                throw new RuntimeException("Cannot determine base directory of the project");
             }
-		}
 
-		@Override
-		public void onSuccess() {
+
+            final ChangeBrowserSettings changeBrowserSettings = new ChangeBrowserSettings();
+            CommittedChangesCache.getInstance(project).getProjectChangesAsync(changeBrowserSettings, REVISIONS_NUMBER, false, new Consumer() {
+                public void consume(List committedChangeList) {
+                    list = committedChangeList;
+                    runCreateReviewTask(true);
+                }
+
+                public void consume(Object o) {
+                    consume((List) o);
+                }
+            }
+
+                    , new Consumer() {
+                        public void consume(List list) {
+                            AbstractVcsHelper helper = AbstractVcsHelper.getInstance(project);
+                            if (helper != null) {
+                                helper.showErrors(list, "Error refreshing VCS history");
+                            }
+                        }
+
+                        public void consume(Object o) {
+                            consume((List) o);
+                        }
+                    });
+
+        }
+
+
+        @Override
+        public void onSuccess() {
             LoggerImpl.getInstance().info("ChangesRefreshTask.onSuccess() - runCreateReviewTask()");
-			runCreateReviewTask(true);
-		}
-	}
+            //runCreateReviewTask(true);
+        }
+    }
 }
