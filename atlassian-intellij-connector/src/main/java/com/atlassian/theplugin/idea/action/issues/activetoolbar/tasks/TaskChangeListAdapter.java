e951941b59ba2094845a4979ebf2c7ad4fc2bc6c
pmaruszak <pmaruszak@localhost>
2009-11-24 09:37:58 +0000

    Refactoring of tasks

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/tasks/TaskChangeListAdapter.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/tasks/TaskChangeListAdapter.java
@@ -1,9 +1,16 @@
 package com.atlassian.theplugin.idea.action.issues.activetoolbar.tasks;
 
+import com.atlassian.theplugin.commons.jira.api.JiraIssueAdapter;
+import com.atlassian.theplugin.idea.IdeaHelper;
+import com.atlassian.theplugin.idea.action.issues.activetoolbar.ActiveIssueUtils;
+import com.atlassian.theplugin.jira.model.ActiveJiraIssue;
+import com.atlassian.theplugin.jira.model.JIRAIssueListModelBuilder;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.changes.ChangeList;
 import com.intellij.openapi.vcs.changes.ChangeListAdapter;
+import com.intellij.openapi.vcs.changes.ChangeListManager;
+import com.intellij.openapi.vcs.changes.LocalChangeList;
 
 /**
  * User: pmaruszak
@@ -29,6 +36,26 @@ public class TaskChangeListAdapter extends ChangeListAdapter {
     public void defaultListChanged(final ChangeList oldDefaultList, final ChangeList newDefaultList) {
         ApplicationManager.getApplication().invokeLater(new SwitchActiveIssueRunnable(project, newDefaultList));
     }
-   
+
+    @Override
+    public void changeListAdded(ChangeList list) {
+        ActiveJiraIssue activeIssue = ActiveIssueUtils.getActiveJiraIssue(project);
+        JIRAIssueListModelBuilder builder = IdeaHelper.getJIRAIssueListModelBuilder(project);
+        final ChangeListManager changeListManager = ChangeListManager.getInstance(project);
+
+        if (builder != null && builder.getModel() != null) {
+            JiraIssueAdapter issue = builder.getModel().findIssue(activeIssue.getIssueKey());
+            if (issue != null) {
+                String changeListName = issue.getKey() + " - " + issue.getSummary() + "\n";
+                if (list instanceof LocalChangeList && list.getComment().isEmpty()) {
+                    ((LocalChangeList)list).setComment(changeListName);
+                }
+
+            }
+        }
+
+    }
+
+
 }
 
