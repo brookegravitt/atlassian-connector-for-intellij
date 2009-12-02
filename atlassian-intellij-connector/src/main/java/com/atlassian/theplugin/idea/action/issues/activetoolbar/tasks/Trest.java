907ad77b93ffcdf2864634650e7145227ace4393
pmaruszak <pmaruszak@localhost>
2009-12-02 10:27:49 +0000

    tersting

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/tasks/Trest.java
@@ -0,0 +1,12 @@
+package com.atlassian.theplugin.idea.action.issues.activetoolbar.tasks;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Dec 2, 2009
+ * Time: 11:27:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Trest {
+    ///test1
+}
