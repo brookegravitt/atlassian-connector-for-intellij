5e376c2e249892251aeff0dfbf853d9e46b80bcd
pmaruszak <pmaruszak@localhost>
2009-12-02 14:28:37 +0000

    PR-1 - LargeIcon

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/tasks/dummyCls.java
@@ -0,0 +1,11 @@
+package com.atlassian.theplugin.idea.action.issues.activetoolbar.tasks;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Dec 2, 2009
+ * Time: 3:28:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class dummyCls {
+}
