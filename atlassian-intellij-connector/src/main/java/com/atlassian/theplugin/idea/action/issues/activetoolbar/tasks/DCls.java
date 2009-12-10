f7c6225268fa7b1a3160ffaadf8abccdbe11edaf
pmaruszak <pmaruszak@localhost>
2009-12-10 10:25:49 +0000

    test YYY

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/tasks/DCls.java
@@ -0,0 +1,11 @@
+package com.atlassian.theplugin.idea.action.issues.activetoolbar.tasks;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Dec 10, 2009
+ * Time: 11:24:54 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DCls {
+}
