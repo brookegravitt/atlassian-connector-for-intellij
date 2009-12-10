f90607b4a5075039245adb43d02de60af95eb1fd
pmaruszak <pmaruszak@localhost>
2009-12-10 10:43:03 +0000

    ZXZXZX

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/tasks/DTL.java
@@ -0,0 +1,11 @@
+package com.atlassian.theplugin.idea.action.issues.activetoolbar.tasks;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Dec 10, 2009
+ * Time: 11:42:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DTL {
+}
