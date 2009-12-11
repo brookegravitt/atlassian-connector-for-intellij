84885b9f022cc18393186b0f9e65acc7fa705dc0
pmaruszak <pmaruszak@localhost>
2009-12-11 10:32:31 +0000

    PL-1935 exception message has changed == no waiting for crucible update to newest changeset

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/issues/activetoolbar/tasks/DMYCls.java
@@ -0,0 +1,11 @@
+package com.atlassian.theplugin.idea.action.issues.activetoolbar.tasks;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: Dec 11, 2009
+ * Time: 11:32:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DMYCls {
+}
