f37b04e03c089410459f4af35d155dd5b290d6f9
jgorycki <jgorycki@localhost>
2009-12-16 11:04:19 +0000

    test commit

--- atlassian-intellij-connector/src/main/java/Class5.java
+++ atlassian-intellij-connector/src/main/java/Class5.java
@@ -2,10 +2,8 @@
  * Created by IntelliJ IDEA.
  * User: kalamon
  * Date: 2009-12-16
- * Time: 11:52:14
+ * Time: 12:03:34
  * To change this template use File | Settings | File Templates.
  */
-public class Class3 {
-    //
-    //
+public class Class5 {
 }
