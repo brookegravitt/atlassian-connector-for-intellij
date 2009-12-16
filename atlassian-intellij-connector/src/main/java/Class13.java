92e71d5c39ae539c8369204292be119fa1175a96
jgorycki <jgorycki@localhost>
2009-12-16 11:23:20 +0000

    test commit

---
+++ atlassian-intellij-connector/src/main/java/Class13.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: kalamon
+ * Date: 2009-12-16
+ * Time: 12:23:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class Class13 {
+}
