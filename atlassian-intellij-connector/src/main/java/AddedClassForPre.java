b4cf881f48afe7d35405cf526468be82c7121702
jgorycki <jgorycki@localhost>
2009-12-16 10:38:37 +0000

    test commit

---
+++ atlassian-intellij-connector/src/main/java/AddedClassForPre.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: kalamon
+ * Date: Oct 15, 2009
+ * Time: 3:56:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AddedClassForPre {
+}
