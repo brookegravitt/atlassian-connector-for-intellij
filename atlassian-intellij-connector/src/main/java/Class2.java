badc1f8a9a3c02afa4687eb59158ee5c95a43ed2
jgorycki <jgorycki@localhost>
2009-12-16 10:47:03 +0000

    test commit

---
+++ atlassian-intellij-connector/src/main/java/Class2.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: kalamon
+ * Date: 2009-12-16
+ * Time: 11:46:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class Class2 {
+}
