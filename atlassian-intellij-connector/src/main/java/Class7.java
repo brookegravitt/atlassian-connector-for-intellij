1436642d480bc3efd8ce798a9045a42c373a75e9
jgorycki <jgorycki@localhost>
2009-12-16 11:09:46 +0000

    test commit

--- atlassian-intellij-connector/src/main/java/Class7.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: kalamon
- * Date: 2009-12-16
- * Time: 12:06:26
- * To change this template use File | Settings | File Templates.
- */
-public class Class7 {
-}
