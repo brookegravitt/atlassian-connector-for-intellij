91636b7c1f2ec28e38163a3b22c46b2b976862f4
jgorycki <jgorycki@localhost>
2009-12-16 11:09:46 +0000

    test commit

--- atlassian-intellij-connector/src/main/java/Class10.java
+++ atlassian-intellij-connector/src/main/java/Class10.java
@@ -2,9 +2,8 @@
  * Created by IntelliJ IDEA.
  * User: kalamon
  * Date: 2009-12-16
- * Time: 12:03:34
+ * Time: 12:09:26
  * To change this template use File | Settings | File Templates.
  */
-public class Class5 {
-    // 
+public class Class10 {
 }
--- atlassian-intellij-connector/src/main/java/Class8.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: kalamon
- * Date: 2009-12-16
- * Time: 12:07:04
- * To change this template use File | Settings | File Templates.
- */
-public class Class8 {
-}
