637b011cca7837e648967e8a3862f8801dc8801c
jgorycki <jgorycki@localhost>
2009-12-16 11:15:13 +0000

    test commit

--- atlassian-intellij-connector/src/main/java/Class12.java
+++ atlassian-intellij-connector/src/main/java/Class12.java
@@ -2,8 +2,8 @@
  * Created by IntelliJ IDEA.
  * User: kalamon
  * Date: 2009-12-16
- * Time: 12:12:35
+ * Time: 12:15:04
  * To change this template use File | Settings | File Templates.
  */
-public class Class11 {
+public class Class12 {
 }
