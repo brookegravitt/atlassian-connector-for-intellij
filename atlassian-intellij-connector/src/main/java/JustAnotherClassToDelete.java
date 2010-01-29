e7ba4bc51f71b6d21d06c39684d11a3ac66e1ac7
pstefaniak <pstefaniak@localhost>
2010-01-29 13:26:53 +0000

    i'm just testing PL-1681 (iterative reviews)

---
+++ atlassian-intellij-connector/src/main/java/JustAnotherClassToDelete.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: pstefaniak
+ * Date: Jan 29, 2010
+ * Time: 2:25:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JustAnotherClassToDelete {
+}
