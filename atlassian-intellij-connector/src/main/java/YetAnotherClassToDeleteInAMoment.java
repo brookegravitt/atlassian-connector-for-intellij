5ebe0a022b5c67943aa332257b792c232fc8b2a0
pstefaniak <pstefaniak@localhost>
2010-01-29 12:44:15 +0000

    i'm just testing PL-1681 (iterative reviews)

---
+++ atlassian-intellij-connector/src/main/java/YetAnotherClassToDeleteInAMoment.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: pstefaniak
+ * Date: Jan 29, 2010
+ * Time: 1:42:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class YetAnotherClassToDeleteInAMoment {
+    // comment 1
+}
