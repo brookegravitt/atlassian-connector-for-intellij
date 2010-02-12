04023ce330849320122ef493c8f7cfa7fe952a2b
pstefaniak <pstefaniak@localhost>
2010-02-12 13:35:25 +0000

    test, please ignore this file and this changeset

---
+++ atlassian-intellij-connector/src/main/java/ClassToDelete2.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: pstefaniak
+ * Date: Feb 12, 2010
+ * Time: 2:33:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClassToDelete2 {
+}
