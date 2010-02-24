685897dca7f7a6a38cf5325fbfd4bcff9116a042
pmaruszak <pmaruszak@localhost>
2010-02-24 12:07:36 +0000

    small fix for EventDispatch thread problem

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/StatusBarIssuesPane.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/StatusBarIssuesPane.java
@@ -2,6 +2,7 @@ package com.atlassian.theplugin.idea.jira;
 
 import com.intellij.ui.HyperlinkLabel;
 
+import javax.swing.*;
 import javax.swing.event.HyperlinkListener;
 
 /**
@@ -9,33 +10,38 @@ import javax.swing.event.HyperlinkListener;
  */
 public class StatusBarIssuesPane extends StatusBarPane {
 
-	private HyperlinkLabel getMoreIssues;
+    private HyperlinkLabel getMoreIssues;
 
 
-	public StatusBarIssuesPane(String initialText) {
-		super(initialText);
+    public StatusBarIssuesPane(String initialText) {
+        super(initialText);
 
-		getMoreIssues = new HyperlinkLabel("Get More Issues...");
-		getMoreIssues.setOpaque(false);
+        getMoreIssues = new HyperlinkLabel("Get More Issues...");
+        getMoreIssues.setOpaque(false);
 
-		enableGetMoreIssues(false);
+        enableGetMoreIssues(false);
 
-		addComponent(getMoreIssues);
-	}
+        addComponent(getMoreIssues);
+    }
 
-	public void addMoreIssuesListener(HyperlinkListener listener) {
-		getMoreIssues.addHyperlinkListener(listener);
-	}
+    public void addMoreIssuesListener(HyperlinkListener listener) {
+        getMoreIssues.addHyperlinkListener(listener);
+    }
 
-	public void removeMoreListener(HyperlinkListener listener) {
-		getMoreIssues.removeHyperlinkListener(listener);
-	}
+    public void removeMoreListener(HyperlinkListener listener) {
+        getMoreIssues.removeHyperlinkListener(listener);
+    }
 
-	public void enableGetMoreIssues(boolean enable) {
-		if (enable) {
-			getMoreIssues.setVisible(true);
-		} else {
-			getMoreIssues.setVisible(false);
-		}
-	}
+    public void enableGetMoreIssues(final boolean enable) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if (enable) {
+                    getMoreIssues.setVisible(true);
+                } else {
+                    getMoreIssues.setVisible(false);
+                }
+            }
+        });
+
+    }
 }
