a057d13317d5215b0ef5bcb0553085c26798dc31
pmaruszak <pmaruszak@localhost>
2010-06-01 15:38:04 +0000

    changed plugin target format to a single jar with dependencies

--- atlassian-intellij-connector/src/main/java/ClassToDelete2.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: pstefaniak
- * Date: Feb 12, 2010
- * Time: 2:33:54 PM
- * To change this template use File | Settings | File Templates.
- */
-public class ClassToDelete2 {
-}
