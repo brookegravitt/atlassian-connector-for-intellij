c7cba202631ea40c313bd88a2ab7500efc248c38
Piotr Maruszak <pmaruszak@atlassian.com>
2010-11-09 13:53:11 +0100

    PL-2001 Failing to bind to direct clickthrough local port, should not cause IntelliJ Fatal Error Popup

--- atlassian-intellij-connector/src/main/java/org/veryquick/embweb/EmbeddedServer.java
+++ atlassian-intellij-connector/src/main/java/org/veryquick/embweb/EmbeddedServer.java
@@ -34,183 +34,184 @@ import java.util.StringTokenizer;
  */
 public final class EmbeddedServer {
 
-	/**
-	 * Logger
-	 */
-	public static final Logger LOGGER = Logger.getLogger(EmbeddedServer.class);
+    /**
+     * Logger
+     */
+    public static final Logger LOGGER = Logger.getLogger(EmbeddedServer.class);
 
-	/**
-	 * Port to serve clients on
-	 */
-	private int serverPort;
+    /**
+     * Port to serve clients on
+     */
+    private int serverPort;
 
-	/**
-	 * Server lives as long as this is true
-	 */
-	private volatile boolean alive;
+    /**
+     * Server lives as long as this is true
+     */
+    private volatile boolean alive;
 
 
-	/**
-	 * Handler
-	 */
-	private HttpRequestHandler clientHandler;
-	private boolean acceptOnlyLocal;
+    /**
+     * Handler
+     */
+    private HttpRequestHandler clientHandler;
+    private boolean acceptOnlyLocal;
 
-	/**
-	 * New instance
-	 *
-	 * @param serverPort
-	 * @param handler
-	 * @param acceptOnlyLocal
-	 * @return instance of http server
-	 * @throws Exception
-	 */
-	public static EmbeddedServer createInstance(int serverPort, HttpRequestHandler handler, boolean acceptOnlyLocal)
-			throws Exception {
-		final EmbeddedServer server = new EmbeddedServer(serverPort, handler, acceptOnlyLocal);
-		Thread thread = new Thread(
-				new Runnable() {
-					public void run() {
-						try {
-							server.start();
-						} catch (IOException e) {
-							LOGGER.warn("Failed to start server", e);
-						}
-					}
-				}, "server thread"
-		);
-		thread.start();
-		return server;
-	}
+    /**
+     * New instance
+     *
+     * @param serverPort
+     * @param handler
+     * @param acceptOnlyLocal
+     * @return instance of http server
+     * @throws Exception
+     */
+    public static EmbeddedServer createInstance(int serverPort, HttpRequestHandler handler, boolean acceptOnlyLocal)
+            throws Exception {
+        final EmbeddedServer server = new EmbeddedServer(serverPort, handler, acceptOnlyLocal);
+        Thread thread = new Thread(
+                new Runnable() {
+                    public void run() {
+                        try {
+                            server.start();
+                        } catch (IOException e) {
+                            LOGGER.warn("Failed to start server", e);
+                        }
+                    }
+                }, "server thread"
+        );
+        thread.start();
+        return server;
+    }
 
-	/**
-	 * New instance
-	 *
-	 * @param serverPort
-	 * @param handler
-	 * @throws Exception
-	 */
-	private EmbeddedServer(int serverPort, HttpRequestHandler handler, boolean acceptOnlyLocal) throws Exception {
-		this.serverPort = serverPort;
-		this.clientHandler = handler;
-		this.acceptOnlyLocal = acceptOnlyLocal;
-	}
+    /**
+     * New instance
+     *
+     * @param serverPort
+     * @param handler
+     * @throws Exception
+     */
+    private EmbeddedServer(int serverPort, HttpRequestHandler handler, boolean acceptOnlyLocal) throws Exception {
+        this.serverPort = serverPort;
+        this.clientHandler = handler;
+        this.acceptOnlyLocal = acceptOnlyLocal;
+    }
 
-	/**
-	 * Start the server
-	 *
-	 * @throws java.io.IOException
-	 */
-	private void start() throws IOException {
-		this.alive = true;
-		ServerSocket serverSocket;
-		if (System.getProperty("javax.net.ssl.keyStore") != null) {
-			ServerSocketFactory ssocketFactory = SSLServerSocketFactory.getDefault();
-			serverSocket = ssocketFactory.createServerSocket(this.serverPort);
-		} else {
-            try {
-			serverSocket = new ServerSocket(this.serverPort);
-            } catch (final IOException e) {
-                LOGGER.warn("Cannot start HTTP server on port:" + this.serverPort + ". Disabling direct click through.");
-                return;                
+    /**
+     * Start the server
+     *
+     * @throws java.io.IOException
+     */
+    private void start() throws IOException {
+        this.alive = true;
+        ServerSocket serverSocket;
+        try {
+            if (System.getProperty("javax.net.ssl.keyStore") != null) {
+                ServerSocketFactory ssocketFactory = SSLServerSocketFactory.getDefault();
+                serverSocket = ssocketFactory.createServerSocket(this.serverPort);
+            } else {
+
+                serverSocket = new ServerSocket(this.serverPort);
             }
-		}
-		LOGGER.info("Server up on " + this.serverPort);
-		while (alive) {
-			Socket clientRequestSocket = serverSocket.accept();
+        } catch (final IOException e) {
+            LOGGER.warn("Cannot start HTTP server on port:" + this.serverPort + ". Disabling direct click through.");
+            return;
+        }
+        LOGGER.info("Server up on " + this.serverPort);
+        while (alive) {
+            Socket clientRequestSocket = serverSocket.accept();
 
-			if (!acceptOnlyLocal || clientRequestSocket.getInetAddress().isLoopbackAddress()) {
-				Thread thread = new Thread(
-						new RequestHandler(clientRequestSocket),
-						clientRequestSocket.getInetAddress().getCanonicalHostName()
-				);
-				thread.start();
-			} else {
-				clientRequestSocket.getOutputStream().close();
-				clientRequestSocket.close();
-			}
-		}
-	}
+            if (!acceptOnlyLocal || clientRequestSocket.getInetAddress().isLoopbackAddress()) {
+                Thread thread = new Thread(
+                        new RequestHandler(clientRequestSocket),
+                        clientRequestSocket.getInetAddress().getCanonicalHostName()
+                );
+                thread.start();
+            } else {
+                clientRequestSocket.getOutputStream().close();
+                clientRequestSocket.close();
+            }
+        }
+    }
 
-	/**
-	 * Request handler
-	 */
-	private class RequestHandler implements Runnable {
+    /**
+     * Request handler
+     */
+    private class RequestHandler implements Runnable {
 
-		/**
-		 * Socket to handle the request on
-		 */
-		private Socket clientRequestSocket;
+        /**
+         * Socket to handle the request on
+         */
+        private Socket clientRequestSocket;
 
 
-		/**
-		 * New handler
-		 *
-		 * @param clientRequestSocket Socket to handle the request on
-		 */
-		public RequestHandler(Socket clientRequestSocket) {
-			this.clientRequestSocket = clientRequestSocket;
-		}
+        /**
+         * New handler
+         *
+         * @param clientRequestSocket Socket to handle the request on
+         */
+        public RequestHandler(Socket clientRequestSocket) {
+            this.clientRequestSocket = clientRequestSocket;
+        }
 
-		/**
-		 * Handle the request
-		 *
-		 * @see Thread#run()
-		 */
-		public void run() {
-			try {
-				InputStream requestInputStream = clientRequestSocket.getInputStream();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(requestInputStream));
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					LOGGER.debug(line);
+        /**
+         * Handle the request
+         *
+         * @see Thread#run()
+         */
+        public void run() {
+            try {
+                InputStream requestInputStream = clientRequestSocket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(requestInputStream));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    LOGGER.debug(line);
 
-					if (line.startsWith("GET") || line.startsWith("POST")) {
-						StringTokenizer tokenizer = new StringTokenizer(line, " ");
-						String requestType = tokenizer.nextToken();
-						String url = tokenizer.nextToken();
+                    if (line.startsWith("GET") || line.startsWith("POST")) {
+                        StringTokenizer tokenizer = new StringTokenizer(line, " ");
+                        String requestType = tokenizer.nextToken();
+                        String url = tokenizer.nextToken();
 
-						Map<String, String> parameters = new HashMap<String, String>();
+                        Map<String, String> parameters = new HashMap<String, String>();
 
-						int indexOfQuestionMark = url.indexOf("?");
-						if (indexOfQuestionMark >= 0) {
-							// there are URL parameters
-							String parametersToParse = url.substring(indexOfQuestionMark + 1);
-							url = url.substring(0, indexOfQuestionMark);
-							StringTokenizer parameterTokenizer = new StringTokenizer(parametersToParse, "&");
-							while (parameterTokenizer.hasMoreTokens()) {
-								String[] keyAndValue = parameterTokenizer.nextToken().split("=");
-								String key = URLDecoder.decode(keyAndValue[0], "utf-8");
-								String value = null;
-								if (keyAndValue.length > 1) {
-									value = URLDecoder.decode(keyAndValue[1], "utf-8");
-								}
-								parameters.put(key, value);
-							}
-						}
+                        int indexOfQuestionMark = url.indexOf("?");
+                        if (indexOfQuestionMark >= 0) {
+                            // there are URL parameters
+                            String parametersToParse = url.substring(indexOfQuestionMark + 1);
+                            url = url.substring(0, indexOfQuestionMark);
+                            StringTokenizer parameterTokenizer = new StringTokenizer(parametersToParse, "&");
+                            while (parameterTokenizer.hasMoreTokens()) {
+                                String[] keyAndValue = parameterTokenizer.nextToken().split("=");
+                                String key = URLDecoder.decode(keyAndValue[0], "utf-8");
+                                String value = null;
+                                if (keyAndValue.length > 1) {
+                                    value = URLDecoder.decode(keyAndValue[1], "utf-8");
+                                }
+                                parameters.put(key, value);
+                            }
+                        }
 
-						HttpRequestHandler.Type type;
-						if ("POST".equals(requestType)) {
-							type = HttpRequestHandler.POST;
-						} else {
-							type = HttpRequestHandler.GET;
-						}
+                        HttpRequestHandler.Type type;
+                        if ("POST".equals(requestType)) {
+                            type = HttpRequestHandler.POST;
+                        } else {
+                            type = HttpRequestHandler.GET;
+                        }
 
-						Response response = EmbeddedServer.this.clientHandler.handleRequest(
-								type, url, parameters
-						);
-						OutputStream outputStream = clientRequestSocket.getOutputStream();
-						response.writeToStream(outputStream);
-						outputStream.close();
-					}
-				}
-			} catch (SocketException e) {
-				LOGGER.debug("Socket error", e);
-			} catch (IOException e) {
-				LOGGER.error("I/O Error", e);
-			}
-		}
+                        Response response = EmbeddedServer.this.clientHandler.handleRequest(
+                                type, url, parameters
+                        );
+                        OutputStream outputStream = clientRequestSocket.getOutputStream();
+                        response.writeToStream(outputStream);
+                        outputStream.close();
+                    }
+                }
+            } catch (SocketException e) {
+                LOGGER.debug("Socket error", e);
+            } catch (IOException e) {
+                LOGGER.error("I/O Error", e);
+            }
+        }
 
-	}
+    }
 
 }
