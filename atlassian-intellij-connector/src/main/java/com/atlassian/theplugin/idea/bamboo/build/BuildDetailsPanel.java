60903abafb09a9ced70591fd467d1101d88d72ba
Piotr Maruszak <pmaruszak@atlassian.com>
2010-12-10 12:49:08 +0100

    style fixes

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/bamboo/build/BuildDetailsPanel.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/bamboo/build/BuildDetailsPanel.java
@@ -41,55 +41,55 @@ public class BuildDetailsPanel extends JPanel implements ActionListener {
     private Project project;
     private final BambooBuildAdapter build;
 
-	private JLabel relativeBuildTime = new JLabel();
-	private static final int MAX_NR_OF_COMMITTERS_TO_LIST_IN_A_DETAILED_WAY = 3;
+    private JLabel relativeBuildTime = new JLabel();
+    private static final int MAX_NR_OF_COMMITTERS_TO_LIST_IN_A_DETAILED_WAY = 3;
     private JPanel issuesPanel;
     private JPanel body;
 
     public BuildDetailsPanel(Project project, BambooBuildAdapter build) {
         this.project = project;
         this.build = build;
-		setLayout(new GridBagLayout());
+        setLayout(new GridBagLayout());
 
-		setLayout(new GridBagLayout());
+        setLayout(new GridBagLayout());
 
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.weightx = 1.0;
-		gbc.weighty = 1.0;
-		gbc.fill = GridBagConstraints.BOTH;
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
 
-		refreshBuildRelativeTime();
+        refreshBuildRelativeTime();
 
-		JScrollPane scroll = new JScrollPane(createBody());
-		scroll.setBorder(BorderFactory.createEmptyBorder());
-		add(scroll, gbc);
-	}
+        JScrollPane scroll = new JScrollPane(createBody());
+        scroll.setBorder(BorderFactory.createEmptyBorder());
+        add(scroll, gbc);
+    }
 
-	public JPanel createBody() {
+    public JPanel createBody() {
         body = new JPanel();
-		body.setLayout(new GridBagLayout());
-		body.setBackground(Color.WHITE);
+        body.setLayout(new GridBagLayout());
+        body.setBackground(Color.WHITE);
 
-		GridBagConstraints gbc1 = new GridBagConstraints();
-		GridBagConstraints gbc2 = new GridBagConstraints();
-		gbc1.gridx = 0;
-		gbc2.gridx = 1;
-		gbc1.gridy = 0;
-		gbc2.gridy = 0;
-		gbc1.weighty = 0.0;
-		gbc2.weighty = 0.0;
-		gbc1.weightx = 0.0;
-		gbc2.weightx = 1.0;
-		gbc1.fill = GridBagConstraints.NONE;
-		gbc2.fill = GridBagConstraints.HORIZONTAL;
-		gbc1.insets = new Insets(Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN,
-				Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
-		gbc2.insets = new Insets(Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN,
-				Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
-		gbc1.anchor = GridBagConstraints.FIRST_LINE_START;
-		gbc2.anchor = GridBagConstraints.FIRST_LINE_START;
+        GridBagConstraints gbc1 = new GridBagConstraints();
+        GridBagConstraints gbc2 = new GridBagConstraints();
+        gbc1.gridx = 0;
+        gbc2.gridx = 1;
+        gbc1.gridy = 0;
+        gbc2.gridy = 0;
+        gbc1.weighty = 0.0;
+        gbc2.weighty = 0.0;
+        gbc1.weightx = 0.0;
+        gbc2.weightx = 1.0;
+        gbc1.fill = GridBagConstraints.NONE;
+        gbc2.fill = GridBagConstraints.HORIZONTAL;
+        gbc1.insets = new Insets(Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN,
+                Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
+        gbc2.insets = new Insets(Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN,
+                Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
+        gbc1.anchor = GridBagConstraints.FIRST_LINE_START;
+        gbc2.anchor = GridBagConstraints.FIRST_LINE_START;
 
         body.add(new BoldLabel("Plan"), gbc1);
         HyperlinkLabel planLink = new HyperlinkLabel(build.getPlanName());
@@ -100,18 +100,18 @@ public class BuildDetailsPanel extends JPanel implements ActionListener {
         });
         planLink.setOpaque(false);
         body.add(planLink, gbc2);
-		gbc1.gridy++;
-		gbc2.gridy++;
+        gbc1.gridy++;
+        gbc2.gridy++;
         gbc1.insets = new Insets(0, Constants.DIALOG_MARGIN, Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
-		gbc2.insets = new Insets(0, Constants.DIALOG_MARGIN, Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
+        gbc2.insets = new Insets(0, Constants.DIALOG_MARGIN, Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
         final BoldLabel boldLabel = new BoldLabel("State");
         body.add(boldLabel, gbc1);
-		body.add(new JLabel(build.getAdjustedStatus().getName(), build.getBuildIcon(), SwingConstants.LEFT), gbc2);
-		gbc1.gridy++;
-		gbc2.gridy++;
-		gbc1.insets = new Insets(0, Constants.DIALOG_MARGIN, Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
-		gbc2.insets = new Insets(0, Constants.DIALOG_MARGIN, Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
-		body.add(new BoldLabel("Last Build"), gbc1);
+        body.add(new JLabel(build.getAdjustedStatus().getName(), build.getBuildIcon(), SwingConstants.LEFT), gbc2);
+        gbc1.gridy++;
+        gbc2.gridy++;
+        gbc1.insets = new Insets(0, Constants.DIALOG_MARGIN, Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
+        gbc2.insets = new Insets(0, Constants.DIALOG_MARGIN, Constants.DIALOG_MARGIN / 2, Constants.DIALOG_MARGIN);
+        body.add(new BoldLabel("Last Build"), gbc1);
 
         HyperlinkLabel link = new HyperlinkLabel("#" + build.getBuildNumberAsString());
         link.setOpaque(false);
@@ -123,41 +123,41 @@ public class BuildDetailsPanel extends JPanel implements ActionListener {
         });
 //		body.add(new JLabel("<html><a href=\"" + build.getResultUrl() + "\">&#35;" + build.getBuildNumberAsString() +"<a></html>"), gbc2);
         body.add(link, gbc2);
-		gbc1.gridy++;
-		gbc2.gridy++;
-		body.add(new BoldLabel("When"), gbc1);
-		body.add(relativeBuildTime, gbc2);
-		// we don't have any way to get build duration yet
+        gbc1.gridy++;
+        gbc2.gridy++;
+        body.add(new BoldLabel("When"), gbc1);
+        body.add(relativeBuildTime, gbc2);
+        // we don't have any way to get build duration yet
 //		gbc1.gridy++;
 //		gbc2.gridy++;
 //		p.add(new BoldLabel("Build Duration"), gbc1);
 //		p.add(HGW???, gbc2);
-		gbc1.gridy++;
-		gbc2.gridy++;
+        gbc1.gridy++;
+        gbc2.gridy++;
 
-		StringBuilder reason = new StringBuilder(build.getReason());
-		Collection<String> committers = build.getCommiters();
-		// bleeeee, ugly ugly
-		if (committers.size() > 0 && reason.toString().equals(BuildTreeNode.CODE_HAS_CHANGED)) {
-			reason = new StringBuilder("Code was changed by ");
-			if (committers.size() > MAX_NR_OF_COMMITTERS_TO_LIST_IN_A_DETAILED_WAY) {
-				reason.append(committers.size()).append(" people");
-			} else {
-				int i = 0;
-				for (String committer : committers) {
-					reason.append(committer);
-					if (++i < committers.size()) {
-						reason.append(", ");
-					}
-				}
-			}
-		}
-		body.add(new BoldLabel("Build Reason"), gbc1);
-		body.add(new JLabel(Html2text.translate(reason.toString()).trim()), gbc2);
-		gbc1.gridy++;
-		gbc2.gridy++;
-		body.add(new BoldLabel("Tests"), gbc1);
-		body.add(new JLabel(build.getTestsPassedSummary() + " failed"), gbc2);
+        StringBuilder reason = new StringBuilder(build.getReason());
+        Collection<String> committers = build.getCommiters();
+        // bleeeee, ugly ugly
+        if (committers.size() > 0 && reason.toString().equals(BuildTreeNode.CODE_HAS_CHANGED)) {
+            reason = new StringBuilder("Code was changed by ");
+            if (committers.size() > MAX_NR_OF_COMMITTERS_TO_LIST_IN_A_DETAILED_WAY) {
+                reason.append(committers.size()).append(" people");
+            } else {
+                int i = 0;
+                for (String committer : committers) {
+                    reason.append(committer);
+                    if (++i < committers.size()) {
+                        reason.append(", ");
+                    }
+                }
+            }
+        }
+        body.add(new BoldLabel("Build Reason"), gbc1);
+        body.add(new JLabel(Html2text.translate(reason.toString()).trim()), gbc2);
+        gbc1.gridy++;
+        gbc2.gridy++;
+        body.add(new BoldLabel("Tests"), gbc1);
+        body.add(new JLabel(build.getTestsPassedSummary() + " failed"), gbc2);
 
         gbc1.gridy++;
         gbc2.gridy++;
@@ -169,30 +169,32 @@ public class BuildDetailsPanel extends JPanel implements ActionListener {
         body.add(new BoldLabel("Issues"), gbc1);
         body.add(issuesPanel, gbc2);
 
-		gbc1.gridy++;
-		gbc1.gridwidth = 2;
-		gbc1.weighty = 1.0;
-		gbc1.fill = GridBagConstraints.VERTICAL;
-		JPanel filler = new JPanel();
-		filler.setOpaque(true);
-		filler.setBackground(Color.WHITE);
-		body.add(filler, gbc1);
+        gbc1.gridy++;
+        gbc1.gridwidth = 2;
+        gbc1.weighty = 1.0;
+        gbc1.fill = GridBagConstraints.VERTICAL;
+        JPanel filler = new JPanel();
+        filler.setOpaque(true);
+        filler.setBackground(Color.WHITE);
+        body.add(filler, gbc1);
 
-		return body;
-	}
+        return body;
+    }
 
-	public void actionPerformed(ActionEvent e) {
-		refreshBuildRelativeTime();
-	}
+    public void actionPerformed(ActionEvent e) {
+        refreshBuildRelativeTime();
+    }
 
-	private void refreshBuildRelativeTime() {
-		relativeBuildTime.setText(DateUtil.getRelativePastDate(build.getCompletionDate()));
-	}
+    private void refreshBuildRelativeTime() {
+        relativeBuildTime.setText(DateUtil.getRelativePastDate(build.getCompletionDate()));
+    }
 
     public void setIssues(@Nullable Collection<BuildIssue> issues) {
+
         issuesPanel.removeAll();
         issuesPanel.validate();
         body.validate();
+
         if (issues == null) {
             issuesPanel.add(new JLabel("No issue information available"));
         } else {
@@ -236,7 +238,10 @@ public class BuildDetailsPanel extends JPanel implements ActionListener {
                 }
             }
         }
+
         issuesPanel.validate();
         body.validate();
+
+
     }
 }
