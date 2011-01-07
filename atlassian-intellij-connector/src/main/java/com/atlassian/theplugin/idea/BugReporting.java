0f16502512b5faeb4a6dbb5b830aa0875e7a8293
Piotr Maruszak <pmaruszak@atlassian.com>
2011-01-07 15:15:19 +0100

    preparation for 2.4.2 release

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/BugReporting.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/BugReporting.java
@@ -83,6 +83,7 @@ public final class BugReporting {
         versionMap.put("2.4.2", "12135");
         versionMap.put("2.5.0", "12038");
         versionMap.put("2.5.1", "12167");
+		versionMap.put("3.0", "12235");
 	}
 
     private static final int MAX_URI_LENGTH = 2048;
