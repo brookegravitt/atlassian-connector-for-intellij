0e617205a48e16a602770602a9561b6c5d5bd33a
Piotr Maruszak <pmaruszak@atlassian.com>
2011-10-12 14:03:34 +0200

    added missing files

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/jira/logtime/LogTimeCheckinHandlerFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2002-2011 Atlassian
+ * All rights reserved.
+ */
+package com.atlassian.theplugin.idea.jira.logtime;
+
+import com.atlassian.theplugin.configuration.JiraWorkspaceConfiguration;
+import com.atlassian.theplugin.idea.jira.LogTimeCheckinHandler;
+import com.intellij.openapi.vcs.CheckinProjectPanel;
+import com.intellij.openapi.vcs.checkin.CheckinHandler;
+import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class LogTimeCheckinHandlerFactory extends CheckinHandlerFactory {
+    private LogTimeCheckinHandler handler;
+
+
+    public LogTimeCheckinHandlerFactory(JiraWorkspaceConfiguration jiraWorkspaceConfiguration) {
+
+        handler = new LogTimeCheckinHandler(jiraWorkspaceConfiguration);
+    }
+
+    @NotNull
+    @Override
+    public CheckinHandler createHandler(CheckinProjectPanel checkinprojectpanel) {
+        return handler.createHandler(checkinprojectpanel);
+    }
+}
