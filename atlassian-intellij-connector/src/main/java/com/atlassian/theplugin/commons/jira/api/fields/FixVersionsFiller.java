2a2f60365ee3e6834205230e1456b84b6130ea69
kalamon <kalamon@localhost>
2012-11-23 14:42:36 +0100

    PL-2739 - use JIRA REST API to talk to JIRA

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/commons/jira/api/fields/FixVersionsFiller.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/commons/jira/api/fields/FixVersionsFiller.java
@@ -3,8 +3,10 @@ package com.atlassian.theplugin.commons.jira.api.fields;
 import com.atlassian.connector.commons.jira.JIRAIssue;
 import com.atlassian.connector.commons.jira.soap.axis.RemoteIssue;
 import com.atlassian.connector.commons.jira.soap.axis.RemoteVersion;
+import com.atlassian.jira.rest.client.domain.Issue;
+import com.atlassian.jira.rest.client.domain.Version;
+import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,20 +14,34 @@ import java.util.List;
  * Date: Apr 6, 2009
  * Time: 4:53:43 PM
  */
-public class FixVersionsFiller implements FieldFiller {
-	public List<String> getFieldValues(String field, JIRAIssue detailedIssue) {
-		RemoteIssue ri = (RemoteIssue) detailedIssue.getApiIssueObject();
-		if (ri == null) {
-			return null;
-		}
-		RemoteVersion[] rv = ri.getFixVersions();
-		List<String> result = new ArrayList<String>();
-		if (rv == null) {
-			return null;
-		}
-		for (RemoteVersion version : rv) {
-			result.add(version.getId());
-		}
-		return result;
-	}
+public class FixVersionsFiller extends AbstractFieldFiller {
+    @Override
+    protected List<String> getFieldValues(String field, JIRAIssue detailedIssue, Issue apiIssueObject) {
+        Iterable<Version> fixVersions = apiIssueObject.getFixVersions();
+        if (fixVersions == null) {
+            return null;
+        }
+        List<String> result = Lists.newArrayList();
+        for (Version fixVersion : fixVersions) {
+            Long id = fixVersion.getId();
+            if (id == null) {
+                continue;
+            }
+            result.add(id.toString());
+        }
+        return result;
+    }
+
+    @Override
+    protected List<String> getFieldValues(String field, JIRAIssue detailedIssue, RemoteIssue apiIssueObject) {
+        RemoteVersion[] rv = apiIssueObject.getFixVersions();
+        List<String> result = Lists.newArrayList();
+        if (rv == null) {
+            return null;
+        }
+        for (RemoteVersion version : rv) {
+            result.add(version.getId());
+        }
+        return result;
+    }
 }
