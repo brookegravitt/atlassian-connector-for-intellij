d2a7c54e965b9ad139570420fc9f462721f964d5
kalamon <kalamon@localhost>
2012-12-06 12:55:06 +0100

    PL-2765 - stack traces not shown as parsed in IDEA 12

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/ui/StackTracePanel.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/ui/StackTracePanel.java
@@ -6,18 +6,18 @@ import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.project.Project;
 
-import javax.swing.JPanel;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import javax.swing.*;
+import java.awt.*;
 
 public class StackTracePanel extends JPanel {
 
 	public StackTracePanel(String stack, Project project) {
+        String fixedStack = stack.replaceAll("(\\(.+?\\.java:\\d+\\)) ?at", "$1\nat");
 
-		TextConsoleBuilderFactory factory = TextConsoleBuilderFactory.getInstance();
+        TextConsoleBuilderFactory factory = TextConsoleBuilderFactory.getInstance();
 		TextConsoleBuilder builder = factory.createBuilder(project);
 		ConsoleView console = builder.getConsole();
-		console.print(stack, ConsoleViewContentType.NORMAL_OUTPUT);
+		console.print(fixedStack, ConsoleViewContentType.NORMAL_OUTPUT);
 
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
