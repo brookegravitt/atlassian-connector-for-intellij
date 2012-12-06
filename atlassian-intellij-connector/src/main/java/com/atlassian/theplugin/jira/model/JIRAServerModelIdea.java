ff27e3b985c89832506e61f1c2c8456749df2161
kalamon <kalamon@localhost>
2012-12-06 10:45:59 +0100

    PL-2553 - Conurrent Modification

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/jira/model/JIRAServerModelIdea.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/jira/model/JIRAServerModelIdea.java
@@ -2,6 +2,8 @@ package com.atlassian.theplugin.jira.model;
 
 import com.atlassian.theplugin.commons.jira.IntelliJJiraServerFacade;
 import com.atlassian.theplugin.commons.jira.cache.JIRAServerModelImpl;
+import com.intellij.openapi.components.ApplicationComponent;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,7 +12,7 @@ import java.util.Collection;
  * User: pmaruszak
  * Date: Jul 2, 2009
  */
-public class JIRAServerModelIdea extends JIRAServerModelImpl implements FrozenModel  {
+public class JIRAServerModelIdea extends JIRAServerModelImpl implements FrozenModel, ApplicationComponent {
     private boolean modelFrozen = false;
 	private Collection<FrozenModelListener> frozenListeners = new ArrayList<FrozenModelListener>();
     
@@ -32,17 +34,31 @@ public class JIRAServerModelIdea extends JIRAServerModelImpl implements FrozenMo
 		fireModelFrozen();
 	}
 
-	public void addFrozenModelListener(FrozenModelListener listener) {
+	public synchronized void addFrozenModelListener(FrozenModelListener listener) {
 		frozenListeners.add(listener);
 	}
 
-	public void removeFrozenModelListener(FrozenModelListener listener) {
+	public synchronized void removeFrozenModelListener(FrozenModelListener listener) {
 		frozenListeners.remove(listener);
 	}
 
-	private void fireModelFrozen() {
+	private synchronized void fireModelFrozen() {
 		for (FrozenModelListener listener : frozenListeners) {
 			listener.modelFrozen(this, modelFrozen);
 		}
 	}
+
+    @Override
+    public void initComponent() {
+    }
+
+    @Override
+    public void disposeComponent() {
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return getClass().getCanonicalName();
+    }
 }
