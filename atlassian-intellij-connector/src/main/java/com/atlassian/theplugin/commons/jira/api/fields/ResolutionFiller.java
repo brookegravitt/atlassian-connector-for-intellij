d5d44b065979b37fa87c2125b698afed1e5c112f
kalamon <kalamon@localhost>
2012-12-06 11:09:29 +0100

    PL-2766 - NPE

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/commons/jira/api/fields/ResolutionFiller.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/commons/jira/api/fields/ResolutionFiller.java
@@ -9,6 +9,7 @@ import com.atlassian.jira.rest.client.domain.input.ComplexIssueInputFieldValue;
 import com.atlassian.jira.rest.client.domain.input.FieldInput;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
@@ -31,7 +32,11 @@ public class ResolutionFiller extends AbstractFieldFiller {
 
     @Override
     protected List<String> getFieldValues(String field, JIRAIssue detailedIssue, RemoteIssue apiIssueObject) {
-        return ImmutableList.of(apiIssueObject.getResolution());
+        String resolution = apiIssueObject.getResolution();
+        if (resolution == null) {
+            return Lists.newArrayList();
+        }
+        return ImmutableList.of(resolution);
     }
 
     @Override
