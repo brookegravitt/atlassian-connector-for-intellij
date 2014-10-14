a1f9359905f2c36ab154fe643731560b39a9f9cc
klopacinski <klopacinski@localhost>
2014-10-14 13:31:21 +0200

    PL-2979 - fix broken tests

--- atlassian-intellij-connector/src/test/java/com/atlassian/theplugin/jira/model/JIRACustomFilterTest.java
+++ atlassian-intellij-connector/src/test/java/com/atlassian/theplugin/jira/model/JIRACustomFilterTest.java
@@ -3,14 +3,14 @@ package com.atlassian.theplugin.jira.model;
 import com.atlassian.connector.commons.jira.beans.JIRAProjectBean;
 import com.atlassian.connector.commons.jira.beans.JIRAQueryFragment;
 import com.google.common.collect.ImmutableList;
-import org.junit.Assert;
-import org.junit.Test;
+import junit.framework.TestCase;
+
 
 /**
  * Created by klopacinski on 2014-10-10.
  */
-public class JIRACustomFilterTest {
-    @Test
+public class JIRACustomFilterTest extends TestCase {
+
     public void testGetJqlWithNormalProjectName() {
         JiraCustomFilter filter = new JiraCustomFilter();
         ImmutableList<JIRAQueryFragment> fragments = ImmutableList.of((JIRAQueryFragment)new JIRAProjectBean(1, "PSI", "PSI"));
@@ -19,10 +19,9 @@ public class JIRACustomFilterTest {
 
         String jql = filter.getJql();
 
-        Assert.assertEquals("(project = PSI)", jql);
+        assertEquals("(project = PSI)", jql);
     }
 
-    @Test
     public void testGetJqlWithReservedProjectName() {
         JiraCustomFilter filter = new JiraCustomFilter();
         ImmutableList<JIRAQueryFragment> fragments = ImmutableList.of((JIRAQueryFragment)new JIRAProjectBean(1, "ABORT", "ABORT"));
@@ -31,6 +30,6 @@ public class JIRACustomFilterTest {
 
         String jql = filter.getJql();
 
-        Assert.assertEquals("(project = \"ABORT\")", jql);
+        assertEquals("(project = \"ABORT\")", jql);
     }
 }
