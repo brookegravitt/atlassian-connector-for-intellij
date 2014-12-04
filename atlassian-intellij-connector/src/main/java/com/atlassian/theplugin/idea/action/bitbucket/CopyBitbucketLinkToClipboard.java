f80f236fd6d3a0f68c2c8b593dc9cf91cd7253fb
marcos <marcos@scriven.org>
2014-12-04 21:38:38 +1100

    Split out into more actions

--- atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/bitbucket/CopyBitbucketLinkToClipboard.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/bitbucket/CopyBitbucketLinkToClipboard.java
@@ -1,22 +1,20 @@
 package com.atlassian.theplugin.idea.action.bitbucket;
 
-import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ide.CopyPasteManager;
 
-public class OpenBitbucketLinkInBrowserAction extends AnAction
+import java.awt.datatransfer.StringSelection;
+
+public class CopyBitbucketLinkToClipboard extends AnAction
 {
 
     @Override
     public void actionPerformed(final AnActionEvent event)
     {
         VcsActionDetails details = IntelliJGitUtils.extractVcsActionDetails(event);
-        String remoteUrl = IntelliJGitUtils.createBitbucketUrl(details);
-
-        if (remoteUrl != null) {
-            BrowserUtil.browse(remoteUrl);
-        }
-
+        String bitbucketUrl = IntelliJGitUtils.createBitbucketUrl(details);
+        CopyPasteManager.getInstance().setContents(new StringSelection(bitbucketUrl));
     }
 
 }
---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/action/bitbucket/OpenCommitMessageInHipChat.java
@@ -0,0 +1,24 @@
+package com.atlassian.theplugin.idea.action.bitbucket;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ide.CopyPasteManager;
+
+import java.awt.datatransfer.StringSelection;
+
+public class OpenCommitMessageInHipChat extends AnAction
+{
+
+    @Override
+    public void actionPerformed(final AnActionEvent event)
+    {
+        VcsActionDetails details = IntelliJGitUtils.extractVcsActionDetails(event);
+        String bitbucketUrl = IntelliJGitUtils.createBitbucketUrl(details);
+
+        // VcsFileRevisionEx revision = (VcsFileRevisionEx)GitHistoryUtils.history(project, VcsUtil.getFilePath(virtualFile.getPath())).get(0);
+        String hipChatUrl = "hipchat://www.hipchat.com/user/mscriven@atlassian.com?message=" + bitbucketUrl;
+        BrowserUtil.browse(hipChatUrl);
+    }
+
+}
