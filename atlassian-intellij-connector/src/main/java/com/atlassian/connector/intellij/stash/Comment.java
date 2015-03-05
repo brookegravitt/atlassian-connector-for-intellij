7f6acda0fc166d352447169dec493be3cc91ff83
Karol Lopacinski <klopacinski@atlassian.com>
2015-03-05 12:10:04 +0100

    entity classes

--- atlassian-intellij-connector/src/main/java/com/atlassian/connector/intellij/stash/Comment.java
+++ atlassian-intellij-connector/src/main/java/com/atlassian/connector/intellij/stash/Comment.java
@@ -4,4 +4,12 @@ package com.atlassian.connector.intellij.stash;
  * Created by klopacinski on 2015-03-05.
  */
 public interface Comment {
+
+    public String getText();
+
+    public Author getAuthor();
+
+    public void setText(String text);
+
+    public void setAuthor(Author author);
 }
