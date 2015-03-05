399fb762f8b8ffa2d9264e7524a893b8077006b1
Bartlomiej Lewandowski <blewandowski@atlassian.com>
2015-03-05 23:27:25 +0100

    Added changed files panel

---
+++ atlassian-intellij-connector/src/main/java/com/atlassian/theplugin/idea/stash/StashChangedFilesPanel.java
@@ -0,0 +1,66 @@
+package com.atlassian.theplugin.idea.stash;
+
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.DataConstants;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeSelectionModel;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class StashChangedFilesPanel extends JPanel {
+    private	JTree tree;
+
+    public StashChangedFilesPanel() {
+        super(new BorderLayout());
+        tree = new JTree();
+        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+        this.add(tree);
+        this.add(new JLabel("Changed files"), BorderLayout.NORTH);
+
+        changeContents(Arrays.asList("a"));
+
+        tree.addTreeSelectionListener(new TreeSelectionListener() {
+            public void valueChanged(TreeSelectionEvent e) {
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+                        tree.getLastSelectedPathComponent();
+
+                if (node == null) return;
+
+                Object nodeInfo = node.getUserObject();
+                String path = nodeInfo.toString();
+
+                DataContext dataContext = DataManager.getInstance().getDataContext();
+                Project project = (Project) dataContext.getData(DataConstants.PROJECT);
+
+                VirtualFile fileByRelativePath = project.getBaseDir().findFileByRelativePath(path);
+
+                FileEditorManager.getInstance(project).openFile(fileByRelativePath, true);
+            }
+        });
+    }
+
+    public void changeContents(List<String> paths)
+    {
+        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("");
+
+        tree.expandRow(0);
+        tree.setRootVisible(false);
+
+        for (String path : paths) {
+            rootNode.add(new DefaultMutableTreeNode(path));
+        }
+
+        tree.setModel(new DefaultTreeModel(rootNode));
+    }
+}
